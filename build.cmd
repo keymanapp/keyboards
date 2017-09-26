f0f89753738b4b8a631da8b927977b5532b27974
Marc Durdin <marc@keyman.com>
2017-09-26 12:47:11 +0700

    Use same console if possible for build.cmd calls

--- build.cmd
+++ build.cmd
@@ -1,2 +1,15 @@
 @echo off
-.\build.sh
+rem
+rem If we call Git Bash directly then we keep the same console
+rem which gives us feedback in CI builds
+rem
+if exist "%programfiles%\git\usr\bin\bash.exe" (
+  set bash_shell="%programfiles%\git\usr\bin\bash.exe" --login
+) else (
+  if exist "%programfiles(x86)%\git\usr\bin\bash.exe" (
+    set bash_shell="%programfiles(x86)%\git\usr\bin\bash.exe" --login
+  ) else (
+    set bash_shell=
+  )
+)
+%bash_shell% .\build.sh
