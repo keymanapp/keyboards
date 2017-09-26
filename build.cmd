605254be9b228f6f9028b33bb8dfa3280235dfb8
Marc Durdin <marc@keyman.com>
2017-09-26 13:38:17 +0700

    Testing build.cmd for CI

--- build.cmd
+++ build.cmd
@@ -1,7 +1,9 @@
 @echo off
 rem
 rem If we call Git Bash directly then we keep the same console
-rem which gives us feedback in CI builds
+rem which gives us feedback in CI builds. Note Teamcity agent
+rem running on 32-bit Java requires programw6432 env var to
+rem find git reliably...
 rem
 setlocal
 set bash_shell=
@@ -13,12 +15,10 @@ if exist "%programfiles%\git\usr\bin\bash.exe" (
     echo Found bash in "%programfiles(x86)%\git\usr\bin\bash.exe"
     set bash_shell="%programfiles(x86)%\git\usr\bin\bash.exe" --login
   ) else (
-    set bash_shell=
+    if exist "%programw6432%\git\usr\bin\bash.exe" set bash_shell="%programw6432%\git\usr\bin\bash.exe" --login
   )
 )
 echo.
-echo Bash should be in: "%programfiles%\git\usr\bin\bash.exe"
-echo 
-echo Using bash shell command %bash_shell%
+echo Using bash shell command: %bash_shell%
 echo.
 %bash_shell% .\build.sh
