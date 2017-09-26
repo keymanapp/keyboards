9907725a6a92fcb99d9776bc1bc2d2b69959c9f2
Marc Durdin <marc@keyman.com>
2017-09-26 13:29:22 +0700

    Testing build.cmd for CI

--- build.cmd
+++ build.cmd
@@ -3,16 +3,22 @@ rem
 rem If we call Git Bash directly then we keep the same console
 rem which gives us feedback in CI builds
 rem
+setlocal
+set bash_shell=
 if exist "%programfiles%\git\usr\bin\bash.exe" (
+  echo Found bash in "%programfiles%\git\usr\bin\bash.exe"
   set bash_shell="%programfiles%\git\usr\bin\bash.exe" --login
 ) else (
   if exist "%programfiles(x86)%\git\usr\bin\bash.exe" (
+    echo Found bash in "%programfiles(x86)%\git\usr\bin\bash.exe"
     set bash_shell="%programfiles(x86)%\git\usr\bin\bash.exe" --login
   ) else (
     set bash_shell=
   )
 )
 echo.
+echo Bash should be in: "%programfiles%\git\usr\bin\bash.exe"
+echo 
 echo Using bash shell command %bash_shell%
 echo.
 %bash_shell% .\build.sh
