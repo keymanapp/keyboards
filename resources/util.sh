050459fee3106346c2991af9b7e9505b24bcaffb
Marc Durdin <marc@keyman.com>
2018-02-01 10:53:04 +0700

    Check minimum Keyman Desktop version before bundling

--- resources/util.sh
+++ resources/util.sh
@@ -111,3 +111,22 @@ function parse_args {
     fi
   done
 }
+
+#
+# Version comparison functions, rely on sort having a -V option
+# From: https://stackoverflow.com/a/4024263/1836776
+#
+
+function verlte {
+  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ] && return 1 || return 0
+}
+
+function verlt {
+  [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+#verlte 2.5.7 2.5.6 && echo "yes" || echo "no" # no
+#verlt 2.4.10 2.4.9 && echo "yes" || echo "no" # no
+#verlt 2.4.8 2.4.10 && echo "yes" || echo "no" # yes
+#verlte 2.5.6 2.5.6 && echo "yes" || echo "no" # yes
+#verlt 2.5.6 2.5.6 && echo "yes" || echo "no" # no
