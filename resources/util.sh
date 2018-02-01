073eecb52e027522af5e89a8b43dd057adf856b6
Marc Durdin <marc@keyman.com>
2018-02-01 14:39:59 +0700

    Fixup broken verlte

--- resources/util.sh
+++ resources/util.sh
@@ -118,15 +118,21 @@ function parse_args {
 #
 
 function verlte {
-  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ] && return 1 || return 0
+  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
 function verlt {
   [ "$1" = "$2" ] && return 1 || verlte $1 $2
 }
 
-#verlte 2.5.7 2.5.6 && echo "yes" || echo "no" # no
-#verlt 2.4.10 2.4.9 && echo "yes" || echo "no" # no
-#verlt 2.4.8 2.4.10 && echo "yes" || echo "no" # yes
-#verlte 2.5.6 2.5.6 && echo "yes" || echo "no" # yes
-#verlt 2.5.6 2.5.6 && echo "yes" || echo "no" # no
+# verlte 2.5.7 2.5.6 && echo "yes" || echo "no" # no
+# verlt 2.4.10 2.4.9 && echo "yes" || echo "no" # no
+# verlt 2.4.8 2.4.10 && echo "yes" || echo "no" # yes
+# verlte 2.5.6 2.5.6 && echo "yes" || echo "no" # yes
+# verlt 2.5.6 2.5.6 && echo "yes" || echo "no" # no 
+
+# if $(verlte 2.5.7 2.5.6) ; then echo "yes" ; else echo "no" ; fi # no
+# if $(verlt 2.4.10 2.4.9) ; then echo "yes" ; else echo "no" ; fi # no
+# if $(verlt 2.4.8 2.4.10) ; then echo "yes" ; else echo "no" ; fi # yes
+# if $(verlte 2.5.6 2.5.6) ; then echo "yes" ; else echo "no" ; fi # yes
+# if $(verlt 2.5.6 2.5.6)  ; then echo "yes" ; else echo "no" ; fi # no 
