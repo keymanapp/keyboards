553e07b0d9d1079f677637501282d5e124e3c474
LornaSIL <lorna_evans@sil.org>
2018-03-22 16:23:31 -0500

    [jorai] added php help file

---
+++ release/j/jorai/source/help/jorai.php
@@ -0,0 +1,38 @@
+<?php
+  $pagename = 'Jorai  Keyboard Help';
+  $pagetitle = 'Jorai  Keyboard Help';
+  require_once('header.php');
+?>
+<h1>Jorai keyboard for Keyman Desktop</h1>
+
+<p>This keyboard is designed for use with the Jorai language of Viet Nam. It includes an
+on screen keyboard which can be viewed by clicking on the Keyman icon and 
+selecting the On Screen Keyboard menu item. </p>
+
+
+<h2>Keyboard Layout</h2>
+
+<div id='osk-container'>
+  <h2>Desktop Keyboard Layout</h2>
+  <div id='osk' data-states='default shift'></div>
+</div>
+
+<h3>Default</h3>
+	  <img width="100%" src="Default.jpg">
+<h3>Shift</h3>
+	  <img width="100%" src="Shift.jpg">
+
+<h3>CTRL+ALT</h3>
+	  <img width="100%" src="CtrlAlt.jpg">
+
+<h3>CTRL+ALT+Shift</h3>
+	  <img width="100%" src="CtrlAltShift.jpg">
+
+<h2>Release History</h2>
+<p>Version 1.0, released 10 March 2018</p>
+
+<h2>Links</h2>
+<ul>
+<li>Home Page: <a href='https://keyman.com/'>https://keyman.com/</a></li>
+<li><a href='http://keymankeyboards.com/iso:jra'>Other keyboards for these languages</a></li>
+</ul>
