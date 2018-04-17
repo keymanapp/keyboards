c9fade831d5e092ece54a01d7fa40d74b82a76bf
darcywong00 <darcy_wong@sil.org>
2018-04-17 11:07:11 +0700

    Add help from welcome.htm content
    For the following keyboards:
    
    * acoli
    * aramaic
    * bu
    * buang
    * busau
    * cameroonqwertyunicode50
    * creekeysdesktop3
    * cs-pinyin
    * deseret
    * dvsp
    * esperantohx
    * farsi_unicode

---
+++ legacy/b/bu/source/help/bu.php
@@ -0,0 +1,35 @@
+<?php /*
+  Name:        BU keyboard
+  Copyright:   2006
+  Description: The BU Keyboard (for Keyman 6.0 and higher) provides access to a wide range of characters for Latin-based scripts in Unicode 4.1.0, including:
+
+<ul>
+<li>English, Spanish, French, German, and other major European languages</li>
+<li>nearly complete set of IPA and Americanist phonetic symbols, include obscure and obsolete symbols</li>
+<li>special characters commonly-used in typesetting</li>
+<li>arrows</li>
+<li>common mathematical, numeric, and currency symbols</li>
+</ul>
+
+
+
+  Authors:     Albert Bickford (SIL)
+*/
+  $pagename = 'Bickford Unicode Keyboard Help';
+  $pagetitle = 'Bickford Unicode Keyboard Help';
+  require_once('header.php');
+?>
+<h1>The BU Keyboarding System</h1>
+<h2>Albert Bickford, May 16, 2006</h2>
+
+<p>The BU Keyboard (for Keyman 6.0 and higher, <a href='http://www.tavultesoft.com/'>www.tavultesoft.com</a>) provides access to a wide range of characters for Latin-based scripts in Unicode 4.1.0 (<a href='http://www.unicode.org/'>www.unicode.org</a>), including:</p>
+<ul>
+<li>English, Spanish, French, German, and other major European languages</li>
+<li>nearly complete set of IPA and Americanist phonetic symbols, include obscure and obsolete symbols</li>
+<li>special characters commonly-used in typesetting</li>
+<li>arrows</li>
+<li>common mathematical, numeric, and currency symbols</li>
+</ul>
+
+<p><a href='bukeys.pdf' target='_blank'>PDF format documentation</a><br/>
+<a href='bukeys.doc' target='_blank'>Microsoft Word format documentation</a></p>
---
+++ legacy/e/esperantohx/source/help/esperantohx.php
@@ -0,0 +1,69 @@
+<<?php /*
+  Name:        Esperanto
+  Copyright:   2014
+  Description:
+  Authors:     Efthimios Mavrogeorgiadis
+*/
+$pagename = 'Esperanto Keyboard Help';
+$pagetitle = 'Esperanto Keyboard Help';
+require_once('header.php');
+?>
+
+<style>
+h1, h2 {
+	color: blue;
+}
+h2 {
+	font-style: italic;
+}
+td {
+	text-align: center;
+	padding: 2px;
+}
+</style>
+</head>
+<body>
+<h1>Esperanto Keyboards</h1>
+<h2>Esperanto Keyboard Based on H</h2>
+<p>This keyboard enables you to write the ĉapelitaj Esperanto letters using [cghjs] + h. Au and eu are automatically converted into aŭ and eŭ. If you double-press h or u respectively, the ĉapelita letter is split into its constituent parts.</p>
+<p>Specifically,</p>
+<table>
+<tr><td>c</td><td>+</td><td>h</td><td>=></td><td>ĉ</td></tr>
+<tr><td>g</td><td>+</td><td>h</td><td>=></td><td>ĝ</td></tr>
+<tr><td>h</td><td>+</td><td>h</td><td>=></td><td>ĥ</td></tr>
+<tr><td>j</td><td>+</td><td>h</td><td>=></td><td>ĵ</td></tr>
+<tr><td>s</td><td>+</td><td>h</td><td>=></td><td>ŝ</td></tr>
+<tr><td>a</td><td>+</td><td>u</td><td>=></td><td>aŭ</td></tr>
+<tr><td>e</td><td>+</td><td>u</td><td>=></td><td>eŭ</td></tr>
+<tr><td colspan="5">&nbsp;</td></tr>
+<tr><td>ĉ</td><td>+</td><td>h</td><td>=></td><td>ch</td></tr>
+<tr><td>ĝ</td><td>+</td><td>h</td><td>=></td><td>gh</td></tr>
+<tr><td>ĥ</td><td>+</td><td>h</td><td>=></td><td>hh</td></tr>
+<tr><td>ĵ</td><td>+</td><td>h</td><td>=></td><td>jh</td></tr>
+<tr><td>ŝ</td><td>+</td><td>h</td><td>=></td><td>sh</td></tr>
+<tr><td>aŭ</td><td>+</td><td>u</td><td>=></td><td>au</td></tr>
+<tr><td>eŭ</td><td>+</td><td>u</td><td>=></td><td>eu</td></tr>
+</table>
+<h2>Esperanto Keyboard Based on X</h2>
+<p>This keyboard enables you to write the ĉapelitaj Esperanto letters using [cghjsu] + x. Au and eu are automatically converted into aŭ and eŭ. If you double-press x or u respectively, the ĉapelita letter is split into its constituent parts.</p>
+<p>Specifically,</p>
+<table>
+<tr><td>c</td><td>+</td><td>x</td><td>=></td><td>ĉ</td></tr>
+<tr><td>g</td><td>+</td><td>x</td><td>=></td><td>ĝ</td></tr>
+<tr><td>h</td><td>+</td><td>x</td><td>=></td><td>ĥ</td></tr>
+<tr><td>j</td><td>+</td><td>x</td><td>=></td><td>ĵ</td></tr>
+<tr><td>s</td><td>+</td><td>x</td><td>=></td><td>ŝ</td></tr>
+<tr><td>u</td><td>+</td><td>x</td><td>=></td><td>ŭ</td></tr>
+<tr><td>a</td><td>+</td><td>u</td><td>=></td><td>aŭ</td></tr>
+<tr><td>e</td><td>+</td><td>u</td><td>=></td><td>eŭ</td></tr>
+<tr><td colspan="5">&nbsp;</td></tr>
+<tr><td>ĉ</td><td>+</td><td>x</td><td>=></td><td>cx</td></tr>
+<tr><td>ĝ</td><td>+</td><td>x</td><td>=></td><td>gx</td></tr>
+<tr><td>ĥ</td><td>+</td><td>x</td><td>=></td><td>hx</td></tr>
+<tr><td>ĵ</td><td>+</td><td>x</td><td>=></td><td>jx</td></tr>
+<tr><td>ŝ</td><td>+</td><td>x</td><td>=></td><td>sx</td></tr>
+<tr><td>ŭ</td><td>+</td><td>x</td><td>=></td><td>ux</td></tr>
+<tr><td>aŭ</td><td>+</td><td>u</td><td>=></td><td>au</td></tr>
+<tr><td>eŭ</td><td>+</td><td>u</td><td>=></td><td>eu</td></tr>
+</table>
+
