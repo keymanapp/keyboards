e369c7692dc4130e4496f8c7f3abb3ae853f504e
darcywong00 <darcy_wong@sil.org>
2018-06-12 11:52:23 +0700

    Update formatting for php help file

--- release/sil/sil_bwe_karen/source/help/sil_bwe_karen.php
+++ release/sil/sil_bwe_karen/source/help/sil_bwe_karen.php
@@ -3,59 +3,43 @@
   $pagetitle = 'SIL Bwe Karen Keyboard Help';
   require_once('header.php');
 ?>
-
-<BODY 
-style="PADDING-RIGHT: 25px; PADDING-LEFT: 25px; PADDING-BOTTOM: 25px; MARGIN: 0px; PADDING-TOP: 25px; max-width:600px">
-
       
-<title>SIL Bwe Karen keyboard for Keyman Desktop</title>
-<style type='text/css'>
-    h1 { font: bold 16pt Tahoma; color: #4444cc }
-    h2 { font: bold 12pt Tahoma; color: #4444cc }
-    span.key { font: bold 10pt Verdana; padding: 0 4px; border: solid 1px #aaaaaa; margin: 0 4px; }
-</style>
-</head>
 <body>
-<h1>SIL Bwe Karen keyboard for Keyman Desktop</h1>
-<html>
-<body lang=EN-US link=black vlink=purple>
 
-<div class=WordSection1>
+<h2>Font</h2>
+<p>The "SIL Bwe Karen" keyboard is designed to work with the Latin script using the SIL Charis Compact font.</p>
 
-<p class=MsoTitle align=left style='text-align:left'><span style='font-family:
-"Arial",sans-serif'>SIL Bwe Karen keyboard (ver 1.0)</span></p>
+<h2>Keyboard Layout</h2>
 
-<p class=Author style='line-height:12.0pt'>SIL International</p>
+<p>This keyboard follows the traditional QWERTY keyboard layout with the exception that it 
+replaces two unused characters with combining diacritics.</p>
 
-<h2>Technical Support</h2>
+<ul>
+  <li>q is replaced with U+0304 (Combining Macron)</li>
+  <li>f is replaced with U+0301 (Combining Acute Accent).</li>
+</ul>  
 
-<p class=MsoBodyText style='line-height:12.0pt'>If you have problems using the <span
-class=AllCaps><span style='font-size:10.0pt'>SIL</span></span> Unicode Bwe Karen
-keyboard, please visit: https://community.software.sil.org/c/keyman</p>
-
-
-<h1 style='margin-top:0in'>Keyboard Layout</h1>
-
-<p>This keyboard follows the traditional QWERTY keyboard layout with the exception that it replaces two unused characters with combining diacritics
-q is replaced with u0304 (Combining Macron) and f is replaced with u0301 (Combining Acute Accent).</p>
-
-      
-	  <div id='osk-container'>
+<div id='osk-container'>
   <h2>Desktop Keyboard Layout</h2>
   <div id='osk' data-states='default shift'></div>
 </div>
 
 <div id='osk-phone-container'>
   <h2>Phone Keyboard Layout</h2>
-  <div id='osk-phone' data-states='default shift symbol'></div>
+  <div id='osk-phone' data-states='default shift Symbol'></div>
 </div>
 
 <div id='osk-tablet-container'>
   <h2>Tablet Keyboard Layout</h2>
-  <div id='osk-tablet' data-states='default shift symbol'></div>
+  <div id='osk-tablet' data-states='default shift Symbol'></div>
 </div>
 
-<H2>License</H2>
-      <P>SIL Bwe Karen Keyman Keyboard is copyright ©2018 SIL 
-      International. It is distributed under The MIT License (MIT)</P>
+<h2>Technical Support</h2>
+<p>If you have problems using the SIL Bwe Karen keyboard, please visit: 
+<a href=https://community.software.sil.org/c/keyman>https://community.software.sil.org/c/keyman</a></p>
 
+<h2>License</h2>
+<p>
+  SIL Bwe Karen keyboard is copyright ©2018 SIL International. 
+  It is distributed under The MIT License (MIT)
+</p>
