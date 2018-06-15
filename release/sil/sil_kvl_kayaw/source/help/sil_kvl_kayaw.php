9c2d0c4b448446639e090f0fa07deff133888dc6
darcywong00 <darcy_wong@sil.org>
2018-06-15 08:49:44 +0700

    Cleanup
    * Remove old artifacts in source/
    * Updated layouts in help/sil_kvl_kayaw.php

---
+++ release/sil/sil_kvl_kayaw/source/help/sil_kvl_kayaw.php
@@ -0,0 +1,50 @@
+<?php
+  $pagename = 'SIL Kayaw Keyboard Help';
+  $pagetitle = 'SIL Kayaw Keyboard Help';
+  require_once('header.php');
+?>
+
+<h2>Keyboard</h2>
+<p>
+  This keyboard layout is designed for Kayaw. It includes
+  an on screen keyboard which can be viewed by clicking on the Keyman icon
+  and selecting the On Screen Keyboard menu item.
+</p>
+
+<div id='osk-container'>
+  <h2>Desktop Keyboard Layout</h2>
+  <div id='osk' data-states='default shift'></div>
+</div>
+
+<div id='osk-phone-container'>
+  <h2>Phone Keyboard Layout</h2>
+  <div id='osk-phone' data-states='default shift Symbol'></div>
+</div>
+
+<div id='osk-tablet-container'>
+  <h2>Tablet Keyboard Layout</h2>
+  <div id='osk-tablet' data-states='default shift Symbol'></div>
+</div>
+
+<h2>License</h2>
+<p>SIL Kayaw Keyman Keyboard is copyright &copy; 2018 SIL International. It is distributed under The MIT License (MIT)</p>
+
+<div class="note">
+  <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:</p>
+
+  <p>The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.</p>
+
+  <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.</p>
+</div>
