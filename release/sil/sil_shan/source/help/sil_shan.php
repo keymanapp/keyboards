6ea0a8d2412a81c14404d5021a4e1584d9730448
andjc <lang.suport@gmail.com>
2018-06-25 17:34:57 +1000

    [sil_shan] Updated metadata, finalised source files

---
+++ release/sil/sil_shan/source/help/sil_shan.php
@@ -0,0 +1,18 @@
+<?php
+  $pagename = 'Shan (SIL) Keyboard Help';
+  $pagetitle = 'Shan (SIL) Keyboard Help';
+  require_once('header.php');
+?>
+
+
+
+
+
+<h2>Default (unshifted)</h2>
+<p><img src="sik_shandU_.png" alt="Shan (SIL) Keyboard layout: Default (unshfifted)"></p>
+<h2>Shift</h2>
+<p><img src="sik_shandU_S.png" alt="Shan (SIL) Keyboard layout: Shfift"></p>
+<h2>AltGr (Right Alt)</h2>
+<p><img src="sik_shandU_RA.png" alt="Shan (SIL) Keyboard layout: AltGr (Right Alt)"></p>
+<h2>Shift + AltGr (Right Alt)</h2>
+<p><img src="sik_shandU_SRA.png" alt="Shan (SIL) Keyboard layout: Shift + AltGr (Right Alt)"></p>
