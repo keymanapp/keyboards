66eb026251a80977205a6b8d96f7e8de8909a05f
andjc <lang.suport@gmail.com>
2018-06-25 22:15:00 +1000

    [sil_kayah_myanmar] migrate source to Unicode

---
+++ release/sil/sil_kayah_mymr/source/help/sil_kayah_mymr.php
@@ -0,0 +1,13 @@
+<?php
+  $pagename = 'Kayah [Myanmar] (SIL) Keyboard Help';
+  $pagetitle = 'Kayah [Myanmar] (SIL) Keyboard Help';
+  require_once('header.php');
+?>
+
+
+<p>This is a keyboard that contains all of the characters needed to type Kayah in the Myanmar script</p>
+<h2>Keyboard layout</h2>
+<h3>Default (Unshifted) state</h3>
+<p><img src="sil_kayah_mymrU_.png" alt="Kayah [Myanmar] (SIL) Keyboard: Unshifted (default) state"></p>
+  <h3>Shift state</h3>
+  <p><img src="sil_kayah_mymrU_S.png" alt="Kayah [Myanmar] (SIL) Keyboard: Shifted state"></p>
