38c44e06502f94ae6631b2fd677d58f4b85c55f6
Makara <makara@keyman.com>
2018-07-06 15:36:47 +0700

    cleaning up and update the package

--- release/rac/rac_saraiki/source/rac_saraiki-layout.js
+++ release/rac/rac_saraiki/source/rac_saraiki-layout.js
@@ -187,10 +187,11 @@
                 "id": "K_SHIFT",
                 "text": "*Shift*",
                 "width": "160",
-                "sp": "1"
+                "sp": "1",
+                "nextlayer": "shift"
               },
               {
-                "id": "K_oE2",
+                "id": "U_002F",
                 "text": "/"
               },
               {
@@ -451,10 +452,11 @@
                 "id": "K_SHIFT",
                 "text": "*Shift*",
                 "width": "160",
-                "sp": "1"
+                "sp": "2",
+                "nextlayer": "default"
               },
               {
-                "id": "K_oE2",
+                "id": "U_007C",
                 "text": "|"
               },
               {
