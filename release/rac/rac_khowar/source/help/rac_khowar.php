aa9ba91f24a40aab2f6903d6c1844d2a6c676037
LornaSIL <lorna_evans@sil.org>
2018-07-09 13:24:01 -0500

    [rac-help] Remove body tag to fix cropped images

--- release/rac/rac_khowar/source/help/rac_khowar.php
+++ release/rac/rac_khowar/source/help/rac_khowar.php
@@ -1,13 +1,12 @@
-<?php 
-  $pagename = 'Rac_Khowar Keyman Keyboard Help';
-  $pagetitle = $pagename;
+<?php
+  $pagename = 'Rachitrali-Khowar Keyboard Help';
+  $pagetitle = 'Rachitrali-Khowar Keyboard Help';
   require_once('header.php');
 ?>
-<h1>The Rac_Khowar Keyman Keyboard Package</h1>
 
 <h2>Font</h2>
 <p style="text-align: justify;">
-The Rac_Khowar Keyman keyboard is designed to work with the  <A href="https://software.sil.org/Scheherazade" target=_blank>Scheherazade</A> 
+The Rachitrali-Khowar Keyman keyboard is designed to work with the  <A href="https://software.sil.org/Scheherazade" target=_blank>Scheherazade</A> 
 font. However, you can use this keyboard with any extended Arabic script font.  
 Unicode font.  
 </p>
@@ -15,7 +14,7 @@ Unicode font.
 
 <h2>Keyboard</h2>
 <p>
-This keyboard layout is designed by Pakistani Linguist Rehmat Aziz Chitrali for the Rac_Khowar language. It includes 
+This keyboard layout is designed by Pakistani Linguist Rehmat Aziz Chitrali for the Khowar language. It includes 
 an on screen keyboard which can be viewed by clicking on the Keyman icon 
 and selecting the On Screen Keyboard menu item.     </p>
 
