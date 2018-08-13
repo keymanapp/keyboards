1874d7522e674aeda584f875bb13eba48c856acf
andjc <lang.support@gmail.com>
2018-08-14 01:53:33 +1000

    [galaxie_greek_positional] Initial migration of files to Github

---
+++ release/g/galaxie_greek_positional/source/galaxie_greek_positional-layout.js
@@ -0,0 +1,609 @@
+{
+  "tablet": {
+    "font": "Tahoma",
+    "layer": [
+      {
+        "id": "default",
+        "row": [
+          {
+            "id": 1,
+            "key": [
+              {
+                "id": "K_1",
+                "text": "1"
+              },
+              {
+                "id": "K_2",
+                "text": "2"
+              },
+              {
+                "id": "K_3",
+                "text": "3"
+              },
+              {
+                "id": "K_4",
+                "text": "4"
+              },
+              {
+                "id": "K_5",
+                "text": "5"
+              },
+              {
+                "id": "K_6",
+                "text": "6"
+              },
+              {
+                "id": "K_7",
+                "text": "7"
+              },
+              {
+                "id": "K_8",
+                "text": "8"
+              },
+              {
+                "id": "K_9",
+                "text": "9"
+              },
+              {
+                "id": "K_0",
+                "text": "0"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "key": [
+              {
+                "id": "K_Q",
+                "text": "θ"
+              },
+              {
+                "id": "K_W",
+                "text": "ω"
+              },
+              {
+                "id": "K_E",
+                "text": "ε"
+              },
+              {
+                "id": "K_R",
+                "text": "ρ"
+              },
+              {
+                "id": "K_T",
+                "text": "τ"
+              },
+              {
+                "id": "K_Y",
+                "text": "ψ"
+              },
+              {
+                "id": "K_U",
+                "text": "υ"
+              },
+              {
+                "id": "K_I",
+                "text": "ι"
+              },
+              {
+                "id": "K_O",
+                "text": "ο"
+              },
+              {
+                "id": "K_P",
+                "text": "π"
+              }
+            ]
+          },
+          {
+            "id": 3,
+            "key": [
+              {
+                "id": "K_A",
+                "text": "α",
+                "pad": ""
+              },
+              {
+                "id": "K_S",
+                "text": "σ"
+              },
+              {
+                "id": "K_D",
+                "text": "δ"
+              },
+              {
+                "id": "K_F",
+                "text": "φ"
+              },
+              {
+                "id": "K_G",
+                "text": "γ"
+              },
+              {
+                "id": "K_H",
+                "text": "η"
+              },
+              {
+                "id": "K_J",
+                "text": "᾿"
+              },
+              {
+                "id": "K_K",
+                "text": "κ"
+              },
+              {
+                "id": "K_L",
+                "text": "λ"
+              },
+              {
+                "id": "K_BKSLASH",
+                "text": "ι",
+                "width": "",
+                "layer": "shift"
+              }
+            ]
+          },
+          {
+            "id": 4,
+            "key": [
+              {
+                "id": "K_SHIFT",
+                "text": "*Shift*",
+                "width": "110",
+                "sp": "1",
+                "nextlayer": "shift"
+              },
+              {
+                "id": "K_Z",
+                "text": "ζ"
+              },
+              {
+                "id": "K_X",
+                "text": "χ"
+              },
+              {
+                "id": "K_C",
+                "text": "ξ"
+              },
+              {
+                "id": "K_V",
+                "text": "´"
+              },
+              {
+                "id": "K_B",
+                "text": "β"
+              },
+              {
+                "id": "K_N",
+                "text": "ν"
+              },
+              {
+                "id": "K_M",
+                "text": "μ"
+              },
+              {
+                "id": "K_PERIOD",
+                "text": ".",
+                "sk": [
+                  {
+                    "text": "·",
+                    "id": "K_COLON",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "_",
+                    "id": "K_HYPHEN",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "-",
+                    "id": "K_HYPHEN",
+                    "layer": "default"
+                  },
+                  {
+                    "text": "*",
+                    "id": "K_8",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "<",
+                    "id": "K_COMMA",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "(",
+                    "id": "K_9",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": ")",
+                    "id": "K_0",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": ">",
+                    "id": "K_PERIOD",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "!",
+                    "id": "K_1",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "?",
+                    "id": "K_SLASH",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "'",
+                    "id": "K_QUOTE"
+                  },
+                  {
+                    "text": "\"",
+                    "id": "K_QUOTE",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "\\",
+                    "id": "K_BKSLASH"
+                  },
+                  {
+                    "text": ":",
+                    "id": "K_COLON",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": ";",
+                    "id": "K_COLON"
+                  },
+                  {
+                    "text": ",",
+                    "id": "K_COMMA"
+                  }
+                ]
+              },
+              {
+                "id": "K_BKSP",
+                "text": "*BkSp*",
+                "width": "90",
+                "sp": "1"
+              }
+            ]
+          },
+          {
+            "id": 5,
+            "key": [
+              {
+                "id": "K_LOPT",
+                "text": "*Menu*",
+                "width": "120",
+                "sp": "1"
+              },
+              {
+                "id": "K_SPACE",
+                "text": "",
+                "width": "775",
+                "sp": "0"
+              },
+              {
+                "id": "K_ENTER",
+                "text": "*Enter*",
+                "width": "140",
+                "sp": "1"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id": "shift",
+        "row": [
+          {
+            "id": 1,
+            "key": [
+              {
+                "id": "K_1",
+                "text": "1",
+                "layer": "default"
+              },
+              {
+                "id": "K_2",
+                "text": "2",
+                "layer": "default"
+              },
+              {
+                "id": "K_3",
+                "text": "3",
+                "layer": "default"
+              },
+              {
+                "id": "K_4",
+                "text": "4",
+                "layer": "default"
+              },
+              {
+                "id": "K_5",
+                "text": "5",
+                "layer": "default"
+              },
+              {
+                "id": "K_6",
+                "text": "6",
+                "layer": "default"
+              },
+              {
+                "id": "K_7",
+                "text": "7",
+                "layer": "default"
+              },
+              {
+                "id": "K_8",
+                "text": "8",
+                "layer": "default"
+              },
+              {
+                "id": "K_9",
+                "text": "9",
+                "layer": "default"
+              },
+              {
+                "id": "K_0",
+                "text": "0",
+                "layer": "default"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "key": [
+              {
+                "id": "K_Q",
+                "text": "Θ"
+              },
+              {
+                "id": "K_W",
+                "text": "Ω"
+              },
+              {
+                "id": "K_E",
+                "text": "Ε"
+              },
+              {
+                "id": "K_R",
+                "text": "Ρ"
+              },
+              {
+                "id": "K_T",
+                "text": "Τ"
+              },
+              {
+                "id": "K_Y",
+                "text": "Ψ"
+              },
+              {
+                "id": "K_U",
+                "text": "Υ"
+              },
+              {
+                "id": "K_I",
+                "text": "Ι"
+              },
+              {
+                "id": "K_O",
+                "text": "Ο"
+              },
+              {
+                "id": "K_P",
+                "text": "Π"
+              }
+            ]
+          },
+          {
+            "id": 3,
+            "key": [
+              {
+                "id": "K_A",
+                "text": "Α",
+                "pad": ""
+              },
+              {
+                "id": "K_S",
+                "text": "Σ"
+              },
+              {
+                "id": "K_D",
+                "text": "Δ"
+              },
+              {
+                "id": "K_F",
+                "text": "Φ"
+              },
+              {
+                "id": "K_G",
+                "text": "Γ"
+              },
+              {
+                "id": "K_H",
+                "text": "Η"
+              },
+              {
+                "id": "K_J",
+                "text": "῾"
+              },
+              {
+                "id": "K_K",
+                "text": "Κ"
+              },
+              {
+                "id": "K_L",
+                "text": "Λ"
+              },
+              {
+                "id": "K_QUOTE",
+                "text": "῀",
+                "width": ""
+              }
+            ]
+          },
+          {
+            "id": 4,
+            "key": [
+              {
+                "id": "K_SHIFT",
+                "text": "*Shift*",
+                "width": "110",
+                "sp": "2",
+                "nextlayer": "default"
+              },
+              {
+                "id": "K_Z",
+                "text": "Ζ"
+              },
+              {
+                "id": "K_X",
+                "text": "Χ"
+              },
+              {
+                "id": "K_C",
+                "text": "Ξ"
+              },
+              {
+                "id": "K_V",
+                "text": "`"
+              },
+              {
+                "id": "K_B",
+                "text": "Β"
+              },
+              {
+                "id": "K_N",
+                "text": "Ν"
+              },
+              {
+                "id": "K_M",
+                "text": "Μ"
+              },
+              {
+                "id": "K_PERIOD",
+                "text": ".",
+                "layer": "default",
+                "sk": [
+                  {
+                    "text": "·",
+                    "id": "K_COLON",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "_",
+                    "id": "K_HYPHEN",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "-",
+                    "id": "K_HYPHEN",
+                    "layer": "default"
+                  },
+                  {
+                    "text": "*",
+                    "id": "K_8",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "<",
+                    "id": "K_COMMA",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "(",
+                    "id": "K_9",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": ")",
+                    "id": "K_0",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": ">",
+                    "id": "K_PERIOD",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "!",
+                    "id": "K_1",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "?",
+                    "id": "K_SLASH",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "'",
+                    "id": "K_QUOTE",
+                    "layer": "default"
+                  },
+                  {
+                    "text": "\"",
+                    "id": "K_QUOTE",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": "\\",
+                    "id": "K_BKSLASH",
+                    "layer": "default"
+                  },
+                  {
+                    "text": ":",
+                    "id": "K_COLON",
+                    "layer": "shift"
+                  },
+                  {
+                    "text": ";",
+                    "id": "K_COLON",
+                    "layer": "default"
+                  },
+                  {
+                    "text": ",",
+                    "id": "K_COMMA",
+                    "layer": "default"
+                  }
+                ]
+              },
+              {
+                "id": "K_BKSP",
+                "text": "*BkSp*",
+                "width": "90",
+                "sp": "1"
+              }
+            ]
+          },
+          {
+            "id": 5,
+            "key": [
+              {
+                "id": "K_LOPT",
+                "text": "*Menu*",
+                "width": "120",
+                "sp": "1"
+              },
+              {
+                "id": "K_SPACE",
+                "text": "",
+                "width": "775",
+                "sp": "0"
+              },
+              {
+                "id": "K_ENTER",
+                "text": "*Enter*",
+                "width": "140",
+                "sp": "1"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
