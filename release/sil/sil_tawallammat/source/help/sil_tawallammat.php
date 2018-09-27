731a8e710928f25b688f17da76f8392fbef619c1
LornaSIL <lorna_evans@sil.org>
2018-09-27 17:06:17 -0500

    [sil_tawallammat] Add new keyboard to repo

---
+++ release/sil/sil_tawallammat/source/help/sil_tawallammat.php
@@ -0,0 +1,30 @@
+<?php
+  $pagename = 'Tawallammat Tifinagh (SIL) Keyboard Help';
+  $pagetitle = 'Tawallammat Tifinagh (SIL) Keyboard Help';
+  require_once('header.php');
+?>
+
+  <p>This keyboard was developed to input characters for Tifinagh scripts.
+  You can use this keyboard with any Tifinagh Unicode font.</p>
+  
+  <p>For typing in the Tawallammat Tamajaq language, we highly recommend 
+  the Tagmukay font at 
+  <a href="http://software.sil.org/tagmukay/" target=_blank>http://software.sil.org/tagmukay</a>. 
+  This font has the required characters and bi-consonant ligatures for Tawallammat.</p>
+
+<h2>Layout</h2>
+<p>The four states of the Tawallammat Tifinagh Keyman keyboard for the Tagmukay font are illustrated
+below. The black characters show characters found in the Tagmukay font. For bi-consonant ligatures,
+type the "consonant joiner" between two consonants. For example, press the following keys in
+sequence: <img src="Image_001.jpg">, RALT + <img src="Image_003.jpg">, and <img src="Image_004.jpg"> to get the <img src="i
+mage_000.jpg"> ligature. Support for bi-consonants will be dependent on the font you use.</p>
+
+    <h3>Default (unshifted)</h3>
+    <p><img src="Image_005.jpg" alt="Tawallammat Tifinagh (SIL) Keyboard: default (unshifted)"></p>
+    <h3>Shift</h3>
+    <p><img src="Image_006.png" alt="Tawallammat Tifinagh (SIL) Keyboard: shift"></p>
+    <h3>Right ALT</h3>
+    <p><img src="Image_007.png" alt="Tawallammat Tifinagh (SIL) Keyboard: ralt"></p>
+    <h3>Shift + Right ALT</h3>
+    <p><img src="Image_008.png" alt="Tawallammat Tifinagh (SIL) Keyboard: shift+ralt"></p>
+
