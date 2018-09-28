3e68d4cf4e23316ac15ac17494b91b51c6b435ac
LornaSIL <lorna_evans@sil.org>
2018-09-28 16:55:19 -0500

    [sil_tawallammat] Move keys off SHIFT RALT and change documentation and graphics files

--- release/sil/sil_tawallammat/source/help/sil_tawallammat.php
+++ release/sil/sil_tawallammat/source/help/sil_tawallammat.php
@@ -13,18 +13,15 @@
   This font has the required characters and bi-consonant ligatures for Tawallammat.</p>
 
 <h2>Layout</h2>
-<p>The four states of the Tawallammat Tifinagh Keyman keyboard for the Tagmukay font are illustrated
+<p>The Three states of the Tawallammat Tifinagh Keyman keyboard for the Tagmukay font are illustrated
 below. The black characters show characters found in the Tagmukay font. For bi-consonant ligatures,
 type the "consonant joiner" between two consonants. For example, press the following keys in
-sequence: <img src="Image_001.jpg">, RALT + <img src="Image_003.jpg">, and <img src="Image_004.jpg"> to get the <img src="i
-mage_000.jpg"> ligature. Support for bi-consonants will be dependent on the font you use.</p>
+sequence: <img src="2d54.png">, <img src="2d7f.png">, and <img src="2d5c.png"> to get the <img src="2d54_2d5c.png"> ligature. Support for bi-consonants will be dependent on the font you use.</p>
 
-    <h3>Default (unshifted)</h3>
-    <p><img src="Image_005.jpg" alt="Tawallammat Tifinagh (SIL) Keyboard: default (unshifted)"></p>
+    <h3>Default (Unshifted)</h3>
+    <p><img src="sil_tawallammatU_.png" alt="Tawallammat Tifinagh (SIL) Keyboard: default (unshifted)"></p>
     <h3>Shift</h3>
-    <p><img src="Image_006.png" alt="Tawallammat Tifinagh (SIL) Keyboard: shift"></p>
+    <p><img src="sil_tawallammatU_S.png" alt="Tawallammat Tifinagh (SIL) Keyboard: shift"></p>
     <h3>Right ALT</h3>
-    <p><img src="Image_007.png" alt="Tawallammat Tifinagh (SIL) Keyboard: ralt"></p>
-    <h3>Shift + Right ALT</h3>
-    <p><img src="Image_008.png" alt="Tawallammat Tifinagh (SIL) Keyboard: shift+ralt"></p>
+    <p><img src="sil_tawallammAtU_RA.png" alt="Tawallammat Tifinagh (SIL) Keyboard: ralt"></p>
 
