9df804e7ae65be7b348dbabf0a34e77733219eab
LornaSIL <lorna_evans@sil.org>
2018-10-18 17:02:42 -0500

    [fv_natwits] merge keyboard and delete kmw

--- release/fv/fv_natwits/source/help/fv_natwits.php
+++ release/fv/fv_natwits/source/help/fv_natwits.php
@@ -32,8 +32,7 @@ This keyboard is designed for the <b>Nedut’en-Witsuwit'en</b> language of the 
 					</ul>
 						 
 						<h2>Unicode Fonts</h2>
-    <p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p><h2>Downloads</h2>
-      <h3><a href="http://www.languagegeek.com/dene/keyboards/Keymaps/NaWits.pdf">Download a pdf of the Keylayout Map</a></h3>
+    <p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
     <h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
     
 <h2>Notes on touch layout</h2>
