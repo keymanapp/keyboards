4c0187a47a81521e8d9d6597eb1c51170f30d54a
LornaSIL <lorna_evans@sil.org>
2018-11-05 13:34:34 -0600

    [basic_kbdlisub] MS Lisu Basic keyboard to Keyman using kmconvert

---
+++ release/basic/basic_kbdlisub/source/help/basic_kbdlisub.php
@@ -0,0 +1,26 @@
+<?php 
+  $pagename = 'Lisu (Basic) Basic Keyboard Help';
+  $pagetitle = 'Lisu (Basic) Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for languages using the Lisu script.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<br/>
+
+<h2>Keyboard Layout</h2>
+<div id='osk'>
+</div>
+
+<h2>Keyboard Layout</h2>
+<div id='osk-tablet'>
+</div>
+
+
