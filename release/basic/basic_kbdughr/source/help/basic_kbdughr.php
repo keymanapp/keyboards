d68d41dbf2e50ebb171cca4d4ab9aef129e11de4
LornaSIL <lorna_evans@sil.org>
2018-11-09 12:53:22 -0600

    [basic_kbdughr] MS Uyghur Basic keyboard to Keyman using kmconvert

---
+++ release/basic/basic_kbdughr/source/help/basic_kbdughr.php
@@ -0,0 +1,26 @@
+<?php 
+  $pagename = 'Uyghur (Legacy) Basic Keyboard Help';
+  $pagetitle = 'Uyghur (Legacy) Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the Uyghur language of China using the Arabic script.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<br/>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet'>
+</div>
+
+
