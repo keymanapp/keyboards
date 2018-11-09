0aa553b0873bff9b0f7d6da2747d4b2ee844b0a4
LornaSIL <lorna_evans@sil.org>
2018-11-09 12:40:18 -0600

    [basic_kbdsora] MS Sora Basic keyboard to Keyman using kmconvert

---
+++ release/basic/basic_kbdsora/source/help/basic_kbdsora.php
@@ -0,0 +1,28 @@
+<?php 
+  $pagename = 'Sora Basic Keyboard Help';
+  $pagetitle = 'Sora Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the Sora language of India (Sora Sompeng script).
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<p>A font must be installed to support this keyboard. Windows includes the <b>Nirmala UI</b> font which supports the <b>Sora Sompeng</b> script.</p>
+
+<br/>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift shift-ctrl'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift shift-ctrl'>
+</div>
+
+
