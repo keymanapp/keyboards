de021c98daea2fa298fb8c23610a125a3c9d729e
LornaSIL <lorna_evans@sil.org>
2018-11-09 16:06:42 -0600

    [basic_kbdfthrk] MS Futhark Basic keyboard to Keyman using kmconvert

---
+++ release/basic/basic_kbdfthrk/source/help/basic_kbdfthrk.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Futhark Basic Keyboard Help';
+  $pagetitle = 'Futhark Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+A font must be installed to support this keyboard. Windows includes the <b>Segoe UI Historic</b> font which supports the <b>Runic</b> script.</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<br/>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+
