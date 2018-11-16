7e13760fdfca7ec991ce2d089f22946257c79ba2
LornaSIL <lorna_evans@sil.org>
2018-11-16 15:15:13 -0600

    [basic_kbdnko] Changes per PR requests, also font info changed

--- release/basic/basic_kbdnko/source/help/basic_kbdnko.php
+++ release/basic/basic_kbdnko/source/help/basic_kbdnko.php
@@ -13,7 +13,7 @@ This keyboard is designed for languages using the N’Ko script.
 </p>
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
-<p>A font must be installed to support this keyboard. Windows includes the <b>Ebrima</b> and <b>Segoe UI</p> fonts which support the <b>N'Ko</b> script.</p>
+<p>A font must be installed to support this keyboard. Windows includes the <b>Ebrima</b> font which supports the <b>N'Ko</b> script.</p>
 <br/>
 
 <h2>Desktop Keyboard Layout</h2>
