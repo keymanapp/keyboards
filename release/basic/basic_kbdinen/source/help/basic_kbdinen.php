23ff777ea57a3bf813d67b40d44fc7ed9618ccf8
LornaSIL <lorna_evans@sil.org>
2018-11-19 17:36:24 -0600

    [basic_kbdinen] documented deadkeys and adjust shift states

--- release/basic/basic_kbdinen/source/help/basic_kbdinen.php
+++ release/basic/basic_kbdinen/source/help/basic_kbdinen.php
@@ -13,6 +13,16 @@
 </p>
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
+<p>This keyboard makes use of deadkeys. </p>
+<p>To get "ā", type "RALT - a". This is available on a, e, i, o, and u.</p>
+<p>To get "ê", type "SHIFT RALT ^ e". This is available on e and o.</p>
+<p>To get "ĕ", type "SHIFT RALT ( e". This is available on e, h, o, and u.</p>
+<p>To get "ḍ", type "RALT . d". This is available on d, h, l, m, n, r, s, and t.</p>
+<p>To get "ḏ", type "SHIFT RALT _ d". This is available on d, h, k, l, n, r, and t.</p>
+<p>To get "ñ", type "SHIFT RALT ~ n"</p>
+<p>To get "ž", type "SHIFT RALT < z". This is only available on z.</p>
+<p>A number of other combining marks are available on deadkeys. Typing a letter + RALT + comma with any of the following: ^*9(o-.:_>`~|' can be used to produce the following: âa̐a̮ăḁāạa̤a̱ȧàãa̲á</p>
+
 <br/>
 
 <h2>Desktop Keyboard Layout</h2>
