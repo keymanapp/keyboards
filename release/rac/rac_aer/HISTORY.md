96f6c8e19033fe967349f495609c64b017402f58
LornaSIL <lorna_evans@sil.org>
2018-12-03 11:13:37 -0600

    [rac_aer] update version and font info and fix up various layout issues

--- release/rac/rac_aer/HISTORY.md
+++ release/rac/rac_aer/HISTORY.md
@@ -1,5 +1,8 @@
 Rachitrali-Aer Change History
 =============================
 
-1.0 (06 June 2018) 
+1.1 (02 Dec 2018) 
+* Improved layout and fonts
+
+1.0 (06 Jun 2018) 
 * Created by Rehmat Aziz Chitrali
--- release/rac/rac_aer/source/help/rac_aer.php
+++ release/rac/rac_aer/source/help/rac_aer.php
@@ -4,9 +4,6 @@
   require_once('header.php');
 ?>
 
-<P><SPAN class=author_date_hits>Rehmat Aziz Chitrali, 
-2018-06-06</SPAN></P>
-
 <h2>Introduction</h2>
 <p>This keyboard is designed for the Aer language spoken in the province  of Sindh Pakistan, in its standard Perso-Arabic script, 
 as used in Sindh Pakistan. It is based on phonetic keyboard for Aer Language 
@@ -65,33 +62,8 @@ and selecting the On Screen Keyboard menu item.     </p>
 </p>
 
 
-      <P class=Caption></P><A name=license></A>
-      <H2>License</H2>
-      <P>Rachitrali-Aer Keyman Keyboard is copyright © 2018 Rehmat Aziz Chitrali. 
-      	It is distributed under The MIT License (MIT)</P>
-      <DIV class=Note>
-<p>Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:</p>
-
-<p>The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.</p>
-
-<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.</p></DIV>
-
-
-
 <h2>Links</h2>
 <ul>
 <li>Home Page: <a href='http://keymankeyboards.com'>http://keymankeyboards.com/</a></li>
-<li><a href='http://keymankeyboards.com/iso:aer'>Other keyboards for these languages</a></li>
+<li><a href='http://keymankeyboards.com/iso:aeq'>Other keyboards for these languages</a></li>
 </ul>
