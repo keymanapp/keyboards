3ae385e82d4662cc58e7c634a5837d55ad09b82f
LornaSIL <lorna_evans@sil.org>
2018-12-06 22:11:37 -0600

    [basic_kbdfthrk] fix typos per PR feedback

--- release/basic/basic_kbdfthrk/source/help/basic_kbdfthrk.php
+++ release/basic/basic_kbdfthrk/source/help/basic_kbdfthrk.php
@@ -1,3 +1,4 @@
+
 <?php 
   $pagename = 'Futhark Basic Keyboard Help';
   $pagetitle = 'Futhark Basic Keyboard Help';
@@ -11,7 +12,7 @@
 <p style='margin: 16px 0 0 0'>
 A font must be installed to support this keyboard. Windows includes the <b>Segoe UI Historic</b> font which supports the <b>Runic</b> script.</p>
 <p>The following characters can also be used with the "\" and "|" deadkeys: ᚬᛒᚼᛅᛘᛦᛋ. For example, you can type "\" + "ᛅ" to output "ᛆ".</p>
-<p>If "\" followed by a space is type, a "~" will be output. If "|" followed by a space is type, a "|" will be output.</p>
+<p>If "\" followed by a space is typed, a "~" will be output. If "|" followed by a space is typed, a "|" will be output.</p>
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 <br/>
