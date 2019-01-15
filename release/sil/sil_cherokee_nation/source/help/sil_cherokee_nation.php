4232e8482afa7c0b65b20929cdf614d5d1e8302e
LornaSIL <lorna_evans@sil.org>
2019-01-15 10:31:21 -0600

    [sil_cherokee_nation] Add Aboriginal Serif font to package and references to it

--- release/sil/sil_cherokee_nation/source/help/sil_cherokee_nation.php
+++ release/sil/sil_cherokee_nation/source/help/sil_cherokee_nation.php
@@ -8,7 +8,8 @@
 
 
 <p style='margin: 16px 0 0 0'>
-A Cherokee Unicode keyboard following the official Cherokee Nation layout. A font must be installed to support this keyboard. Windows includes a number of fonts which support the <b>Cherokee</b> script including the <b>Gadugi</b> and <b>Plantagenet Cherokee</b> fonts.</p>
+A Cherokee Unicode keyboard following the official Cherokee Nation layout. A font must be installed to support this keyboard. This keyboard comes with the <b>Aboriginal Serif</b> font which supports the Cherokee script. Windows includes a number of fonts which support the <b>Cherokee</b> script including the <b>Gadugi</b> and <b>Plantagenet Cherokee</b> fonts.</p>
+However, you can use this keyboard with any Cherokee script Unicode font.  </p>
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 <br/>
