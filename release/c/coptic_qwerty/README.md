8b8c016c82b308337c13063096e6a000516b1eb5
LornaSIL <lorna_evans@sil.org>
2019-02-19 13:40:38 -0600

    [coptic_qwerty] New keyboard submitted on behalf of author

---
+++ release/c/coptic_qwerty/README.md
@@ -0,0 +1,34 @@
+Coptic (Qwerty) keyboard
+========================
+
+© 2010-2019 Michael Everson
+
+Version 2.2
+
+Description
+-----------
+This keyboard layout is designed for typing the Coptic
+script. This English QWERTY-based keyboard layout is 
+optimized for English-language typists. It is based on an 
+English-specific transliteration and is probably going to 
+be most popular with English-speaking Copticists. 
+
+Links
+-----
+Home: https://keyman.com/keyboards/coptic_qwerty
+Help:	https://help.keyman.com/keyboard/coptic_qwerty
+
+Supported Platforms
+-------------------
+ * Windows
+ * macOS
+ * Linux
+ * Web
+ * iPhone
+ * iPad
+ * Android phone
+ * Android tablet
+ * Mobile devices
+ * Desktop devices
+ * Tablet devices
+
---
+++ release/c/coptic_qwerty/source/help/coptic_qwerty.php
@@ -0,0 +1,53 @@
+<?php
+$pagename = 'Coptic (Qwerty) Keyboard Help';
+$pagetitle = $pagename;
+require_once('header.php');
+?>
+
+<p>
+This keyboard layout is designed for typing the Coptic
+script. This English QWERTY-based keyboard layout is 
+optimized for English-language typists. It is based on an 
+English-specific transliteration and is probably going to 
+be most popular with English-speaking Copticists. 
+</p>
+<p>The <a href="https://www.evertype.com/fonts/coptic/" target="_blank">Antinoou</a> font was developed by the author of this keyboard and supports the standard Coptic characters. However, it does not yet support the Coptic Epact Numbers. The 
+<a href="http://users.teilar.gr/~g1951d/" target="_blank">Symbola</a> font does include support for the Coptic Epact Numbers.</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<br/>
+
+  <h2>Keyboard layout</h2>
+  <p>The main keyboard layout is listed below. There are some extra keystrokes needed in order to access more uncommon characters.</p>
+<ul>
+	<li>; (macron): Ⲁ̄ⲁ̄(ⲁ) Ⲃ̄ⲃ̄(ⲃ) Ⲅ̄ⲅ̄(ⲅ) …</li>
+	<li>shift-, (left half macron): Ⲁ︤ⲁ︤(ⲁ) Ⲃ︤ⲃ︤(ⲃ) Ⲅ︤ⲅ︤(ⲅ) …</li>
+	<li>shift-; (conjoining macron): Ⲁ︦ⲁ︦(ⲁ) Ⲃ︦ⲃ︦(ⲃ) Ⲅ︦ⲅ︦(ⲅ) …</li>
+	<li>shift-. (right half macron): Ⲁ︥ⲁ︥(ⲁ) Ⲃ︥ⲃ︥(ⲃ) Ⲅ︥ⲅ︥(ⲅ) …</li>
+	<li>shift-ralt-\ (overline for numbers): Ⲁ̅ⲁ̅(ⲁ) Ⲃ̅ⲃ̅(ⲃ) Ⲅ̅ⲅ̅(ⲅ) …</li>
+	<li>ralt-1 (dot below): Ⲁ̣ⲁ̣(ⲁ) Ⲃ̣ⲃ̣(ⲃ) Ⲅ̣ⲅ̣(ⲅ) …</li>
+	<li>ralt-2 (dot above): Ⲁ̇ⲁ̇(ⲁ) Ⲃ̇ⲃ̇(ⲃ) Ⲅ̇ⲅ̇(ⲅ) …</li>
+	<li>ralt-3 (circumflex): Ⲁ̂ⲁ̂(ⲁ) Ⲃ̂ⲃ̂(ⲃ) Ⲅ̂ⲅ̂(ⲅ) …</li>
+	<li>ralt-7 (diaeresis): Ⲁ̈ⲁ̈(ⲁ) Ⲃ̈ⲃ̈(ⲃ) Ⲅ̈ⲅ̈(ⲅ) …</li>
+	<li>` (grave): Ⲁ̀ⲁ̀(ⲁ) Ⲃ̀ⲃ̀(ⲃ) Ⲅ̀ⲅ̀(ⲅ) … </li>
+	<li>shift-` (acute): Ⲁ́ⲁ́(ⲁ) Ⲃ́ⲃ́(ⲃ) Ⲅ́ⲅ́(ⲅ) … </li>
+	<li>shift-ralt-/ (double circumflex): no precomposed characters</li>
+	<li>ralt-m (ni above): no precomposed characters</li>
+	<li>shift-ralt-b (spiritus asper): Ⲏ⳰ⲏ⳰(ⲏ)</li>
+	<li>ralt-k (spiritus lenis): Ⲏ⳱ⲏ⳱(ⲏ)</li>
+	<li>shift-ralt-3 (‖): ⳥(ⲙ) ⳦(ⲡ) ⳧(ⲥ) ⳨(ⲧ) ⳩(ⲭ) ⳪(ϭ) ⳤ(ⲕ) ‖(space)</li>
+	<li>ralt-b (⸖): Ⲷⲷ(ⲉ) Ⲽⲽ(ⲛ) Ⳬⳬ(ϣ) Ⳮⳮ(ϫ) ⸏ (-) ⸐ (=)Ⳳⳳ(ⲭ) ⸴(;) ⸳(:) ⳹(.) ⳼(,) ⳺(/) ⳻(\) ⸖(space)</li>
+	<li>ralt-\ (epact numbers): 𐋡  (1), 𐋢  (2), 𐋣 (3), 𐋤  (4), 𐋥  (5), 𐋦  (6),  𐋧  (7), 𐋨  (8), 𐋩 (9)
+	<li>shift-ralt-\ (epact numbers): 𐋪  (1), 𐋫  (2), 𐋬  (3), 𐋭  (4), 𐋮  (5), 𐋯  (6), 𐋰 (7), 𐋱  (8), 𐋲  (9)</li>
+	<li>shift-ralt-3 (epact numbers): 𐋳 (1), 𐋴  (2), 𐋵  (3), 𐋶  (4), 𐋷 (5), 𐋸  (6), 𐋹  (7), 𐋺  (8), 𐋻  (9), 𐋠 (,)</li>
+</ul>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift rightctrl-rightalt rightctrl-rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
+
+<p>© 2010-2019 Michael Everson, evertype.com. All rights reserved.</p>
