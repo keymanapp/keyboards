a005793fe6db9ccecdbd55fc2b75adaba1d9554e
LornaSIL <lorna_evans@sil.org>
2019-02-25 13:58:25 -0600

    [rac_ormuri] New keyboard on behalf of author

---
+++ release/rac/rac_ormuri/source/help/rac_ormuri.php
@@ -0,0 +1,62 @@
+<?php 
+  $pagename = 'Rachitrali-Ormuri Keyboard Help';
+  $pagetitle = 'Rachitrali-Ormuri';
+  require_once('header.php');
+?>
+<p>This keyboard is designed for the Ormuri language spoken in the Waziristan of Pakistan in its standard Perso-Arabic script, as used Pakistan. It is based on phonetic keyboard for Ormuri Language developed by Pakistani linguist, author, researcher, scientist,  Inventor and Software Developer Mr.Rehmat Aziz Chitrali for Summer Institute of Linguistics(SIL), and later approved by Khowar Academy(an literary Association working on the language and culture documentation in Northern Pakistan) used in Windows 8.1 and above as Ormuri Keyboard.
+</p>
+
+<p>This is the first Ormuri language keyboard created by Rehmat Aziz Chitrali, Pakistani linguist, Scientist,  Inventor,  Software Developer and Researcher. This keyboard is for Ormuri Language based on the keyboard layout as developed by Mr.Rehmat Aziz Chitrali and approved by Khowar Academy of Pakistan as standard keyboard for Ormuri language.</p>
+
+<p>The Ormuri Keyboard Project(OKP) was supervised and developed by Rehmat Aziz Chitrali, who heads the Khowar Academy Chitral Pakistan.</p>
+
+<p>Khowar Academy Chitral was established in April 1996 specifically to promote the endangered  languages of Chitral Pakistan. The Academy has worked extensively on Ormuri linguistics, standardization for computing, and modeling of script, speech and language. Research is being conducted in all aspects of Ormuri, including acoustic phonetics, phonology, morphology, syntax, grammar and semantics.</p>
+
+<h2>Font</h2>
+<p style="text-align: justify;">
+The RAChitrali-Ormuri Keyman keyboard is designed to work with any extended 
+Arabic script font. The Scheherazade font fully supports all the special characters of Ormuri language.
+</p>
+
+<h2>Keyboard</h2>
+<p>
+This keyboard layout is designed for the RAChitrali-Ormuri language. It includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. 
+
+<p>The keyboard layout is displayed below in Keyboard graphics.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift symbol'>
+</div>
+  
+<h2>How to use the RAChitrali-Ormuri Keyboard? Please read the following:-</h2>
+
+<p style="text-align: right; dir=rtl; font-size:200%">ہمارے متعلق
+</p>
+<p style="text-align: right; dir=rtl; font-size:200%">ارمڑی زبان
+</p>
+<p style="text-align: right; dir=rtl; font-size:150%">
+	ارمڑی زبان پاکستان کے قبائلی علاقوں میں بولی جاتی ہے۔اس زبان نے سب سے زیادہ اثر پشتو، اردو اور فارسی زبانوں سے قبول کیا ہے لیکن دیگر پاکستانی زبانوں کے الفاظ بھی بے شمار تعداد میں اس زبان میں موجود ہیں۔ اس زبان میں جہاں رحمت عزیز چترالی کا ارمڑی قاعدہ، ارمڑی حروف تہجی، ارمڑی خوشخطی، ارمڑی کیبورڈ منظر عام پہ آ چکے ہیں وہاں کھوار اکیڈمی، ارمڑی اکیڈمی اور مقامی لوگوں نے بھی نے بھی ارمڑی کتابیں لکھنے اور شایع کرنے کا سلسلہ شروع کیا ہے ۔ ارمڑی زبان کی اپنی مخصوص اصوات بھی ہیں جوکہ کھوار اور شینا زبان کی اصوات سے مماثلت رکھتی ہیں اور یہ وہ مخصوص آوازیں ہیں جوکہ اردو اور دیگر پاکستانی زبانوں میں موجود نہیں۔ ارمڑی زبان کی ان مخصوص آوازوں کے لیے سب سے پہلے رحمت عزیز چترالی نے چند حروف ایجاد کیے اور ان مخصوص حروف کو اپنے ارمڑی کیبورڈ، ارمڑی سافٹویر ارمڑی حروف تہجی میں شامل کیا، اور کھوار اکیڈمی کے قیام کے بعد پاکستان کے ضلع چترال سے تعلق رکھنے والے پاکستنانی ماہر لسانیات رحمت عزیز چترالی نے ارمڑی کے لیے یہ کلیدی تختہ ایجاد کیا ہے اور اس کا سہرہ آپ ہی کے سر جاتا ہے اور آپ کے ایجاد کردہ ارمڑی کیبورڈ کی مدد سے ارمڑی زبان میں تحریر کا سلسلہ جاری ہے اور رحمت عزیز چترالی کا یہ کام ارمڑی زبان کی تاریخ میں ایک سنگ میل کی حیثیت رکھتا ہے، پاکستانی زبانوں کے لیے بے لوث خدمات انجام دینے پر  ہم حکومت پاکستان سے رحمت عزیز چترالی کے لیے  ''ایوارڈ برائے حسن کارکردگی'' کی سفارش کرتے ہیں امید ہے کہ حکومت پاکستان رحمت عزیز  اور ان جیسے دیگر  پاکستانی ماہرین لسانیات اور تحقیق کاروں کے لیے ایوارڈو اعزازات کا اعلان کرے گی۔
+</p>
+<p style="text-align: right; dir=rtl; font-size:200%">
+ارمڑی کیبورڈ کے متعلق
+</p>
+<p style="text-align: right; dir=rtl; font-size:150%">
+ارمڑی کیبورڈ ارمڑی زبان کا پہلا سافٹ ویر اور صوتی کیبورڈ ہے جسے کھوار اکیڈمی چترال سے منسلک ماہر لسانیات رحمت عزیز چترالی نے ایجاد کیا ہے  اس کلیدی تختی میں ڈاکومینٹیشن کے زریعے کیبورڈ کے استعمال کے طریقہء کار کی وضاحت کی گئی ہے، اس کیبورڈ کے تین ورژن ہیں پہلا ورژن'' بیس ورژن'' کہلاتا ہے دوسرا ورژن ''شفٹ ورژن'' ہے اور تیسرا ورژن ''کنٹرول آلٹر ورژن'' کہلاتا ہے، رحمت عزیز چترالی نے یہ ارمڑی کیبورڈ اور ارمڑی سافٹ ویر کھوار اکیڈمی کے تعاون و اشتراک سے سمر انسٹیٹیوٹ آف لنگوسٹکس کے لیے تیار کیا ہے جوکہ آپ کی پاکستان میں بولی جانے والی معدومیت کا شکار زبان ارمڑی کے لیے ایک بہت بڑے کارنامے اور احسان سے کم نہیں، رحمت عزیز چترالی کا یہ کارنامہ ہے کہ انہوں نے ارمڑی زبان کے اس سافٹ ویر اور کیبورڈ کو مایکروسافٹ کی ممبر کمپنی ٹاول سافٹ، سمر انسٹیٹوٹ آف لنگوسٹکس اور کیمین کے ساتھ رجسٹر اور ریلیز کروایا اور  اس ارمڑی سافٹ ویر کو استفادہء عام کے لیے کھوار اکیڈمی نے ٹاول سافٹ اور کیمین کے تعاون و اشتراک سے ریلیز کردیا ہے امید ہے کہ یہ سافٹ ویر آپ کو پسند آئے گا</p>
+</p>
+<p style="text-align: right; dir=rtl; font-size:200%">
+ہمارے متعلق
+</p>
+<p style="text-align: right; dir=rtl; font-size:150%">
+ارمڑی سافٹ ویر اور ارمڑی ٹیکسٹ ایڈیٹر ایجاد کرنے کا تاریخی کام پاکستان کے ضلع چترال سے تعلق رکھنے والے لسانی ماہر جناب رحمت عزیز چترانی نے شروع کیا ہے۔ اور آج اس سافٹ ویر کے زریعے رحمت عزیز چترالی نے ارمڑی جیسی غیر تحریری زبان کو تحریری زبان میں تبدیل کرکے کمپیوٹر اور موبائل کی زبان بنا دیا ہے اور ارمڑی بولنے والے لوگ اب فیس بک اور ٹویٹر میں بھی اس سافٹ ویر کے زریعے ارمڑی لکھ رہے ہیں جو کہ ارمڑی دان طبقے کے لیے یقینا بہت بڑی خوشی کی بات ہے
+</p>
+
+<h2>Links</h2>
+<ul>
+<li>Home Page: <a href='http://keymankeyboards.com'>http://keymankeyboards.com/</a></li>
+<li><a href='https://keyman.com/keyboards/languages/oru'>Other keyboards for these languages</a></li>
+</ul>
+
