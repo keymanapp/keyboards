fb3d21df54fa33ad0781c283fb4c3e1de4d29f7b
LornaSIL <lorna_evans@sil.org>
2019-02-27 13:31:57 -0600

    [sanskrit_unicode] Created a php file and point to another transliteration kbd since we cannot update this one

---
+++ legacy/s/sanskrit unicode/source/help/sanskrit unicode.php
@@ -0,0 +1,15 @@
+<?php
+  $pagename = 'Sanskrit Unicode Keyboard Help';
+  $pagetitle = 'Sanskrit Unicode Keyboard Help';
+  require_once('header.php');
+?>
+
+
+<p>There are two keyboards included here. Sanskrit CSX is for a non-Unicode font - any of the CSX (Classical Sanskrit eXtended) fonts, of which one public domain version is included with the keyboard.  The other is a Unicode keyboard for the public domain Indic Times font, also included. They are essentially very simple simple keyboards as the majority of the Romanised Sanskrit/Pali characters are the same as our normal European letters.</p>
+
+<p>The long vowels with bars above them, ie. à ã å â ä æ and the consonants with dots underneath, eg. ñ ó õ ë are all typed by pressing the deadkey / and then the ordinary letter. Thus ñ is written / followed by "t". This covers the great majority of extra characters. However there are just too many "n" characters for our keys, so in addition ¤ is written with deadkey ~ (on the British keyboard, | on the American, I believe) followed by n. There is still ï of course. This is written with deadkey * (star up above!) followed by n. The Sanskrit ù character is written like all the other characters with a dot underneath, and ÷ character is written with the ~ deadkey, like the ¤ character, both having similar diacritical marks over the letter.</p>
+
+<p>Finally the rare characters (only used in Sanskrit) with double diacritical marks, é ê í î are written with deadkey _ followed by the appropriate character.</p>
+
+
+
