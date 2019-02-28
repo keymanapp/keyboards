8747ef69878395b552d20cdc0b311b28cdf4d66f
David Rowe <david_rowe@sil.org>
2019-02-27 17:02:44 -0800

    [rac_dogri] Keyboard submitted on behalf of author

---
+++ release/rac/rac_dogri/source/help/rac_dogri.php
@@ -0,0 +1,85 @@
+<?php 
+  $pagename = 'Rachitrali-Dogri Keyboard Help';
+  $pagetitle = 'Rachitrali-Dogri';
+  require_once('header.php');
+?>
+<p>This keyboard is designed for the Dogri language spoken in Pakistan, Kashmir and India, in its standard Perso-Arabic script, as used in Asia. It is based on phonetic keyboard for Dogri Language developed by Pakistani linguist, author, Scientist,  Inventor researcher and Software Developer Mr.Rehmat Aziz Chitrali for Khowar Academy(an literary Association working on the language and culture documentation in Northern Pakistan) used in Windows 8.1 and above as Dogri Keyboard.
+</p>
+
+<p>This is the first Dogri language keyboard created by Rehmat Aziz Chitrali, Pakistani linguist, Scientist, Inventor, Software Developer and Researcher. This keyboard is for Dogri Language based on the keyboard layout as developed by Mr.Rehmat Aziz Chitrali and approved by Khowar Academy of Pakistan as standard keyboard for Dogri language.</p>
+
+<p>The Dogri Keyboard Project(DKP) was supervised and developed by Rehmat Aziz Chitrali, who heads the Khowar Academy Chitral Pakistan.</p>
+
+<p>Khowar Academy Chitral was established in April 1996 specifically to promote the endangered  languages of Chitral Pakistan. The Academy has worked extensively on Dogri linguistics, standardization for computing, and modeling of script, speech and language. Research is being conducted in all aspects of Dogri, including acoustic phonetics, phonology, morphology, syntax, grammar and semantics.</p>
+
+<p/>
+<h2>Font</h2>
+<p style="text-align: justify;">
+The Rachitrali-Dogri Keyman keyboard is designed to work with any extended 
+Arabic script font.  
+</p>
+
+
+<h2>Keyboard</h2>
+<p>
+This keyboard layout is designed for the Dogri language by Rehmat Aziz Chitrali linguist and researcher at Khowar Academy Chitral Pakistan. It includes 
+an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item.     </p>
+
+<h3>Desktop Keyboard Layout</h3>
+<div id='osk' data-states='default shift ctrlalt'>
+</div>
+
+<h3>Mobile/Tablet Keyboard Layout</h3>
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+      	
+<h2>How to use the Rachitrali-Dogri Keyboard? Please read the following:-</h2>
+
+<p style="text-align: right; dir=rtl; font-size:200%">ہمارے متعلق
+</p>
+<p style="text-align: right; dir=rtl; font-size:200%">ڈوگری زبان
+</p>
+<p style="text-align: right; dir=rtl; font-size:150%">
+	ڈوگری زبان پاکستان، کشمیر اور ہندوستان میں بولی جاتی ہے۔ اس زبان میں جہاں رحمت عزیز چترالی کا ڈوگری قاعدہ، ڈوگری حروف تہجی، ڈوگری خوشخطی اور ڈوگری کیبورڈ منظر عام پہ آ چکے ہیں وہاں کھوار اکیڈمی اور مقامی لوگوں نے بھی نے بھی ڈوگری کتابیں لکھنے اور شایع کرنے کا سلسلہ شروع کیا ہے ۔ ڈوگری زبان کی اپنی مخصوص اصوات بھی ہیں جوکہ دیگرایشیاٗئی زبانوں کی اصوات سے مماثلت رکھتی ہیں اور یہ وہ مخصوص آوازیں ہیں جوکہ اردو اور دیگر پاکستانی زبانوں میں موجود نہیں۔ ڈوگری زبان کی ان مخصوص آوازوں کے لیے سب سے پہلے رحمت عزیز چترالی نے چند حروف ایجاد کیے اور ان مخصوص حروف کو اپنے ڈوگری کیبورڈ، ڈوگری سافٹویر ڈوگری حروف تہجی میں شامل کیا، اور کھوار اکیڈمی کے قیام کے بعد رحمت عزیز چترالی نے ڈوگری کے لیے پہلا کلیدی تختہ ایجاد کیا ہے اور اس کا سہرہ آپ ہی کے سر جاتا ہے اور آپ کے ایجاد کردہ ڈوگری کیبورڈ کی مدد سے ڈوگری زبان میں تحریر کا سلسلہ جاری ہے اور رحمت عزیز چترالی کا یہ کام ڈوگری زبان کی تاریخ میں ایک سنگ میل کی حیثیت رکھتا ہے، پاکستانی زبانوں کے لیے بے لوث خدمات انجام دینے پر  ہم حکومت پاکستان سے رحمت عزیز چترالی کے لیے  ایوارڈ برائے حسن کارکردگی کی سفارش کرتے ہیں امید ہے کہ حکومت پاکستان رحمت عزیز  اور ان جیسے دیگر  پاکستانی ماہرین لسانیات اور تحقیق کاروں کے لیے ایوارڈو اعزازات کا اعلان کرے گی۔
+</p>
+<p style="text-align: right; dir=rtl; font-size:200%">
+ڈوگری کیبورڈ کے متعلق
+</p>
+<p style="text-align: right; dir=rtl; font-size:150%">
+ڈوگری کیبورڈ ڈوگری زبان کا پہلا سافٹ ویر اور کیبورڈ ہے جسے کھوار اکیڈمی چترال سے منسلک ماہر لسانیات رحمت عزیز چترالی نے ایجاد کیا ہے  اس کلیدی تختی میں ڈاکومینٹیشن کے زریعے کیبورڈ کے استعمال کے طریقہء کار کی وضاحت کی گئی ہے، اس کیبورڈ کے تین ورژن ہیں پہلا ورژن'' بیس ورژن'' کہلاتا ہے دوسرا ورژن ''شفٹ ورژن'' ہے اور تیسرا ورژن ''کنٹرول آلٹر ورژن'' کہلاتا ہے، رحمت عزیز چترالی نے یہ ڈوگری کیبورڈ اور ڈوگری سافٹ ویر کھوار اکیڈمی کے تعاون و اشتراک سے سمر انسٹیٹیوٹ آف لنگوسٹکس کے لیے تیار کیا ہے جوکہ آپ کی پاکستان میں بولی جانے والی معدومیت کا شکار زبان ڈوگری کے لیے ایک بہت بڑے کارنامے اور احسان سے کم نہیں، رحمت عزیز چترالی کا یہ کارنامہ ہے کہ انہوں نے ڈوگری زبان کے اس سافٹ ویر اور کیبورڈ کو مایکروسافٹ کی ممبر کمپنی ٹاول سافٹ، سمر انسٹیٹوٹ آف لنگوسٹکس اور کیمین کے ساتھ رجسٹر اور ریلیز کروایا اور  اس ڈوگری سافٹ ویر کو استفادہء عام کے لیے کھوار اکیڈمی نے ٹاول سافٹ اور کیمین کے تعاون و اشتراک سے ریلیز کردیا ہے امید ہے کہ یہ سافٹ ویر آپ کو پسند آئے گا</p>
+</p>
+<p style="text-align: right; dir=rtl; font-size:200%">
+ہمارے متعلق
+</p>
+<p style="text-align: right; dir=rtl; font-size:150%">
+ڈوگری سافٹ ویر اور ڈوگری ٹیکسٹ ایڈیٹر ایجاد کرنے کا تاریخی کام پاکستان کے ضلع چترال سے تعلق رکھنے والے لسانی ماہر جناب رحمت عزیز چترانی نے شروع کیا ہے۔ اور آج اس سافٹ ویر کے زریعے رحمت عزیز چترالی نے ڈوگری جیسی غیر تحریری زبان کو تحریری زبان میں تبدیل کرکے کمپیوٹر اور موبائل  کی زبان بنا دیا ہے اور ڈوگری بولنے والے لوگ اب فیس بک اور ٹویٹر میں بھی اس سافٹ ویر کے زریعے ڈوگری لکھ رہے ہیں جو کہ ڈوگری دان طبقے کے لیے یقینا بہت بڑی خوشی کی بات ہے
+</p>
+
+      <P class=Caption></P><A name=license></A>
+      <H2>License</H2>
+      <P>Rachitrali-Dogri Keyman Keyboard is copyright © 2018 Rehmat Aziz Chitrali. 
+      	It is distributed under The MIT License (MIT)</P>
+      <DIV class=Note>
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:</p>
+
+<p>The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.</p>
+
+<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</p></DIV>
+
+<h2>Links</h2>
+<ul>
+<li><a href='http://keymankeyboards.com/iso:doi'>Other keyboards for these languages</a></li>
+</ul>
+
