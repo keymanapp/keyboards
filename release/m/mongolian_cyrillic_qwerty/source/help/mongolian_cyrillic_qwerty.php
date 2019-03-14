6408f6a265d997885f62917b3170da4860f40d86
Makara <makara@keyman.com>
2019-03-14 13:28:49 +0700

    add help and update layouts

---
+++ release/m/mongolian_cyrillic_qwerty/source/help/mongolian_cyrillic_qwerty.php
@@ -0,0 +1,24 @@
+<?php 
+  $pagename = 'Mongolian Cyrillic QWERTY Keyboard Help';
+  $pagetitle = 'Mongolian Cyrillic QWERTY Keyboard Help';
+  require_once('header.php');
+
+
+?>
+
+<p>
+This phonetic keyboard layout is designed for Khalkha Mongolian Cyrillic. </br>
+It is based on the English QWERTY keyboard and also aligns with Chinese Pinyin.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Layout</h2>
+<img src="../welcome/keyboard_layout.png" alt="Desktop Layout" style="width:800px";>
+<h2>Touch Layout</h2>
+<p>Default Layer</p>
+<img src="../welcome/touch_layout_default.png" alt="Default Layer" style="width:800px";>
+<p>Shift Layer</p>
+<img src="../welcome/touch_layout_shift.png" alt="Shift Layer" style="width:800px";>
+<p>Symbol Layer</p>
+<img src="../welcome/touch_layout_symbol.png" alt="Symbol Layer" style="width:800px";>
