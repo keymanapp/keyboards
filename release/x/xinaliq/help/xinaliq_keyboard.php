79fc06842c6904c2d84c821bc279a6aaa482b08c
KenK-3-21 <KenK-3-21@users.noreply.github.com>
2019-03-28 17:12:07 -0500

    new folder for xinaliq kbd

---
+++ release/x/xinaliq/help/xinaliq_keyboard.php
@@ -0,0 +1,21 @@
+<?php 
+  $pagename = 'Xinaliq Keyboard Help';
+  $pagetitle = 'Xinaliq Keyboard Help';
+  require_once('header.php');
+?>
+
+<p style='margin: 16px 0 0 0'>
+Keyboard for Xinaliq (kjj) using the Latin Orthography approved by the Ministry of Education of the Republic of Azerbaijan.</p>
+
+<p style='margin: 16px 0 0 0'>
+The desktop keyboard layout uses a rota. That means that if you press once, you get the base keyboard. If you press twice, you get a different letter.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+
