305410d28a6dcdc570dbd475d13b43f88e3c7669
KenK-3-21 <KenK-3-21@users.noreply.github.com>
2019-04-03 21:58:58 -0500

    changes to welcome.htm
    added a description of the rota, with an illustration of the keyboard.

--- release/x/xinaliq/source/help/xinaliq.php
+++ release/x/xinaliq/source/help/xinaliq.php
@@ -8,7 +8,7 @@
 Keyboard for Xinaliq (kjj) using the Latin Orthography approved by the Ministry of Education of the Republic of Azerbaijan.</p>
 
 <p style='margin: 16px 0 0 0'>
-The desktop keyboard layout uses a rota. That means that if you press once, you get the base keyboard. If you press twice, you get a different letter.</p>
+This keyboard uses a rota to access alternate characters based on the default shape. To get "g", you type "g", to get "ğ" type "gg". Characters that use the rota system are c, e, g, h, i, k, o, p, q, s, u, t, x, z (and their uppercase counterparts).</p>
 
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift'>
