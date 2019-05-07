cbaa99b6131141815b213ae690532044d57b4d98
LornaSIL <lorna_evans@sil.org>
2019-05-07 16:35:09 -0500

    [urdu_phonetic] Move keyboard from legacy to release with permission of author

---
+++ release/u/urdu_phonetic/source/help/urdu_phonetic.php
@@ -0,0 +1,18 @@
+<?php 
+  $pagename = 'Urdu Phonetic Keyboard Keyboard Help';
+  $pagetitle = 'Urdu Phonetic Keyboard Keyboard Help';
+  require_once('header.php');
+?>
+
+<p>
+This package includes a phonetic keyboard called <b>Urdu Phonetic</b> and the font <b>Pak Nastaleeq</b> designed for the URdu languages.
+</p>
+
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift ctrl shift-ctrl alt shift-alt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
