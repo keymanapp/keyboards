8b0ae11c8c34ca9c4165af1b0a45e41251910ea4
Makara <makara@keyman.com>
2019-06-12 14:08:09 +0700

    update script tag, icon, misspelling, support to any

--- release/basic/basic_kbdazst/source/help/basic_kbdazst.php
+++ release/basic/basic_kbdazst/source/help/basic_kbdazst.php
@@ -11,11 +11,11 @@
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 
 <h2>Desktop Keyboard Layout</h2>
-<div id='osk'>
+<div id='osk' data-states='default shift rightalt'>
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
-<div id='osk-tablet'>
+<div id='osk-tablet' data-states='default shift numeric'>
 </div>
 
 
