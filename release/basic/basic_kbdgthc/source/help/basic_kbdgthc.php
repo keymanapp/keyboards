d4ed3dd67448fa0cbca45291395bf9217bbfc2e2
Makara <makara@keyman.com>
2019-06-24 11:55:18 +0700

    initial commit

---
+++ release/basic/basic_kbdgthc/source/help/basic_kbdgthc.php
@@ -0,0 +1,23 @@
+<?php 
+  $pagename = 'Gothic Basic Keyboard Help';
+  $pagetitle = 'Gothic Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<br/>
+
+<p>This keyboard makes use of SHIFT+RALT keys which are set by default when Keyman is installed. To use this keyboard, you must go to <b>Configuration.../Hotkeys</b> and select the hotkey for "Switch Keyman Desktop Off". Either change the hotkey or turn it off. Do the same for "Open Keyboard Menu" and for "Open Language Switcher".</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default numeric'>
+</div>
