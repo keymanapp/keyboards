1bc6ead33f45ac422b789e13d7496a58c6c89d03
dhenchoz <38858239+dhenchoz@users.noreply.github.com>
2019-06-26 11:51:07 +0200

    Fifth submission for Soqotri keyboard

---
+++ release/s/soqotri_arabic/source/help/soqotri_arabic.php
@@ -0,0 +1,30 @@
+﻿<?php
+  $pagename = 'Soqotri Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+<h1  dir="rtl" style='margin-bottom: 2px'>لوحة المفاتيح السقطرية</h1>
+<p  dir="rtl">لوحة المفاتيح هذه مجعولة للكتابة باللغة السقطرية.</p>
+
+<h2  dir="rtl">تخطيط لوحة المفاتيح </h2>
+      <P  dir="rtl">تخطيط لوحة المفاتيح معروض أدناه في الرسومات البيانية </P>
+      	
+      	<h3 dir="rtl">النسخة الأصلية</h3>
+      	<img dir="rtl" src="SoqotriU_.png" alt="Base Version" style="float:right;width:600px;">
+
+      	<h3  dir="rtl">نسخة التحويل </h3>
+      	<img src="SoqotriU_S.png"  dir="rtl" alt="Shift Version"style="float:right;width:600px;">
+
+
+<h1 style='margin-bottom: 2px'>Soqotri Keyboard</h1>
+<p>This is a keyboard for entering Soqotri.</p>
+
+<h2>Keyboard Layout</h2>
+      <P>The keyboard layout is displayed below in Keyboard graphics.</P>
+      	
+      	<h3>Base Version</h3>
+      	<img src="SoqotriU_.png" alt="Base Version" style="width:600px;">
+
+      	<h3>Shift Version</h3>
+      	<img src="SoqotriU_S.png" alt="Shift Version" style="width:600px;">
+                                      
