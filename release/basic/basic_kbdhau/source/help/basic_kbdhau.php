4eef727bd82b09f2e886101f2e4b4cb28bd36416
Makara <makara@keyman.com>
2019-06-26 09:05:15 +0700

    update language tag, icon, help and welcome

--- release/basic/basic_kbdhau/source/help/basic_kbdhau.php
+++ release/basic/basic_kbdhau/source/help/basic_kbdhau.php
@@ -15,6 +15,8 @@ Hausa Basic keyboard 1.0 is generated from template.
 </p>
 <br/>
 
+<p>This keyboard makes use of SHIFT+RALT keys which are set by default when Keyman is installed. To use this keyboard, you must go to <b>Configuration.../Hotkeys</b> and select the hotkey for "Switch Keyman Desktop Off". Either change the hotkey or turn it off. Do the same for "Open Keyboard Menu" and for "Open Language Switcher".</p>
+
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift rightalt rightalt-shift'>
 </div>
