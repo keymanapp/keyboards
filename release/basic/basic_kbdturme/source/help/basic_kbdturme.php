fc83c381ffad170fd7623006af9675c2d9c72cbb
Lorna Evans <LornaSIL@users.noreply.github.com>
2019-07-08 14:22:12 -0500

    [basic_kbdturme] Import from Windows and merge in mobile

---
+++ release/basic/basic_kbdturme/source/help/basic_kbdturme.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Turkmen Basic Keyboard Help';
+  $pagetitle = 'Turkmen Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+<p>
+    Turkmen Basic 1.1 (Latin script) generated from template.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift Numeric'>
+</div>
+
+
+<p>© 2009-2019 SIL International</p>
+
+</body>
+</html>
