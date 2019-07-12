e601fd87f444bccf95b8e2cb9ac09022e217c3b1
Lorna Evans <LornaSIL@users.noreply.github.com>
2019-07-12 10:51:13 -0500

    [basic_kbdkyr] Kyrgyz Cyrillic Basic import

---
+++ release/basic/basic_kbdkyr/source/help/basic_kbdkyr.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Kyrgyz Cyrillic Basic Keyboard Help';
+  $pagetitle = 'Kyrgyz Cyrillic Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+<p>
+    Kyrgyz Cyrillic Basic 1.1 generated from template.
+</p>
+<p>This keyboard makes use of the SHIFT+RALT+K key which is set by default when Keyman is installed. To use this keyboard, you must go to <b>Configuration.../Hotkeys</b> and select the hotkey for "Switch Keyman Desktop Off". Either change the hotkey or turn it off. Do the same for "Open Keyboard Menu" and for "Open Language Switcher".</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+
+<p>© 2009-2019 SIL International</p>
