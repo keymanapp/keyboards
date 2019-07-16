b2c7b41c74b389da692ffad39587536877e48042
rmlockwood <rlbooks@web.de>
2019-07-16 10:45:43 +0200

    Initial version of Balochi Persian

---
+++ release/b/balochi_persian/source/help/balochi_persian.php
@@ -0,0 +1,22 @@
+<?php 
+  $pagename = 'Balochi Persian Keyboard Help';
+  $pagetitle = 'Balochi Persian Keyboard Help';
+  require_once('header.php');
+
+
+?>
+
+
+<p>This keyboard layout is designed for transcribing Balochi languages using the standard Persian Windows keyboard layout. 
+It includes an on screen keyboard which can be viewed by clicking on the Keyman icon 
+and selecting the On Screen Keyboard menu item. Similar keyboards are also available for other desktop and mobile platforms.</p>
+
+<h2>Keyboard Layout</h2>
+<div id='osk'>
+</div>
+
+<p>For Android and iOS, you may use either long press or key combinations.</p>
+
+<h2>Release History</h2>
+<p>Version 1.0, released 16 July 2019</p>
+
