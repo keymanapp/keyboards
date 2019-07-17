d4a1e36a843a9f77c9d24c7479d72baf01d73090
ind-nt <52841347+ind-nt@users.noreply.github.com>
2019-07-17 10:52:20 -0600

    Add files via upload

--- release/i/indigenous_nt/source/help/indigenous_nt.php
+++ release/i/indigenous_nt/source/help/indigenous_nt.php
@@ -1,2 +1,34 @@
-<?php /* see keyboard layout pdf for more info.
+<?php 
+  $pagename = 'Indigenous NT Integrated Keyboard Help';
+  $pagetitle = 'Indigenous NT Integrated Keyboard Help';
+  require_once('header.php');
 ?>
+
+<?php
+?>
+
+<style>
+  body {
+   font-family: Cambria, Georgia, serif;  
+  }
+  h1, h2, h3, h4, h5 {
+    font-family: Calibri, Helvetica, Arial, sans-serif;
+  }
+</style>
+
+<h1>Indigenous NT Integrated Keyboard for Keyman</h1>
+
+<p><small>Version 3.2 (July 2019)<br/>
+  Rajiv Rawat</small></p>
+
+<p>This is the Keyman version of the universal keyboard for the 11 official languages of the Northwest Territories (Inuktitut in Roman Orthography). Additional characters for other circumpolar languages have also been included.</p>
+<p>It retains the standard US keyboard layout while providing access to special characters and diacritics through the <strong>Alt</strong> key on Desktop computers.</p>
+<p>On mobile devices such as tablets and smartphones, the longpress popup provides the same functionality (glottal stops can be accessed through the <strong>period</strong> key).</p>
+<p>The keyboard is intended to be intuitive and easy to use for the widest number of users. As such, it hopes to facilitate widescale acceptance and adoption of the incredible linguistic diversity in the Canadian North.</p>
+
+  <h3>Indigenous NT Layout (PC)</h3>
+  
+  <img style="width: 100%;" src="indigenous_nt_layout.png">
+
+	<p>Note that the diacritics are typed after the vowel as in the Vowel First Dene convention.</p>
+	<p><a href="indigenous_nt_layout.pdf">A PDF version of the keyboard layout (desktop) is available to print.</a></p>
