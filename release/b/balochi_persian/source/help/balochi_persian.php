4126c984816791e5826c269d9570c21c9d1ed3bc
rmlockwood <rlbooks@web.de>
2019-07-18 12:08:41 +0200

    fixed items from review

--- release/b/balochi_persian/source/help/balochi_persian.php
+++ release/b/balochi_persian/source/help/balochi_persian.php
@@ -11,8 +11,16 @@
 It includes an on screen keyboard which can be viewed by clicking on the Keyman icon 
 and selecting the On Screen Keyboard menu item. Similar keyboards are also available for other desktop and mobile platforms.</p>
 
-<h2>Keyboard Layout</h2>
-<div id='osk'>
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift ctrl'>
+</div>
+
+<h2>Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+<h2>Phone Keyboard Layout</h2>
+<div id='osk-phone' data-states='default shift numeric'>
 </div>
 
 <p>For Android and iOS, you may use either long press or key combinations.</p>
