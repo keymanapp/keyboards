12cafc4c2f1e7dd4013fb33ab1865e199ce35435
Makara <makara@keyman.com>
2019-08-14 13:57:39 +0700

    fix deadkeys and add touch layout

--- release/basic/basic_kbdrum/HISTORY.md
+++ release/basic/basic_kbdrum/HISTORY.md
@@ -3,4 +3,5 @@ Russian - Mnemonic Basic Change History
 
 1.0 (2019-08-13)
 ----------------
-* Created by Makara SOK
+* Imported from Win10 by Makara SOK using the import tool
+* Add touch layout
---
+++ release/basic/basic_kbdrum/source/help/basic_kbddrum.php
@@ -0,0 +1,40 @@
+<?php 
+  $pagename = 'Russian - Mnemonic Basic Keyboard Help';
+  $pagetitle = 'Russian - Mnemonic Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+
+<p>This keyboard use five deakeys, such as: <kbd class="key">ц</kbd>	<kbd class="key">Ц</kbd>	<kbd class="key">й</kbd>	<kbd class="key">Й</kbd>	<kbd class="key">с</kbd>	<kbd class="key">С</kbd>	<kbd class="key">ы</kbd>	<kbd class="key">Ы</kbd>	and <kbd class="key">¨</kbd>. Press one of these keys and then another key to produce the desired output. For instance, press <kbd class="key">ц</kbd> and then the <kbd class="key">Space</kbd> to get ц; press <kbd class="key">ц</kbd> and then <kbd class="key">х</kbd> to get ч etc. Please see the tables below for a complete combination.</p>
+
+<table>
+<thead><tr><th></th><th><kbd class="key">Space</kbd></th><th><kbd class="key">х</kbd></th><th><kbd class="key">Х</kbd></th></tr></thead><tbody>
+ <tr><td><kbd class="key">ц</kbd></td><td>ц</td><td>ч</td><td>&nbsp;</td></tr>
+ <tr><td><kbd class="key">Ц</kbd></td><td>Ц</td><td>Ч</td><td>Ч</td></tr>
+ <tr><td><kbd class="key">с</kbd></td><td>с</td><td>ш</td><td>&nbsp;</td></tr>
+ <tr><td><kbd class="key">С</kbd></td><td>С</td><td>Ш</td><td>Ш</td></tr>
+</tbody></table>
+<br>
+<table>
+<thead><tr><th></th><th><kbd class="key">Space</kbd></th><th><kbd class="key">а</kbd></th><th><kbd class="key">А</kbd></th><th><kbd class="key">е</kbd></th><th><kbd class="key">Е</kbd></th><th><kbd class="key">о</kbd></th><th><kbd class="key">О</kbd></th><th><kbd class="key">у</kbd></th><th><kbd class="key">У</kbd></th></tr></thead><tbody>
+ <tr><td><kbd class="key">й</kbd></td><td>й</td><td>я</td><td>&nbsp;</td><td>э</td><td>&nbsp;</td><td>ё</td><td>&nbsp;</td><td>ю</td><td>&nbsp;</td></tr>
+ <tr><td><kbd class="key">Й</kbd></td><td>Й</td><td>Я</td><td>Я</td><td>Э</td><td>Э</td><td>Ё</td><td>Ё</td><td>Ю</td><td>Ю</td></tr>
+ <tr><td><kbd class="key">ы</kbd></td><td>ы</td><td>я</td><td>&nbsp;</td><td>э</td><td>&nbsp;</td><td>ё</td><td>&nbsp;</td><td>ю</td><td>&nbsp;</td></tr>
+ <tr><td><kbd class="key">Ы</kbd></td><td>Ы</td><td>Я</td><td>Я</td><td>Э</td><td>Э</td><td>Ё</td><td>Ё</td><td>Ю</td><td>Ю</td></tr>
+</tbody></table>
+<br>
+<table>
+<thead><tr><th></th><th><kbd class="key">Space</kbd></th><th><kbd class="key">а</kbd></th><th><kbd class="key">А</kbd></th><th><kbd class="key">е</kbd></th><th><kbd class="key">Е</kbd></th><th><kbd class="key">о</kbd></th><th><kbd class="key">О</kbd></th><th><kbd class="key">у</kbd></th><th><kbd class="key">У</kbd></th><th><kbd class="key">б</kbd></th><th><kbd class="key">Б</kbd></th><th><kbd class="key">г</kbd></th><th><kbd class="key">Г</kbd></th><th><kbd class="key">и</kbd></th><th><kbd class="key">И</kbd></th><th><kbd class="key">ш</kbd></th><th><kbd class="key">Ш</kbd></th><th><kbd class="key">ы</kbd></th><th><kbd class="key">Ы</kbd></th><th><kbd class="key">з</kbd></th><th><kbd class="key">З</kbd></th><th><kbd class="key">¨</kbd></th></tr></thead><tbody>
+ <tr><td><kbd class="key">¨</kbd></td><td>¨</td><td>э</td><td>Э</td><td>ё</td><td>Ё</td><td>ё</td><td>Ё</td><td>ю</td><td>Ю</td><td>ъ</td><td>Ъ</td><td>ѓ</td><td>Ѓ</td><td>ї</td><td>Ї</td><td>щ</td><td>Щ</td><td>ў</td><td>Ў</td><td>ж</td><td>Ж</td><td>`</td></tr>
+</tbody></table>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
