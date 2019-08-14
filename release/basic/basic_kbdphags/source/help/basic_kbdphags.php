eb6db5d1111378b15cff6bb94e40a26f7a8a9e02
Makara <makara@keyman.com>
2019-08-14 16:06:07 +0700

    update help page

--- release/basic/basic_kbdphags/source/help/basic_kbdphags.php
+++ release/basic/basic_kbdphags/source/help/basic_kbdphags.php
@@ -8,6 +8,17 @@
 
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 
+<p>This keyboard uses various kinds of spaces and joiners in addition to the normal white space. They are listed below.</p>
+
+<ul>
+	<li>ZWJ -- Zero Width Joiner (U+200D) is placed on "Shift + 8".</li>
+	<li>ZWNJ -- Zero Width Non-Joiner (U+200C), "Shift + 6".</li>
+	<li>NBSP -- No-Break Space (U+00A0), "Shift + Hyphen".</li>
+	<li>NNBSP -- Narrow No-Break Space (U+202F), "Hyphen".</li>
+	<li>TS -- Thin Space (U+2009), "Shift + Spacebar".</li>
+</ul>
+<p>On touch layout, they are on the longpress of the Spacebar.</p>
+
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift rightalt rightalt-shift'>
 </div>
