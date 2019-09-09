ffae177a3a21057aa4e5a2303b492e555dee7b9d
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-09 09:18:13 -0600

    Add Spanish documentation to PHP help.

--- release/e/eddieantonio_pid_piaroa/source/help/eddieantonio_pid_piaroa.php
+++ release/e/eddieantonio_pid_piaroa/source/help/eddieantonio_pid_piaroa.php
@@ -3,6 +3,13 @@
   $pagetitle = 'Piaroa Keyboard Help';
   require_once('header.php');
 ?>
+
+<nav>
+  <p> <a href="#eddieantonio_pid_piaroa-es">Lee este documento en español.</a> </p> 
+</nav>
+
+<a id="eddieantonio_pid_piaroa-en">
+
 <p>The Piaroa keyboard allows you to type Piaroa vowels which are not present in the Spanish alphabet. These vowels utilize the <kbd>AltGr</kbd> key, which is the <kbd>Alt</kbd> key to the right of the spacebar.</p>
 
 <h1>Fonts</h1>
@@ -11,7 +18,7 @@
 
 <h1>Characters</h1>
 
-<p>Apart from the key sequences in the table below, all keys on a regular Latin keyboard should work the same as in your default keyboard layout. </h1>
+<p>Apart from the key sequences in the table below, all keys on a regular Latin keyboard should work the same as in your default keyboard layout. </p>
 <table>
   <thead>
     <tr>
@@ -36,6 +43,57 @@
 <h1>Keyboard Source</h1>
 
 <p>This keyboard is available online at <a href='https://keyman.com/keyboards/eddieantonio_pid_piaroa'>keyman.com/keyboards/eddieantonio_pid_piaroa</a></p>
+
+<hr>
+
+<a id="eddieantonio_pid_piaroa-es">
+
+<h1 lang="es">Usando el teclado Piaora</h1>
+
+<p lang="es"> Este teclado usa la tecla <kbd>Alt Gr</kbd> para escribir vocales que no
+hay en español.  La tecla <kbd>Alt Gr</kbd> es la tecla <kbd>Alt</kbd> u
+<kbd>⌥ Opción</kbd> que esta ubicada al lado <strong>derecho</strong> de la
+barra espaciadora; la tecla <kbd>Alt</kbd> a la izquierda <em>no
+funcionará!</em></p>
+
+<h1 lang="es">Fuetes</h1>
+
+<p lang="es">Piaroa no requiere un fuente especial. Sin embargo, algunos fuentes no son capazes de renderizar la cedilla (<span style="font-family: Calibri, Helvetica Neue">&nbsp;&#x0327;</span>) debajo de vocales.  En sistemas Windows, el fuente Calibri renderiza la cedilla correctamente (<span style="font-family: Calibri">a̧, ä̧, ȩ, i̧, o̧, ö̧, u̧</span>); en sistemas macOS, la fuente Helvetica Neue renderiza la cedilla correctamente (<span style="font-family: Helvetica Neue">a̧, ä̧, ȩ, i̧, o̧, ö̧, u̧</span>).</p>
+
+<aside>
+  <p>Anteriormente, era necesario usar el fuente Piaroa SILSophia, pero este
+  fuente no es compatible con Unicode. Por eso, ya no es consegable usar un
+  fuente especial.</p>
+</aside>
+
+<h1 lang="es">Teclas</h1>
+
+<table lang="es">
+  <thead>
+    <tr>
+      <th>Para escribir&hellip;</th> <th>Presiona&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ä</td> <td> <kbd>Alt Gr</kbd> + <kbd>A</kbd></td> </tr>
+    <tr> <td>ö</td> <td> <kbd>Alt Gr</kbd> + <kbd>O</kbd></td> </tr>
+    <tr> <td>Ä</td> <td> <kbd>⇧ Mayús</kbd> + <kbd>Alt Gr</kbd> + <kbd>A</kbd></td> </tr>
+    <tr> <td>Ö</td> <td> <kbd>⇧ Mayús</kbd> + <kbd>Alt Gr</kbd> + <kbd>O</kbd></td> </tr>
+    <tr> <td>a&#x327;</td> <td> <kbd>A</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+    <tr> <td>e&#x327;</td> <td> <kbd>E</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+    <tr> <td>i&#x327;</td> <td> <kbd>I</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+    <tr> <td>o&#x327;</td> <td> <kbd>O</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+    <tr> <td>u&#x327;</td> <td> <kbd>U</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+    <tr> <td>ä&#x327;</td> <td> <kbd>Alt Gr</kbd> + <kbd>A</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+    <tr> <td>ö&#x327;</td> <td> <kbd>Alt Gr</kbd> + <kbd>O</kbd>, luego <kbd>Alt Gr</kbd> + <kbd>,</kbd> </td> </tr>
+  </tbody>
+</table>
+
+<h1>Para descargar el teclado</h1>
+
+<p>Este teclado se puede decargar aquí: <a href='https://keyman.com/keyboards/eddieantonio_pid_piaroa'>keyman.com/keyboards/eddieantonio_pid_piaroa</a></p>
+
+<hr>
 
 <h1>Change History</h1>
 <p>1.1.1: Prepare for inclusion in Keyman Keyboards repository.</p>
