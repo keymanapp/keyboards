5a6a4edf032c036787caa15c33bf21924631af8c
Lorna Evans <LornaSIL@users.noreply.github.com>
2019-11-11 13:53:34 -0600

    [basic_kbduk] United Kingdom Basic Win10 import

---
+++ release/basic/basic_kbduk/source/help/basic_kbduk.php
@@ -0,0 +1,22 @@
+<?php 
+  $pagename = 'United Kingdom Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    United Kingdom Basic 1.1 generated from template.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
+</html>
