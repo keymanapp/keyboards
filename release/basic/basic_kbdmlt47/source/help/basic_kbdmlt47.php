cdde5e5599ca4f8cbfe3418ecba90d0fce8243aa
Lorna Evans <LornaSIL@users.noreply.github.com>
2019-11-11 17:11:45 -0600

    [basic_kbdmlt47] Maltese 47-Key Basic Win10 import

---
+++ release/basic/basic_kbdmlt47/source/help/basic_kbdmlt47.php
@@ -0,0 +1,21 @@
+<?php 
+  $pagename = 'Maltese 47-Key Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    Maltese 47-Key Basic 1.1 generated from template.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
