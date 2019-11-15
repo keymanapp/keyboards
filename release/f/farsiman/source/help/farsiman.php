bc0ab6fff75c82510289f262806f55bb8b073ccf
Makara <makara@keyman.com>
2019-11-15 08:33:12 +0700

    change branch name

---
+++ release/f/farsiman/source/help/farsiman.php
@@ -0,0 +1,22 @@
+<?php 
+  $pagename = 'Farsiman Keyboard Help';
+  $pagetitle = 'Farsiman Keyboard Help';
+  require_once('header.php');
+?>
+
+<p>Farsiman is a Farsi phonetic keyboard that enables anyone who uses a reqular English keybaord write in Farsi. Look at the three screenshots in the welcome.html and you are immediately ready to write away in Farsi.</p>
+
+<div id='osk-container'>
+  <h2>Desktop Keyboard Layout</h2>
+  <div id='osk' data-states='default shift rightalt ctrlalt'></div>
+</div>
+
+<div id='osk-phone-container'>
+  <h2>Phone Keyboard Layout</h2>
+  <p>Note: Longpress keys are not currently visible in this static documentation, and so some additional letters may be accessible through these longpress keys.</p>
+  <div id='osk-tablet' data-states='default shift numeric'></div>
+</div>
+
+
+<h2>Release History</h2>
+<p>Version 1.0, released 01 November 2019</p>
