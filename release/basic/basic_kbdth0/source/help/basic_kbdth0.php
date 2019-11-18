9ebc5caeb996596ccd5ef79860c64848237c2681
Lorna Evans <LornaSIL@users.noreply.github.com>
2019-11-18 10:45:00 -0600

    [basic_kbdth0] Thai Kedmanee Basic Win10 import

---
+++ release/basic/basic_kbdth0/source/help/basic_kbdth0.php
@@ -0,0 +1,19 @@
+<?php 
+  $pagename = 'Thai Kedmanee Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    Thai Kedmanee Basic 1.1 generated from template.
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift leftctrl'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
