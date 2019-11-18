7ba0ee093b639825e29d892e68c0a3a243959f5f
Lorna Evans <LornaSIL@users.noreply.github.com>
2019-11-18 14:56:42 -0600

    [bangla_joy] Bangla Joy keyboard legacy to release

---
+++ release/b/bangla_joy/source/help/bangla_joy.php
@@ -0,0 +1,17 @@
+<?php 
+  $pagename = 'Bangla Joy Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+  <p>
+  This keyboard is designed for use with the Bengali language of Bangladesh, India, and other regions in South Asia. It uses standard Windows fonts. This is the most popular layout in Bangladesh.
+  </p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
