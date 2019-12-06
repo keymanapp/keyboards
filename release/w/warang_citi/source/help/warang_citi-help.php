eed3eed0b63affd5aad4c3f70d27bbf59978cc67
Mangu Purty <mpurty1@gmail.com>
2019-12-07 02:44:56 +0530

    Update warang_citi

---
+++ release/w/warang_citi/source/help/warang_citi-help.php
@@ -0,0 +1,36 @@
+<?php 
+  $pagename = 'Warang Citi Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>Start typing Warang Citi phonetically according to English alphabet.<br>
+	Type DH for Warang Citi letter DA: <br>
+	Type TH for Warang Citi letter OT <br>
+	Type NG for Warang Citi letter ONG <br>
+	Type NJ for Warang Citi letter BINJ <br>
+Type NN for Warang Citi letter ENN <br>
+Type RR for Warang Citi letter HORR <br>
+Type V for Warang Citi letter ANG <br>
+Type X for Warang Citi letter SSU <br>
+Type @ for Warang Citi letter OWANG <br>
+<p>
+    Warang Citi Keyboard 1.0
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
+
+
+<p>© 2019 Mangu Purty</p>
+
