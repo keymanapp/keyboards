e7d7da484c7c47bd9cf7024dce9c89748560f5fc
David Rowe <david_rowe@sil.org>
2019-12-18 16:36:46 -0800

    [meitei_legacy] initial version in experimental section

---
+++ experimental/m/meitei_legacy/source/help/meitei_legacy.php
@@ -0,0 +1,22 @@
+<?php
+  $pagename = 'Meitei (Legacy) Keyboard Help';
+  $pagetitle = 'Meitei (Legacy) Keyboard Help';
+  require_once('header.php');
+?>
+<p>
+This is a keyboard for entering Unicode data in the Meitei script.
+It follows the keystrokes of the legacy font Ratha99, but produces Unicode data.
+</p>
+
+<h2>Keyboard Layout</h2>
+
+<div id='osk-container'>
+  <h3>Desktop Keyboard Layout</h3>
+  <div id='osk' data-states='default shift'></div>
+</div>
+
+<h2 Usage Notes</h2>
+<p>
+Type "/" before a key to get the original English character.
+
+<p>© 2019 SIL International</p>
