b6f1036f6878902baf7e4a317b6c41abba4a9af5
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-01-17 16:28:22 -0600

    [basic_kbdgae] Gaelic Basic Win10 import

---
+++ release/basic/basic_kbdgae/source/help/basic_kbdgae.php
@@ -0,0 +1,31 @@
+<?php 
+  $pagename = 'Gaelic Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    Gaelic Basic 1.1 generated from template.
+</p>
+
+<p>This keyboard makes use of the SHIFT+RALT+O keys which are set by default when Keyman is installed. 
+	To use this keyboard, you must go to <b>Configuration.../Hotkeys</b> and select the hotkey for "Switch Keyman Desktop Off". 
+	Either change the hotkey or turn it off.</p>
+
+
+<p>This keyboard makes use of "deadkeys" which are listed below:</p>
+
+<p>`a will produce à (available on aAeEiIoOuU).</p>
+<p>´a will produce á (available on aAeEiIoOuUyY).</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
