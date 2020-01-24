b275640ada60afec94c6cbe83c2534813bff2ff4
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-01-24 16:16:25 -0600

    [basic_kbdgr1] fix documentation

--- release/basic/basic_kbdgr1/source/help/basic_kbdgr1.php
+++ release/basic/basic_kbdgr1/source/help/basic_kbdgr1.php
@@ -11,14 +11,14 @@
 <p>
     This keyboard layout is designed for German.  It includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. The keyboard layout follows the Windows 10 German (IBM) layout.
 </p>
-<p>This keyboard makes use of the SHIFT+RALT+M keys which are set by default when Keyman is installed. 
+<p>This keyboard makes use of the SHIFT+RALT+M key which is set by default when Keyman is installed. 
 	To use this keyboard, you must go to <b>Configuration.../Hotkeys</b> and select the hotkey for "Open Keyboard Menu". 
 	Either change the hotkey or turn it off.</p>
 
 <p>This keyboard makes use of "deadkeys" which are listed below:</p>
 
 <p>^a will produce â (available on aAeEiIoOuU).</p>
-<p>´a will produce á (available on aAcEiIoOuUyY).</p>
+<p>´a will produce á (available on aAeEiIoOuUyY).</p>
 <p>`a will produce à (available on aAeEiIoOuU).</p>
 
 <h1>Keyboard Layout</h1>
