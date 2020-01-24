ef7050ae1d4ba67572266af0c0f33710c75d66a9
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-01-24 13:29:02 -0600

    [basic_kbdgr1] German (IBM) Basic Win10 import

---
+++ release/basic/basic_kbdgr1/source/help/basic_kbdgr1.php
@@ -0,0 +1,29 @@
+<?php 
+  $pagename = 'German (IBM) Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<h1>Start Using German (IBM) Basic</h1>
+
+<p>
+    This keyboard layout is designed for German.  It includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. The keyboard layout follows the Windows 10 German (IBM) layout.
+</p>
+<p>This keyboard makes use of "deadkeys" which are listed below:</p>
+
+<p>^a will produce â (available on aAeEiIoOuU).</p>
+<p>´a will produce á (available on aAcEiIoOuUyY).</p>
+<p>`a will produce à (available on aAeEiIoOuU).</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
