008d19fb3a7b702c9cff8d4af24c2f0ed7200408
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-01-29 16:50:35 -0600

    [basic_kbdtuf] Changes to documentation re RALT SHIFT M key

--- release/basic/basic_kbdtuf/source/help/basic_kbdtuf.php
+++ release/basic/basic_kbdtuf/source/help/basic_kbdtuf.php
@@ -9,9 +9,9 @@
 <p>
     This keyboard layout is designed for Turkish.  It includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. The keyboard layout follows the Windows 10 Turkish F layout.
 </p>
-<p>This keyboard makes use of the SHIFT+RALT+O keys which are set by default when Keyman is installed. 
+<p>This keyboard makes use of the SHIFT+RALT+O and Shift+Alt+M keys which are set by default when Keyman is installed. 
 	To use this keyboard, you must go to <b>Configuration.../Hotkeys</b> and select the hotkey for "Switch Keyman Desktop Off". 
-	Either change the hotkey or turn it off. </p>
+	Either change the hotkey or turn it off. Do the same for "Open Keyboard Menu".</p>
 <p>This keyboard makes use of deadkeys. Typing the deadkey before the base character will output the combined character. For example:</p>
 <p>^a will produce ä (available on aAeEıIoOuUiİ).</p>
 <p>¨a will produce ä (available on aAeEıIoOuUyiİ).</p>
