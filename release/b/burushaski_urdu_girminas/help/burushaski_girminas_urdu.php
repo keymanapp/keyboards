cdbe66bee602881d89366807f7277f0f2d239400
quwatsunny <48961603+quwatsunny@users.noreply.github.com>
2020-02-07 01:10:28 -0500

    Add files via upload

---
+++ release/b/burushaski_urdu_girminas/help/burushaski_girminas_urdu.php
@@ -0,0 +1,153 @@
+<?php 
+  $pagename = 'Burushaski Urdu Girminas Help';
+  $pagetitle = 'Burushaski Urdu Girminas Help';
+  require_once('header.php');
+?>
+
+<html xmlns:keys="http://www.tavultesoft.com/xml/keys" xmlns:oskexportdetails="http://www.tavultesoft.com/xml/oskexportdetails">
+<head>
+<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Burushaski Girminas Urdu</title>
+<style type="text/css">
+p { font: 10pt Tahoma }
+h1 {
+	font: bold 16pt Tahoma;
+	color: #4444cc;
+	font-size: 24pt;
+}
+h2 { font: bold 12pt Tahoma; color: #4444cc }
+table.display tr .gap { width: 16px; border: none; }
+table.display tr td { font: 10pt Tahoma; border: solid 1px #ccccff; padding: 4px }
+table.display tr th { font: bold 10pt Tahoma; border: solid 1px #ccccff; padding: 4px; text-align: left }
+table.display { border-collapse: collapse; }
+        .key { 
+            float: left;
+            display: block; 
+            position: relative;
+            overflow: hidden;
+            height: 35px;
+            margin: 2px 0px 0px 4px;
+            
+        }
+        
+        #K_SPACE {
+          width: 234px; 
+          
+            background-image: url('key-space.gif'); 
+          
+        }
+
+        #K_BKSLASH {
+          width: 42px; 
+          
+            background-image: url('key-bkslash.gif'); 
+          
+        }
+        
+        #K_oE2 {
+          
+            display:none;
+          
+        }
+        
+        #K_SHIFTL {
+          width: 80px;background-image: url('key-shiftl.gif');
+        }
+        
+        .plain {
+          
+            background-image: url('key-plain.gif');
+          
+          background-repeat: no-repeat;
+          width: 34px;
+        }
+        
+        .special {
+          background-repeat: no-repeat;
+          
+        }
+        
+        .keycap {
+          font: bold 7pt Arial; 
+          position: absolute; 
+          left: 6px; 
+          top: 6px;
+        }
+        
+        .key .keycap {          
+          display: block;
+        }
+        
+        .special .keycap {
+          display: block;
+        }
+        
+        .keytext {
+          font: 12pt "Pak Nastaleeq"; 
+          position: absolute; 
+          display: block;
+          right: 5px;
+          bottom: 4px;
+          color: blue;
+        }
+a {
+	font-family: Urdu Typesetting;
+}
+h3 {
+	font-size: 24px;
+}
+.style1 {color: #FF0000}
+h1,h2,h3,h4,h5,h6 {
+	font-family: Urdu Typesetting;
+}
+body {
+	margin-left: 10%;
+	margin-top: 0px;
+	margin-right: 10%;
+}
+.style2 {color: #FF6600; }
+.style3 {font-family: Calibri}
+.style4 {
+	font-family: "Urdu Typesetting";
+	font-size: 14pt;
+}
+.style5 {color: #0000FF}
+.style6 {color: #333333}
+.style7 {color: #000000}
+.style8 {color: #0000CC}
+.style9 {font-family: "Urdu Typesetting"}
+</style>
+</head>
+<body>
+<h1 align="center" class="style2" style='margin-bottom: 2px'>بُری شاسکی کی بورڈ </h1>
+<p align="center" style='margin-bottom: 2px'><i></i></p>
+<div align="center">
+  <p align="right">By Quwat K. Sunny     </p>
+  <p align="right">Burushaski Girminas is also available in Latin/Roman script <a href="https://keyman.com/keyboards/burushaski_girminas">click here</a></p>
+</div>
+<h3 align="right">یہ اردو رسم الخط میں بُری شاسکی لکھنے کا  کی بورڈ ہے۔  اگر آپ نے پہلے  فونیٹک اردو کی بورڈ استعمال کیا ہے تو آپ آسانی سے بری شاسکی میں بھی لکھ سکتےہیں  ۔ اضافی بروشاسکی صوتی حروف جو  مرحوم علامہ نصیر الدین ہنزائی کی  کتابوں میں یا اُن کی ڈکشنیری میں نظر آتے ہیں اُن کو فونیٹک کی بورڈ میں شامل کیا گیا ہے۔  آسانی کے لیے تمام بُری شاسکی اضافی صوتی حروف  شفٹ اور آلٹ  بٹن  دبا کر مندرجہ ذیل گائید کو دیکھ کر لکھ سکتےہیں۔  اچھی خوش خطی کے لیے ہم جمیل نستلیق فونٹ کا مشورہ دیتے ہیں۔   </h3>
+<p align="right" class="style4">موبائل پر یا ٹیبلٹ پر لکھنے کے لیے آپ کو شفٹ کے بجائے بٹن کو دبا کر ہولڈ کرنا پڑے گا تاکہ آپ کو اضافی تہجی نظر ائے </p>
+<h3 align="right"> Keyboard layout</h3>
+<h3 align="right">Default (unshifted) </h3>
+	<p align="right"><a href="LayoutU_.png"><img class="keyboard" src="LayoutU_.png" alt="Default (unshifted) state" /></a></p>
+	<h3 align="right">Shift</h3>
+	<p align="right"><a href="LayoutU_S.png"><img class="keyboard" src="LayoutU_S.png" alt="Shift state" /></a></p>
+	<h3 align="right">CTRL + Shift (<span class="style1">اضافی بُری شاسکی صوتی الفاظ کی تہجی</span>) </h3>
+	<h1 align="right" class="style3">ڏ ݽ څ ݣ ݼ ژ  ݳ ݴ ݸ ݹ ݵ ݶ ݶݺ ݻ ݻ</h1>
+	<p align="right"><a href="LayoutU_SA.png"><img src="LayoutU_SA.png" alt="ALT Shift state" class="keyboard" /></a></p>
+	<p align="right">&nbsp;</p>
+	<h3 align="right">Mobile/ Tablet </h3>
+	<p align="right"><a href="LayoutU_A.png"></a><span class="style5">ا</span> کو دبا کر ہولڈ کریں تو آپ کو <span class="style1"> ݳ ݴ</span> نظر آجائے گا۔ </p>
+	<h3 align="right"><span class="style5">ش</span> کو دبا کر ہولڈ کریں تو آپ کو <span class="style1">څ</span> نظر آجائے گا۔ </h3>
+	<h3 align="right"><span class="style5">چ</span> کو دبا کر ہولڈ کریں تو آپ کو <span class="style1">ݼ</span> نظر آجائے گا۔ </h3>
+	<h3 align="right"><span class="style5">ے</span> کو دبا کر ہولڈ کریں تو آپ کو <span class="style1">ݻ ݻ</span> نظر آجائے گا۔ </h3>
+	<h3 align="right"><span class="style8">ی</span><span class="style7"> کو دبا کر ہولڈ کریں تو آپ کو</span> <span class="style1">ݶ ݶ </span> <span class="style6">نظر آجائے گا۔ </span></h3>
+	<h3 align="right"><span class="style8">و </span><span class="style7"> کو دبا کر ہولڈ کریں تو آپ کو</span> <span class="style1"> ݸ ݹ </span> <span class="style6"> نظر آجائے گا۔ </span></h3>
+	<h3 align="right" class="style9"><span class="style8">س </span><span class="style7"> کو دبا کر ہولڈ کریں تو آپ کو</span> <span class="style1"> ݽ  </span> <span class="style6"> نظر آجائے گا۔ </span></h3>
+	<h3 align="right" class="style9"><span class="style8">ن </span><span class="style7"> کو دبا کر ہولڈ کریں تو آپ کو</span> <span class="style1">ݣ</span> <span class="style6"> نظر آجائے گا۔ </span></h3>
+	<h3 align="right" class="style9"><span class="style8">ج </span><sڈpan class="style7"> کو دبا کر ہولڈ کریں تو آپ کو</span> <span class="style1">ڞ</span> <span class="style6"> نظر آجائے گا۔ </span></h3>
+	<h3 align="right" class="style9"><span class="style8">د </span><span class="style7"> کو دبا کر ہولڈ کریں تو آپ کو</span> <span class="style1"> ڏ</span> <span class="style6"> نظر آجائے گا۔ </span></h3>
+	<p align="right" class="style9">&nbsp;</p>
+	<hr>
+</body>
+</html>
