48b0c324e06a5f8dedc1d730e126033f2f2e6854
Darcy Wong <darcy_wong@sil.org>
2020-02-08 21:24:41 +0700

    Add documentation

---
+++ release/s/saraiki/source/help/saraiki.php
@@ -0,0 +1,22 @@
+<?php
+  $pagename = 'Saraiki Keyboard';
+  $pagetitle = 'Saraiki Keyboard';
+  require_once('header.php');
+?>
+
+<h1>Saraiki Keyboard Help</h1>
+
+<p>
+    Desktop keyboard for typing Saraiki.
+</p>
+
+<h1>Desktop Keyboard Layout</h1>
+  <h2>Default (unshifted)</h2>
+  <p><img src="sairiki_unshifted.png" alt="Sairiki Keyboard: default (unshifted)"></p>
+  <h2>Shift</h2>
+  <p><img src="sairiki_shifted.png" alt="Sairiki Keyboard: shift"></p>
+  <h2>CTRL ALT (unshifted)</h2>
+  <p><img src="sairiki_ctrl_alt.png" alt="Sairiki Keyboard: ctrl-alt"></p>
+
+
+<p>&copy; 2020 Parvez Qadir </p>
