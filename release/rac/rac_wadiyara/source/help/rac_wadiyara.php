4cd57a7779354fca904a43f11590467c5fab9fe2
Makara <makara@keyman.com>
2020-02-21 10:26:15 +0700

    update touch layout

--- release/rac/rac_wadiyara/source/help/rac_wadiyara.php
+++ release/rac/rac_wadiyara/source/help/rac_wadiyara.php
@@ -647,19 +647,14 @@ the Keyman icon and selecting the On Screen Keyboard menu item. </p>
 
 <p>The keyboard layout is displayed below in Keyboard graphics.</p>
 
-<h3><span style='mso-fareast-font-family:"Times New Roman"'>Base Version<o:p></o:p></span></h3>
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
 
-<p class=MsoNormal><span style='mso-fareast-font-family:"Times New Roman";
-mso-no-proof:yes'><img width=908 height=307 id="_x0000_i1026"
-src="rac_wadiyara%20base.png" alt="Base Version"></span><span style='mso-fareast-font-family:
-"Times New Roman"'><o:p></o:p></span></p>
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
 
-<h3><span style='mso-fareast-font-family:"Times New Roman"'>Shift Version<o:p></o:p></span></h3>
-
-<p class=MsoNormal><span style='mso-fareast-font-family:"Times New Roman";
-mso-no-proof:yes'><img width=909 height=308 id="_x0000_i1025"
-src="rac_wadiyara%20shift.png" alt="Shift Version"></span><span style='mso-fareast-font-family:
-"Times New Roman"'><o:p></o:p></span></p>
 
 <h2><span style='mso-fareast-font-family:"Times New Roman"'>How to use the
 Rachitrali-Wadiyara Keyboard? Please read the following:-<u1:p></u1:p><o:p></o:p></span></h2>
