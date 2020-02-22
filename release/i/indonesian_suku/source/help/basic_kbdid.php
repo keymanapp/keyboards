98e32d691436945c23270ecde0e403284372c6fa
Benny Lin <bknliem@yahoo.com>
2020-02-23 03:27:56 +0700

    Initial version 1.0
    Layout for Indonesian keyboard, for Indonesian language and Indonesian local languages.
    
    Based on, and an improvement of US Basic layout.

---
+++ release/i/indonesian_suku/source/help/basic_kbdid.php
@@ -0,0 +1,23 @@
+<?php 
+  $pagename = 'Indonesian Basic Keyboard Help';
+  $pagetitle = 'Indonesian Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the Indonesia language and Indonesian languages (700+ bahasa suku). It will also be useful for other languages that do not require special characters.
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+
