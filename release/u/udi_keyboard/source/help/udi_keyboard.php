ddcb4fdf3abff8e2bd78228c486b35566d8c6a52
smartpurplecloud <muasya.mark@gmail.com>
2020-02-25 14:14:39 +0400

    Initial version of Udi Keyboard
     This keyboard layout is designed for Udi language of Azerbaijan. It includes an on-screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On-Screen Keyboard menu item.
    
    Эта раскладка клавиатуры предназначена для языка уди Азербайджана. Он включает в себя экранную клавиатуру, которую можно просмотреть, щелкнув значок Keyman и выбрав пункт меню.

---
+++ release/u/udi_keyboard/source/help/udi_keyboard.php
@@ -0,0 +1,42 @@
+<?php 
+  $pagename = 'Udi Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+<p>
+This keyboard was developed for use by BTL (Bible Translation & Literacy) for language teams in Kenya.
+</p>
+
+<p>This keyboard makes use of modifier keys. Typing the modifier key before the base character will output the new or combined character. For example:</p>
+	<p>[c (Open Bracket and c) will produce c (available on cC eE ?? gG iI jJ oO sS uU).</p>
+	<p>]o (Close Bracket and o) will produce ö ( available on oO uU).</p>
+	<p>]s (Close Bracket and s) will produce s .</p>
+	<p>`c (Back quote and c) will produce ç (available on sS).</p>
+	<p>'c (Quote and c) will produce c' (available on cC kK qQ pP sS tT).</p>
+	<p>< and < will produce «.</p>
+	<p>> and > will produce ».</p>
+<h1>Keyboard Layout</h1>
+
+  <h2>Desktop Keyboard layout</h2>
+	<h3>Default (unshifted)</h3>
+	<p><a href="Layout_U.png"><img class="keyboard" src="Layout_U.png" alt="Default (unshifted) state" /></a></p>
+	<h3>Shift</h3>
+	<p><a href="Layout_S.png"><img class="keyboard" src="Layout_S.png" alt="Shift state" /></a></p>
+
+
+<p></p>
+<p></p>
+<p></p>
+
+  <h2>Mobile Keyboard layout</h2>
+  <p> Due to the size and number of keys, some characters are hidden in the long press. Press and hold on the key with a little dot on the top right to reveal them.</p>
+	<h3>Default (unshifted)</h3>
+	<p><a href="Mobile_D.png"><img class="keyboard" src="Mobile_D.png" alt="Mobile Default (unshifted) state" /></a></p>
+	<h3>Shift</h3>
+	<p><a href="Mobile_S.png"><img class="keyboard" src="Mobile_S.png" alt="Mobile Shift state" /></a></p>
+	<h3>Numeric</h3>
+	<p><a href="Mobile_N.png"><img class="keyboard" src="Mobile_N.png" alt="Mobile Numeric state" /></a></p>
+
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
