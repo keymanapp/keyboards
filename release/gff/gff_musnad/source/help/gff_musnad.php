2ed8cbc5e5950d8c52aca901f9e98a267d545584
Daniel Yacob <yacob@geez.org>
2020-03-03 08:04:04 -0500

    Updates for help file and reorganize welcome pages.

--- release/gff/gff_musnad/source/help/gff_musnad.php
+++ release/gff/gff_musnad/source/help/gff_musnad.php
@@ -1,24 +1,29 @@
 <?php
-$pagename = 'Hazargi Keyboard Help';
+$pagename = '𐩣𐩯𐩬𐩵 (Musnad) Keyboard Help';
 $pagetitle = $pagename;
 // Header
 require_once('header.php');
 ?>
 
 <p>
-Hazargi keyboard based on Persian.
+The GFF Musnad keyboard supports the entry of Musnad Script which is also commonly referred to as “Old South Arabian” and “Sabaean” script by different communities.
+The keyboard requires a font with the Musnad letters that is compatible with Unicode 5.2 encoding of Musnad.
 </p>
 
 <h1>Keyboard Layout</h1>
 
 <h2>Desktop Keyboard Layout</h2>
-<div id='osk' data-states='default shift shift-ctrl'>
+<div id='osk' data-states='default shift'>
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
+
 <p>Due to the size and number of keys, some characters are hidden in the long press.
 Press and hold on the key with a little dot on the top right to reveal them.</p>
 
-<div id='osk-phone' data-states='default shift numeric'>
+<div id='osk-tablet' data-states='default'>
+
+
+<p>© 2020 Ge'ez Frontier Foundation</p>
 
 </div>
