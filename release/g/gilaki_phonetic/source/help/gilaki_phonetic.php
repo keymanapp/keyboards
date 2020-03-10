e7540dc755c34a0cb04565003b044563c0fca48a
rmlockwood <rlbooks@web.de>
2020-03-10 17:09:29 +0100

    Initial version of Gilaki keyboards

---
+++ release/g/gilaki_phonetic/source/help/gilaki_phonetic.php
@@ -0,0 +1,23 @@
+<?php
+$pagename = 'Gilaki Phonetic Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+Gilaki Phonetic keyboard based on Persian.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
