3f7c9cfe3c373249291cdca4a9c3e10d7375854d
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-03-12 12:42:03 -0500

    [basic_kbdropr] Romanian (Programmers) Basic Win10 import

---
+++ release/basic/basic_kbdropr/source/help/basic_kbdropr.php
@@ -0,0 +1,37 @@
+<?php 
+  $pagename = 'Romanian (Programmers) Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+This keyboard layout is designed for Romanian.  It includes an on screen keyboard which can be 
+viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. The keyboard 
+layout follows the Windows 10 Romanian (Programmers) layout.
+</p>
+
+<p>This keyboard makes use of "deadkeys" which are listed below:</p>
+<p>˝o will produce ő (available on oOuU).</p>
+<p> ~ space will produce tilde.</p>
+<p>ˇc will produce č (available on cCdDeElLnNrRsStTzZ).</p>
+<p>^a will produce â (available on aAeEiIoOuU).</p>
+<p>˘a will produce ă (available on aA).</p>
+<p>°u will produce ů (available on uU).</p>
+<p>˛a will produce ą (available on aAeE).</p>
+<p>`a will produce à (available on aAeEiIoOuU).</p>
+<p>·z will produce ż (available on zZ).</p>
+<p>´a will produce á (available on aAcCeEiIlLnNoOrRsSuUyYzZ).</p>
+<p>¸c will produce ç (available on cCsStT).</p>
+<p>¨a will produce ä (available on aAeEiIoOuUyY).</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
