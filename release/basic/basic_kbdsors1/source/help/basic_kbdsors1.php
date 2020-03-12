7adf923b20e325fa427bc7249bf464226e7fc762
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-03-12 12:48:32 -0500

    [basic_kbdsors1] Sorbian Standard Basic Win10 import

---
+++ release/basic/basic_kbdsors1/source/help/basic_kbdsors1.php
@@ -0,0 +1,23 @@
+<?php 
+  $pagename = 'Sorbian Standard Basic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+This keyboard layout is designed for Sorbian.  It includes an on screen keyboard which can be 
+viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. The keyboard 
+layout follows the Windows 10 Sorbian Standard layout.
+</p>
+
+<h1>Keyboard Layout</h1>
+<p>This keyboard makes use of "deadkeys" which are listed below:</p>
+
+<p>^c will produce č (available on cCeErRsSzZ).</p>
+<p>´a will produce ć (available on cClLnNoOrRsSzZ to become ćĆłŁńŃóÓŕŔśŚźŹ).</p>
+<p>`a will produce á (available on aAeEiIoOuU).</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
