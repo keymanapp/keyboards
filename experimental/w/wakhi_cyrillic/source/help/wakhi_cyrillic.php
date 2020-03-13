7cc22c0efe4a09a33280af5cb970cc9e6e759eb4
DavidLRowe <DavidLRowe@users.noreply.github.com>
2020-03-12 17:01:12 -0700

    [wakhi_cyrillic] submission on behalf of author

---
+++ experimental/w/wakhi_cyrillic/source/help/wakhi_cyrillic.php
@@ -0,0 +1,23 @@
+<?php
+$pagename = 'Wakhi Cyrillic Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+Wakhi Cyrillic keyboard.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift ctrl'>
+</div>
+
+<h2>Mobile Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
