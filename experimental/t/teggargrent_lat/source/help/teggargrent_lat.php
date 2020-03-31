5303d6ff3d2913140b02a70527b8f7743e85773e
Juanan O <61866048+Juanan-Ochoa@users.noreply.github.com>
2020-03-31 03:02:37 +0100

    [teggargrent_lat] initial submission
    I have close the previous Pull Request 1098: o\oua\teg2 --> and make a new one keyboard (imposible to change the name for me)
    This keyboard is for the Teggargrent (oua) and Tumzabt (mzb) languages.
    I pretend create base in this one the other two scripts: Arabic and Tifinagh.

---
+++ experimental/t/teggargrent_lat/source/help/teggargrent_lat.php
@@ -0,0 +1,25 @@
+<?php
+$pagename = 'Teggargrent Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+Teggargrent keyboard based on French Azerty keyboard.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift shift-ctrl'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
