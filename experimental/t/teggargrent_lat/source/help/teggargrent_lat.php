d16ae36c0c57b3627d2596908ba4fc90f295b853
Juanan O <61866048+Juanan-Ochoa@users.noreply.github.com>
2020-04-01 00:09:14 +0100

    Deleting 'Tablet layer' in On Screen keyboard
    In the the "symbols" layer I select the *123* button and change the "Next Layer" to "numbers".

--- experimental/t/teggargrent_lat/source/help/teggargrent_lat.php
+++ experimental/t/teggargrent_lat/source/help/teggargrent_lat.php
@@ -12,14 +12,12 @@ Teggargrent keyboard based on French Azerty keyboard.
 <h1>Keyboard Layout</h1>
 
 <h2>Desktop Keyboard Layout</h2>
-<div id='osk' data-states='default shift shift-ctrl'>
+<div id='osk' data-states='default shift alt shift-alt'>
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
 <p>Due to the size and number of keys, some characters are hidden in the long press.
 Press and hold on the key with a little dot on the top right to reveal them.</p>
 
-<div id='osk-phone' data-states='default shift numeric'>
-</div>
-<div id='osk-tablet' data-states='default shift numeric'>
+<div id='osk-phone' data-states='default shift numbers symbols'>
 </div>
