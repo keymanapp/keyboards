e5ddd30f56a7e2f2263977079bf2c19c492b7bbe
Eddie Antonio Santos <easantos@ualberta.ca>
2020-03-31 10:39:00 -0600

    Use stores instead for KMN rules.

--- release/fv/fv_plains_cree/extra/generate-kmn-rules.py
+++ release/fv/fv_plains_cree/extra/generate-kmn-rules.py
@@ -3,7 +3,7 @@
 
 """
 Generates KMN rules for all syllabics sequences with the
-[T_LONG_DOT] and [T_W_DOT] keys.
+[T_LONG_DOTn] and [T_W_DOTn] keys.
 
 This script requires Python 3.6+.
 """
@@ -11,8 +11,10 @@ This script requires Python 3.6+.
 import csv
 from distutils.util import strtobool
 from pprint import pprint
-from typing import Dict, NamedTuple, Optional
+from typing import Dict, List, NamedTuple, NewType, Optional
 from unicodedata import normalize
+
+Syllabic = NewType("Syllabic", str)
 
 
 class Index(NamedTuple):
@@ -30,21 +32,6 @@ class Index(NamedTuple):
         return not self.is_long
 
 
-class Syllabic(NamedTuple):
-    char: str
-
-    @property
-    def codepoint(self):
-        return f"{ord(self.char):04X}"
-
-    @property
-    def uplus(self):
-        return f"U+{self.codepoint}"
-
-    def __str__(self) -> str:
-        return self.char
-
-
 def to_bool(val: str) -> bool:
     return bool(strtobool(val or "False"))
 
@@ -56,6 +43,7 @@ def without_diacritic(s: str) -> str:
 table: Dict[Index, Syllabic] = {}
 
 
+# Build the table from syllabics.tsv.
 with open("./syllabics.tsv", "r", encoding="UTF-8") as f:
     reader = csv.DictReader(f, delimiter="\t")
     for entry in reader:
@@ -75,21 +63,51 @@ with open("./syllabics.tsv", "r", encoding="UTF-8") as f:
 
         table[Index(consonant, vowel, has_w, is_long)] = Syllabic(entry["cans"])
 
+# Generate parallel stores so that we can use the index() function:
+without_w_store: List[Syllabic] = []
+with_w_store: List[Syllabic] = []
+without_long_store: List[Syllabic] = []
+with_long_store: List[Syllabic] = []
+
+# Generate 'w' stores
 for index, syllabic in table.items():
-    consonant, vowel, has_w, is_long = index
-    if index.lacking_long_diacritic:
-        try:
-            with_long_dot = table[Index(consonant, vowel, has_w, True)]
-        except KeyError:
-            pass
-        else:
-            print(f"  {syllabic.uplus} + [T_LONG_DOT] > {with_long_dot.uplus}", end="")
-            print(f"  c {syllabic} -> {with_long_dot}")
-    if index.lacking_w_diacritic:
-        try:
-            with_w_dot = table[Index(consonant, vowel, True, is_long)]
-        except KeyError:
-            pass
-        else:
-            print(f"  {syllabic.uplus} + [T_W_DOT] > {with_w_dot.uplus}", end="")
-            print(f"     c {syllabic.char} -> {with_w_dot.char}")
+    if index.has_w:
+        continue
+    consonant, vowel, _has_w, is_long = index
+    try:
+        with_w_syllabic = table[Index(consonant, vowel, True, is_long)]
+    except KeyError:
+        # Does not have a w-dot equivilent.
+        continue
+
+    without_w_store.append(syllabic)
+    with_w_store.append(with_w_syllabic)
+
+# Generate long dot stores
+for index, syllabic in table.items():
+    if index.is_long:
+        continue
+    consonant, vowel, has_w, _is_long = index
+    try:
+        with_long_syllabic = table[Index(consonant, vowel, has_w, True)]
+    except KeyError:
+        # Does not have a w-dot equivilent.
+        continue
+
+    without_long_store.append(syllabic)
+    with_long_store.append(with_long_syllabic)
+
+assert len(without_w_store) == len(without_w_store)
+assert len(without_long_store) == len(without_long_store)
+print(f"store(withoutW) '{''.join(without_w_store)}'")
+print(f"store(withW) '{''.join(with_w_store)}'")
+print(f"store(withoutLong) '{''.join(without_long_store)}'")
+print(f"store(withLong) '{''.join(with_long_store)}'")
+
+print()
+
+for key in ("[T_W_DOT1]", "[T_W_DOT2]"):
+    print(f"  any(withoutW) + {key} > index(withW, 1)")
+
+for key in ("[T_LONG_DOT1]", "[T_LONG_DOT2]"):
+    print(f"  any(withoutLong) + {key} > index(withLong, 1)")
