9e5615085b0826e2e27946150a4b9c72377445db
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-04-14 14:41:22 -0500

    [basic_kbdno1] changes per PR feedback

--- release/basic/basic_kbdno1/source/help/basic_kbdno1.php
+++ release/basic/basic_kbdno1/source/help/basic_kbdno1.php
@@ -14,10 +14,10 @@ layout follows the Windows 10 Norwegian with Sami layout.
 
 <p>This keyboard makes use of "deadkeys" which are listed below:</p>
 
-<p>¨a will produce ä (available on aAeEiIoOuUy).</p>
-<p>^a will produce â (available on aAeEiIoOuU).</p>
+<p>¨a will produce ä (available on aAeEiIoOuUyY).</p>
+<p>^a will produce â (available on aAeEiIoOuUyY).</p>
 <p>~a will produce ã (available on aAnNoO).</p>
-<p>`a will produce à (available on aAeEiIoOuU).</p>
+<p>`a will produce à (available on aAeEiIoOuUyY).</p>
 <p>´a will produce á (available on aAeEiIoOuUyY).</p>
 
 <h1>Keyboard Layout</h1>
