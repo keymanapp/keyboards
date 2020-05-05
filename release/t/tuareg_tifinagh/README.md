8f283624d721cbc408fe66667866c788c16766b5
Jon Coblentz <jon_coblentz@sil.org>
2020-05-05 12:14:34 -0600

    [tuareg_tifinagh] initial submission

---
+++ release/t/tuareg_tifinagh/README.md
@@ -0,0 +1,33 @@
+Tuareg Tifinagh keyboard
+==============
+
+©
+
+Version 1.0
+
+Description
+-----------
+
+Tuareg Tifinagh 1.0 is designed for typing Tifinagh characters in the Tamahaq and Tamashek languages. 
+
+It is based on the Standard Moroccan Tamazight Tifinagh AZERTY keyboard and Microsoft's Central Atlas Tamazight (Tifinagh Extended) keyboard layout spec. 
+
+Additional keys include the Tifinagh Consonant Joiner (U+2D7F) to create bi-consonant ligatures. Also included are the Right to Left Override key (U+202E) for right to left writing and POP Directional Formatting key (U+202C) to reverse the right to left writing.
+
+Links
+-----
+
+Supported Platforms
+-------------------
+ * Windows
+ * macOS
+ * Linux
+ * Web
+ * iPhone
+ * iPad
+ * Android phone
+ * Android tablet
+ * Mobile devices
+ * Desktop devices
+ * Tablet devices
+
---
+++ release/t/tuareg_tifinagh/source/help/tuareg_tifinagh.php
@@ -0,0 +1,52 @@
+<?php
+$pagename = 'Tuareg Tifinagh Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p> Tuareg Tifinagh 1.0 is designed for typing Tifinagh characters in the Tamahaq and Tamashek languages. </p>
+<p>
+	It is based on the Standard Moroccan Tamazight Tifinagh AZERTY keyboard and 
+	Microsoft's Central Atlas Tamazight (Tifinagh Extended) keyboard layout spec.
+	Additional keys include the Tifinagh Consonant Joiner (U+2D7F) to create bi-consonant ligatures. 
+	Also included are the Right to Left Override key (U+202E) for right to left writing and 
+	POP Directional Formatting key (U+202C) to reverse the right to left writing.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<!-- Insert Keyboard Layout Images or HTML here -->
+<p><b>Unshifted</b></p>
+<img src='tuareg_tifinaghU_.png' alt='Unshifted Keyboard Layout'>
+<p><b>Shifted</b></p>
+<img src='tuareg_tifinaghU_S.png' alt='Shifted Keyboard Layout'>
+<p><b>RightAlt (AltGr)</b></p>
+<img src='tuareg_tifinaghU_RA.png' alt='RightAlt Keyboard Layout'>
+
+<p>
+   <b>Some tips for obscure keys</b><br>
+   Use RightAlt+7 for TIFINAGH CONSONANT JOINER<br>
+   Use RightAlt+d for RIGHT to LEFT OVERRIDE (rappelez-vous <b>d</b>roit à gauche)<br>
+   Use RightAlt+f for POP DIRECTIONAL FORMATTING<br>
+   Use RightAlt+g for LEFT to RIGHT OVERRIDE (rappelez-vous <b>g</b>auche à droit)
+</p>
+
+<p>The <b>Akatab</b> font is recommended for this keyboard.
+<b><a href="https://software.sil.org/akatab/" title="Download the latest version of the Akatab font" target="_blank" rel="noopener noreferrer">Download the latest version of the Akatab font</a></b>.
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
+
+<p>© 2020 SIL International</p>
