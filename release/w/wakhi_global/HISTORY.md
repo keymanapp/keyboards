98528a0d32f69a459498009f0ba9d71e13791506
WX-JO <66054189+WX-JO@users.noreply.github.com>
2020-05-28 12:48:17 +0200

    [wakhi_global] initial submission

---
+++ release/w/wakhi_global/HISTORY.md
@@ -0,0 +1,6 @@
+Wakhi Global Change History
+====================
+
+1.0 (2020-05-28)
+----------------
+* Created by JO
---
+++ release/w/wakhi_global/source/help/wakhi_global.php
@@ -0,0 +1,30 @@
+<?php
+$pagename = 'Wakhi Global Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+Wakhi Global keyboard based on English.
+</p>
+<p>
+For the letters combined with the tilde (e.g. «s̃»), first type the letter, then type the tilde symbol on the top left corner.
+
+<p>
+If you have problem using the Wakhi Global keyboard based on English please contact: alimusofer@gmail.com, or on WhatsApp: +92 344 5323009.
+</p>
+
+<p> See <a href="file:wakhi_global_help.pdf">here</a> for more information.</p>
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift ctrl'>
+</div>
+
+<h2>Mobile Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
