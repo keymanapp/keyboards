86863eedca5093012e80df09b93abb7c8d131d02
Jon Coblentz <jon_coblentz@sil.org>
2020-05-29 14:02:01 -0600

    Commit more revisions for tablet and phone layout for review

--- release/t/tuareg_tifinagh/source/help/tuareg_tifinagh.php
+++ release/t/tuareg_tifinagh/source/help/tuareg_tifinagh.php
@@ -10,8 +10,8 @@ require_once('header.php');
 	It is based on the Standard Moroccan Tamazight Tifinagh AZERTY keyboard and 
 	Microsoft's Central Atlas Tamazight (Tifinagh Extended) keyboard layout spec.
 	Additional keys include the Tifinagh Consonant Joiner (U+2D7F) to create bi-consonant ligatures. 
-	Also included are the Right to Left Override key (U+202E) for right to left writing and 
-	POP Directional Formatting key (U+202C) to reverse the right to left writing.
+	Also included are the Right-To-Left Override key (U+202E) for right-to-left writing and 
+	POP Directional Formatting key (U+202C) to reverse the right-to-left writing.
 </p>
 
 <h1>Keyboard Layout</h1>
@@ -27,9 +27,9 @@ require_once('header.php');
 <p>
    <b>Some tips for obscure keys</b><br>
    Use RightAlt+7 for TIFINAGH CONSONANT JOINER<br>
-   Use RightAlt+d for RIGHT to LEFT OVERRIDE (rappelez-vous <b>d</b>roit à gauche)<br>
+   Use RightAlt+d for RIGHT-TO-LEFT OVERRIDE (rappelez-vous <b>d</b>roit à gauche)<br>
    Use RightAlt+f for POP DIRECTIONAL FORMATTING<br>
-   Use RightAlt+g for LEFT to RIGHT OVERRIDE (rappelez-vous <b>g</b>auche à droit)
+   Use RightAlt+g for LEFT-TO-RIGHT OVERRIDE (rappelez-vous <b>g</b>auche à droit)
 </p>
 
 <p>The <b>Akatab</b> font is recommended for this keyboard.
@@ -44,9 +44,9 @@ require_once('header.php');
 <p>Due to the size and number of keys, some characters are hidden in the long press.
 Press and hold on the key with a little dot on the top right to reveal them.</p>
 
-<div id='osk-phone' data-states='default shift numeric'>
+<div id='osk-phone' data-states='default shift rightalt'>
 </div>
-<div id='osk-tablet' data-states='default shift numeric'>
+<div id='osk-tablet' data-states='default shift rightalt'>
 </div>
 
 <p>© 2020 SIL International</p>
