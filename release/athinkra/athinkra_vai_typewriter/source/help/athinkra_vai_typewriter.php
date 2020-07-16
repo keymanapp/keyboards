d813bb5df0605412af07dba4f68d37adc4db76e8
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-07-16 13:07:50 -0500

    [athinkra_vai_typewriter] New keyboard on behalf of Athinkra

---
+++ release/athinkra/athinkra_vai_typewriter/source/help/athinkra_vai_typewriter.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Vai Typewriter (Athinkra) Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>
+    A Keyman keyboard for the Vai language of Liberia, developed by the team at Athinkra, LLC. This keyboard uses fewer keystrokes than the "Vai (Athinkra)" keyboard and is more ergonomic.
+</p>
+<p>Because there are so many characters in the Vai script, this keyboard requires multiple keystrokes to access most of the characters.</p>
+
+<h1>Keyboard Layout</h1>
+<p>Vowel marks and nasilized vowels are on the corresponding vowel keys.  
+	<p>To type a consonant, you must type the consonant first, and then the vowel. For example:</p>
+<ul>ɓꔀ (be) will produce ꔇ.</ul>
+
+<p>The hyphen characters on the RIGHT ALT layer are used for accessing historic syllables. Type the character followed by a vowel and the historic syllable will be formed.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<p>(c) 2008-2020 Athinkra, LLC</p>
