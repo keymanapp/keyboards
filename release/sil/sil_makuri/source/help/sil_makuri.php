6d7b2d16cb330136f20669f9596db4e72cadaba1
Darcy Wong <darcy_wong@sil.org>
2020-08-14 07:37:18 +0700

    Update documentation
    * Add tablet layout to php help
    * Resize png's to 50%

--- release/sil/sil_makuri/source/help/sil_makuri.php
+++ release/sil/sil_makuri/source/help/sil_makuri.php
@@ -5,16 +5,27 @@ $pagetitle = $pagename;
 require_once('header.php');
 ?>
 
-<p>Makuri Keyboard with Composed ë and ü 1.0 generated from template.</p>
+<h2>Keyboard</h2>
+<p>
+This keyboard layout is designed for the Makuri language with composed ë and ü.
+It includes an on screen keyboard which can be viewed by clicking on the Keyman icon
+and selecting the On Screen Keyboard menu itme. 
+</p>
 
-<h1>Keyboard Layout</h1>
-
-<h2>Desktop Keyboard Layout</h2>
+<h3>Desktop Keyboard Layout</h3>
 <div id='osk' data-states='default shift'>
 </div>
 
-<h2>Mobile Keyboard Layout</h2>
+<h3>Mobile/Tablet Keyboard Layout </h2>
+<div id='osk-tablet' data-states='default shift Num1 Num2'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
 <div id='osk-phone' data-states='default shift Num1 Num2'>
 </div>
 
-</div>
+<h2>Links</h2>
+<ul>
+<li>Home Page: <a href='https://keyman.com/keyboards/sil_makuri'>
+  https://keyman.com/keyboards/sil_makuri</a></li>
+</ul>
