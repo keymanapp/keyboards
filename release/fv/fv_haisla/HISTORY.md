6751c8ba4fcd6ef06ed1e9bcd439f28f227777f0
madebybridget <58344928+madebybridget@users.noreply.github.com>
2020-08-17 14:47:46 -0700

    fv_haisla edits, v2
    Updated fv_haisla keyboard with correct glottalization unicode, and added rule that ʷ + w = www
    Also updated the instructions to include these changes, and the version

---
+++ release/fv/fv_haisla/HISTORY.md
@@ -0,0 +1,7 @@
+Haisla Change History
+====================
+
+1.1 (2020-08-14)
+1.0 (2020-02-27)
+----------------
+* Created by Bridget Chase, bridget@fpcc.ca
---
+++ release/fv/fv_haisla/source/help/fv_haisla.php
@@ -0,0 +1,162 @@
+<?php
+$pagename = 'Haisla Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+The Haisla keyboard has been developed by FirstVoices and updated to suit their orthographic changes.
+<p>
+<p>
+More information about the Haisla archive on FirstVoices can be found here: https://www.firstvoices.com/explore/FV/sections/Data/Haisla/Haisla/Haisla
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<p>
+The Haisla keyboard uses the English keyboard as a base, with added key combinations in order to create special characters.
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>; + [character] becomes [character with an accent]</h2>
+
+<table style="width:30%">
+
+  <tr>
+    <td>;a and ;A</td>
+    <td>becomes</td>
+    <td>á and Á</td>
+  </tr>
+  <tr>
+    <td>;c and ;C</td>
+    <td>becomes</td>
+    <td>c̓ and C̓</td>
+  </tr>
+  <tr>
+    <td>;e and ;E</td>
+    <td>becomes</td>
+    <td>é and É</td>
+  </tr>
+  <tr>
+    <td>;k and ;K</td>
+    <td>becomes</td>
+    <td>k̓ and K̓</td>
+  </tr>
+  <tr>
+    <td>;l and ;L</td>
+    <td>becomes</td>
+    <td>l̓ and L̓</td>
+  </tr>
+  <tr>
+    <td>;m and ;M</td>
+    <td>becomes</td>
+    <td>m̓ and M̓</td>
+  </tr>
+  <tr>
+    <td>;n and ;N</td>
+    <td>becomes</td>
+    <td>n̓ and N̓</td>
+  </tr>
+  <tr>
+    <td>;p and ;P</td>
+    <td>becomes</td>
+    <td>p̓ and P̓</td>
+  </tr>
+  <tr>
+    <td>;q and ;Q</td>
+    <td>becomes</td>
+    <td>q̓ and Q̓</td>
+  </tr>
+  <tr>
+    <td>;t and ;T</td>
+    <td>becomes</td>
+    <td>t̓ and T̓</td>
+  </tr>
+  <tr>
+    <td>;u and ;U</td>
+    <td>becomes</td>
+    <td>ú and Ú</td>
+  </tr>
+  <tr>
+    <td>;w and ;W</td>
+    <td>becomes</td>
+    <td>w̓ and W̓</td>
+  </tr>
+  <tr>
+    <td>;y and ;Y</td>
+    <td>becomes</td>
+    <td>y̓ and Y̓</td>
+  </tr>
+</table>
+
+<h2>= + [character] becomes [character with a bar or line]</h2>
+
+<table style="width:28%">
+  
+  <tr>
+    <td>=g and =G</td>
+    <td>becomes</td>
+    <td>ḡ and Ḡ</td>
+  </tr>
+  <tr>
+    <td>=l and =L</td>
+    <td>becomes</td>
+    <td>ɫ and Ɫ</td>
+  </tr>
+  <tr>
+    <td>=x and =X</td>
+    <td>becomes</td>
+    <td>x̄ and x̄</td>
+ 
+  </tr>
+</table>
+
+<h2>CTRL + l + [character] becomes [varieties of λ]</h2>
+
+<table style="width:18%">
+  
+  <tr>
+    <td>CTRL + l</td>
+    <td>becomes</td>
+    <td>λ</td>
+  </tr>
+  <tr>
+    <td>CTRL + l-</td>
+    <td>becomes</td>
+    <td>ƛ</td>
+  </tr>
+  <tr>
+    <td>CTRL + l--</td>
+    <td>becomes</td>
+    <td>ƛ̓</td>
+ 
+  </tr>
+</table>
+
+<h2>^ + [character] becomes [superscript character]</h2>
+
+<table style="width:18%">
+  
+  <tr>
+    <td>^w</td>
+    <td>becomes</td>
+    <td>ʷ</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+
+<div id='osk-phone' data-states='default shift numeric symbol'>
+</div>
