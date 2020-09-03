53354f86a2b3342fb64c818729ac1855de81c3c0
MakaraSok <makara@keyman.com>
2020-09-03 19:15:06 +0700

    update the welcome page and a rule for U+1013

---
+++ release/s/sgaw_karen/source/help/sgaw_karen.php
@@ -0,0 +1,31 @@
+<?php 
+  $pagename = 'Sgaw Karen Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<h1>Start Using Sgaw Karen</h1>
+
+<p>
+    This keyboard is designed for S’gaw Karen (Kayin, ကညီ) language spoken in Myanmar and many other countries.
+</p>
+
+<h1>Keyboard Layout</h1>
+<p>Since the majority of users of S'gaw Karen language will likely be bilingual with Burmese, some Burmese characters are added to the keyboard convenient purposes. Users do not have to switch keyboards to type characters from loanwords. Those characters are: ်, ာ, ​ေ, ဗ, ဉ and ယ. Some characters combinations are also added to the keyboard for these same purposes: ◌ၢၢ်, ◌ၢၣ်, ◌ၢာ်, လီၤ, ◌ၢၤ, ◌ှ့ and ဒီး.</p> 
+<p>The characters ဒ and ူ can be found on both Unshift and Shift layers as this is more about convenience for transition from the Burmese keyboard.</p>
+<p>​ေ has to be typed before the consonant it is attached to. For example, type ​ေ and then ဂ to get ​ဂေ. The same rule applies to all consonants.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+
+<h3>Long press</h3>
+<p>Press and hold on the key with a little dot on the top right to reveal more keys.</p>
+<p><a href="mobile_layout_longpress.png"><img class="keyboard" src="mobile_layout_longpress.png" width=40% /></a></p>
