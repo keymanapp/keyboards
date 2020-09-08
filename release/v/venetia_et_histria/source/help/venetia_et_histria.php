97695b0bd5b45647353cba9b1b6db9bed211a4a5
MakaraSok <makara@keyman.com>
2020-09-08 14:04:28 +0700

    move to release from legacy

---
+++ release/v/venetia_et_histria/source/help/venetia_et_histria.php
@@ -0,0 +1,42 @@
+<?php 
+  $pagename = 'Venetia et Histria Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the <a href="http://en.wikipedia.org/wiki/Venetian_language"><b>Venetian</b></a> language of Veneto in Italy.  It uses <b>standard Windows fonts</b>.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+<br/>
+
+<h2>Keyboard Layout</h2>
+
+<div id='osk-container'>
+  <h3>Desktop Keyboard Layout</h3>
+  <div id='osk' data-states='default shift ctrl shift-ctrl ctrl-alt shift-ctrl-alt'></div>
+</div>
+
+<div id='osk-tablet-container'>
+  <h3>Mobile/Tablet Keyboard Layout</h3>
+  <div id='osk-tablet' data-states='default shift'></div>
+</div>
+
+<div style="text-align: center; font: 11pt Arial Unicode MS;">
+Type yellow symbols, followed by pink letters / Dìgita i segni xali, e dòpo i caràteri in ròxa:
+<br /><br />
+<img src="tast.png" />
+<br /><br />
+<table style="font: 11pt Arial Unicode MS;"><tr>
+<td style="background: #FEFF89; padding: 5px 10px 3px 10px;">. + -</td>
+<td style="padding: 3px 5px;">Press these keys twice to insert the symbol without combining with the character.</td>
+</tr><tr>
+<td style="background: #FEFF89; padding: 5px 10px 3px 10px;">. + -</td>
+<td style="padding: 3px 5px;">Par inserir el seño sensa conbinarse co ’l caràtere, strucar el tasto do vòlte.</td>
+</tr></table>
+<br /><br />
+</div>
+
+<p>© 2007-2020 Paul Durdin and Fabio Lazarin</p>
