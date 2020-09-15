f899cd502dd317dcd21cb06ac7265789090833f9
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-09-15 16:39:15 -0500

    Update sil_korda_latin.php
    Added head back in

--- release/sil/sil_korda_latin/source/help/sil_korda_latin.php
+++ release/sil/sil_korda_latin/source/help/sil_korda_latin.php
@@ -10,8 +10,12 @@ The Korean KORDA keyboards are built to type Korean using the transliteration sy
   $pagetitle = 'Korean KORDA Latin (SIL) Keyboard Help';
   require_once('header.php');
 ?>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Korean KORDA Jamo (SIL) Keyboard Help</title>
+  <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
 
-   <div id='overview'>
     <h2>Overview</h2>
     <p>The Korean KORDA (SIL) keyboard is built to type Korean using the transliteration system KORDA (Korean Romanization for Data Application). KORDA is designed to make Latin-Korean transliteration easy, accurate, and computerizable. For more information on KORDA, see <a href='about_korda.pdf'>About KORDA</a>.</p>
     <p>The Korean KORDA Latin (SIL) keyboard creates hangeul syllables from latin letters each time you press <kbd>Spacebar</kbd>.</p>
@@ -27,7 +31,6 @@ The Korean KORDA keyboards are built to type Korean using the transliteration sy
         <li>Gungsuh</li>
         <li>Malgun Gothic</li>
     </ul>    
-  <div id='layout'>
    <h2>Keyboard Layout</h2>
     <h3 style="clear:left">Korean KORDA Latin (SIL) Keyboard</h3>
     <div class="key plain" id="K_BKQUOTE">
@@ -260,7 +263,7 @@ The Korean KORDA keyboards are built to type Korean using the transliteration sy
     <div style="width: 54px; display: block; float: left;"> </div>
     <div class="key special" id="K_CTRLR">
     <div class="keycap">Ctrl</div>
-    </div>
+    
     <br style="clear: left" />    
 
     <h2 class='details'>Keyboard Details</h2>
