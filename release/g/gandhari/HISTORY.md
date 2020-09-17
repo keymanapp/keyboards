cb9a3b3690f9cfcc364262d67e2bcd5cf774009b
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-09-17 14:34:22 -0500

    [gandhari] Moved from legacy to release

---
+++ release/g/gandhari/HISTORY.md
@@ -0,0 +1,16 @@
+Gandhari Change History
+====================
+
+2.8 (2019-07-09)
+----------------
+* Moved source to github repo
+* Changed license from freeware to MIT with permission of author
+
+2.7 (2011-10-21)
+----------------
+* Unknown changes
+* Moved to github repo without source
+
+1.0 (2004-01-10)
+----------------------
+* Initial Desktop keyboard
---
+++ release/g/gandhari/source/help/gandhari.php
@@ -0,0 +1,16 @@
+<?php 
+  $pagename = 'Gandhari Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>
+    Gandhari Sanskrit keyboard developed for Latin transliteration of Gāndhārī language written in the Kharoṣṭhī script.
+</p>
+<p>This keyboard makes heavy use of "deadkeys". These keys are described in the downloadable documentation below. Where the keyboard mentions the CTRL key, the LEFT CTRL key should be used. Where the keyboard mentions the ALT key, the RIGHT ALT key should be used.</p>
+<p>
+    Documentation for this keyboard is available here: <a href="gandhari-keyboard_keyman.pdf">gandhari-keyboard_keyman.pdf</a>
+</p>
+
