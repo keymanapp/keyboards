30ea6846a84b0424172b1dde2b2dc0546ded6596
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-09-22 14:47:08 -0500

    [help_wancho] Add online help file for Wancho keyboard

---
+++ experimental/w/wancho/source/help/wancho.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Wancho Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    This keyboard is designed for the Wancho language of India using the Wancho script created by Banwang Losu.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift ctrl'>
+</div>
+
+<p>©2020 Banwang Losu</p>
