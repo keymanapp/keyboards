7acdfa6ae01b872cd2a36315552d7a736af07fe1
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-10-08 14:24:52 -0500

    [sil_yna_plrd] Changes per PR feedback

--- release/sil/sil_yna_plrd/source/help/sil_yna_plrd.php
+++ release/sil/sil_yna_plrd/source/help/sil_yna_plrd.php
@@ -8,7 +8,7 @@
 ?>
 
 
-<p>This keyboard was designed for the Aluo / Gan Yi [yna] language using the Miao script. A font must be installed to support this keyboard. A font must be installed to support this keyboard. A Miao font specifically designed for use with the yna language is available here: <a href="http://software.sil.org/shimenkan/">http://software.sil.org/shimenkan/</a>. The Shimenkan Zonghe font is also packaged with this keyboard.</p>
+<p>This keyboard was designed for the Aluo / Gan Yi [yna] language using the Miao script. A font must be installed to support this keyboard. A Miao font specifically designed for use with the yna language is available here: <a href="http://software.sil.org/shimenkan/">http://software.sil.org/shimenkan/</a>. The Shimenkan Zonghe font is also packaged with this keyboard.</p>
 
 
 <h2>Desktop Keyboard Layout</h2>
