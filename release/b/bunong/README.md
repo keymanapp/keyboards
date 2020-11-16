152d1287776a47bd7cd32cac72b45860f0b5d332
MakaraSok <makara@keyman.com>
2020-11-16 14:42:19 +0700

    initial commit

---
+++ release/b/bunong/README.md
@@ -0,0 +1,23 @@
+Bunong Keyboard Data
+------------------------------
+
+Copyright (C) 2018 Makara SOK
+Version 1.0
+
+Description
+-----------
+
+This is the first Bunong keyboard.
+
+Links
+-----
+
+ * Contact: 	<makara@keyman.com>
+
+Supported Platforms
+-------------------
+ * Windows
+ * Web
+ * macOS
+ * iOS
+ * Android
---
+++ release/b/bunong/source/help/bunong.php
@@ -0,0 +1,20 @@
+<?php 
+  $pagename = 'Bunong Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    This keyboard is designed for typing Bonong language using Khmer characters.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+  <h2>Desktop</h2>
+   <div id='osk' data-states='default shift'></div>
+  <h2>Mobile</h2>
+   <div id='osk-tablet' data-states='default shift'></div>
+
+
+<p>(c) 2020 Makara Sok</p>
