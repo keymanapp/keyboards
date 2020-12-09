49a9a5104823eb71a590d4a4951c2830ca9f1aaa
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-12-09 10:28:45 -0600

    Update bj_naskapi_classic.php
    Added comment to know how to access narrow space.

--- release/bj/bj_naskapi_classic/source/help/bj_naskapi_classic.php
+++ release/bj/bj_naskapi_classic/source/help/bj_naskapi_classic.php
@@ -5,7 +5,7 @@
   require_once('header.php');
 ?>
 <p>The Naskapi (Classic) keyboard is a "typewriter-style" layout (type one key for one character). It uses Unified Canadian Aboriginal Syllabics.</p>
-
+<p>Although SHIFT+\ looks blank, it outputs a narrow space character.</p>
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift rightalt rightalt-shift'>
 </div>
