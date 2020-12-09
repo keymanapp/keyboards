b767a5f6f9a722654efd5669da2aca464f6da15e
MakaraSok <makara@keyman.com>
2020-12-09 11:15:08 +0700

    initial commit

---
+++ release/bj/bj_cree_west_latn/source/help/bj_cree_west_latn.php
@@ -0,0 +1,18 @@
+<?php
+  $pagename = 'SRO (west) Keyboard Help';
+  $pagetitle = 'SRO (west) Keyboard Help';
+  require_once('header.php');
+?>
+
+<p>
+    Roman keyboard for typing vowel length (macrons over vowels) in Standard Roman Orthography (SRO-Western Cree).
+
+</p>
+
+<h1>Keyboard Layout</h1>
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+<h2>Mobile Keyboard Layout</h2>
+<div id='phone' data-states='default shift numeric'>
+</div>
