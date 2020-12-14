84dab69aa568becb8d6f0bd6da9c3f0081eff847
Lorna Evans <LornaSIL@users.noreply.github.com>
2020-12-14 14:22:39 -0600

    [plrd] Added font info to keyboard_info file

--- release/sil/sil_ygp_plrd/source/help/sil_ygp_plrd.php
+++ release/sil/sil_ygp_plrd/source/help/sil_ygp_plrd.php
@@ -1,6 +1,6 @@
 <?php 
-  $pagename = 'Bai Yi – White Yi (SIL) Keyboard Help';
-  $pagetitle = 'Bai Yi – White Yi (SIL) Keyboard Help';
+  $pagename = 'Bai Yi -- White Yi (SIL) Keyboard Help';
+  $pagetitle = 'Bai Yi -- White Yi (SIL) Keyboard Help';
   // Header we will tidy up later  
   require_once('header.php');
 
@@ -8,7 +8,7 @@
 ?>
 
 
-<p>This keyboard was designed for the Gepo – Bai Yi [ygp] language using the Miao script. A font must be installed to support this keyboard. The Shimenkan GSM font is also packaged with this keyboard.</p>
+<p>This keyboard was designed for the Gepo -- Bai Yi [ygp] language using the Miao script. A font must be installed to support this keyboard. The Shimenkan GSM font is also packaged with this keyboard.</p>
 
 
 <h2>Desktop Keyboard Layout</h2>
