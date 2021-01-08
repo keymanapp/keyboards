cc20c6727999ce5540a593570cadb1806d7bdb3b
Lorna Evans <LornaSIL@users.noreply.github.com>
2021-01-08 10:02:57 -0600

    [sil_bari] Added Charis SIL to package for better display on mobile

--- release/sil/sil_bari/source/help/sil_bari.php
+++ release/sil/sil_bari/source/help/sil_bari.php
@@ -1,8 +1,19 @@
 <?php
   $pagename = 'Bari (SIL) Keyboard Help';
-  $pagetitle = 'Bari (SIL) Keyboard Help';
+  $pagetitle = $pagename;
   require_once('header.php');
 ?>
-    <h1>Bari (SIL) Keyboard</h1>
-    <p>Bari (SIL) keyboard for the Bari language of South Sudan.</p>
-<img src='keyboard_layout.png' alt='Keyboard Layout' width='720px'>
+<p>Bari (SIL) keyboard for the Bari language of South Sudan.</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
