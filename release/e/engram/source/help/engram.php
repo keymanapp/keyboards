a1f6b10324a4cd083a266168d8f6fcdc9b89e37c
Arno Klein <binarybottle@gmail.com>
2021-01-18 18:05:53 -0500

    Update with new key layout and R-Alt

--- release/e/engram/source/help/engram.php
+++ release/e/engram/source/help/engram.php
@@ -5,6 +5,7 @@
   require_once('header.php');
 ?>
 
+<h1>Arno's Engram key layout</h1>
 
 <p>The Engram layout (<a href="https://engram.dev">website</a> and <a href="https://github.com/binarybottle/engram-layout">GitHub repo</a>) 
 is a keyboard layout optimized for comfortable and efficient touch typing in English created by <a href="https://binarybottle.com">Arno Klein</a>, 
@@ -12,22 +13,20 @@ with open source code to create other optimized key layouts.
 Letters are optimally arranged to reduce lateral finger movements, 
 following ergonomic principles, and facilitating high-frequency letter pairs. 
 Punctuation marks are grouped together in the middle columns 
-(accessed by the Shift and Ctrl keys) and numbers are paired with symbols that resemble them 
+(accessed by the Shift and R-Alt keys) and numbers are paired with symbols that resemble them 
 (accessed by the Shift Key):</p>
 
-<p>
-#~ &nbsp;&nbsp; 1| &nbsp;&nbsp; 2= &nbsp;&nbsp; 3< &nbsp;&nbsp; 4+ &nbsp;&nbsp; 5$ &nbsp;&nbsp; 6@ &nbsp;&nbsp; 7> &nbsp;&nbsp; 8& &nbsp;&nbsp; 9% &nbsp;&nbsp; 0* &nbsp;&nbsp; ^`  <br>
+<p>#$ &nbsp;&nbsp; 1- &nbsp;&nbsp; 2= &nbsp;&nbsp; 3~ &nbsp;&nbsp; 4+ &nbsp;&nbsp; 5< &nbsp;&nbsp; 6> &nbsp;&nbsp; 7/ &nbsp;&nbsp; 8& &nbsp;&nbsp; 9% &nbsp;&nbsp; 0* &nbsp;&nbsp; |^ &nbsp;&nbsp; \`  <br>
 <br>
-&nbsp;&nbsp;&nbsp;&nbsp; Y &nbsp;&nbsp; G &nbsp;&nbsp; U &nbsp;&nbsp; K &nbsp;&nbsp; '-/ &nbsp;&nbsp; "_\ &nbsp;&nbsp; B &nbsp;&nbsp; L &nbsp;&nbsp; D &nbsp;&nbsp; F &nbsp;&nbsp; Q  <br> 
-&nbsp;&nbsp;&nbsp;&nbsp; I &nbsp;&nbsp; O &nbsp;&nbsp; E &nbsp;&nbsp; A &nbsp;&nbsp; ,;? &nbsp;&nbsp; .:! &nbsp;&nbsp; R &nbsp;&nbsp; T &nbsp;&nbsp; S &nbsp;&nbsp; N &nbsp;&nbsp; J  <br>
-&nbsp;&nbsp;&nbsp;&nbsp; V &nbsp;&nbsp; Z &nbsp;&nbsp; X &nbsp;&nbsp; C &nbsp;&nbsp; ([{ &nbsp;&nbsp; )]} &nbsp;&nbsp; H &nbsp;&nbsp; W &nbsp;&nbsp; P &nbsp;&nbsp; M                 <br>
-<br>
+&nbsp;&nbsp;&nbsp;&nbsp; Y &nbsp;&nbsp; O &nbsp;&nbsp; U &nbsp;&nbsp; X &nbsp;&nbsp; ([{ &nbsp;&nbsp; )]} &nbsp;&nbsp; W &nbsp;&nbsp; D &nbsp;&nbsp; C &nbsp;&nbsp; V &nbsp;&nbsp; Q  <br> 
+&nbsp;&nbsp;&nbsp;&nbsp; I &nbsp;&nbsp; H &nbsp;&nbsp; E &nbsp;&nbsp; A &nbsp;&nbsp; ,;: &nbsp;&nbsp; .?! &nbsp;&nbsp; R &nbsp;&nbsp; T &nbsp;&nbsp; S &nbsp;&nbsp; N &nbsp;&nbsp; Z  <br>
+&nbsp;&nbsp;&nbsp;&nbsp; P &nbsp;&nbsp; K &nbsp;&nbsp; J &nbsp;&nbsp; G &nbsp;&nbsp; "#@ &nbsp;&nbsp; '-_ &nbsp;&nbsp; L &nbsp;&nbsp; B &nbsp;&nbsp; F &nbsp;&nbsp; M                 
 </p>
 
 <h1>Keyboard Layout</h1>
 
 <h2>Desktop Keyboard Layout</h2>
-<div id='osk' data-states='default shift ctrl'>
+<div id='osk' data-states='default shift ralt'>
 </div>
 
 <p>© 2021 Arno Klein</p>
