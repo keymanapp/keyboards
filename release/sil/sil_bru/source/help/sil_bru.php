805e29c9e83524776829a500dc54f22aec1a8d73
Lorna Evans <LornaSIL@users.noreply.github.com>
2021-01-22 16:11:09 -0600

    [sil_bru] New keyboard based on an MSKLC keyboard

---
+++ release/sil/sil_bru/source/help/sil_bru.php
@@ -0,0 +1,22 @@
+<?php 
+  $pagename = 'Bru Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>
+    This is a keyboard that contains all of the characters needed to type Eastern Bru in the Latin script. It is very similar to the Vietnamese keyboard, but adds characters needed for Bru using the breve. The keyboard produces NFC Unicode.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default shift rightalt'>
+</div>
