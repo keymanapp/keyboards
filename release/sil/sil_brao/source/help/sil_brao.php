8353450a5c8f58f13c94232dc8ca748b560aa0a8
MakaraSok <makara@keyman.com>
2021-03-25 18:13:17 +0700

    update touch theme and meta data

---
+++ release/sil/sil_brao/source/help/sil_brao.php
@@ -0,0 +1,33 @@
+<?php 
+  $pagename = 'Brao (SIL) Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>
+    Brao (SIL) keyboard is designed for typing Brao Lave language spoken in north-eastern Cambodia. Khmer script is used for this keyboard. The desktop layout is similar to that of the Khmer NiDA keyboard, but there are less vowel characters. Only two subscripts are used in this language, ្រ and ្ល.
+</p>
+<p>
+	Four extra characters are used in this language: 
+	<ul>
+	<li>អ្យ on <kbd>o</kbd> key,</li> 
+	<li>ឝ on Shift <kbd>o</kbd> key,</li>
+	<li>ឞ on Shift <kbd>v</kbd> key and</li>
+	<li>៝ on <kbd>e</kbd> key.</li>
+	</ul>
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile Keyboard Layout</h2>
+<div id='osk-phone' data-states='default shift symbol'>
+
+<h2>Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
