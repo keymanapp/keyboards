cc7fd63d55aa0b76da063de821e6c3eaf96e9805
Katelem <53642368+katelem247@users.noreply.github.com>
2021-03-27 05:03:59 +0100

    Create obolo_chwerty.php

---
+++ source/help/obolo_chwerty.php
@@ -0,0 +1,96 @@
+<?php 
+  $pagename = 'Obolo Chwerty Keyboard Help';
+    $pagetitle = $pagename;
+      // Header we will tidy up  
+        require_once('header.php');
+        ?>
+        	<p>Obolo Chwerty is a simple Unicode keyboard for typing the Obolo language. The CHWERTY layout includes <samp>ch</samp>, <samp>ọ</samp>, <sample>n̄<sample> and <samp>sh</samp>.</p> 
+        		<h4>Function</h4>
+        			<p>The Obolo Chwerty keyboard follows the English (QWERTY US) layout. You can type all of the special Obolo letters with a single keystroke using this keyboard. Here is how:</p>
+        				<article class='guide'>
+        						<table class='grid colleft'>
+        									<col class='keys' />
+        												<col class='char' />	
+        															<thead>		
+        																			<tr>
+        																								<th>Key</th>
+        																													<th>Accent</th>
+        																																	</tr>
+        																																				</thead>
+        																																							<tbody>				
+        																																											<tr>
+        																																																<td><kbd>q</kbd></td>
+        																																																					<td><samp>ch</samp></td>
+        																																																									</tr>
+        																																																													<tr>
+        																																																																		<td><kbd>h</kbd></td>
+        																																																																							<td><samp>sh</samp></td>
+        																																																																											</tr>
+        																																																																															<tr>
+        																																																																																				<td><kbd>x</kbd></td>
+        																																																																																									<td><samp>ọ</samp></td>
+        																																																																																													</tr>
+        																																																																																																	<tr>
+        																																																																																																						<td><kbd>c</kbd></td>
+        																																																																																																											<td><samp>n̄</samp></td>
+        																																																																																																															</tr>		
+        																																																																																																																			<tr>
+        																																																																																																																								<td><kbd>^</kbd></td>
+        																																																																																																																													<td><samp>₦</samp></td>
+        																																																																																																																																	</tr>					
+        																																																																																																																																				</tbody>
+        																																																																																																																																						</table>
+        																																																																																																																																							</article>	
+        																																																																																																																																								<p class='clear'>You can type all of the Obolo accents with this keyboard. Here is how:</p>
+        																																																																																																																																									<article class='guide'>
+        																																																																																																																																											<table class='grid colleft'>
+        																																																																																																																																														<col class='keys' />
+        																																																																																																																																																	<col class='char' />	
+        																																																																																																																																																				<thead>		
+        																																																																																																																																																								<tr>
+        																																																																																																																																																													<th>Key</th>
+        																																																																																																																																																																		<th>Accent</th>
+        																																																																																																																																																																						</tr>
+        																																																																																																																																																																									</thead>
+        																																																																																																																																																																												<tbody>				
+        																																																																																																																																																																																<tr>
+        																																																																																																																																																																																					<td><kbd>`</kbd></td>
+        																																																																																																																																																																																										<td><samp>`</samp></td>
+        																																																																																																																																																																																														</tr>
+        																																																																																																																																																																																																		<tr>
+        																																																																																																																																																																																																							<td><kbd>\</kbd></td>
+        																																																																																																																																																																																																												<td><samp>ˆ</samp></td>
+        																																																																																																																																																																																																																</tr>
+        																																																																																																																																																																																																																				<tr>
+        																																																																																																																																																																																																																									<td><kbd>shift </kbd><kbd>`</kbd></td>
+        																																																																																																																																																																																																																														<td><samp></samp></td>
+        																																																																																																																																																																																																																																		</tr>
+        																																																																																																																																																																																																																																						<tr>
+        																																																																																																																																																																																																																																											<td><kbd>shift</kbd><kbd>\</kbd></td>
+        																																																																																																																																																																																																																																																<td><samp>ˇ</samp></td>
+        																																																																																																																																																																																																																																																				</tr>
+        																																																																																																																																																																																																																																																								<tr>
+        																																																																																																																																																																																																																																																													<td><kbd>[</kbd></td>
+        																																																																																																																																																																																																																																																																		<td><samp>ˉ</samp></td>
+        																																																																																																																																																																																																																																																																						</tr>				
+        																																																																																																																																																																																																																																																																									</tbody>
+        																																																																																																																																																																																																																																																																											</table>
+        																																																																																																																																																																																																																																																																												</article>
+        																																																																																																																																																																																																																																																																													<p class='clear'>Accents are typed after the base leter. Accents can be added to the letters <samp>a</samp>, <samp>e</samp>, <samp>i</samp>, <samp>i</samp>, <samp>m</samp>, <samp>n</samp>, <samp>o</samp>, <samp>ọ</samp>, and <samp>u</samp>.</p>
+        																																																																																																																																																																																																																																																																														<p class='example'><b>Example:</b> type <kbd>a</kbd><kbd>`</kbd> to get <samp>à</samp>.</p>
+        																																																																																																																																																																																																																																																																															<p class='example'><b>Example:</b> type <kbd>M</kbd><kbd>|</kbd> to get <samp>M̄</samp>.</p>	
+        																																																																																																																																																																																																																																																																																<p>To get the English output for any key instead of Obolo, type the <kbd>]</kbd> first. This is shown as a <samp class='LTN'>&#10807</samp> on the touchscreen keyboard.</p>
+        																																																																																																																																																																																																																																																																																	<p class='example'><b>Example:</b> on touchscreen, type <kbd>&#10807</kbd><kbd>CH</kbd> to get <samp>Q</samp> (instead of <samp>CH</samp>).</p>
+        																																																																																																																																																																																																																																																																																		<p class='example'><b>Example:</b> on desktop, type <kbd>]</kbd><kbd>CH</kbd> to get <samp>Q</samp> (instead of <samp>ˆ</samp>).</p>
+        																																																																																																																																																																																																																																																																																			<p class='example'><b>Example:</b> type <kbd>&#10807</kbd><kbd>ọ</kbd> to get <samp>x</samp> (instead of <samp>ọ</samp>).</p>
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
