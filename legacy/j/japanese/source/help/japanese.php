826c90d6ffe770fcf9e04c39f68c66689da45f80
Marc Durdin <marc@durdin.net>
2021-03-29 08:59:41 +1100

    chore(japanese): use unminified version
    This is part of investigation for #1493. Using the unminified version
    will make testing easier, and should not have negative consequences for
    for end users; the file size is slightly larger but not significant.

--- legacy/j/japanese/source/help/japanese.php
+++ legacy/j/japanese/source/help/japanese.php
@@ -1,25 +1,12 @@
 <?php /*
   Name:             Keyboard_japanese
-  Copyright:        Copyright (C) 2010 Tavultesoft Pty Ltd.
-  Documentation:    
-  Description:      
-  Create Date:      8 Jan 2010
-
-  Modified Date:    5 Mar 2010
-  Authors:          mcdurdin, jmdurdin
-  Related Files:    
-  Dependencies:     
-
-  Bugs:             
-  Todo:             
-  Notes:            
   History:          8 jam 2010 - jmdurdin - Initial version
                     05 Mar 2010 - mcdurdin - Publish Japanese version
 */
   $pagename = 'Japanese Keyboard Help';
   $pagetitle = 'Japanese Keyboard Help';
   $style = <<<END
-table.usage {margin:10px;}  
+table.usage {margin:10px;}
 table.usage tr td {padding-right:10px;vertical-align:top;}
 table.usage tr td.demo {padding-right:10px;vertical-align:middle;}
 table.usage tr td.key {padding-right:10px;vertical-align:top;padding-top:4px}
@@ -28,7 +15,7 @@ a.int:link {color: #a64826;}
 a.int:visited {color: #a64826;}
 a.int:hover {color: #f00000}
 END;
-  
+
   require_once('header.php');
 
 ?>
@@ -55,12 +42,12 @@ END;
 <a href="#Authorship" class="int">Authorship and Acknowledgements</a><br/>
 
 <div id='Overview'>
-		<!-- A brief introduction to the keyboard, including intended users and font/hardware requirements.  This is basically 
+		<!-- A brief introduction to the keyboard, including intended users and font/hardware requirements.  This is basically
 the info available on the keyboard download site.  Instructions on using the keyboard DO NOT go here. -->
 
 <h2>Overview</h2>
 <p>
-This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.org/wiki/Japanese_writing_system">Japanese</a> language as used in Japan (and elsewhere).  Japanese text is entered using Latin alphabet characters (<a target="_blank" href="http://en.wikipedia.org/wiki/W%C4%81puro_r%C5%8Dmaji">W&#x0101;puro r&#x014d;maji</a>) for the corresponding Japanese letters, and are output as the equivalent <a target="_blank"  href="http://en.wikipedia.org/wiki/Hiragana">Hiragana</a> or <a target="_blank" href="http://en.wikipedia.org/wiki/Katakana">Katakana</a>.  An "input method editor" (IME) window is used to select from a  list of matching <a target="_blank" href="http://en.wikipedia.org/wiki/Kanji">Kanji</a> characters (with or without <a target="_blank" href="http://en.wikipedia.org/wiki/okurigana">okurigana</a>).   
+This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.org/wiki/Japanese_writing_system">Japanese</a> language as used in Japan (and elsewhere).  Japanese text is entered using Latin alphabet characters (<a target="_blank" href="http://en.wikipedia.org/wiki/W%C4%81puro_r%C5%8Dmaji">W&#x0101;puro r&#x014d;maji</a>) for the corresponding Japanese letters, and are output as the equivalent <a target="_blank"  href="http://en.wikipedia.org/wiki/Hiragana">Hiragana</a> or <a target="_blank" href="http://en.wikipedia.org/wiki/Katakana">Katakana</a>.  An "input method editor" (IME) window is used to select from a  list of matching <a target="_blank" href="http://en.wikipedia.org/wiki/Kanji">Kanji</a> characters (with or without <a target="_blank" href="http://en.wikipedia.org/wiki/okurigana">okurigana</a>).
 </p>
 
 <p>The keyboard works best with a QWERTY (English) keyboard.</p>
@@ -85,12 +72,6 @@ This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.o
 <p>If the same Hiragana string matches (or partly matches) more than nine Japanese words, use <b>PageDown</b> and <b>PageUp</b> to scroll through the list. </p>
 </div>
 
-<!--
-<div id="Examples">
-<h3>Example</h3>
-<?php renderLanguageExamples(); ?>
-</div>
--->
 <div id="Documentation">
 <h3>Keyboard Use Details</h3>
 
@@ -129,7 +110,7 @@ This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.o
 <h4 id="romaji">Romanization (W&#x0101;puro r&#x014d;maji) conventions</h4>
 <p>Input follows widely-used <a target="_blank" href="http://en.wikipedia.org/wiki/W%C4%81puro_r%C5%8Dmaji">W&#x0101;puro r&#x014d;maji</a> conventions, but note the following: </p>
 <ul>
-<li>Long vowels are entered using kana spelling rules, thus, <span class='highlightExample'>kou</span> for <span class='highlightExample'>こう</span> and <span class='highlightExample'>kuu</span> for <span class='highlightExample'>くう</span>.</li> 
+<li>Long vowels are entered using kana spelling rules, thus, <span class='highlightExample'>kou</span> for <span class='highlightExample'>こう</span> and <span class='highlightExample'>kuu</span> for <span class='highlightExample'>くう</span>.</li>
 <li>Small kana can be entered by prefacing them with <span class='highlightExample'>x</span>, e.g. <span class='highlightExample'>xa</span> for <span class='highlightExample'>ぁ</span>, or <span class='highlightExample'>xtu</span> for <span class='highlightExample'>っ</span>.</li>
 <li>When typing, the letter <span class='highlightExample'>l</span> is generally equivalent to <span class='highlightExample'>r</span>.</li>
 <li><span class='highlightExample'>じゃ</span>, <span class='highlightExample'>じゅ</span> and <span class='highlightExample'>じょ</span> may be input by typing either <span class='highlightExample'>ja</span>, <span class='highlightExample'>ju</span> and <span class='highlightExample'>jo</span>, or <span class='highlightExample'>jya</span>, <span class='highlightExample'>jyu</span> and <span class='highlightExample'>jyo</span>.</li>
@@ -140,7 +121,7 @@ This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.o
 <h4 id="type_ahead">Typing ahead</h4>
 <p>You do not need to wait for the IME to display the list of possible Japanese words after each key-stroke.  The IME recognizes if more characters have been typed and will only display the suggestions for the complete string that has been entered.  If you know which word you will want from the final list for a typed string, you can type the selection digit and keep on typing.  In most cases, the IME will remember what has been typed and process the entire input sequence correctly.</p>
 <p>If the IME for any reason does not process the string at the insertion point correctly, press <b>Escape</b> to close the IME window, re-position the cursor as required, then continue entering text normally.</p>
- 
+
 <h4 id="editing">Editing existing text</h4>
 <p>Japanese text already entered into an input area may be edited in the normal way, using cut and paste or the <b>Backspace</b> and <b>Delete</b> keys, and moving through the document with the cursor (arrow) keys.  But once a letter is re-typed at any point, the IME will convert the input to Hiragana and try and find matching Japanese words.</p>
 
@@ -156,20 +137,20 @@ This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.o
 <h3>Troubleshooting</h3>
 <h4 id="no_IME">The IME window is not visible</h4>
 <p>If the IME window does not appear when a letter key is typed, then either the site has not been configured to use KeymanWeb, or else the input area you are entering text into cannot accept Japanese text.  For example, it is not yet generally possible to use email addresses written with Japanese Kanji, so email address fields will not accept Kanji.</p>
-<p>A delay of one or two seconds may be experienced in some cases before the IME window is updated.  This is normal, and the more the IME is used, the faster the average response will be, since it remembers the results for words already looked up.</p> 
+<p>A delay of one or two seconds may be experienced in some cases before the IME window is updated.  This is normal, and the more the IME is used, the faster the average response will be, since it remembers the results for words already looked up.</p>
 
 <h4 id="disappeared">After I started typing, the IME window disappeared...</h4>
 <p>If the IME window disappears after typing some letters but before the selected Kanji has been inserted, check:
 <ul>
-<li>Was <b>Space</b>, <b>Escape</b> or a punctuation key pressed?  If so, the IME will assume that the entered text should remain displayed as Hiragana or Katakana.</li>  
+<li>Was <b>Space</b>, <b>Escape</b> or a punctuation key pressed?  If so, the IME will assume that the entered text should remain displayed as Hiragana or Katakana.</li>
 <li>Has the focus been moved to another input area? Moving the focus also causes the IME to leave the entered text as currently displayed.</li>
 <li>Has the keyboard been disabled? Check the KeymanWeb toolbar - the selected language should be <b>Japanese</b>.</li>
 <li>Is the input area near the bottom of the screen? If so, the IME window may be off-screen, so scroll down so that the input area is no longer near the edge of the screen.</li>
 </ul>
 
-<p>Please read the <a target="_blank" href="/troubleshooting/">KeymanWeb troubleshooting guide</a> for further information.</p> 
+<p>Please read the <a target="_blank" href="/troubleshooting/">KeymanWeb troubleshooting guide</a> for further information.</p>
 
-<p>For any other questions, <a target="_blank" href="http://www.tavultesoft.com/contact.php">contact us</a>.</p>
+<p>For any other questions, <a target="_blank" href="https://community.software.sil.org/c/keyman">contact us</a>.</p>
 
 </div>
 
@@ -179,16 +160,10 @@ This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.o
 <p>It is recommended that you use an English QWERTY hardware keyboard with this keyboard.
 </p>
 <h3>Unicode Version</h3>
-<p>This keyboard complies with Unicode 5.1</p>
+<p>This keyboard complies with Unicode 13.0</p>
 
 </div>
-<!-- **get keyboard uploaded and correct ID before including this!**
-<div id="Resources">
-<h2>Further Resources</h2>
-<p>Keyman Desktop can be supplied with a Japanese IME keyboard, allowing use with a large number of Windows applications, not just web pages. Download a trial version of 
-<a href="http://www.tavultesoft.com/keyman/downloads/keyboards/details.php?KeyboardID=355">Keyman Desktop - Japanese</a> and evaluate it freely for 30 days.</p>
-<div>
--->
+
 <h3>Other Internet Resources</h3>
 <ul><li><a target="_blank" href="http://www.omniglot.com/writing/japanese.htm">Information about written Japanese at Omniglot.com</a></li>
 <li><a target="_blank" href="http://en.wikipedia.org/wiki/Japanese_language">An introduction to Japanese language at Wikipedia.org</a></li>
@@ -203,26 +178,15 @@ This keyboard is designed for the <a target="_blank" href="http://en.wikipedia.o
 <div id="Author">
 <h2>Keyboard Authorship and Acknowledgements</h2>
 <p>
-This keyboard was created by Tavultesoft.  Tavultesoft graciously acknowledges the use of the <a target="_blank" href="http://www.csse.monash.edu.au/~jwb/edict.html">EDICT</a> Japanese dictionary developed and made available by Dr Jim Breen, Electronic Dictionary Research Group, Clayton School of Information Technology, Monash University, Clayton, VIC 3800, Australia.
+This keyboard was originally created by Tavultesoft.  SIL International graciously acknowledges the use of the <a target="_blank" href="http://www.csse.monash.edu.au/~jwb/edict.html">EDICT</a> Japanese dictionary developed and made available by Dr Jim Breen, Electronic Dictionary Research Group, Clayton School of Information Technology, Monash University, Clayton, VIC 3800, Australia.
 </p>
 </div>
 
 <div id="Copyright">
 <h3>Copyright and Terms of Use</h3>
 <p>
-The Japanese IME keyboard for KeymanWeb is copyright &copy; 2010 Tavultesoft Pty Ltd.  It must not be modified or adapted in any way without written permission from Tavultesoft.  The Japanese dictionary used in this keyboard is copyright &copy; 
+The Japanese IME keyboard for KeymanWeb is copyright &copy; 2010-2020 SIL International and made available under the terms of the MIT license.
+The Japanese dictionary used in this keyboard is copyright &copy;
 <a href='http://www.csse.monash.edu.au/~jwb/edict.html' target='_blank'>Electronic Dictionary Research Group</a>.
-</p>
-<p>
-THIS SOFTWARE IS PROVIDED BY TAVULTESOFT PTY LTD "AS IS" AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL TAVULTESOFT PTY LTD BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 </p>
 </div>
