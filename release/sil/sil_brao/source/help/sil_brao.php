c3a6963718ce305f4023c5261a0c1502357fe556
MakaraSok <makara@keyman.com>
2021-03-29 14:28:56 +0700

    add icon and inline html font style

--- release/sil/sil_brao/source/help/sil_brao.php
+++ release/sil/sil_brao/source/help/sil_brao.php
@@ -7,15 +7,15 @@
 
 
 <p>
-    Brao (SIL) keyboard is designed for typing Brao Lave language spoken in north-eastern Cambodia. Khmer script is used for this keyboard. The desktop layout is similar to that of the Khmer NiDA keyboard, but there are less vowel characters. Only two subscripts are used in this language, ្រ and ្ល.
+    Brao (SIL) keyboard is designed for typing Brao Lave language spoken in north-eastern Cambodia. Khmer script is used for this keyboard. The desktop layout is similar to that of the Khmer NiDA keyboard, but there are less vowel characters. Only two subscripts are used in this language, <span style="font-family:Khmer Mondulkiri;">្រ</span> and <span style="font-family:Khmer Mondulkiri;">្ល</span>.
 </p>
 <p>
 	Four extra characters are used in this language: 
 	<ul>
-	<li>អ្យ on <kbd>o</kbd> key,</li> 
-	<li>ឝ on Shift <kbd>o</kbd> key,</li>
-	<li>ឞ on Shift <kbd>v</kbd> key and</li>
-	<li>៝ on <kbd>e</kbd> key.</li>
+	<li><span style="font-family:Khmer Mondulkiri;">អ្យ</span> on <kbd>o</kbd> key,</li> 
+	<li><span style="font-family:Khmer Mondulkiri;">ឝ</span> on Shift <kbd>o</kbd> key,</li>
+	<li><span style="font-family:Khmer Mondulkiri;">ឞ</span> on Shift <kbd>v</kbd> key and</li>
+	<li><span style="font-family:Khmer Mondulkiri;">៝</span> on <kbd>e</kbd> key.</li>
 	</ul>
 </p>
 
