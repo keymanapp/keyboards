44c4a7e3db4591a4cd78f2deb9e8c9d8c66102fb
Sirus Laia <laia.sirus@gmail.com>
2021-04-04 21:31:12 +0100

    Nias keyboard version 1.0

---
+++ release/n/nias/HISTORY.md
@@ -0,0 +1,5 @@
+# Nias Keyboard Change History
+
+## 1.0 (2021-04-04)
+
+* Created by Sirus Laia
---
+++ release/n/nias/source/help/nias.php
@@ -0,0 +1,32 @@
+<?php 
+  $pagename = 'Nias Keyboard Help';
+  $pagetitle = 'Nias Keyboard Help';
+  require_once('header.php');
+?>
+
+
+<p>This is a basic keyboard with little modification to accomodate the Nias characters ö and ŵ.</p>
+
+<p>On PC: press together CTRL-RIGHT+o to produce ö and CTRL-RIGHT+w to produce ŵ. Add SHIFT to that to get the capital letters.</p>
+
+<p>On tablets/smartphones: ö and ŵ keys are already available on the keyboard.</p>
+
+<p>Di laptop/desktop: tekan bersama-sama tombol CTRL-KANAN+o untuk mengetik ö dan tombol CTRL-KANAN+w untuk menghasilkan ŵ. Tambahkan SHIFT ke kombinasi tsb. untuk mengetik huruf besar.</p>
+
+<p>Di tablet/smartphone: huruf ö dan ŵ telah tersedia di keyboard.</p>
+
+<h2>Links</h2>
+
+<p>Download: <a href="https://keyman.com/keyboards/nias">keyman.com/keyboards/nias</a></p>
+
+<p>Source code: <a href="https://github.com/sslaia/keyboards">https://github.com/sslaia/keyboards</a> (under release/n/nias)</p>
+
+<p>© 2021 Sirus Laia</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
