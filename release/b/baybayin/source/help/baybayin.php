8faa529041368295b906df3c7f53536b5f04cde0
ilhamkang <ilhamnurwansah@gmail.com>
2021-04-22 07:08:49 +0700

    Update baybayin.php

--- release/b/baybayin/source/help/baybayin.php
+++ release/b/baybayin/source/help/baybayin.php
@@ -9,9 +9,8 @@
 This keyboard lets you type Baybayin script in Unicode. This keyboard includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item.
 </p>
 
-<h1>Keyboard Layout</h1>
-
-<h2>Desktop Keyboard Layout</h2>
+<h1>Desktop Keyboard Layout</h1>
+<img src="/source/welcome/baybayin-kb1.png"; height="250"> 
 <div id='osk' data-states='default shift'>
 </div>
 
