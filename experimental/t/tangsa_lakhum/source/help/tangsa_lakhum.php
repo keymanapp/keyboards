d5f5fe5a9abc6dc2e306935ac85491c938fe9019
Lorna Evans <LornaSIL@users.noreply.github.com>
2021-04-28 17:03:20 -0500

    [tangsa_lakhum] Add font to keyboard_info and help file

---
+++ experimental/t/tangsa_lakhum/source/help/tangsa_lakhum.php
@@ -0,0 +1,17 @@
+<?php 
+  $pagename = 'Tangsa Lakhum Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    Tangs Lakhum Unicode 1.0 generated from template.
+</p>
+
+<p>This layout uses a deadkey on the left bracket "[" position to generate additional Lakhum characters.</p>
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift ctrl'>
+</div>
