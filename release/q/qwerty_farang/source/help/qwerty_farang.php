a4b94f590d99e1e617aba698e582d4f0a35d2a75
MakaraSok <makara@keyman.com>
2021-05-10 18:53:36 +0700

    update .kmn and draft documentation

---
+++ release/q/qwerty_farang/source/help/qwerty_farang.php
@@ -0,0 +1,23 @@
+<?php 
+  $pagename = "QWERTY Farang";
+  $pagetitle = "QWERTY Farang Keyboard Help";
+  require_once('header.php');
+?>
+
+<p>
+    QWERTY Farang keyboard created for Thai learners who are more familiar with the QWERTY layout. It uses "longpress" and "rota" to house characters with identical phonetics by order of frequency.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+  <h2>Desktop</h2>
+   <div id='osk' data-states='default shift rightalt'></div>
+
+  <h2>Touch</h2>
+   <div id='osk-tablet' data-states='default numeric'></div>
+   <h3>Longpress</h3>
+	<p>Keys with a little dot on the top right can be pressed and held for more keys.</p>
+	<p><a href="touch_longpress.png"><img class="keyboard" src="touch_longpress.png" alt="Longpress state" /></a></p>
+
+	
+<p>© 2021 Sebastian Restrepo & LatamAsia</p>
--- release/q/qwerty_farang/source/help/thai_qwerty.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  $pagename = "QWERTY Farang";
-  $pagetitle = "QWERTY Farang Keyboard Help";
-  require_once('header.php');
-?>
