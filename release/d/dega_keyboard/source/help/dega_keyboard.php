126a72cc211267e4f2ad6dfa1d92593340e436b4
MakaraSok <makara@keyman.com>
2021-05-11 14:33:10 +0700

    initial commit

---
+++ release/d/dega_keyboard/source/help/dega_keyboard.php
@@ -0,0 +1,24 @@
+<?php 
+  $pagename = "Dega Keyboard";
+  $pagetitle = "Dega Keyboard Help";
+  require_once('header.php');
+?>
+
+<p>
+    Dega Keyboard 1.0 generated from template.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+  <h2>Desktop</h2>
+   <div id='osk' data-states='default shift'></div>
+
+  <h2>Phone</h2>
+   <div id='osk-phone' data-states='default shift numeric'></div>
+
+   <h3>Longpress</h3>
+	<p>Keys with a little dot on the top right can be pressed and held for more keys.</p>
+	<p><a href="touch_longpress.png"><img class="keyboard" src="touch_longpress.png" alt="Longpress state" /></a></p>
+	
+  <h2>Tablet</h2>
+   <div id='osk-tablet' data-states='default shift'></div>
