1c4c3a1fe5efd634bd049d9aacfd898864999922
Lorna Evans <LornaSIL@users.noreply.github.com>
2021-05-20 16:09:04 -0500

    [sil_ipa] Changes per PR feedback

--- release/sil/sil_ipa/source/help/sil_ipa.php
+++ release/sil/sil_ipa/source/help/sil_ipa.php
@@ -2,16 +2,15 @@
   $pagename = 'IPA (SIL) Keyboard Help';
   $pagetitle = $pagename;
   $pagestyle = <<<END
- #section2 td p a {
-     text-decoration: none; color: blue
-    a {text-decoration: none; color: blue} 
-    h1 { font: bold 16pt Tahoma; color: #4444cc }
-    h2 { font: bold 14pt Tahoma; color: #4444cc }
-    h3 { font: bold 12pt Tahoma; color: #4444cc }
-    span.key { font: bold 10pt Verdana; padding: 0 4px; border: solid 1px #aaaaaa; margin: 0 4px; }
- }
-  require_once('header.php');
+    #section2 td p a {
+      text-decoration: none; color: blue
+    }
+    #section2 td h1 { font: bold 16pt Tahoma; color: #4444cc }
+    #section2 td h2 { font: bold 14pt Tahoma; color: #4444cc }
+    #section2 td h3 { font: bold 12pt Tahoma; color: #4444cc }
+    #section2 td span.key { font: bold 10pt Verdana; padding: 0 4px; border: solid 1px #aaaaaa; margin: 0 4px; }
 END;
+  require_once('header.php');
 ?>
 
 <h2 style='margin-top:0in'>Keyboard Layout</h2>
@@ -38,6 +37,8 @@ be used by other traditions such as Americanist, Chinese or Russian). </span></p
 	Switch back to <i>After</i> using the RALT EQUAL sequence (RALT+=). This method will change to <i>Before</i> during just this session of 
 	using the keyboard. After you stop using the keyboard it will return to <i>After</i>.</li>
 </ol>
+
+<p>Version 1.8.5 of the keyboard resolves a conflict with `{` key with German hardware keyboard</p>
 
 
 <h3>Consonants</h3>
