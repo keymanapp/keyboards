67bcc7e3896fb646aa850b6768aaa5768cd1a552
MakaraSok <makara@keyman.com>
2021-06-15 10:55:09 +0700

    localize metadata and icon

--- release/sil/sil_brao/README.md
+++ release/sil/sil_brao/README.md
@@ -8,10 +8,7 @@ Version 1.0
 Description
 -----------
 
-Brao (SIL) generated from template
-
-Links
------
+Brao (SIL) keyboard is designed for typing Brao Lave language spoken in north-eastern Cambodia. Khmer script is used for this keyboard. The desktop layout is similar to that of the Khmer NiDA keyboard, but there are less vowel characters.
 
 Supported Platforms
 -------------------
--- release/sil/sil_brao/source/help/sil_brao.php
+++ release/sil/sil_brao/source/help/sil_brao.php
@@ -2,32 +2,38 @@
   $pagename = 'Brao (SIL) Keyboard Help';
   $pagetitle = $pagename;
   // Header we will tidy up later  
+    $style = <<<END
+    p { font: 12pt Khmer Mondulkiri; }
+	ul { font: 12pt Khmer Mondulkiri; }
+    h1 { font: bold 16pt Khmer Mondulkiri; color: #4444cc; margin-bottom: 2px }
+    h2 { font: bold 14pt Khmer Mondulkiri; color: #4444cc; }
+END;
+  
   require_once('header.php');
 ?>
 
 
 <p>
-    Brao (SIL) keyboard is designed for typing Brao Lave language spoken in north-eastern Cambodia. Khmer script is used for this keyboard. The desktop layout is similar to that of the Khmer NiDA keyboard, but there are less vowel characters. Only two subscripts are used in this language, <span style="font-family:Khmer Mondulkiri;">្រ</span> and <span style="font-family:Khmer Mondulkiri;">្ល</span>.
+    ក្ដារចុច Brao (SIL) ត្រូវ​បាន​បង្កើតឡើងសម្រាប់​វាយជាភាសាព្រៅ ដែលជាភាសានិយាយនៅភាគ​ឦសាននៃប្រទេសកម្ពុជា ដោយប្រើតួអក្សរខ្មែរ។ ប្លង់​ក្ដារចុចសម្រាប់​ប្រើប្រាស់​លើ​កុំព្យូទ័រ​គឺ​ស្រដៀង​នឹង​ប្លង់ក្ដារចុចខ្មែរ NiDA ប៉ុន្តែ​មាន​ស្រៈ​តិចជាង។ គេ​ប្រើ​ជើងព្យញ្ជនៈ​តែ​ពីរ​ប៉ុណ្ណោះក្នុង​ភាសានេះ៖ «្រ» និង «្ល»។
 </p>
 <p>
-	Four extra characters are used in this language: 
+	មានតួអក្សរបួនទៀតដែលគេប្រើក្នុងភាសាព្រៅ៖ 
 	<ul>
-	<li><span style="font-family:Khmer Mondulkiri;">អ្យ</span> on <kbd>o</kbd> key,</li> 
-	<li><span style="font-family:Khmer Mondulkiri;">ឝ</span> on Shift <kbd>o</kbd> key,</li>
-	<li><span style="font-family:Khmer Mondulkiri;">ឞ</span> on Shift <kbd>v</kbd> key and</li>
-	<li><span style="font-family:Khmer Mondulkiri;">៝</span> on <kbd>e</kbd> key.</li>
+	<li>«អ្យ» នៅលើគ្រាប់ចុច <kbd>o</kbd></li> 
+	<li>«ឝ» នៅលើគ្រាប់ចុច <kbd>Shift</kbd> <kbd>o</kbd></li>
+	<li>«ឞ» នៅលើគ្រាប់ចុច <kbd>Shift</kbd> <kbd>v</kbd> និង</li>
+	<li>«៝» នៅលើគ្រាប់ចុច <kbd>e</kbd>។</li>
 	</ul>
 </p>
 
-<h1>Keyboard Layout</h1>
-
-<h2>Desktop Keyboard Layout</h2>
+<h1>ប្លង់ក្ដារចុច</h1>
+	<h2>ប្លង់ក្ដារចុចសម្រាប់កុំព្យូទ័រ</h2>
 <div id='osk' data-states='default shift'>
 </div>
 
-<h2>Mobile Keyboard Layout</h2>
+	<h2>ប្លង់ក្ដារចុចសម្រាប់ទូរសព្ទ</h2>
 <div id='osk-phone' data-states='default shift symbol'>
 
-<h2>Tablet Keyboard Layout</h2>
+	<h2>ប្លង់ក្ដារចុចសម្រាប់ថេប្លេត</h2>
 <div id='osk-tablet' data-states='default shift'>
 </div>
