37b400dddc0115f18a1b418aabfd1aaec268c251
ilhamkang <ilhamnurwansah@gmail.com>
2021-06-29 05:17:11 +0700

    updated buhid.kmn and added buhid.php

---
+++ release/b/buhid/help/buhid.php
@@ -0,0 +1,27 @@
+<?php 
+  $pagename = 'Buhid Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p class="p1"> Basic Buhid <span style="font-family: Noto Sans Buhid">(ᝊᝓᝑᝒᝇ)</span> Keyboard for Philippines languages, included Tagalog, Buhid, etc. This keyboard covered all Buhid characters on the <a href="http://www.unicode.org/charts/PDF/U1740.pdf">Unicode Standard 13.0</a>. Other additional characters can be included in the future if necesarry. There is no standard keyboard layout yet for the Buhid Keyboard at the moment, so this keyboard can be one of the alternative options.</p>
+
+<p class="p1">This keyboard layout designed based on suggestions from the user group in social media. I would like to thank to the admins of <a href="https://www.facebook.com/groups/1136372230134825/">Writing Script in the Philippines</a> on Facebook : John Andy Untalan Uy, Joliana Juan Sawal, Luke Biglang-Awa, Melgazar Galvan, Nathaniel Pønce de Leøn, & Raymond Bondoc Figueroa for their inputs. If there any other suggestions about the layout design, please let me know.</p>  
+
+<p class="p1">The font used along with this keyboard is Noto Sans Buhid. In case you have not install it on your machine, you can simply download the font from the <a href="https://www.google.com/get/noto/">Google Noto website</a> and install it.</p>   
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile Keyboard Layout</h2>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+
+<p class="footer">This keyboard is part of the <a href="https://kairaga.com>Kairaga.com">Kairaga.com</a> keyboard development program. Please send your thoughts to <a href="mailto:info@kairaga.com">info@kairaga.com</a></p> 
+
