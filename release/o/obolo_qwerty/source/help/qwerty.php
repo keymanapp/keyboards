4e7064fad672838221d99c12a41006513f4bed77
Katelem <53642368+katelem24@users.noreply.github.com>
2021-07-23 18:40:31 +0100

    Add files via upload

---
+++ release/o/obolo_qwerty/source/help/qwerty.php
@@ -0,0 +1,76 @@
+﻿<?php
+ $pagename = 'Obolo Qwerty Keyboard Help';
+ $pagetitle = $pagename;
+ require_once('header.php');
+?>
+<p>
+Obolo Qwerty is a simple Unicode keyboard for the Obolo language, using the QWERTY layout.
+<p>
+This is an alternative to the Obolo Chwerty Keyboard. This layout is for those who prefer to type Obolo using the English (US) QWERTY keyboard layout.
+<p>
+ In this language, note that:
+ <p>(1) You cannot place more than one accent on a letter.
+ <p>(2) Some accents are optional and are not required. They are only used in primers and for showing pronounciations.
+ <p>(3) Some letters do not accept some accents.
+ <p>(4) Some letters do not accept accents at all. 
+<p> 
+Except for the optional accents which are not required, the keyboard will notify you when you inadvertently go against the spelling rules. 
+<p>
+<h4>Function</h4>
+<p>The Obolo Qwerty keyboard follows the English (QWERTY US) layout. On computers, you can type all of the special Obolo letters with a single keystroke using this keyboard. Here is how:</p>
+<img src="qwerty_keycombi.png"
+alt="Key Combination"
+style="width:695px;height:189px;">
+<p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<!-- Insert Keyboard Layout Images or HTML here --> 
+<img src="obolo_qwerty_default.png"
+alt="Keyboard Layout (default)"
+style="width:695px;height:189px;">
+<p>
+<img src="obolo_qwerty_shifted.png"
+alt="Keyboard Layout (shift)"
+style="width:695px;height:189px;">
+<p>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<!-- Insert Keyboard Layout Images or HTML here --> 
+<img src="obolo_qwerty_mobidefault.png"
+alt="Keyboard Layout Mobile (default)"
+style="width:695px;height:189px;">
+<p>
+<img src="obolo_qwerty_mobishifted.png"
+alt="Keyboard Layout Mobile (shift)"
+style="width:695px;height:189px;">
+<p>
+<img src="obolo_qwerty_mobialt.png"
+alt="Keyboard Layout Mobile (alt)"
+style="width:695px;height:189px;">
+
+<p>
+If what you are doing does not support 'n̄' or does not render it well, then you can tap the BACKSLASH key after the 'n̄' on desktop to get 'ñ'. Remember you also use this BACKSLASH key to place falling tone on the vowels.
+<p>
+The position of your BACKSLASH key depends on type of keyboard you are using. You can tell where your BACKSLASH key is by looking at the shape of your ENTER/RETURN key.
+<p>The three keyboard types and the respective position of the BACKSLASH keys are shown below:
+<p>
+ <img src="qwertyenterkey1.png"
+alt="ANSI Keyboard">
+<p>
+<img src="qwertyenterkey2.png"
+alt="ISO/JIS Keyboard">
+<p>
+<img src="qwertyenterkey3.png"
+alt="Backward-L Keyboard">
+<p>
+Keyman ... type to the world in your language.
+<p>
+Obolo Qwerty ... ge inu me usem kwun̄ ria si ere geelek me linyọn̄.
+<p>
+Onirọ-o!
+<p>
+Itọn̄ ikup inyi Awaji!</p>
