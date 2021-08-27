2e658d2e1c201be5a4c38e37c27126dcb9a5ff99
Lorna Evans <LornaSIL@users.noreply.github.com>
2021-08-27 09:33:00 -0500

    [basic_kbdwol] fixes per PR feedback

--- release/basic/basic_kbdwol/source/help/basic_kbdwol.php
+++ release/basic/basic_kbdwol/source/help/basic_kbdwol.php
@@ -12,7 +12,7 @@
 </p>
 
 <p>This keyboard makes use of deadkeys. Typing the deadkey before the base character will output the combined character. For example:</p>
-<p>^a will produce ä (available on aAeEiIoOuU). (The ^ is on the US "[" key.)</p>
+<p>^a will produce â (available on aAeEiIoOuU). (The ^ is on the US "[" key.)</p>
 <p>¨a will produce ä (available on aAeEiIoOuUy). (The ¨ is on the US "{" key.)</p>
 <p>~o will produce õ (available on aAnNoO).(The ~ is on the US "Right Alt 2" key.)</p>
 
