350bbcd6a4180db8613253fa8a16bddab8644a52
jackrittenberry <88164608+jackrittenberry@users.noreply.github.com>
2021-08-31 11:49:50 -0500

    Create uma_graphic.php

---
+++ release/u/uma_graphic/source/help/uma_graphic.php
@@ -0,0 +1,13 @@
+<?php 
+  $pagename = 'Uma Graphic Keyboard Keyboard Help';
+  $pagetitle = 'Uma Graphic Keyboard Keyboard Help';
+  require_once('header.php');
+?>
+
+<h1>Start Using Uma Graphic</h1>
+
+<p>
+    Uma Graphic Keyboard version 1.0 created for the Houma Language Project for typing in Uma.
+</p>
+
+<p>©Houma Language Project</p>
