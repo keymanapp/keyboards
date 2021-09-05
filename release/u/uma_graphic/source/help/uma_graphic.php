690724e6ecefe32eec4860af217c11f93405c88b
DavidLRowe <DavidLRowe@users.noreply.github.com>
2021-09-04 18:59:34 -0700

    add welcome.htm typing instructions to .php file

--- release/u/uma_graphic/source/help/uma_graphic.php
+++ release/u/uma_graphic/source/help/uma_graphic.php
@@ -9,5 +9,12 @@
 <p>
     Uma Graphic Keyboard version 1.0 created for the Houma Language Project for typing in Uma.
 </p>
+<h2>Typing Nasal Vowels</h2>
+<p>To type nasal vowels, press the tilde key "~" and then type "a, e, i, or u". The tilde should appear directly above the vowel.</p>
+<br><br>
+
+<h2>Typing Long Vowels</h2>
+<p>To type long vowels, press the backtick key "`" and then type "a, e, i, or u". The accent should appear directly above the vowel.</p>
+<br>
 
 <p>©Houma Language Project</p>
