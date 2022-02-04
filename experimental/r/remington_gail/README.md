b16ad3bbc5fea72216b3fecdcbf2bd9bdfc2d720
Lorna Evans <LornaSIL@users.noreply.github.com>
2022-02-04 13:06:54 -0600

    [remington_gail] Experimental keyboard for testing

---
+++ experimental/r/remington_gail/README.md
@@ -0,0 +1,29 @@
+Remington GAIL (SIL) keyboard
+==============
+
+© SIL International
+
+Description
+-----------
+
+This keyboard mimics the Remington GAIL typewriter layout for Hindi. 
+This keyboard is considered experimental until it has been sufficiently tested. 
+
+Links
+-----
+https://keyman.com/keyboards/remington_gail
+
+Supported Platforms
+-------------------
+ * Windows
+ * macOS
+ * Linux
+ * Web
+ * iPhone
+ * iPad
+ * Android phone
+ * Android tablet
+ * Mobile devices
+ * Desktop devices
+ * Tablet devices
+
---
+++ experimental/r/remington_gail/source/help/remington_gail.php
@@ -0,0 +1,30 @@
+<?php 
+  $pagename = 'Remington GAIL (SIL) Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>
+    This keyboard mimics the Remington GAIL typewriter layout for Hindi.
+</p>
+
+
+  <h2>Desktop Keyboard layout</h2>
+<p>The keyboard layout is demonstrated in the keyboard images below.</p>
+<p>Half forms and full consonant letters are sometimes present in same key (on default and shift states). For example  क and क्‍ are placed on same key.</p>
+<p>Some consonant letters do not have dedicated keys for half letters. To get half letter we need to type halant ( ्) after full consonent letter. For example there is no key for typing छ्‍. we have to type छ and then ् to get छ्; </p>
+<p>The are no dedicated full consonant letters for some consonants. For example there is no dedicated key for  भ, so you cannot type  भ with just one single key. You need to type  भ्‍ and then  ा to get  भ .</p>
+<p>To get  ई first you need to type the letter  इ then you have to type  र् (SHIFT+Z).</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-tablet' data-states='default shift'>
+</div>
