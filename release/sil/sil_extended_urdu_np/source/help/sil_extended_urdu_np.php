e0a07ea22b3c31d1224cb66371dd0dd0b3dc451f
Lorna Evans <LornaSIL@users.noreply.github.com>
2022-03-03 15:53:06 -0600

    [sil_extended_urdu_np] New keyboard

---
+++ release/sil/sil_extended_urdu_np/source/help/sil_extended_urdu_np.php
@@ -0,0 +1,26 @@
+<?php 
+  $pagename = 'Extended Urdu NP (SIL) Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p>
+    Keyboard for typing Urdu, Panjabi, and other Northern Pakistan languages.
+</p>
+
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+
+<p>© SIL International</p>
