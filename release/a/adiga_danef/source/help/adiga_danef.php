af4a4ab3b76740e7c267021018590eaa1028c49f
showgan <haroon.showgan@gmail.com>
2022-04-17 12:21:38 +0300

    [adiga_danef] First release of the Adiga Danef keyboard.

---
+++ release/a/adiga_danef/source/help/adiga_danef.php
@@ -0,0 +1,13 @@
+<?php
+  $pagename = 'Adiga Danef Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+This keyboard is for the Adiga (AKA Adygha, Adyga, Circassian) language based on the Adiga Latin phonetic alphabet.
+</p>
+<h1>Keyboard Layout</h1>
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift ctrl'>
+</div>
