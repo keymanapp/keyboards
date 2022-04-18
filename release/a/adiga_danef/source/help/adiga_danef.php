0d06d6c2c1a2d7885d267745f38a222bb069efa9
showgan <haroon.showgan@gmail.com>
2022-04-18 22:02:19 +0300

    Add files via upload

--- release/a/adiga_danef/source/help/adiga_danef.php
+++ release/a/adiga_danef/source/help/adiga_danef.php
@@ -8,6 +8,34 @@
 This keyboard is for the Adiga (AKA Adygha, Adyga, Circassian) language based on the Adiga Latin phonetic alphabet.
 </p>
 <h1>Keyboard Layout</h1>
-<h2>Desktop Keyboard Layout</h2>
-<div id='osk' data-states='default shift ctrl'>
-</div>
+  <h2>Desktop Keyboard Layout</h2>
+  <div id='osk' data-states='default shift'>
+
+    <h2>Physical Keyboard Key Combinations</h2>
+    <p>
+      In order to print the extra characters which don't appear on a regular English QWERTY keyboard use the following key Combinations:<br>
+      ;s => ş<br>
+      ;d => ś<br>
+      ;f => š<br>
+      ;g => ǵ<br>
+      ;b => ğ<br>
+      ;h => ḣ<br>
+      ;k => ḱ<br>
+      ;l => ĺ<br>
+      ;a => á<br>
+      ;z => ź<br>
+      ;c => ç<br>
+      ;v => ć<br>
+      ;e => é<br>
+      ;t => ṫ<br>
+      ;u => ü<br>
+      ;i => ı<br>
+      ;o => ö<br>
+      ;p => ṕ<br>
+      <br>
+      The above list works for upper case as well, e.g.:<br>
+      ;S => Ş
+    </p>
+
+  <h2>Touch Keyboard Layout</h2>
+  <div id='osk-phone' data-states='default shift numeric'>
