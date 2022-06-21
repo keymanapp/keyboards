bd00f325c01113c6e5d99ade51faa4afc60a9a32
miketvo <ikxplicitie@gmail.com>
2022-06-21 20:25:57 +0700

    Update extras/vn_kmn_compiler/shared/KeymanRule.py

--- release/v/vietnamese_telex/extras/vn_kmn_compiler/shared/KeymanRule.py
+++ release/v/vietnamese_telex/extras/vn_kmn_compiler/shared/KeymanRule.py
@@ -7,4 +7,4 @@ class KeymanRule:
         self.kmn_ologic = kmn_ologic  # Output logic
 
     def to_string(self):
-        return 'TelexRule: {\n\tbase: ' + self.base + '\n\tmodifier: ' + self.modifier + '\n\tresult: ' + self.result + '\n\tkmn_clogic: ' + str(self.kmn_clogic) + '\n\tkmn_ologic: ' + str(self.kmn_ologic)
+        return 'KeymanRule: {\n\tbase: ' + self.base + '\n\tmodifier: ' + self.modifier + '\n\tresult: ' + self.result + '\n\tkmn_clogic: ' + str(self.kmn_clogic) + '\n\tkmn_ologic: ' + str(self.kmn_ologic)
