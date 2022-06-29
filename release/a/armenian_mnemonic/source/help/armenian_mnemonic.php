3808ab441361bbc0adda872922be22cec02ca20a
Dotland <tisarukhan@gmail.com>
2022-06-29 12:25:41 +0400

    Add online help
    Add online help for the Armenian Mnemonic keyboard.

---
+++ release/a/armenian_mnemonic/source/help/armenian_mnemonic.php
@@ -0,0 +1,41 @@
+<?php 
+  $pagename = 'Armenian Mnemonic Keyboard Help';
+  $pagetitle = 'Armenian Mnemonic Keyboard Help';
+  
+  require_once('header.php');
+?>
+
+<p style='margin: 16px 0 0 0'>
+A font must be installed to support this keyboard. Windows includes a number of fonts which support the <b>Armenian</b> script.</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+
+<p>
+Visit <a href="https://github.com/dotland/mnemonic-kb-hy/blob/main/README.md">GitHub.com/dotland/mnemonic-kb-hy<a> for extensive documentation on using the keyboard.
+</p>
+
+
+<h2>Desktop Keyboard Layout</h2>
+
+<p>
+The following figures show the Armenian Mnemonic keyboard layout in various modifier key states.
+</p>
+
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<br />
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<p>
+The following figures show the Armenian Mnemonic keyboard layout for touch devices in various states.
+</p>
+
+<div id='osk-tablet' data-states='default shift capslock numeric numeric2'>
+</div>
+
+<p>© <a href="https://github.com/dotland">Dotland<a></p>
+
+</body>
+</html>
