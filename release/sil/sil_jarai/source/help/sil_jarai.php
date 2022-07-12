29b5d51225c61ba6f3aa7805faad876af114c3f8
MakaraSok <makara@keyman.com>
2022-07-12 17:20:38 +0700

    add caps layer where shift layer can be locked

--- release/sil/sil_jarai/source/help/sil_jarai.php
+++ release/sil/sil_jarai/source/help/sil_jarai.php
@@ -18,6 +18,11 @@
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
+
+<p>
+	To stay on the Shift layer, double tap on the Shift key.
+</p>
+
 <div id='osk-tablet' data-states='default shift alt'>
 </div>
 
