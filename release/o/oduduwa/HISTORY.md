6c446baeb06d57be1ba1775c0283080d6e102807
Vyshantha Simha <yshant@gmail.com>
2022-08-13 11:28:57 +0200

    Oduduwa Keyboard for Yoruba Community

---
+++ release/o/oduduwa/HISTORY.md
@@ -0,0 +1,6 @@
+oduduwa Change History
+====================
+
+0.1 (2022-07-09)
+----------------
+* Created by Vyshantha Simha (yshant@gmail.com)
---
+++ release/o/oduduwa/source/help/oduduwa.php
@@ -0,0 +1,40 @@
+<?php 
+  $pagename = 'Oduduwa Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+    The Oduduwa Alphabet also known as ÀÉBÁJÌOGBÈ is an earliest alphabet which has been in existence after the water-deluges (Ife-Ooyelagbo) of the 12th centuries, during the era of legendary Oduduwa the progenitor of Yoruba race.
+    <br/>
+    The legendary Oduduwa and his clan used the ÀÉBÁJÌOGBÈ alphabetic system in their communications. The 'talking alphabet', ÀÉBÁJÌOGBÈ is universally differentiated. When collectively pronounced, it goes in sentences thus; 
+    <br/>
+    <br/>
+        "if you are using it, you shall successively be ahead in your endeavors in life. Enshrine divinity within your actions as you accept responsibility for your thoughts and actions. God’s answer is the final approval on all supplications. You shall live long enough to fulfil your God ordained purpose for your life on earth". 
+    <br/>
+    <br/>
+    The alphabet gone into extermination over many years, but was divinely re-enacted back to this generation through Ifa Oracle consultations by late Chief Olókun Ìdòwú Awópẹtù, who was the Àwíṣẹ of Ìjèṣà land. The alphabet symbols and sound were found and re-enacted by Chief Tolulase Oguntosin Taye in 2011 and 2015 respectively.
+    <br/>
+    The alphabet was recognized as the ancient Yoruba alphabet by Late HRM Oba Sanni Yekini Adeleye Ajasa (Onikoyi Abesan the 5th), hence he named it ODUDUWA ALPHABET in 2016 and was approved by the then prince, currently the HRM Oba Sanni Ramane Aderemilekun Ajao the Alajase Onikoyi Abesan 6th. <br/>
+    The alphabet was noticed to be a talking alphabetic system by Princess Nafisat Adefoumilayo Olajide Sanni, the in-lining words were interpreted by her. <br/>
+    Other contributors to the developmental works of the alphabetic system included; HRM Oba Olawuni Bamijegbin Alao Oba Akire of Ile-Ife, Professor Félix Ayoh'Omidire - Director, institute of cultural studies (Ilé Irunmole), Obafemi Awolowo University, Ile-Ife, Nigeria, Dhikrullah Abdullah Adesina, Princess Tanimowo Ibukun Okusaga, prince Mutairou Adeyemi Alao Sanni, Quadri Ebenezer Oluwagbemiga and Engr Vyshantha Simha.  
+    <br/><br/>
+    The ÀÉBÁJÌOGBÈ keyboard application, when installed can help build your content on any device phone, tablet, laptop. You should install the Oduduwa Font before using the keyboard.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+Oduduwa Keyboard Layout is based of off Arabic (Egypt)-Arabic(101) or Arabic-PC Default Keyboard <br/>
+<h2>DEFAULT Layout</h2>
+<h3>Numeral Typed from Left to Right</h3>
+<img src='Layout_Default' alt='Default Oduduwa Keyboard Layout' /> <br/>
+
+Oduduwa Shift Keyboard Layout is based of off Arabic (Egypt)-Arabic(101) or Arabic-PC Shift Keyboard <br/>
+<h2>Shift Layout</h2>
+<h3>SHIFT Key Press Layout (for both Left & Right Shift Keys)</h3>
+<img src='Layout_Shift' alt='Shift Oduduwa Keyboard Layout' /> <br/>
+
+Oduduwa Keyboard Alt Layout is based of off Adlam  Default Keyboard : : Numerals Typed from Right to Left <br/>
+<h2>ALT or ALT-GR Layout</h2>
+<h3>ALT or ALT-GR Key Press Layout (for both Left & Right Alt or Alt-Gr Keys)</h3>
+<img src='Layout_Alt' alt='Alt Oduduwa Keyboard Layout' /> <br/>
