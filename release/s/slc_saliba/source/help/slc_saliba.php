30bd33aab57467e49f2a3a719eb73a47bf30ec51
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-09-22 10:16:05 +0100

    [slc_saliba] new keyboard

---
+++ release/s/slc_saliba/source/help/slc_saliba.php
@@ -0,0 +1,176 @@
+<?php
+  $pagename = 'Sáliba Keyboard Help';
+  $pagetitle = 'Sáliba Keyboard Help';
+  require_once('header.php');
+?>
+
+<nav>
+  <p lang="es"> <a href="#slc_saliba-es">Lee este documento en español.</a> </p>
+</nav>
+
+<a id="slc_saliba-en"></a>
+
+<p>The Sáliba keyboard allows you to type ã/ẽ/ĩ/õ/ũ/ñ on your keyboard.</p>
+
+<h1>Characters</h1>
+
+<p>In this keyboard <kbd>Ñ</kbd> becomes a “dead key” which means pressing
+it once will not type any letter. You must press the <kbd>Ñ</kbd> key, and
+then follow it up with a second key to type the letter you want. This is similar
+to the way that <kbd>&nbsp;&#x301;</kbd> is used to type an stress mark on
+vowels in Spanish.</p>
+
+<p>For keyboards that lack a <kbd>Ñ</kbd> key, you can use
+<kbd>Alt</kbd>+<kbd>N</kbd>.</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>To type&hellip;</th> <th>Press&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Ñ</kbd> (press <kbd>Ñ</kbd> twice) </td></tr>
+    <tr> <td>ã</td> <td> <kbd>Ñ</kbd>, then <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>Ñ</kbd>, then <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>Ñ</kbd>, then <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>Ñ</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>Ñ</kbd>, then <kbd>Space</kbd></td> </tr>
+  </tbody>
+</table>
+
+<h2>How to use <kbd>Alt</kbd>+<kbd>N</kbd></h2>
+
+<p>In this style, you must press <kbd>Alt</kbd> and <kbd>N</kbd> at the same
+time, let go, and then press the letter that the ~ will go
+over.</p>
+
+<p>Note: macOS users will press <kbd>⌥ Option</kbd> instead of <kbd>Alt</kbd>.</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>To type&hellip;</th> <th>Press&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>N</kbd></td> </tr>
+    <tr> <td>ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>N</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Space</kbd></td> </tr>
+  </tbody>
+</table>
+
+<h1>Keyboard Source</h1>
+
+<p>This keyboard is available online at <a href='https://keyman.com/keyboards/slc_saliba'>keyman.com/keyboards/slc_saliba</a></p>
+
+<hr>
+
+<a id="slc_saliba-es"></a>
+
+<h1 lang="es">Usando el teclado sáliba</h1>
+
+<p>El teclado sáliba te deja entrara las letras ã/ẽ/ĩ/õ/ũ/ñ en tu computadora.</p>
+
+<h1 lang="es">Teclas</h1>
+
+<p lang="es">Este teclado admite dos estilos de uso</p>
+
+<ol lang="es">
+  <li>estilo <kbd>Ñ</kbd> como “tecla muerta” (aconsejado por uso con el teclado latinoamericano)</li>
+  <li>estilo <kbd>Alt</kbd> + <kbd>,</kbd> (aconsejado por uso con teclados que sin tecla <kbd>Ñ</kbd>)</li>
+</ol>
+
+<h2 lang="es">Como usar el estilo <kbd>Ñ</kbd> “tecla muerta”</h2>
+
+<p lang="es"p>En este teclado, la tecla <kbd>Ñ</kbd> se convierta en “tecla muerta”.
+Presionando esta tecla no escribe una letra por sí mismo; hay que presionar
+la tecla <kbd>Ñ</kbd>, luego presionar otra tecla para obtener la letra que
+quieres. En esta manera, se parece a la tecla <kbd>&nbsp;&#x301;</kbd> para
+escribir el acento gráfico sobre vocales en español.</p>
+
+<p lang="es">Para teclados que no tienen un a tecla <kbd>Ñ</kbd>, puedes
+usar la combinación <kbd>Alt</kbd>+<kbd>N</kbd> en lugar de <kbd>Ñ</kbd></p>
+
+
+<table lang="es">
+  <thead>
+    <tr>
+      <th>Para escribir&hellip;</th> <th>Presionn&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Ñ</kbd> (presione <kbd>Ñ</kbd> dos veces) </td></tr>
+    <tr> <td>ã</td> <td> <kbd>Ñ</kbd>, luego <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>Ñ</kbd>, luego <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>Ñ</kbd>, luego <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>Ñ</kbd>, luego <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>Ñ</kbd>, luego <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Mayús</kbd>+<kbd>Ñ</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>Ñ</kbd>, luego <kbd>Mayús</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Mayús</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Mayús</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Mayús</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Mayús</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>Ñ</kbd>, luego <kbd>Espacio</kbd></td> </tr>
+  </tbody>
+</table>
+
+<h2 lang="es">Como usar <kbd>Alt</kbd>+<kbd>,</kbd></h2>
+
+<p lang="es">Si no tienes una tecla <kbd>Ñ</kbd>, hay que presionar las teclas
+<kbd>Alt</kbd> y <kbd>n</kbd> a la misma vez, suelta las teclas, y
+luego presionar la letra en que quieres que colocar la virgulilla (~)</p>
+
+<p lang="es">N. B. usuarios de macOS deben presionar <kbd>⌥ Opción</kbd> en lugar de <kbd>Alt</kbd>.</p>
+
+<table lang="es">
+  <thead>
+    <tr>
+      <th>Para escribir&hellip;</th> <th>Presione&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>N</kbd></td> </tr>
+    <tr> <td>ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>N</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Espacio</kbd></td> </tr>
+  </tbody>
+</table>
+
+<h1>Para descargar el teclado</h1>
+
+<p>Este teclado se puede descargar aquí: <a href='https://keyman.com/keyboards/slc_saliba'>keyman.com/keyboards/slc_saliba</a></p>
+
+<hr>
+
+<h1>Change History</h1>
+<p>1.0.0: Initial release.</p>
+
+<p>&copy; 2022 Jorge Emilio Rosés Labrada and Eddie Antonio Santos</p>
