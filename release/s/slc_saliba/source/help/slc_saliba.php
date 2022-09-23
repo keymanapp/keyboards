7c12163e7cbfe9d45338e288e5f0eb88bfd6f771
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-09-23 11:23:09 +0100

    [slc_saliba] use right alt instead

--- release/s/slc_saliba/source/help/slc_saliba.php
+++ release/s/slc_saliba/source/help/slc_saliba.php
@@ -10,18 +10,25 @@
 
 <a id="slc_saliba-en"></a>
 
-<p>The Sáliba keyboard allows you to type ã/ẽ/ĩ/õ/ũ/ñ on your keyboard.</p>
+<p>The Sáliba keyboard allows you to type ã/ẽ/ĩ/õ/ũ/ñ of the Saliban alphabet
+on your existing keyboard. All of the other letters used in Sáliba can already
+be typed on a typical keyboard (for example, on the Latin American keyboard
+included with most computers in Colombia), and those letters have the same
+keys on this keyboard layout.</p>
 
 <h1>Characters</h1>
 
 <p>In this keyboard <kbd>Ñ</kbd> becomes a “dead key” which means pressing
-it once will not type any letter. You must press the <kbd>Ñ</kbd> key, and
-then follow it up with a second key to type the letter you want. This is similar
-to the way that <kbd>&nbsp;&#x301;</kbd> is used to type an stress mark on
-vowels in Spanish.</p>
+it once will not insert any letter (as opposed to the Latin American keyboard,
+where pressing this key will insert the letter ñ); rather, <kbd>Ñ</kbd> You
+must press the <kbd>Ñ</kbd> is a placeholder, and it
+must be followed up with a second key to type one of the letters mentioned
+above. This is similar to the way that the accent key (´) is used to type an
+stress mark over vowels in Spanish.</p>
 
-<p>For keyboards that lack a <kbd>Ñ</kbd> key, you can use
-<kbd>Alt</kbd>+<kbd>N</kbd>.</p>
+<p>For keyboards that lack a dedicated <kbd>Ñ</kbd> key, you can use the
+combination <kbd>AltGr</kbd>+<kbd>N</kbd> (Windows/Linux) or
+<kbd>⌥ Option</kbd>+<kbd>N</kbd> (macOS).</p>
 
 <table>
   <thead>
@@ -30,7 +37,7 @@ vowels in Spanish.</p>
     </tr>
   </thead>
   <tbody>
-    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Ñ</kbd> (press <kbd>Ñ</kbd> twice) </td></tr>
+    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Ñ</kbd> (that is, press <kbd>Ñ</kbd> twice) </td></tr>
     <tr> <td>ã</td> <td> <kbd>Ñ</kbd>, then <kbd>A</kbd></td> </tr>
     <tr> <td>ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>E</kbd></td> </tr>
     <tr> <td>ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>I</kbd></td> </tr>
@@ -46,13 +53,15 @@ vowels in Spanish.</p>
   </tbody>
 </table>
 
-<h2>How to use <kbd>Alt</kbd>+<kbd>N</kbd></h2>
+<h2>How to use <kbd>AltGr</kbd>+<kbd>N</kbd></h2>
 
-<p>In this style, you must press <kbd>Alt</kbd> and <kbd>N</kbd> at the same
-time, let go, and then press the letter that the ~ will go
-over.</p>
+<p>If you do not have a dedicated <kbd>Ñ</kbd> key on your keyboard, you must
+press <kbd>AltGr</kbd> and <kbd>N</kbd> at the same time, let go of both keys,
+then press the letter that the ~ will go over.</p>
 
-<p>Note: macOS users will press <kbd>⌥ Option</kbd> instead of <kbd>Alt</kbd>.</p>
+<p>Note: <kbd>AltGr</kbd> is the <kbd>Alt</kbd> key to the right of the spacebar.</p>
+
+<p>Note: macOS users will press either <kbd>⌥ Option</kbd> key instead of <kbd>AltGr</kbd>.</p>
 
 <table>
   <thead>
@@ -61,19 +70,19 @@ over.</p>
     </tr>
   </thead>
   <tbody>
-    <tr> <td>ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>N</kbd></td> </tr>
-    <tr> <td>ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>A</kbd></td> </tr>
-    <tr> <td>ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>E</kbd></td> </tr>
-    <tr> <td>ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>I</kbd></td> </tr>
-    <tr> <td>õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>O</kbd></td> </tr>
-    <tr> <td>ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>U</kbd></td> </tr>
-    <tr> <td>Ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>N</kbd></td></tr>
-    <tr> <td>Ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
-    <tr> <td>Ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
-    <tr> <td>Ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
-    <tr> <td>Õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
-    <tr> <td>Ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
-    <tr> <td>~</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, then <kbd>Space</kbd></td> </tr>
+    <tr> <td>ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>N</kbd></td> </tr>
+    <tr> <td>ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>N</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Space</kbd></td> </tr>
   </tbody>
 </table>
 
@@ -81,33 +90,45 @@ over.</p>
 
 <p>This keyboard is available online at <a href='https://keyman.com/keyboards/slc_saliba'>keyman.com/keyboards/slc_saliba</a></p>
 
+<h1>Credits</h1>
+
+<p>The creation of this keyboard was supported by a SSHRC Insight Grant to
+Jorge Emilio Rosés Labrada [Award No. 435-2021-0468].</p>
+
 <hr>
 
 <a id="slc_saliba-es"></a>
 
 <h1 lang="es">Usando el teclado sáliba</h1>
 
-<p>El teclado sáliba te deja entrara las letras ã/ẽ/ĩ/õ/ũ/ñ en tu computadora.</p>
+<p lang="es">El teclado sáliba te permite escribir las letras ã/ẽ/ĩ/õ/ũ/ñ del
+alfabeto sáliba en tu computadora de una manera fácil. Todas las otras letras
+del alfabeto sáliba ya se pueden escribir con cualquier teclado (por ejemplo,
+el teclado del español que viene en la mayoría de las computadores en
+Colombia) y tienen la misma correspondencia en este teclado.</p>
 
 <h1 lang="es">Teclas</h1>
 
-<p lang="es"p>En este teclado, la tecla <kbd>Ñ</kbd> se convierta en “tecla muerta”.
-Presionando esta tecla no escribe una letra por sí mismo; hay que presionar
-la tecla <kbd>Ñ</kbd>, luego presionar otra tecla para obtener la letra que
-quieres. En esta manera, se parece a la tecla <kbd>&nbsp;&#x301;</kbd> para
-escribir el acento gráfico sobre vocales en español.</p>
+<p lang="es"p>En este teclado, la tecla <kbd>Ñ</kbd> se convierta en una
+“tecla muerta”.  Presionar esta tecla no nos permite escribe una letra como en
+otros teclados (por ejemplo, en el teclado de español donde esta tecla da la
+letra ñ) sino que se utiliza como un comodín y hay que presionar la tecla
+<kbd>Ñ</kbd>, luego presionar otra tecla para obtener un de las 6 letras
+mencionadas arriba.  En esta manera, se parece a la tecla del acento (´) en el
+teclado del español la cual se utiliza para escribir el acento gráfico sobre
+las vocales del español.</p>
 
 <p lang="es">Para teclados que no tienen un a tecla <kbd>Ñ</kbd>, puedes
-usar la combinación <kbd>Alt</kbd>+<kbd>N</kbd> en lugar de <kbd>Ñ</kbd>.</p>
+usar la combinación <kbd>AltGr</kbd>+<kbd>N</kbd> en lugar de <kbd>Ñ</kbd>.</p>
 
 <table lang="es">
   <thead>
     <tr>
-      <th>Para escribir&hellip;</th> <th>Presionn&hellip;</th>
+      <th>Para escribir&hellip;</th> <th>Presione&hellip;</th>
     </tr>
   </thead>
   <tbody>
-    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Ñ</kbd> (presione <kbd>Ñ</kbd> dos veces) </td></tr>
+    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, luego <kbd>Ñ</kbd> (es decir, presione <kbd>Ñ</kbd> dos veces) </td></tr>
     <tr> <td>ã</td> <td> <kbd>Ñ</kbd>, luego <kbd>A</kbd></td> </tr>
     <tr> <td>ẽ</td> <td> <kbd>Ñ</kbd>, luego <kbd>E</kbd></td> </tr>
     <tr> <td>ĩ</td> <td> <kbd>Ñ</kbd>, luego <kbd>I</kbd></td> </tr>
@@ -123,13 +144,18 @@ usar la combinación <kbd>Alt</kbd>+<kbd>N</kbd> en lugar de <kbd>Ñ</kbd>.</p>
   </tbody>
 </table>
 
-<h2 lang="es">Como usar <kbd>Alt</kbd>+<kbd>N</kbd></h2>
+<h2 lang="es">Como usar <kbd>AltGr</kbd>+<kbd>N</kbd></h2>
 
-<p lang="es">Si no tienes una tecla <kbd>Ñ</kbd>, hay que presionar las teclas
-<kbd>Alt</kbd> y <kbd>n</kbd> a la misma vez, suelta las teclas, y
-luego presionar la letra en que quieres que colocar la virgulilla (~).</p>
+<p lang="es">Si no tienes una tecla <kbd>Ñ</kbd> en tu teclado, hay que
+presionar las teclas <kbd>AltGr</kbd> y <kbd>N</kbd> a la misma vez, después
+soltar ambas teclas y luego presionar la letra en que quieres que colocar la
+virgulilla (~).</p>
 
-<p lang="es">N. B. usuarios de macOS deben presionar <kbd>⌥ Opción</kbd> en lugar de <kbd>Alt</kbd>.</p>
+<p lang="es">N. B. la <kbd>AltGr</kbd> es la tecla <kbd>Alt</kbd> al lado
+derecho de la barra espaciadora.</p>
+
+<p lang="es">N. B. usuarios de macOS deben presionar la tecla
+<kbd>⌥ Opción</kbd> en lugar de <kbd>AltGr</kbd>.</p>
 
 <table lang="es">
   <thead>
@@ -138,25 +164,31 @@ luego presionar la letra en que quieres que colocar la virgulilla (~).</p>
     </tr>
   </thead>
   <tbody>
-    <tr> <td>ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>N</kbd></td> </tr>
-    <tr> <td>ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>A</kbd></td> </tr>
-    <tr> <td>ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>E</kbd></td> </tr>
-    <tr> <td>ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>I</kbd></td> </tr>
-    <tr> <td>õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>O</kbd></td> </tr>
-    <tr> <td>ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>U</kbd></td> </tr>
-    <tr> <td>Ñ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>N</kbd></td></tr>
-    <tr> <td>Ã</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>A</kbd></td> </tr>
-    <tr> <td>Ẽ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>E</kbd></td> </tr>
-    <tr> <td>Ĩ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>I</kbd></td> </tr>
-    <tr> <td>Õ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>O</kbd></td> </tr>
-    <tr> <td>Ũ</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>U</kbd></td> </tr>
-    <tr> <td>~</td> <td> <kbd>Alt</kbd>+<kbd>N</kbd>, luego <kbd>Espacio</kbd></td> </tr>
+    <tr> <td>ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>N</kbd></td> </tr>
+    <tr> <td>ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>N</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Mayús</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, luego <kbd>Espacio</kbd></td> </tr>
   </tbody>
 </table>
 
-<h1>Para descargar el teclado</h1>
+<h1 lang="es">Para descargar el teclado</h1>
 
-<p>Este teclado se puede descargar aquí: <a href='https://keyman.com/keyboards/slc_saliba'>keyman.com/keyboards/slc_saliba</a></p>
+<p lang="es">Este teclado se puede descargar aquí: <a href="https://keyman.com/keyboards/slc_saliba">keyman.com/keyboards/slc_saliba</a></p>
+
+<h1 lang="es">Crédito</h1>
+
+<p lang="es">La creación de este teclado fue posible gracias al apoyo
+financiero de un proyecto SSHRC Insight Grant de Jorge Emilio Rosés Labrada
+[Proyecto No. 435-2021-0468]</p>
 
 <hr>
 
