543222c31f3a4111f8040700820a4a1eb671e4ef
Atabek <atabekmur@gmail.com>
2022-09-25 14:56:43 +1000

    [karakalpak] Initial submission of karakalpak keyboards

---
+++ release/k/karakalpak_cyrillic/source/help/karakalpak_cyrillic.php
@@ -0,0 +1,73 @@
+<?php 
+  $pagename = 'Karakalpak Cyrillic Keyboard Help';
+  $pagetitle = 'Karakalpak Cyrillic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for Cyrillic script of Karakalpak Language.
+</p>
+
+<br/>
+
+<h2>Desktop Keyboard Layout</h2>
+
+<p>This keyboard utilizes a deadkey on the backslash key (\). To get the desired character, press the deadkey
+    and the character to the next shown in Key combination row in the below table. 
+</p>
+<p>For example, if you want to get letter 'ә', you need to first type '\', then 'а'.</p>
+
+<p>To access the "deadkey" itself (\), type a space after it, or double tap it.</p>
+
+<table style="border: 1px solid;">
+    <tr>
+        <th style="border: 1px solid;  text-align: center;">&nbsp;&nbsp;Key combination&nbsp;&nbsp;</th>
+        <th style="border: 1px solid;  text-align: center;">&nbsp;&nbsp;Desired character&nbsp;&nbsp;</th>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + а</td>
+        <td style="border: 1px solid;  text-align: center;">ә</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + г</td>
+        <td style="border: 1px solid;  text-align: center;">ғ</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + к</td>
+        <td style="border: 1px solid;  text-align: center;">қ</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + н</td>
+        <td style="border: 1px solid;  text-align: center;">ң</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + о</td>
+        <td style="border: 1px solid;  text-align: center;">ө</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + у</td>
+        <td style="border: 1px solid;  text-align: center;">ў</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + и</td>
+        <td style="border: 1px solid;  text-align: center;">ү</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + х</td>
+        <td style="border: 1px solid;  text-align: center;">ҳ</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid;  text-align: center;">\ + [space]</td>
+        <td style="border: 1px solid;  text-align: center;">\</td>
+    </tr>
+</table>
+<br/>
+
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift numeric'>
+</div>
