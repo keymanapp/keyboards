f3e492d3b3522828ce7ede9b26a8471d0af1ee0e
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-09-26 09:43:47 +0100

    [slc_saliba]: swap English/Spanish in documentation

--- release/s/slc_saliba/source/help/slc_saliba.php
+++ release/s/slc_saliba/source/help/slc_saliba.php
@@ -5,97 +5,8 @@
 ?>
 
 <nav>
-  <p lang="es"> <a href="#slc_saliba-es">Lee este documento en español.</a> </p>
+  <p> <a href="#slc_saliba-en">Jump to the English version</a> </p>
 </nav>
-
-<a id="slc_saliba-en"></a>
-
-<p>The Sáliba keyboard allows you to type ã/ẽ/ĩ/õ/ũ/ñ of the Saliban alphabet
-on your existing keyboard. All of the other letters used in Sáliba can already
-be typed on a typical keyboard (for example, on the Latin American keyboard
-included with most computers in Colombia), and those letters have the same
-keys on this keyboard layout.</p>
-
-<h1>Characters</h1>
-
-<p>In this keyboard <kbd>Ñ</kbd> becomes a “dead key” which means pressing
-it once will not insert any letter (as opposed to the Latin American keyboard,
-where pressing this key will insert the letter ñ); rather, <kbd>Ñ</kbd> You
-must press the <kbd>Ñ</kbd> is a placeholder, and it
-must be followed up with a second key to type one of the letters mentioned
-above. This is similar to the way that the accent key (´) is used to type an
-stress mark over vowels in Spanish.</p>
-
-<p>For keyboards that lack a dedicated <kbd>Ñ</kbd> key, you can use the
-combination <kbd>AltGr</kbd>+<kbd>N</kbd> (Windows/Linux) or
-<kbd>⌥ Option</kbd>+<kbd>N</kbd> (macOS).</p>
-
-<table>
-  <thead>
-    <tr>
-      <th>To type&hellip;</th> <th>Press&hellip;</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Ñ</kbd> (that is, press <kbd>Ñ</kbd> twice) </td></tr>
-    <tr> <td>ã</td> <td> <kbd>Ñ</kbd>, then <kbd>A</kbd></td> </tr>
-    <tr> <td>ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>E</kbd></td> </tr>
-    <tr> <td>ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>I</kbd></td> </tr>
-    <tr> <td>õ</td> <td> <kbd>Ñ</kbd>, then <kbd>O</kbd></td> </tr>
-    <tr> <td>ũ</td> <td> <kbd>Ñ</kbd>, then <kbd>U</kbd></td> </tr>
-    <tr> <td>Ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>Ñ</kbd></td></tr>
-    <tr> <td>Ã</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
-    <tr> <td>Ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
-    <tr> <td>Ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
-    <tr> <td>Õ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
-    <tr> <td>Ũ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
-    <tr> <td>~</td> <td> <kbd>Ñ</kbd>, then <kbd>Space</kbd></td> </tr>
-  </tbody>
-</table>
-
-<h2>How to use <kbd>AltGr</kbd>+<kbd>N</kbd></h2>
-
-<p>If you do not have a dedicated <kbd>Ñ</kbd> key on your keyboard, you must
-press <kbd>AltGr</kbd> and <kbd>N</kbd> at the same time, let go of both keys,
-then press the letter that the ~ will go over.</p>
-
-<p>Note: <kbd>AltGr</kbd> is the <kbd>Alt</kbd> key to the right of the spacebar.</p>
-
-<p>Note: macOS users will press either <kbd>⌥ Option</kbd> key instead of <kbd>AltGr</kbd>.</p>
-
-<table>
-  <thead>
-    <tr>
-      <th>To type&hellip;</th> <th>Press&hellip;</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr> <td>ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>N</kbd></td> </tr>
-    <tr> <td>ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>A</kbd></td> </tr>
-    <tr> <td>ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>E</kbd></td> </tr>
-    <tr> <td>ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>I</kbd></td> </tr>
-    <tr> <td>õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>O</kbd></td> </tr>
-    <tr> <td>ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>U</kbd></td> </tr>
-    <tr> <td>Ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>N</kbd></td></tr>
-    <tr> <td>Ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
-    <tr> <td>Ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
-    <tr> <td>Ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
-    <tr> <td>Õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
-    <tr> <td>Ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
-    <tr> <td>~</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Space</kbd></td> </tr>
-  </tbody>
-</table>
-
-<h1>Keyboard Source</h1>
-
-<p>This keyboard is available online at <a href='https://keyman.com/keyboards/slc_saliba'>keyman.com/keyboards/slc_saliba</a></p>
-
-<h1>Credits</h1>
-
-<p>The creation of this keyboard was supported by a SSHRC Insight Grant to
-Jorge Emilio Rosés Labrada [Award No. 435-2021-0468].</p>
-
-<hr>
 
 <a id="slc_saliba-es"></a>
 
@@ -189,6 +100,95 @@ derecho de la barra espaciadora.</p>
 <p lang="es">La creación de este teclado fue posible gracias al apoyo
 financiero de un proyecto SSHRC Insight Grant de Jorge Emilio Rosés Labrada
 [Proyecto No. 435-2021-0468]</p>
+
+<hr>
+
+<a id="slc_saliba-en"></a>
+
+<p>The Sáliba keyboard allows you to type ã/ẽ/ĩ/õ/ũ/ñ of the Saliban alphabet
+on your existing keyboard. All of the other letters used in Sáliba can already
+be typed on a typical keyboard (for example, on the Latin American keyboard
+included with most computers in Colombia), and those letters have the same
+keys on this keyboard layout.</p>
+
+<h1>Characters</h1>
+
+<p>In this keyboard <kbd>Ñ</kbd> becomes a “dead key” which means pressing
+it once will not insert any letter (as opposed to the Latin American keyboard,
+where pressing this key will insert the letter ñ); rather, <kbd>Ñ</kbd> You
+must press the <kbd>Ñ</kbd> is a placeholder, and it
+must be followed up with a second key to type one of the letters mentioned
+above. This is similar to the way that the accent key (´) is used to type an
+stress mark over vowels in Spanish.</p>
+
+<p>For keyboards that lack a dedicated <kbd>Ñ</kbd> key, you can use the
+combination <kbd>AltGr</kbd>+<kbd>N</kbd> (Windows/Linux) or
+<kbd>⌥ Option</kbd>+<kbd>N</kbd> (macOS).</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>To type&hellip;</th> <th>Press&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Ñ</kbd> (that is, press <kbd>Ñ</kbd> twice) </td></tr>
+    <tr> <td>ã</td> <td> <kbd>Ñ</kbd>, then <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>Ñ</kbd>, then <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>Ñ</kbd>, then <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>Ñ</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>Ñ</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>Ñ</kbd>, then <kbd>Space</kbd></td> </tr>
+  </tbody>
+</table>
+
+<h2>How to use <kbd>AltGr</kbd>+<kbd>N</kbd></h2>
+
+<p>If you do not have a dedicated <kbd>Ñ</kbd> key on your keyboard, you must
+press <kbd>AltGr</kbd> and <kbd>N</kbd> at the same time, let go of both keys,
+then press the letter that the ~ will go over.</p>
+
+<p>Note: <kbd>AltGr</kbd> is the <kbd>Alt</kbd> key to the right of the spacebar.</p>
+
+<p>Note: macOS users will press either <kbd>⌥ Option</kbd> key instead of <kbd>AltGr</kbd>.</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>To type&hellip;</th> <th>Press&hellip;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr> <td>ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>N</kbd></td> </tr>
+    <tr> <td>ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>A</kbd></td> </tr>
+    <tr> <td>ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>E</kbd></td> </tr>
+    <tr> <td>ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>I</kbd></td> </tr>
+    <tr> <td>õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>O</kbd></td> </tr>
+    <tr> <td>ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>U</kbd></td> </tr>
+    <tr> <td>Ñ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>N</kbd></td></tr>
+    <tr> <td>Ã</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>A</kbd></td> </tr>
+    <tr> <td>Ẽ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>E</kbd></td> </tr>
+    <tr> <td>Ĩ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>I</kbd></td> </tr>
+    <tr> <td>Õ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>O</kbd></td> </tr>
+    <tr> <td>Ũ</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Shift</kbd>+<kbd>U</kbd></td> </tr>
+    <tr> <td>~</td> <td> <kbd>AltGr</kbd>+<kbd>N</kbd>, then <kbd>Space</kbd></td> </tr>
+  </tbody>
+</table>
+
+<h1>Keyboard Source</h1>
+
+<p>This keyboard is available online at <a href='https://keyman.com/keyboards/slc_saliba'>keyman.com/keyboards/slc_saliba</a></p>
+
+<h1>Credits</h1>
+
+<p>The creation of this keyboard was supported by a SSHRC Insight Grant to
+Jorge Emilio Rosés Labrada [Award No. 435-2021-0468].</p>
 
 <hr>
 
