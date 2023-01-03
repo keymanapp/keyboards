ac1937ea787ed36b079377010ff418d5f3d6b42e
gichegemi <117494817+gichegemi@users.noreply.github.com>
2023-01-02 21:06:00 -0600

    Add files via upload

---
+++ release/o/otoe_missouria/source/help/otoe_missouria.php
@@ -0,0 +1,24 @@
+
+&lt;?phpbani
+$pagename = 'Otoe-Missouria Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?&gt;
+
+&lt;p&gt;
+Otoe-Missouria keyboard based on basic keyboard.
+&lt;/p&gt;
+
+&lt;h1&gt;Keyboard Layout&lt;/h1&gt;
+
+&lt;h2&gt;Desktop Keyboard Layout&lt;/h2&gt;
+&lt;div id='osk' data-states='default shift'&gt;
+&lt;/div&gt;
+
+&lt;h2&gt;Mobile Keyboard Layout&lt;/h2&gt;
+&lt;p&gt;The key to add the stress accent should be located next to the a on the left side.&lt;/p&gt;
+&lt;div id='osk-phone' data-states='default shift numeric'&gt;
+&lt;/div&gt;
+
+
