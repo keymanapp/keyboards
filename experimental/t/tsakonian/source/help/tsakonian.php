45369e65ab364daad2b44ba6624c48abe895a9b8
Lorna Evans <LornaSIL@users.noreply.github.com>
2023-01-17 13:02:18 -0600

    Update tsakonian.php
    Fix up formatting a bit.

--- experimental/t/tsakonian/source/help/tsakonian.php
+++ experimental/t/tsakonian/source/help/tsakonian.php
@@ -3,14 +3,13 @@
 	$pagetitle = $pagename ;
 	require_once( 'header.php' ) ;
 ?>
-<body>
-<h1>Tsakonian Keyboard Help for Keyman</h1>
 
-<p><h2>Ελληνική</h2>
-Το τσακώνικο πληκτρολόγιο έχει σχεδιαστεί για την πληκτρολόγηση της τσακώνικης γλώσσας. Περιλαμβάνει επίσης διάφορους πρόσθετους ελληνικούς χαρακτήρες και σύμβολα για την ευκολία χρήσης των Αρχαίων Ελληνικών.<br>Περιλαμβάνει ένα πληκτρολόγιο οθόνης, το οποίο μπορείτε να δείτε κάνοντας κλικ στο εικονίδιο Keyman και επιλέγοντας το στοιχείο μενού On Screen Keyboard. Παρόμοια πληκτρολόγια είναι επίσης διαθέσιμα για άλλες πλατφόρμες γραφείου και κινητών τηλεφώνων.
-<p>
+<h2>Ελληνική</h2>
+<p>Το τσακώνικο πληκτρολόγιο έχει σχεδιαστεί για την πληκτρολόγηση της τσακώνικης γλώσσας. Περιλαμβάνει επίσης διάφορους πρόσθετους ελληνικούς χαρακτήρες και σύμβολα για την ευκολία χρήσης των Αρχαίων Ελληνικών.<br>Περιλαμβάνει ένα πληκτρολόγιο οθόνης, το οποίο μπορείτε να δείτε κάνοντας κλικ στο εικονίδιο Keyman και επιλέγοντας το στοιχείο μενού On Screen Keyboard. Παρόμοια πληκτρολόγια είναι επίσης διαθέσιμα για άλλες πλατφόρμες γραφείου και κινητών τηλεφώνων.</p>
+
 <h2>English</h2>
-The Tsakonian keyboard is designed for typing the Tsakonian language. It includes additional Greek symbols and characters for convenience.<br>It includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. Similar keyboards are also available for other desktop and mobile platforms.
+<p>
+The Tsakonian keyboard is designed for typing the Tsakonian language. It includes additional Greek symbols and characters for convenience.<br>It includes an on screen keyboard which can be viewed by clicking on the Keyman icon and selecting the On Screen Keyboard menu item. Similar keyboards are also available for other desktop and mobile platforms.</p>
 <br>
 	
 <h2>Tablet/Mobile Layout</h2>
@@ -24,11 +23,11 @@ The Tsakonian keyboard is designed for typing the Tsakonian language. It include
 
 <h3>Alt+Shift</h3>
 <p><img src="tsakonian-layout4-altshift-mobile.jpg" alt=Alt & Shift></p>
-<p>
+
 	
 <h2>Desktop Layout</h2>
 (Only look at the main black character, not the grey in the corner.)
-<p><img src="tsakonian-layout1.jpg" alt=layout>
+<p><img src="tsakonian-layout1.jpg" alt=layout></p>
 
 <h3>Shifted</h3>
 <p><img src="tsakonian-layout2-shift.jpg" alt=shift></p>
