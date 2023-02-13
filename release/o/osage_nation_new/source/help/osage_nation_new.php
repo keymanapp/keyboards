0ea8dcd6c0e8eba8b0ae7e02793620ad465de002
Lorna Evans <LornaSIL@users.noreply.github.com>
2023-02-13 09:50:11 -0600

    [osage_nation] make old keyboard classic and add reformed keyboard

---
+++ release/o/osage_nation_new/source/help/osage_nation_new.php
@@ -0,0 +1,24 @@
+<?php
+  $pagename = 'Osage Keyboard Help';
+  $pagetitle = 'Osage Keyboard Help';
+  require_once('header.php');
+?>
+<p>This keyboard layout was developed on behalf of the Osage Nation Language Department 
+with the intention of replacing previous layouts (partially for reasons pertaining 
+to orthography reforms that have been carried since the last Osage keyboard was developed)
+</p>
+<h2>Font</h2>
+<p>
+The keyboard comes with the Pawhuska font. This and other Osage fonts are available from <a href="http://osagelanguagetools.appspot.com/downloads/" target=_blank>here</a>. 
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
