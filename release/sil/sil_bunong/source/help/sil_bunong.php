a76ce899d31829ea6da715d307b2a2061f3451b7
MakaraSok <makara@keyman.com>
2023-04-28 17:03:41 +0700

    updated html and php help documentations

--- release/sil/sil_bunong/source/help/sil_bunong.php
+++ release/sil/sil_bunong/source/help/sil_bunong.php
@@ -5,59 +5,92 @@
   require_once('header.php');
 ?>
 
+<h1>នតុមត្រ៝ង់​ កាដារនទើល៝ត់ «Bunong ឞូន៝ង (SIL)»</h1>
 <h1>Start Using Bunong ឞូន៝ង (SIL) Keyboard</h1>
 
 <p style="text-align: justify;">
+កាដារនទើ​ល៝ត់អ្យា ញជេងគៃមាញចីសរាឆ៝តានាវងើយឞូន៝ង អីទ៝ង់រាឆ៝ទ្រោន។
+</p>
+<p style="text-align: justify;">
 This keyboard is designed for typing Bunong language using Khmer characters.</p>
 
+<p style="text-align: justify;">
+នតីលញចីស «Khmer Mondulkiri» គែសលាប់តាត្រោម កាដារនទើល៝ត់​ «Bunong ឞូន៝ង (SIL)»។  នោកឞ៝ន់ងលាប់ មោលាសរាហ្វាត់ កាដារនទើល៝ត់​ «Bunong ឞូន៝ង (SIL)»  គែសងលាប់​ មោលាសគែសរាហ្វាត់ នតីលញចីស «Khmer Mondulkiri» នទ្រែលដ៝ង់។ 
+</p>
 <p style="text-align: justify;">
 Khmer Mondulkiri font is included in the keyboard package. It will be installed and uninstalled along with the keyboard.
 </p>
 
 <p style="text-align: justify;">
+ទឹងលែៈរាង្លៃរាង្លាប់តាត្រោមតោរាឆ៝ប់គែសញ៝ម់នអឹរ គៃមាកើលអ៝ន់ឞ៝ន់ទើសមាឆៃរាង្លៃរាង្លាប់។
+</p>
+<p style="text-align: justify;">
 All vowels on the phone layout are highlighted to help you locate where the Bunong vowels are.
 </p>
 
+<p style="text-align: justify;">
+អ្វាញ់តាត្រោមតារាងនាវល៝ត់​រាខាច់ឞើសទ៝ង់អ្យាគៃមាទើសជ៝យ លាសកើយ្សរាឆ៝អេងៗតឹងនទើល៝ត់៖ ​
+</p>
 <p style="text-align: justify;">
 Look in the following table of Rotation (Rota) rules to see the character options/choices for each key:
 </p>
 
 <table>
-<thead><tr><th>Rotation</th><th>Phone/Tablet Key</th><th>Computer Key</th></tr></thead><tbody>
- <tr><td>ឹ > ឺ</td><td><kbd>ឹ</kbd></td><td><kbd>W</kbd></td></tr>
- <tr><td>េ > ែ > ៃ </td><td><kbd>េ</kbd></td><td><kbd>E</kbd></td></tr>
- <tr><td>ុ > ូ > ួ </td><td><kbd>ុ</kbd></td><td><kbd>U</kbd></td></tr>
- <tr><td>ិ > ី </td><td><kbd>ិ</kbd></td><td><kbd>I</kbd></td></tr>
- <tr><td>ោ > ៅ </td><td><kbd>ោ</kbd></td><td><kbd>O</kbd></td></tr>
- <tr><td>ៀ > ឿ </td><td><kbd>ៀ</kbd></td><td><kbd>[</kbd></td></tr>
- <tr><td>៖ > : </td><td><kbd>៖</kbd></td><td><kbd>]</kbd></td></tr>
- <tr><td>\ > / > |</td><td><kbd>\</kbd></td><td><kbd>\</kbd></td></tr>
- <tr><td>" > ' </td><td><kbd>"</kbd></td><td><kbd>Shift</kbd> <kbd>'</kbd></td></tr>
- <tr><td>« > " > '</td><td><kbd>«</kbd></td><td><kbd>`</kbd></td></tr>
- <tr><td>» > " > '</td><td><kbd>»</kbd></td><td><kbd>Shift</kbd> <kbd>`</kbd></td></tr>
- <tr><td>- > — </td><td><kbd>-</kbd></td><td><kbd>-</kbd></td></tr>
- <tr><td>( > [ > { </td><td><kbd>(</kbd></td><td><kbd>Shift</kbd> <kbd>9</kbd></td></tr>
- <tr><td>) > ] > } </td><td><kbd>)</kbd></td><td><kbd>Shift</kbd> <kbd>0</kbd></td></tr>
- <tr><td>។ > ៕ > ! > ? > ៗ </td><td><kbd>។</kbd></td><td><kbd>.</kbd></td></tr>
- <tr><td>ZWSP > ⎵ </td><td><kbd>Spacebar</kbd></td><td><kbd>Spacebar</kbd></td></tr>
+<thead><tr><th>នាវរាខាច់ <br/> ការផ្លាស់ប្ដូរ <br/> Rotation</th><th>តា​តោរាឆ៝ប់​ល៝ត់ <br/> នៅលើទូរសព្ទឆ្លាតវៃ  <br/> On Smart Phone</th><th>តា​តោរាឆ៝ប់ល៝ត់រាច៝ក់ក្វ៝ង់ <br/> នៅលើថេប្លេត <br/> On Tablet</th><th>តា​តោរាឆ៝ប់ល៝ត់រាច៝ក់ក្វ៝ង់ <br/> នៅលើកុំព្យូទ័រ<br/> On Computer</th></tr></thead><tbody> 
+ <tr><td>ឹ > ឺ</td><td style="color:white; background-color:black"><kbd>ឹ</kbd></td><td style="color:white; background-color:black"><kbd>ឹ</kbd></td><td><kbd>W</kbd></td></tr>
+ <tr><td>េ > ែ > ៃ </td><td style="color:white; background-color:black"><kbd>េ</kbd></td><td style="color:white; background-color:black"><kbd>េ</kbd></td><td><kbd>E</kbd></td></tr>
+ <tr><td>ុ > ូ > ួ </td><td><kbd>ុ</kbd></td><td><kbd>ុ</kbd></td><td><kbd>U</kbd></td></tr>
+ <tr><td>ិ > ី </td><td style="color:white; background-color:black"><kbd>ិ</kbd></td><td style="color:white; background-color:black"><kbd>ិ</kbd></td><td><kbd>I</kbd></td></tr>
+ <tr><td>ោ > ៅ </td><td><kbd>ោ</kbd></td><td style="color:white; background-color:black"><kbd>ោ</kbd></td><td><kbd>O</kbd></td></tr>
+ <tr><td>ៀ > ឿ </td><td style="color:white; background-color:black"><kbd>ៀ</kbd></td><td style="color:white; background-color:black"><kbd>ៀ</kbd></td><td><kbd>[</kbd></td></tr>
+ <tr><td>៖ > : </td><td><kbd>៖</kbd></td><td><kbd>៖</kbd></td><td><kbd>]</kbd></td></tr>
+ <tr><td>\ > / > |</td><td><kbd>\</kbd></td><td><kbd>\</kbd></td><td><kbd>\</kbd></td></tr>
+ <tr><td>" > ' </td><td><kbd>"</kbd></td><td><kbd>"</kbd></td><td><kbd>Shift</kbd> <kbd>'</kbd></td></tr>
+ <tr><td> « > ‹ </td><td><kbd>«</kbd></td><td><kbd>«</kbd></td><td><kbd>`</kbd></td></tr>
+ <tr><td> » > › </td><td><kbd>»</kbd></td><td><kbd>»</kbd></td><td><kbd>Shift</kbd> <kbd>`</kbd></td></tr>
+ <tr><td>- > — </td><td><kbd>-</kbd></td><td><kbd>-</kbd></td><td><kbd>-</kbd></td></tr>
+ <tr><td>( > [ > { </td><td><kbd>(</kbd></td><td><kbd>(</kbd></td><td><kbd>Shift</kbd> <kbd>9</kbd></td></tr>
+ <tr><td>) > ] > } </td><td><kbd>)</kbd></td><td><kbd>)</kbd></td><td><kbd>Shift</kbd> <kbd>0</kbd></td></tr>
+ <tr><td>។ > ៕ > ! > ? > ៗ </td><td><kbd>។</kbd></td><td><kbd>។</kbd></td><td><kbd>.</kbd></td></tr>
+ <tr><td>ZWSP > ⎵ </td><td><kbd>Spacebar</kbd></td><td><kbd>Spacebar</kbd></td><td><kbd>Spacebar</kbd></td></tr>
+ <tr><td>់ > ៈ</td><td><kbd>់</kbd></td><td><kbd>់</kbd></td><td><kbd>'</kbd></td></tr>
 </tbody></table>
 
 <p style="text-align: justify;">
+នាវមឞើសមានទើល៝ត់រាខាច់ងគ្លែៈទុត ល៝ត់ <kbd>Spacebar</kbd> ទូត៝ង់គៃមាគិតទូនាវងើយ ល៝ត់ ២ត៝ង់គៃមា​អ្វាញ់ឆៃងខាស​ងាយ។ គៃមាតឹមងខាសជេសតេៈរាញាងកេងល៝ត់ <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>Spacebar</kbd>។
+</p>
+<p style="text-align: justify;">
+សេចក្ដីពន្យល់សម្រាប់គ្រាប់ចុចដកឃ្លា៖ ចុច <kbd>Spacebar</kbd> ម្ដងដើម្បីសម្គាល់ចុងពាក្យ [ដំណកឃ្លាមើលមិនឃើញ (ZWSP)] ហើយចុចពីរដងដើម្បីបានដំណកឃ្លាធម្មតា។ ដើម្បីដកឃ្លាស្ដើង សូមចុច <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>Spacebar</kbd>។
+</p>
+<p style="text-align: justify;">
 Explanation of the final Rota rule: Press <kbd>Spacebar</kbd> once to mark the end of a word [invisible space (ZWSP)] and twice for a visible space.
 </p>
+
+<h1>កាដារនទើញចីស</h1>
+<h1>ប្លង់ក្ដារចុច</h1>
 <h1>Keyboard Layout</h1>
 
+  <h2>កុមព្យូតើ</h2>
+  <h2>កុំព្យូទ័រ</h2>
   <h2>Desktop</h2>
    <div id='osk' data-states='default shift'></div>
-   
+  
+  <h2>តោរាឆ៝ប់ </h2>
+  <h2>ទូរសព្ទ</h2>
   <h2>Phone</h2>
    <div id='osk-phone' data-states='default shift'></div>
+   <h3>ល៝ត់ជ៝ៈ</h3>
+   <h3>ចុចឱ្យជាប់</h3>
    <h3>Longpress</h3>
-	<p>Keys with a little dot on the top right can be pressed and held for more keys. Note also these keys can be pressed on again and again to cycle through characters shown in the longpress. See the rota rules above.</p>
+	<p style="text-align: justify;">នទើល៝ត់​អីគែសរាព្លៃជេៈៗឞើសលើមពែសឞើសមា ទើយ​ល៝ត់គៃ​មាអ្វាញ់រាឆ៝​អេងៗជិត នោកឞ៝ន់ល៝ត់ជ៝ៈ។</p>
+	<p style="text-align: justify;">ចុចឱ្យជាប់លើគ្រាប់ចុចដែលមានចំណុចតូចមួយនៅខាងលើផ្នែកខាងស្ដាំ ដើម្បីបង្ហាញគ្រាប់ចុចច្រើនទៀត។</p>
+	<p>Keys with a little dot on the top right can be pressed and held for more keys. </p>
 	<p><a href="phone_longpress.png"><img class="keyboard" src="phone_longpress.png" alt="Longpress state" /></a></p>
 	
-     <h2>Tablet</h2>
+  <h2>តោរាឆ៝ប់រាលាតក្វ៝ង់</h2>
+  <h2>ថេប្លេត</h2>
+  <h2>Tablet</h2>
    <div id='osk-tablet' data-states='default shift'></div>
 
 
-<p>(c) 2021 - 2022 SIL International</p>
+<p>(c) 2021 - 2023 SIL International</p>
