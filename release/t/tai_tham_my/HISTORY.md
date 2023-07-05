240a1a228ce5d8b7721a60b78d55e1b6401c03f1
MakaraSok <makara@keyman.com>
2023-07-05 16:22:32 +0700

    added help documentation in Tai Tham

--- release/t/tai_tham_my/HISTORY.md
+++ release/t/tai_tham_my/HISTORY.md
@@ -1,9 +1,12 @@
 Tai Tham MY Change History
 ====================
 
+1.2 (2023-07-05)
+* Add help documentation in Tai Tham language
+
 1.1 (2023-06-02)
 * Fix to "e" key
-* 
+
 1.0 (2022-06-08)
 ----------------
 * Created by Wyn Owen
--- release/t/tai_tham_my/source/help/tai_tham_my.php
+++ release/t/tai_tham_my/source/help/tai_tham_my.php
@@ -5,68 +5,54 @@
   require_once('header.php');
 ?>
 
-	<p style="text-align: justify;">
-	The Tai Tham MY keyboard was developed for typing Tai Lue language in Tai Tham script. The keyboard is available for computers as well as mobile devices.</p>
+<div id="tab-content2" class="content" style="font-family:'A Tai Tham KH New V3';">
+		
+	<h1>Start Using Tai Tham MY Keyboard</h1>
+		<p style="text-align: justify;">
+		The Tai Tham MY keyboard was developed for typing Tai Lue language in Tai Tham script. The keyboard is available for computers as well as mobile devices.</p>
 
-<h2>Keyboard Layouts</h2>
+	<h2>Keyboard Layouts for Computer</h2>
+		<p>There are 3 layouts in the keyboard map: “Default” layout; “Shift” layout and “Right Alt” layout.</p>
+	<h3>Computer - Default</h3>
+		<p>The Default layout is shown below.</p>
+		<ol>
+			<li>To type the character you want, press the appropriate key. For example, <kbd>A</kbd> gives “ᨼ”; <kbd>B</kbd> gives “ᩥ” etc.</li>
+			<li>The vowels ᩮ ᩯ ᩱ ᩰ and the medial consonant  ᩕ are typed after the desired consonant. For example, ᨠ + ᩱ gives ᨠᩱ </li>
+			<li>The ᩠ character has a special function: it allows you to create a subjoined character. Having typed the ᩠ character, just type the character you want to be subjoined. For example, ᨴ +ᩱ + ᩠ + ᨿ gives ᨴᩱ᩠ᨿ  </li>
+		</ol>
+		<p><a href="desktop_layout_default.png"><img width="90%" class="keyboard" src="desktop_layout_default.png" alt="Default (unshifted) state" /></a></p>
 
-<h3>Computer - Unshifted [Same as 'Default']</h3>
-	<p>There are 3 parts to the keyboard map: “Normal” part; “Shift” part and “Right Alt” part. The Normal part is shown below. To type the character you want, press the appropriate key.</[>
-	<p><a href="desktop_layout_default.png"><img width="90%" class="keyboard" src="desktop_layout_default.png" alt="Default (unshifted) state" /></a></p>
+	<h3>Computer - Shift</h3>
+		<p>The Shift part is shown below. To type the character you want, press <kbd>Shift</kbd> + the appropriate key (at the same time). For example, <kbd>Shift</kbd> + <kbd>A</kbd> gives ᩀ.</p>
+		<p><a href="desktop_layout_shift.png"><img width="90%" class="keyboard" src="desktop_layout_shift.png" alt="Shift state" /></a></p>
 
-<h3>Computer - Shifted</h3>
-	<p>The Shift part is shown below. To type the character you want, press SHIFT + the appropriate key (at the same time).</p>
-	<p><a href="desktop_layout_shift.png"><img width="90%" class="keyboard" src="desktop_layout_shift.png" alt="Shift state" /></a></p>
-
-<h3>Computer - Right Alt</h3>
-	<p>The Right Alt part is shown below. To type the character you want, press RIGHT ALT + the appropriate key (at the same time).</p>
-	<p><a href="desktop_layout_altgr.png"><img width="90%" class="keyboard" src="desktop_layout_altgr.png" alt="AltGr state" /></a></p>
-
-	<p lang="en-US" style="margin-bottom: 0in; line-height: 100%">The <font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6752;</font>
-	</span></font>character (found in the Normal part) has a special
-	function: it allows you to create a subjoined character. Having typed
-	the <font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6752;</font>
-	</span></font>character, just type the character you want to be
-	subjoined. For example,<span lang="en-GB"> </span><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6725;
-	</font></span></font><font face="A Tai Tham KH New V3">+</font><font face="A Tai Tham KH New V3"><span lang="en-GB">
-	</span></font><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6752;</font><font face="A Tai Tham KH New V3">
-	</font></span></font><font face="A Tai Tham KH New V3">+</font><font face="A Tai Tham KH New V3"><span lang="en-GB">
-	</span></font><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6710;
-	</font></span></font><font face="A Tai Tham KH New V3">+</font><font face="A Tai Tham KH New V3"><span lang="en-GB">
-	</span></font><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6754;</font>
-	</span></font>gives<span lang="en-GB"> </span><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6725;&#6752;&#6710;&#6754;</font>
-	 </span></font>
-	</p>
+	<h3>Computer - Right Alt</h3>
+		<p>The Right Alt part is shown below. To type the character you want, press Right <kbd>Alt</kbd> + the appropriate key (at the same time). For example, Right <kbd>Alt</kbd> + <kbd>1</kbd> gives ᪑.</p>
+		<p><a href="desktop_layout_altgr.png"><img width="90%" class="keyboard" src="desktop_layout_altgr.png" alt="AltGr state" /></a></p>
 
 
-<h3>Tablet - Default</h3>
-	<p>There are 3 parts to the keyboard map: “Normal” part; “Shift” part and “Right Alt” part. The Normal part is shown below. To type the character you want, press the appropriate key.</p>
-	<p><a href="tablet_layout_default.png"><img width="60%" class="keyboard" src="tablet_layout_default.png" alt="Default state" /></a></p>
-
-<h3>Tablet - Shift</h3>
-	<p>The Shift part is shown below. To type the character you want, press SHIFT + the appropriate key (at the same time).</p>
-	<p><a href="tablet_layout_shift.png"><img width="60%" class="keyboard" src="tablet_layout_shift.png" alt="Shift state" /></a></p>
-
-<h3>Tablet - Right Alt</h3>
-	<p>The Right Alt part is shown below. To type the character you want, press RIGHT ALT + the appropriate key (at the same time).</p>
-	<p><a href="tablet_layout_altgr.png"><img width="60%" class="keyboard" src="tablet_layout_altgr.png" alt="AltGr state" /></a></p>
-
-	<p lang="en-US" style="margin-bottom: 0in; line-height: 100%">The <font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6752;</font>
-	</span></font>character (found in the Normal part) has a special
-	function: it allows you to create a subjoined character. Having typed
-	the <font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6752;</font>
-	</span></font>character, just type the character you want to be
-	subjoined. For example,<span lang="en-GB"> </span><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6725;
-	</font></span></font><font face="A Tai Tham KH New V3">+</font><font face="A Tai Tham KH New V3"><span lang="en-GB">
-	</span></font><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6752;</font><font face="A Tai Tham KH New V3">
-	</font></span></font><font face="A Tai Tham KH New V3">+</font><font face="A Tai Tham KH New V3"><span lang="en-GB">
-	</span></font><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6710;
-	</font></span></font><font face="A Tai Tham KH New V3">+</font><font face="A Tai Tham KH New V3"><span lang="en-GB">
-	</span></font><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6754;</font>
-	</span></font>gives<span lang="en-GB"> </span><font face="Arial"><span lang="hi-IN"><font face="A Tai Tham KH New V3">&#6725;&#6752;&#6710;&#6754;</font><font face="Liberation Serif, serif">
-	 </font></span></font>
-	</p>
+	<h2>Keyboard Layouts for Mobile Devices</h2>
+		<p>There are 3 layouts in the keyboard map: “Default” layout; “Shift” layout and “Right Alt” layout.</p>
 	
-	<p>For the PDF version, click <a href="tai_tham_my_keyboard_map_en.pdf">here</a>.</p>
+	<h3>Phone/Tablet - Default</h3>
+		<p>The Default layout is shown below.</p>
+		<ol>
+			<li>To type the character you want, press the appropriate key. For example, <kbd>A</kbd> gives “ᨼ”; <kbd>B</kbd> gives “ᩥ” etc.</li>
+			<li>The vowels ᩮ ᩯ ᩱ ᩰ and the medial consonant  ᩕ are typed after the desired consonant. For example, ᨠ + ᩱ gives ᨠᩱ </li>
+			<li>The ᩠ character has a special function: it allows you to create a subjoined character. Having typed the ᩠ character, just type the character you want to be subjoined. For example, ᨴ +ᩱ + ᩠ + ᨿ gives ᨴᩱ᩠ᨿ  </li>
+		</ol>
+	
+		<p><a href="tablet_layout_default.png"><img width="60%" class="keyboard" src="tablet_layout_default.png" alt="Default state" /></a></p>
 
-	<p>© 2023 Tai Lue Literacy Committee</p>
+	<h3>Phone/Tablet - Shift</h3>
+		<p>The Shift part is shown below. To type the character you want, press <kbd>Shift</kbd> + the appropriate key (at the same time). For example, <kbd>Shift</kbd> + <kbd>A</kbd> gives ᩀ.</p>
+		<p><a href="tablet_layout_shift.png"><img width="60%" class="keyboard" src="tablet_layout_shift.png" alt="Shift state" /></a></p>
+
+	<h3>Tablet - Right Alt</h3>
+		<p>The Right Alt part is shown below. To type the character you want, press Right <kbd>Alt</kbd> + the appropriate key (at the same time). For example, RIGHT <kbd>Alt</kbd> + <kbd>1</kbd> gives ᪑.</p>
+		<p><a href="tablet_layout_altgr.png"><img width="60%" class="keyboard" src="tablet_layout_altgr.png" alt="AltGr state" /></a></p>
+		
+		<p>For the PDF version, in English, click <a href="tai_tham_my_en.pdf">here</a> and, in Tai Tham, click <a href="tai_tham_my_en.pdf">here</a>.</p>
+
+		<p>© 2023 Tai Lue Literacy Committee</p>
+</div>
