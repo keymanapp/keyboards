54c19137f32e01f2594243819caee31651c80388
Lorna Evans <LornaSIL@users.noreply.github.com>
2023-07-31 16:32:20 -0500

    [el_dari_clra] Update to release and deprecate dari_clra

---
+++ release/el/el_dari_clra/source/help/el_dari_clra.php
@@ -0,0 +1,20 @@
+<?php 
+  $pagename = 'Dari (Farsi - CLRA) Keyboard Help';
+  $pagetitle = 'Dari (Farsi - CLRA) Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p>
+This keyboard is designed for use with the <a href="https://en.wikipedia.org/wiki/Dari"><b>Dari</b></a> language of Afghanistan and other regions in South Asia.  It uses <b>standard Windows fonts</b>.
+</p>
+<br/>
+
+<h2>Keyboard Layout</h2>
+<div id='osk'>
+</div>
+
+
