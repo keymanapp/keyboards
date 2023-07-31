b327a99e43971017bf1bdaf05bd778cee5c3d42a
HopsAndHops <meryl.bishop@gmail.com>
2023-07-31 11:07:02 -0700

    updated touch layout table

--- release/fv/fv_nsilxcen/source/help/fv_nsilxcen.php
+++ release/fv/fv_nsilxcen/source/help/fv_nsilxcen.php
@@ -44,9 +44,9 @@ This keyboard is designed for the <b>Nsilxcən</b> language of the BC Interior r
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
 <div class='vspace'></div>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>q q̓</td><td  align='center'>w w̓</td><td  align='center'>ə ə́</td><td  align='center'>r r̓</td><td  align='center'>t t̓</td><td  align='center'>y y̓</td><td  align='center'>u ú</td><td  align='center'>i í</td><td  align='center'>ƛ̓</td><td  align='center'>p p̓</td></tr>
-<tr ><td  align='center'>a á</td><td  align='center'>s</td><td  align='center'>ʔ</td><th  align='center'>´</th><td  align='center'>ɣ ɣ̓</td><td  align='center'>h ḥ</td><td  align='center'>ʷ</td><td  align='center'>k k̓</td><td  align='center'>l l̓</td><td  align='center'>ł</td></tr>
-<tr ><td >&nbsp;</td><td  align='center'>ˇ</td><td  align='center'>x x̌ x̣</td><td  align='center'>c c̓</td><th  align='center'>’</th><td  align='center'>ʕ ʕ̓</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
+<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>q q̓</td><td  align='center'>w w̓</td><td  align='center'>ə ə́ e é</td><td  align='center'>r r̓</td><td  align='center'>t t̓</td><td  align='center'>y y̓</td><td  align='center'>u ú o ó</td><td  align='center'>i í</td><td  align='center'>ƛ̓</td><td  align='center'>p p̓</td></tr>
+<tr ><td  align='center'>a á</td><td  align='center'>s š</td><td  align='center'>ʔ</td><th  align='center'>´</th><td  align='center'>ɣ ɣ̓</td><td  align='center'>h ḥ</td><td  align='center'>ʷ</td><td  align='center'>k k̓</td><td  align='center'>l l̓</td><td  align='center'>ɬ</td></tr>
+<tr ><td >&nbsp;</td><td  align='center'>ˇ</td><td  align='center'>x x̌ x̣</td><td  align='center'>c c̓ č</td><th  align='center'>’</th><td  align='center'>ʕ ʕ̓</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
 </table>
 <div class='vspace'></div><ul><li>keys with a red background are combining accents.
 </li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
