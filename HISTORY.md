a3b2c059abaee624afee5d968c409af9b4c333d6
ᚱᚢᚾ᛫ᛗᚫᚾ <115298797+Runeboard@users.noreply.github.com>
2023-08-08 03:45:32 +0800

    Elfdalian
    I'm uploading this on behalf of someone else. If there are any problems please let me know and I'll fill him in. Thanks.

---
+++ HISTORY.md
@@ -0,0 +1,6 @@
+eldalian_words Change History
+====================
+
+1.0 (2022-01-16)
+----------------
+* Created by Craig Cornelius
---
+++ craig_cornelius.ovd-latn.eldalian_words.model.ts
@@ -0,0 +1,14 @@
+/*
+  eldalian_words 1.0 generated from template.
+  
+  This is a minimal lexical model source that uses a tab delimited wordlist.
+  See documentation online at https://help.keyman.com/developer/ for
+  additional parameters.
+*/
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['../../elfdahlian_words.tsv'],
+};
+export default source;
