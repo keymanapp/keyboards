9b1607d0e2c1964c21a5fa729032986fd0ab6f44
Lorna Evans <LornaSIL@users.noreply.github.com>
2023-10-11 11:05:07 -0500

    [gaahmg] Update keyboard to remove macron and limit tilde

--- experimental/g/gaahmg/source/help/gaahmg.php
+++ experimental/g/gaahmg/source/help/gaahmg.php
@@ -8,6 +8,10 @@
   Keyman keyboard for the Gəəmg language of Sudan. This keyboard produces NFD (decomposed) characters.
 </p>
 
+<p>
+  The keys surrounded by square brackets, such as [ˋ] will output the appropriate combining mark (in this case a grave accent) after a vowel. The combining marks can only be positioned on vowels. However, in the Gəəmg orthography, the "o" vowel does not occur with a tilde or a caron.
+</p>
+
 <h2>Desktop Keyboard layout</h2>
 
 <div id='osk' data-states='default shift rightalt rightalt-shift'>
