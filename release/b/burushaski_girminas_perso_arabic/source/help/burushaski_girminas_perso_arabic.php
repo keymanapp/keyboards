74ca7f5ef0ad8a0841c5a2a6625e3f7a80ed12eb
quwatsunny <48961603+quwatsunny@users.noreply.github.com>
2023-10-24 21:28:39 -0700

    updated
    updated

---
+++ release/b/burushaski_girminas_perso_arabic/source/help/burushaski_girminas_perso_arabic.php
@@ -0,0 +1,18 @@
+<?php 
+  $pagename = 'Burushaski Girminas (بروشسکی) - Perso Arabic Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Burushaski Perso Arabic Keyboard Help</title>
+  </head>
+  <body> <img src="image_keyboard_guide.png" alt="Keyboard Perso Arabic Guide"
+
+      title="Keyboard Perso Arabic Guide"><br>
+    <p style="text-align: left;"><a href="burushaski_perso_arabic_guide.pdf" title="PDF Guide">Download
+        PDF Guide</a><br>
+    </p>
+  </body>
