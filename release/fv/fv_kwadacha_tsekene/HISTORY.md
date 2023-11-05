ebab6fa80175bd6884aee4981ce2fd3965318d32
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2023-11-05 15:33:43 -0800

    building mobile and desktop keyboard

--- release/fv/fv_kwadacha_tsekene/HISTORY.md
+++ release/fv/fv_kwadacha_tsekene/HISTORY.md
@@ -1,6 +1,6 @@
 fv_kwadacha_tsekene Change History
 ====================
 
-1.0 (2 Nov 2023)
+1.0 (5 Nov 2023)
 ----------------
-* Created desktop and mobile layouts
+* Created mobile and desktop layout
---
+++ release/fv/fv_kwadacha_tsekene/source/help/fv_kwadacha_tsekene.php
@@ -0,0 +1,47 @@
+<?php 
+  $pagename = 'Kwadacha Tsek’ene Keyboard Help';
+  $pagetitle = 'Kwadacha Tsek’ene Keyboard Help';
+  $pagestyle = <<<END
+  
+#wikitext table { margin: 12px; }
+#wikitext table td { border: solid 1px #888888 !important }
+#wikitext table th { border: solid 1px #888888 !important; background: #ffcccc }
+.lang2 {font-family:"Aboriginal Sans"}  
+END;
+
+  require_once('header.php');
+?>
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the <b>Kwadacha Tsek’ene/b> language of the BC Interior region of Canada.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+
+<img src="KwadachaTsekeneU_.png" alt="" height="198" width="514" border="0" /></h2>
+					<h2>Keyboard Layout </h2>
+					<li>The special Characters of Kwadacha Tsek’ene have been assigned to various punctuation keys</li>
+						<li>All accents are typed after the base character: <strong>à</strong> is typed <strong>a</strong> then <strong>backquote `</strong></li>
+            <li> To type the low tone accent when it appears over a single letter use the  <strong> slash key / </strong><br/>
+                <strong>à</strong> is typed <strong>a</strong> then <strong> / </strong></li>
+            <li> To type the high tone accent when it appears over a single letter use the <strong> backquote key `</strong><br/>
+                <strong>í</strong> is typed <strong>i</strong> then <strong>`</strong></li>
+						<li>To type the nasal accent when it appears under a single letter use the <strong>left bracket key [</strong><br/>
+							  <strong>ǫ</strong> is typed <strong>o</strong> then <strong>[</strong></li>
+						<li>To type	the combined low tone and nasal accents over a single letter use the <strong> right bracket key ]</strong><br/>
+                <strong>ǫ̀</strong> is typed <strong>o</strong> then <strong>]</strong></li>
+            <li> To type the combined high tone and nasal accents over a single letter use the <strong> backslash key \</strong><br/>
+                <strong>ǫ́</strong> is typed <strong>o</strong> then <strong>\</strong></li>
+            <li>The combined low tone or high tone and nasal accents can also be typed by typing the accents individually in either order</li>
+            <li> To type any of the accents over double vowels, type the double vowels followed by the key for desired accents <br/>
+                <strong>ǫ́ǫ́</strong> is typed <strong>oo</strong> then <strong>\</strong></li>
+						<li>All changed punctuation can type their original value by holding down the Right-Alt or Option key. To get the original value of the left-bracket [ type Right-Alt+Left-Bracket (Windows) Option+Left-Bracket (Mac).</li>
+						<ul></li>
+    </ul>
+
+<br/>
+
+<h2>Keyboard Layout</h2>
+<div id='osk'>
+</div>
+<div id='osk-tablet' data-states='default shift numeric'></div>
