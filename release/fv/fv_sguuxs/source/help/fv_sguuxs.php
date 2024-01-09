badacebce6b9fef611896d0a38eb12ae785a2db9
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-01-09 14:11:03 -0800

    Building documentation, help

---
+++ release/fv/fv_sguuxs/source/help/fv_sguuxs.php
@@ -0,0 +1,51 @@
+<?php 
+  $pagename = 'SGUUXS Keyboard Help';
+  $pagetitle = 'SGUUXS Keyboard Help';
+  $pagestyle = <<<END
+  
+#wikitext table { margin: 12px; }
+#wikitext table td { border: solid 1px #888888 !important }
+#wikitext table th { border: solid 1px #888888 !important; background: #ffcccc }
+.lang2 {font-family:"Aboriginal Sans"}  
+END;
+
+  require_once('header.php');
+?>
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the <b>SGUUXS</b> language of the BC Coast region of Canada.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+
+<img src="Nuucaanul.png" alt="Nuucanul" width="514" height="198" /></h2>
+					 
+<ul>
+				<li>The special characters of SGUUXS have been assigned to punctuation keys.</li>
+                  <li>All accents are typed after the base character: a̱ is typed a then Hyphen - </li>
+                  <li>To type the low line use the Hyphen Key — ḵ is typed k then Hyphen - </li>
+                  <li>To type the diaeresis accent use the Left-Bracket Key [ - ü is typed u then Left-Bracket [ </li>
+                      <li> üü can be typed uu then Left-Bracket [ </li>
+                  <li>All changed punctuation can type their original value by holding down the Right-Alt or Option key. The Left-Bracket [ is typed Right-Alt+Left-Bracket (Windows) Option+Left-Bracket (Mac).</li>
+								</ul>
+							 </ul>
+    
+<h2>Notes on touch layout</h2>
+<div id='wikitext'>
+<div class='vspace'></div>
+<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>? !</td><td  align='center'>w ẅ</td><td  align='center'>e</td><th  align='center'>¨</th><td  align='center'>t</td><td  align='center'>y</td><td  align='center'>u ü</td><td  align='center'>i</td><td  align='center'>o</td><td  align='center'>p</td></tr>
+<tr ><td  align='center'>a a̱</td><td  align='center'>s</td><td  align='center'>d</td><td  align='center'>' "</td><td  align='center'>g g̱</td><td  align='center'>h</td><td  align='center'>k ḵ</td><td  align='center'>l</td><td  align='center'>ł</td></tr>
+<tr ><td >&nbsp;</td><td  align='center'>z</td><td  align='center'>x</td><td  align='center'>c</td><td  align='center'>_</td><td  align='center'>b</td><td  align='center'>n</td><td  align='center'>m</td><td  colspan='2'>&nbsp;</td></tr>
+</table>
+<div class='vspace'></div><ul><li>keys with a red background are combining accents.
+</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
+</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
+<ul><li>SGUUXS
+</li></ul>
+</div>
+
+<br/>
+
+<h2>Keyboard Layout</h2>
+<div id='osk'>
+</div>
