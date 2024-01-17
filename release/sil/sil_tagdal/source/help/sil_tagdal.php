470dd5980f040adb5bce20666b38196212d3dfcb
Darcy Wong <darcy_wong@sil.org>
2024-01-17 08:46:29 +0700

    add touch screenshots

--- release/sil/sil_tagdal/source/help/sil_tagdal.php
+++ release/sil/sil_tagdal/source/help/sil_tagdal.php
@@ -3,7 +3,14 @@
   $pagetitle = $pagename;
   require_once('header.php');
 ?>
+
 <p>Tagdal keyboard with Latin script and some IPA characters accessible on the Right-Alt layer.</p>
+
+Key features:
+    <ol>
+      <li>Press ] to add combining dot below</li>
+      <li>Access some IPA charactes with AltGr (Right Alt)</li>
+    </ol>
 
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift rightalt'>
@@ -13,3 +20,5 @@
 <div id='osk-phone' data-states='default shift rightalt numeric'>
 </div>
 
+<h2>Touch - Longpress key</h2>
+  <p>Any key with a little gray dot on the top right can be pressed and held to reveal more key(s).</p>
