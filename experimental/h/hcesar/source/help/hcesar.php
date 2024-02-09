8f3efa35979226770360d75ccbc619bbb0794521
dariomatheuz <159530313+dariomatheuz@users.noreply.github.com>
2024-02-09 10:17:38 +0000

    Update hcesar.php

--- experimental/h/hcesar/source/help/hcesar.php
+++ experimental/h/hcesar/source/help/hcesar.php
@@ -1,5 +1,5 @@
 <?php 
-  $pagename = 'Hcesar (portugal) Keyboard Help';
+  $pagename = 'Hcesar (Portugal) Keyboard Help';
   $pagetitle = $pagename;
   // Header we will tidy up later  
   require_once('header.php');
@@ -7,7 +7,7 @@
 
 <p>
     A disposição do teclado hcesar, também chamado "teclado nacional", foi utilizado por máquinas de escrever vendidas em Portugal desde 1937 até aos anos 80. Foi concebido para colocar o máximo
-chaves normalmente utilizadas no meio do layout.</p>
+de teclas normalmente utilizadas no meio do layout.</p>
 <p>The hcesar keyboard layout, also called "teclado nacional," was used by typewriters sold in Portugal from 1937 until the 1980s. It was designed to put the most
 commonly used keys in the middle of the layout. 
 </p>
