eaea8d14e53eaeb60e93ee58d09649fe2bdd82ae
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-02-20 16:07:07 -0800

    Updated shift key
    Updated documentation

--- release/fv/fv_southern_carrier/source/help/fv_southern_carrier.php
+++ release/fv/fv_southern_carrier/source/help/fv_southern_carrier.php
@@ -12,45 +12,65 @@ END;
   require_once('header.php');
 ?>
 
-<p style='margin: 16px 0 0 0'>
-This keyboard is designed for the <b>ᑐᑊᘁᗕᑋᗸ</b> language of the BC Interior region of Canada.
-</p>
-<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
-</p>
 
 
-<h2>Notes on touch layout</h2>
-<div id='wikitext'>
-<div class='vspace'></div><div class='dakelh' >
-<div class='vspace'></div><h3>Unshifted</h3>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>ᐊ</td><td  align='center'>ᐸ</td><td  align='center'>ᗉ</td><td  align='center'>ᗕ</td><td  align='center'>ᑕ</td><td  align='center'>ᗭ</td><td  align='center'>ᗴ</td><td  align='center'>ᘇ</td><td  align='center'>ᘍ</td><td  align='center'>ᘓ</th></td>
-<tr ><td  align='center'>ᘧ</td><td  align='center'>ᘭ</td><td  align='center'>ᘳ</td><td  align='center'>ᘹ</td><td  align='center'>ᘿ</td><td  align='center'>ᙅ</td><td  align='center'>ᙍ</td><td  align='center'>ᙓ</td><td  align='center'>ᙦ</td><td  align='center'></td></tr>
-<tr ><td  align='center'>⇧</td><th  align='center'>|</th><td  align='center'></td><td  align='center'></td><td  align='center'></td><td  align='center'></td><td  align='center'></td><td  align='center'></td><th  align='center'>〈</th><td align='center'>⌫</td></tr>
-<tr ><td  align='center'>#</td><td  align='center'>lang</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
-</table>
-<div class='vspace'></div><h3>Shifted</h3>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>ᑋ</td><td  align='center'>ᐥ</td><td  align='center'>ᐪ</td><td  align='center'>ᗮ</td><td  align='center'>ᐟ</td><td  align='center'>ᐠ</td><td  align='center'>ᘁ</td><td  align='center'>ᐣ</td><td  align='center'>ᓑ</td><td  align='center'>ᒼ</td></tr>
-<tr ><td  align='center'></td><td  align='center'>ᑊ</td><td  align='center'>ᒡ</td><td  align='center'>ᐦ</td><td  align='center'>ᙆ</td><td  align='center'>ᔆ</td><td  align='center'>ᙚ</td><td  align='center'>r</td><td  align='center'>+</td><td  align='center'></td></tr>
-<tr ><td  align='center'>⇧</td><td  align='center'></td><td  align='center'></td><td  align='center'>ᶣ</td><td  align='center'>ᗮᗮ</td><td  align='center'>ᘁᗮ</td><td  align='center'>ᙇᙚ</td><td  align='center'></td><td  align='center'></td><td  align='center'>⌫</td></tr>
-<tr ><td  align='center'>#</td><td  align='center'>lang</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
-</table>
-<div class='vspace'></div><ul><li>keys with a red background are combining accents.
-</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li><li> The keys present on the unshifted layer show many of the syllables in their 'a' orientation. Selecting the key will bring up the other forms:
-<div class='vspace'></div><h3>Vowels</h3>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><th  align='center'>ᐊ</th><td  align='center'>ᐸ</td><td  align='center'>ᗉ</td><td  align='center'>ᗕ</td><td  align='center'>ᑕ</td><td  align='center'>ᗭ</td><td  align='center'>ᗴ</td><td  align='center'>ᘇ</td><td  align='center'>ᘍ</td><td  align='center'>ᘓ</th></td>
-<tr ><td  align='center'>ᘧ</td><td  align='center'>ᘭ</td><td  align='center'>ᘳ</td><td  align='center'>ᘹ</td><td  align='center'>ᘿ</td><td  align='center'>ᙅ</td><td  align='center'>ᙍ</td><td  align='center'>ᙓ</td><td  align='center'>ᙦ</td><td  align='center'></td></tr>
-<tr ><td  align='center'>⇧</td><th  align='center'>|</th><th  align='center'>ᐊ</h><th  align='center'>ᐅ</th><th  align='center'>ᐈ</th><th  align='center'>ᐉ</th><th  align='center'>ᐃ</th><th  align='center'>ᐁ</th><th  align='center'>〈</th><td align='center'>⌫</td></tr>
-<tr ><td  align='center'>#</td><td  align='center'>lang</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
-</table>  
-</li><li>The | and 〈 keys modify the keys shown on the default keyboard to their related forms:
-<ul><li>ᑕ + | = ᗡ
-</li><li>ᗉ + | = ᗏ
-</li><li>ᑕ + 〈 = ᗧ
-</li><li>ᙦ + 〈 = ᙬ
-</li><li>Your device’s font may not differentiate fully between syllabic “s” ᔆ and “sh” ᙚ
-</li></ul></div>
-<div class='vspace'></div>
-</div>
+<li>The default layer shows most of the base syllablics in their "a" orientation.</li>
+        <ul>
+            <li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+            <li>Tap on a syllable to show other orientations: a u e i o oo: </li>
+                <ul>
+                    <li>Tapping on the ᐊ will show all of the vowel orientations, which can then be selected.</li>
+                
+                    <li><div class='vspace'>
+                        <table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><th  align='center'>ᐊ</th><td  align='center'>ᐸ</td><td  align='center'>ᗉ</td><td  align='center'>ᗕ</td><td  align='center'>ᑕ</td><td  align='center'>ᗭ</td><td  align='center'>ᗴ</td><td  align='center'>ᘇ</td><td  align='center'>ᘍ</td><td  align='center'>ᘓ</th></td>
+                        <tr ><td  align='center'>ᘧ</td><td  align='center'>ᘭ</td><td  align='center'>ᘳ</td><td  align='center'>ᘹ</td><td  align='center'>ᘿ</td><td  align='center'>ᙅ</td><td  align='center'>ᙍ</td><td  align='center'>ᙓ</td><td  align='center'>ᙦ</td><td  align='center'></td></tr>
+                        <tr ><td  align='center'>ᑋᘁᙚ</td><th  align='center'>|</th><th  align='center'>ᐊ</h><th  align='center'>ᐅ</th><th  align='center'>ᐈ</th><th  align='center'>ᐉ</th><th  align='center'>ᐃ</th><th  align='center'>ᐁ</th><th  align='center'>〈</th><td align='center'>⌫</td></tr>
+                        <tr ><td  align='center'>#</td><td  align='center'>lang</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
+                        </table> </li>
+                    <li>The initial a form of a syllable that is typed when tapping on the base key will be replaced by the selected orientation.</li> 
+                    <li>If there is an associated final, it will appear above the Backspace key when the "a" orientation of a syllabic is selected.</li>
+                        <ul>
+                            <li><b>Note : </b>the full set of finals can always be accessed by tapping the <b>ᑋᘁᙚ</b> key.</li>
+                        </ul>
+                </ul>
+        </ul>
+        
+    <li>The <b>|</b> and <b>〈</b> keys are modifying keys. Tapping on either key will show other related forms of the syllabics shown on the default layer.:</li>
+        <ul> 
+            <li><div class='vspace'> <h4>|</h4></div>
+                <table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><th  align='center'> </th><td  align='center'> </td><td  align='center'>ᗏ</td><td  align='center'>ᗛ</td><td  align='center'>ᗡ</td><td  align='center' width="15px"> </td><td  align='center'>ᗺ</td><td  align='center'> </td><td  align='center'> </td><td  align='center'>ᘛ</th></td>
+                    <tr ><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td> <td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'>ᙙ</td><td  align='center'>ᙠ</td><td  align='center'></td></tr>
+                    <tr ><td  align='center'>ᑋᘁᙚ</td><th  align='center'>|</th><th  align='center'> </h><th  align='center'> </th><th  align='center'> </th><th  align='center'> </th><th  align='center'> </th><th  align='center'> </th><th  align='center'>〈</th><td align='center'>⌫</td></tr>
+                    <tr ><td  align='center'>#</td><td  align='center'>🌐</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
+                    </table> </li>
+        </ul>
+        <ul>
+            <li><div class='vspace'> <h4>〈</h4></div>
+                <table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><th  align='center'> </th><td  align='center'> </td><td  align='center' width="15px"> </td><td  align='center' width="15px"> </td><td  align='center'>ᗧ</td><td  align='center' width="15px"> </td><td  align='center'>ᘀ</td><td  align='center'width="15px"> </td><td  align='center'> </td><td  align='center'>ᘡ</th></td>
+                    <tr ><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'> </td><td  align='center'>ᙬ</td><td  align='center'></td></tr>
+                    <tr ><td  align='center'>ᑋᘁᙚ</td><th  align='center'>|</th><th  align='center'> </h><th  align='center'> </th><th  align='center'> </th><th  align='center'> </th><th  align='center'> </th><th  align='center'> </th><th  align='center'>〈</th><td align='center'>⌫</td></tr>
+                    <tr ><td  align='center'>#</td><td  align='center'>🌐</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
+                    </table> </li> 
+        </ul>        
 
-<br/>
+            
+    
+    <div class='vspace'></div><h3>Syllable Finals (ᑋᘁᙚ)</h3>
+    <table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>ᑋ</td><td  align='center'>ᐥ</td><td  align='center'>ᐪ</td><td  align='center'>ᗮ</td><td  align='center'>ᐟ</td><td  align='center'>ᐠ</td><td  align='center'>ᘁ</td><td  align='center'>ᐣ</td><td  align='center'>ᓑ</td><td  align='center'>ᒼ</td></tr>
+    <tr ><td  align='center'></td><td  align='center'>ᑊ</td><td  align='center'>ᒡ</td><td  align='center'>ᐦ</td><td  align='center'>ᙆ</td><td  align='center'>ᔆ</td><td  align='center'>ᙚ</td><td  align='center'>r</td><td  align='center'>+</td><td  align='center'></td></tr>
+    <tr ><td  align='center'>ᑋᘁᙚ</td><td  align='center'></td><td  align='center'></td><td  align='center'>ᶣ</td><td  align='center'>ᗮᗮ</td><td  align='center'>ᘁᗮ</td><td  align='center'>ᙇᙚ</td><td  align='center'></td><td  align='center'></td><td  align='center'>⌫</td></tr>
+    <tr ><td  align='center'>#</td><td  align='center'>🌐</td><td  align='center' colspan='6'>space</td><td align='center'>. ᐧ ᕯ</td><td  align='center'>⏎</td></tr>
+    </table>
+    <div class='vspace'></div>
+    <ul>
+        <li>You can return to the default layer by tapping on either <b>|</b> or <b>〈</b> </li>
+    </ul>
+
+
+    </li><li>Your device’s font may not differentiate fully between syllabic “s” ᔆ and “sh” ᙚ
+    </li></ul></div>
+    <div class='vspace'></div>
+    </div>
+    
+    <br/>
+    
