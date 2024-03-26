c9106c1d413606c4f83991f7144bcfe9a489e874
mauriu <161212377+mauriu@users.noreply.github.com>
2024-03-26 03:53:18 +0000

    encoding disclaimer

--- experimental/p/paleohispanic/source/help/paleohispanic.php
+++ experimental/p/paleohispanic/source/help/paleohispanic.php
@@ -7,6 +7,10 @@
 
 <p>This keyboard is designed for typing the Iberian language, using the Northern Paleohispanic script.</p>
 
+<p>Iberian was a pre-roman language spoken in the Iberian Peninsula region of Spain and Portugal from before the 7th to 1rd century BC.</p>
+
+<p>Please note that this keyboard (and associated font) is using codepoints that are not officially accepted into The Unicode Standard. The Northern Paleohispanic script has not yet been accepted for encoding into the standard. It is currently on the Unicode roadmap, but these codepoints can, and probably will, change. Once the script is accepted into Unicode the encoding assignments may change. Because this script is not encoded in Unicode, mobile devices will not include fonts supporting this keyboard. Additionally, on mobile devices the keyboard will only display the correct characters within the Keyman application.<p>
+
 <h1>Keyboard Layout</h1>
 
 <h2>Desktop Keyboard Layout</h2>
