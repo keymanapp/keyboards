e1afca707d2bb40881c65e4e8b6d7919ef31b678
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-04-01 15:23:37 -0500

    [lydian] fix osk syntax

--- release/l/lydian/source/help/lydian.php
+++ release/l/lydian/source/help/lydian.php
@@ -10,9 +10,9 @@
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 
 <h2>Desktop Keyboard Layout</h2>
-<div id='osk' data-states='default shift'>
+<div id='osk' data-states='default'>
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
-<div id='osk-tablet' data-states='default shift'>
+<div id='osk-tablet' data-states='default'>
 </div>
