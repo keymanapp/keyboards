193228f7d38600b37c68770e6535a614b6ef6d3f
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-04-08 12:49:23 -0500

    Update vithkuqi.php
    Updated description.

--- release/v/vithkuqi/source/help/vithkuqi.php
+++ release/v/vithkuqi/source/help/vithkuqi.php
@@ -7,7 +7,7 @@
 
 
 <p>
-    Vithkuqi 1.0 generated from template.
+    This keyboard layout is designed for Albanian using the Vithkuqi script.
 </p>
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 
