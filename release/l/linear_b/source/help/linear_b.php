e9585fad0671f7b42d0bcd0e1508286d6e79098a
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-04-11 14:21:25 -0500

    Update linear_b.php
    There is no phone layout.

--- release/l/linear_b/source/help/linear_b.php
+++ release/l/linear_b/source/help/linear_b.php
@@ -8,7 +8,7 @@
 ?>
 
 
-<p style='margin: 16px 0 0 0'>
+<p>
 This keyboard is designed for the Linear B syllabary.
 </p>
 
@@ -20,9 +20,5 @@ This keyboard is designed for the Linear B syllabary.
 <div id='osk-tablet' data-states='default shift'>
 </div>
 
-<h2>Phone Keyboard Layout</h2>
-<p>Due to the size and number of keys, some characters are hidden in the long press. 
-	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
-<div id='osk-phone' data-states='default shift numeric symbol currency'>
-</div>
+
 
