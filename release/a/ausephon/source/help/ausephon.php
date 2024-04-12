a87d8b4b50cebf1e23321e63b39e54e92a12b822
benaustralia <49857456+benaustralia@users.noreply.github.com>
2024-04-12 20:31:31 +1000

    Update ausephon.php

--- release/a/ausephon/source/help/ausephon.php
+++ release/a/ausephon/source/help/ausephon.php
@@ -5,15 +5,15 @@
   require_once('header.php');
 ?>
 
-      <p>The Aus Phon keyboards are a work in progres. So far we have designed a keyboard for phonemic (broad) transcription of Australian accented English speech. In the near future we will release a phonetic (narrow) keyboard.&nbsp;&nbsp;</p>
-      <p>We find the phonemic keyboard useful for touch typing symbols quickly and becoming&nbsp; fluent with transcribing the HCE (Harrington/Cox/Evans) system.</p>
+      <p>The ausephon keyboards are a work in progress. So far we have designed a keyboard for phonemic (broad) transcription of Australian accented English speech. Sometime in the future, we will release a phonetic (narrow) keyboard.&nbsp;&nbsp;</p>
+      <p>We find the phonemic keyboard useful for quickly touch typing symbols and becoming&nbsp; fluent with transcribing the HCE (Harrington/Cox/Evans) system.</p>
 
 
      <img src="layout150.png" width="600">
   
       <h1>Key Sequences</h1>
       <h4>The SHIFT key</h4>
-      <p>Pressing the shift key (in yellow), then one of those white keys in the layout (right) provides access to the symbols shown in the top right hand area of each key. e.g. the shift key, then the 'e' key will output schwa /ə/.</p>
+      <p>Pressing the shift key (in yellow), and then one of those white keys in the layout (below) provides access to the symbols shown in the top right-hand area of each key. e.g. the shift key, then the 'e' key will output schwa /ə/.</p>
     
-      <h4>The BACKTICK key</h4>
-      <p>The backtick key (in yellow), then the 'l', 'm', 'n', or 'r' key will produce the syllabic consonat marker below the character: l̩, m̩, n̩, ɹ̩.</p>
+      <h4>Diacritics</h4>
+      <p>To type diacritics you will first need to type the letter to which you want to add a diacritic. For example, to type the consonant marker below 'l', 'm', 'n', or 'r', type one of these aforementioned letters and then the BACKTICK key (in yellow). This will produce the syllabic consonant marker below the character: l̩, m̩, n̩, ɹ̩.</p>
