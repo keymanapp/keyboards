60654aaf02e7a77209801f08d803786627dd1dcd
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-05-22 09:27:29 -0500

    [help_php] Add online help files and add font to keyboard_info

---
+++ experimental/e/elymaic/source/help/elymaic.php
@@ -0,0 +1,13 @@
+<?php 
+  $pagename = 'Elymaic Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+The Elymaic keyboard is designed for typing the Achaemenid Aramaic language in the Elymaic script. It could feasibly also be used to type other Aramaic dialects in the Elymaic script.
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default'>
+</div>
