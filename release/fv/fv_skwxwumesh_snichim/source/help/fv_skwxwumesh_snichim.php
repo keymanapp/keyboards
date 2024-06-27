bb98c341b65952d4e99fba5d5e68090d26f0167f
Helen <6339740+xyzhelen@users.noreply.github.com>
2024-06-27 12:14:19 -0700

    fv_skwxwumesh - updated touch layout, mobile help

--- release/fv/fv_skwxwumesh_snichim/source/help/fv_skwxwumesh_snichim.php
+++ release/fv/fv_skwxwumesh_snichim/source/help/fv_skwxwumesh_snichim.php
@@ -27,17 +27,23 @@ This keyboard is designed for the <b>Sḵwx̱wú7mesh sníchim</b> language of
 <div id='wikitext'>
 <div class='vspace'></div>
 <table border='1' style='border-collapse:collapse' cellpadding='5' >
-<tr ><th  align='center'>’</th><td  align='center'>w w̓</td><td  align='center'>e é</td><td  align='center'>7</td><td  align='center'>t</td><td  align='center'>y y̓</td><td  align='center'>u ú</td><td  align='center'>i í</td><td >&nbsp;</td><td  align='center'>p</td></tr>
-<tr ><th  align='center'>´</th><td  align='center'>a á</td><td  align='center'>s</td><td  align='center'>h</td><td  align='center'>ḵ</td><td  align='center'>k</td><td  align='center'>l l̓</td><td  align='center'>'</td></tr>
-<tr ><td >&nbsp;</td><td  align='center'>x̱</td><td  align='center'>x</td><td  align='center'>c</td><th  align='center'>_</th><td  align='center'>.</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
+<tr ><th  align='center'>’</th><td  align='center'>w w̓</td><td  align='center'>e é</td><td  align='center'>7</td><td  align='center'>t t’ tl’</td><td  align='center'>ts ts’</td><td  align='center'>y y̓</td><td  align='center'>u ú</td><td  align='center'>i í</td><td  align='center'>p p’</td></tr>
+<tr ><td  align='center'>a á</td><td  align='center'>s sh</td><td  align='center'>h</td><td  align='center'>ḵ ḵ’</td><td  align='center'>ḵw ḵw’</td><td  align='center'>k k’</td><td  align='center'>kw kw’</td><td  align='center'>l l̓</td><td  align='center'>lh</td><td  align='center'>’</td></tr>
+<tr ><td >&nbsp;</td><td  align='center'>x̱</td><td  align='center'>x̱w</td><td  align='center'>x</td><td  align='center'>xw</td><td  align='center'>ch ch’</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
+<tr align="center"><td>123</td><td>🌐</td><th>´</th><td colspan="4">space</td><td>.</td><td colspan="2">⏎</td></tr>
 </table>
-<div class='vspace'></div><ul><li>keys with a red background are combining accents.
-</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
-<ul><li>Squamish
-</li></ul>
+<div class='vspace'></div>
+<ul>
+	<li>Keys with a red background are combining accents.</li>
+	<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+	<li>Common punctuation marks can be found by pressing and holding the period key to the right of the space bar.</li>
+</ul>
+<div class='vspace'></div>
+<h3>Alternate Name</h3>
+<ul>
+<li>Squamish</li>
+</ul>
 </div>
-
 <br/>
 
 
