5636c944df3a4be39959cd7f353f5623062bc4e1
HopsAndHops <meryl.bishop@gmail.com>
2024-06-29 15:12:22 -0700

    fixed mobile keyboard table, updated name, tidied documentation

--- release/fv/fv_anishinaabemowin/source/help/fv_anishinaabemowin.php
+++ release/fv/fv_anishinaabemowin/source/help/fv_anishinaabemowin.php
@@ -1,6 +1,6 @@
 <?php 
-  $pagename = 'Anishinaabemowin Keyboard Help';
-  $pagetitle = 'Anishinaabemowin Keyboard Help';
+  $pagename = 'Anishinaabemowin - Anihšināpēmowin Keyboard Help';
+  $pagetitle = 'Anishinaabemowin - Anihšināpēmowin Keyboard Help';
   $pagestyle = <<<END
   
 #wikitext table { margin: 12px; }
@@ -21,28 +21,65 @@ This keyboard is designed for the <b>Anishinaabemowin - Anihšināpēmowin</b> l
 <h2>Desktop Layout</h2>
 <img src="anishinaabemowinU_.png" alt="" height="198" width="514" border="0" /></h2>
 					<ul>
-						<li>Accents are typed after the base letter — ā is typed a then Left-Bracket [</li>
+						<li>Accents are typed after the base letter — ā is typed a then Left-Bracket [
               				<ul>
 						    	<li>To type the macron accent use the Left-Bracket key [ —  ō is typed o then Left-Bracket [</li>
 						    	<li>To type the circumflex accent use the Right-Bracket key ] - ê is typed e then Right-Bracket ]</li>
-						    	<li>To type the caron accent use the Backslash key \ — š is typed a then Backslash</li>
+						    	<li>To type the caron accent use the Backslash key \ — š is typed a then Backslash \</li>
               				</ul>
-						<li>You can type the original English punctuation by holding down the Right-Alt or Option key. The Semicolon ; is typed Right-Alt+Semicolon (Windows) Option+Semicolon (Mac).</li>
-					</ul>	
+						</li>
+						<li>You can type the original English punctuation by holding down the Right-Alt or Option key — the Semicolon ; is typed Right-Alt+Semicolon (Windows) Option+Semicolon (Mac).</li>
+					</ul>
+					<h2>Unicode Fonts</h2>
+					<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
+    				<h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>	
 
 
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
 <div class='vspace'></div>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>? !</td><td  align='center'>w</td><td  align='center'>e</td><td ></td><td  align='center'>t</td><td  align='center'>y</td><td >ˆ</td><td  align='center'>i</td><td  align='center'>o</td><td  align='center'>p</td></tr>
-<tr ><td  align='center'>a</td><td  align='center'>s</td><td  align='center'>d</td><td  align='center'>-</td><td  align='center'>g</td><td  align='center'>h</td><td  align='center'>j</td><td  align='center'>k</td><td  align='center'>' "</td></tr>
-<tr ><td >&nbsp;</td><td  align='center'>z</td><td  align='center'>,</td><td  align='center'>c</td><td  align='center'>š</td><td  align='center'>b</td><td  align='center'>n</td><td  align='center'>m</td><td  colspan='2'>&nbsp;</td></tr>
+<table border='1' style='border-collapse:collapse' cellpadding='5' >
+	<tr >
+		<td  align='center'>? !</td>
+		<td  align='center'>w</td>
+		<td  align='center'>e</td>
+		<th  align='center'>¯</th>
+		<td  align='center'>t</td>
+		<td  align='center'>y</td>
+		<th  align='center'>ˆ</th>
+		<td  align='center'>i</td>
+		<td  align='center'>o</td>
+		<td  align='center'>p</td>
+	</tr>
+	<tr >
+		<td  align='center'>a</td>
+		<td  align='center'>s</td>
+		<td  align='center'>d</td>
+		<td  align='center'>-</td>
+		<td  align='center'>g</td>
+		<td  align='center'>h</td>
+		<td  align='center'>j</td>
+		<td  align='center'>k</td>
+		<td  colspan="2" align='center'>' "</td>
+	</tr>
+	<tr >
+		<td >&nbsp;</td>
+		<td  align='center'>z</td>
+		<td  align='center'>,</td>
+		<td  align='center'>c</td>
+		<td  align='center'>š</td>
+		<td  align='center'>b</td>
+		<td  align='center'>n</td>
+		<td  align='center'>m</td>
+		<td  colspan='2'>&nbsp;</td>
+	</tr>
 </table>
-<div class='vspace'></div><ul><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
-<ul><li>Ojibwe
-</li></ul>
-</div>
+<div class='vspace'></div>
+	<ul>
+		<li>Keys with a red background are combining accents. These keys will have green text on your mobile device.</li>
+		<li>The first character in a cell is the "one-tap" key. Any further characters are "hold-select" keys.</li>
+</ul>
+<div class='vspace'></div>
 
 <br/>
 
