d06e77a5202e4434c7995365e069efa65d73c56e
Helen <6339740+xyzhelen@users.noreply.github.com>
2024-07-02 11:58:42 -0700

    fv_skwxwumesh
    knm code tidying, php help formatting

--- release/fv/fv_skwxwumesh_snichim/source/help/fv_skwxwumesh_snichim.php
+++ release/fv/fv_skwxwumesh_snichim/source/help/fv_skwxwumesh_snichim.php
@@ -71,11 +71,12 @@ This keyboard is designed for the <b>Sḵwx̱wú7mesh sníchim</b> language of
 		<td colspan="2">⏎</td>
 	</tr>
 </table>
+
 <div class='vspace'></div>
 <ul>
-	<li>Keys with a red background are combining accents.</li>
+	<li>Keys with a red background are combining accents. On your mobile device, they will have green text.</li>
 	<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
-	<li>Common punctuation marks can be found by pressing and holding the period key to the right of the space bar.</li>
+	<li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space bar.</li>
 </ul>
 <div class='vspace'></div>
 <h3>Alternate Name</h3>
