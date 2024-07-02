85824a06e805c0da40fd11b7016c271e6d34c3e6
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-07-01 18:03:27 -0700

    Minor updates
    Normalized outputs on mobile and desktop keyboards, tidied rules and stores, straightened mobile layout, updated to new capital barred lambda character, added additional stacking prevention, tidied documentation

--- release/fv/fv_stlatlimxec/source/help/fv_stlatlimxec.php
+++ release/fv/fv_stlatlimxec/source/help/fv_stlatlimxec.php
@@ -31,15 +31,14 @@ This keyboard is designed for the <b>Sƛ̓aƛ̓imxəc</b> language of the BC Int
 						<li>To type the grave accent use the backquote key ` — à is typed a then Backquote `<br />
 							This accent is occasionally used for linguistic purposes. </li>
 						<li>All changed punctuation can type their original value by holding down the Right-Alt or Option key. The Semicolon ; is typed Right-Alt+Semicolon (Windows) Option+Semicolon (Mac).</li>
-						 
-							 
-								<li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.</li>
-						<ul>
-									<li>single: ‘ Left-Bracket — ’ Right-Bracket</li>
-									<li>double: “ Shift+Left-Bracket — ” Shift+Right-Bracket</li>
-									<li>single ‹ Right-Alt+Shift+9 — › Right-Alt+Shift+0</li>
-									<li>double « Right-Alt+9 — » Right-Alt+0</li>
-								</ul>
+						<li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.
+							<ul>
+								<li>single: ‘ Left-Bracket — ’ Right-Bracket</li>
+								<li>double: “ Shift+Left-Bracket — ” Shift+Right-Bracket</li>
+								<li>single ‹ Right-Alt+Shift+9 — › Right-Alt+Shift+0</li>
+								<li>double « Right-Alt+9 — » Right-Alt+0</li>
+							</ul>
+						</li>
 						<h2>Unicode Fonts</h2>
 						<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
     <h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
@@ -47,19 +46,52 @@ This keyboard is designed for the <b>Sƛ̓aƛ̓imxəc</b> language of the BC Int
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
 <div class='vspace'></div>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>q q̓</td><td  align='center'>w w̓</td><td  align='center'>ə ə́ ə̣ ə̣́</td><th  align='center'>’</th><td  align='center'>t</td><td  align='center'>y y̓</td><td  align='center'>u ú ụ ụ́</td><td  align='center'>i í ị ị́</td><td  align='center'>ƛ̓</td><td  align='center'>p p̓</td></tr>
-<tr ><td  align='center'>a á ạ ạ́</td><td  align='center'>s ṣ</td><td  align='center'>ʔ</td><th  align='center'>´</th><td  align='center'>ɣ ɣ̓</td><td  align='center'>h</td><td  align='center'>ʷ</td><td  align='center'>k k̓</td><td  align='center'>l l̓ ḷ ḷ̓</td><td  align='center'>ɬ</td></tr>
-<tr ><td >&nbsp;</td><td  align='center'>z z̓</td><td  align='center'>x</td><td  align='center'>c c̓ c̣</td><td  align='center'>x̌</td><th  align='center'>.</th><td  align='center'>ʕ ʕ̓</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
+<table border='1' style='border-collapse:collapse' cellpadding='5' >
+	<tr >
+		<td  align='center'>q q̓</td>
+		<td  align='center'>w w̓</td>
+		<td  align='center'>ə ə́ ə̣ ə̣́</td>
+		<th  align='center'>’</th>
+		<td  align='center'>t</td><td  align='center'>y y̓</td>
+		<td  align='center'>u ú ụ ụ́</td>
+		<td  align='center'>i í ị ị́</td>
+		<td  align='center'>ƛ̓</td>
+		<td  align='center'>p p̓</td>
+	</tr>
+	<tr >
+		<td  align='center'>a á ạ ạ́</td>
+		<td  align='center'>s ṣ</td>
+		<td  align='center'>ʔ</td>
+		<th  align='center'>´</th>
+		<td  align='center'>ɣ ɣ̓</td>
+		<td  align='center'>h</td>
+		<td  align='center'>ʷ</td>
+		<td  align='center'>k k̓</td>
+		<td  align='center'>l l̓ ḷ ḷ̓</td>
+		<td  align='center'>ɬ</td>
+	</tr>
+	<tr >
+		<td >&nbsp;</td><td  align='center'>z z̓</td>
+		<td  align='center'>x</td>
+		<td  align='center'>c c̓ c̣</td>
+		<td  align='center'>x̌</td>
+		<th  align='center'>.</th>
+		<td  align='center'>ʕ ʕ̓</td>
+		<td  align='center'>n n̓</td>
+		<td  align='center'>m m̓</td>
+		<td  colspan='2'>&nbsp;</td>
+	</tr>
 </table>
-<div class='vspace'></div><ul><li>keys with a red background are combining accents.
-</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
-<ul><li>Lillooet
-</li></ul>
-</div>
+<div class='vspace'></div>
+	<ul>
+		<li>Keys with a red background are combining accents. On your mobile device, they will have green text.</li>
+		<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+		<li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space bar.</li>
+	</ul>
+
+<div class='vspace'></div>
 
 <br/>
 
 <h2>Keyboard Layout</h2>
-<div id='osk'>
-</div>
+<div id='osk'></div>
