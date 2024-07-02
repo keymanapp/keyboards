7ef4dfd2cfd057ba8427248b5e8e9faa01c0a459
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-07-02 16:55:32 -0700

    new desktop build and documentation

--- release/fv/fv_hulquminum_combine/source/help/fv_hulquminum_combine.php
+++ release/fv/fv_hulquminum_combine/source/help/fv_hulquminum_combine.php
@@ -18,52 +18,80 @@ This keyboard is designed for the <b>Hul̓q̓umin̓um̓</b> language of the BC C
 <p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 
+ 
+<h2><img src="fv_hulquminum_combine_U_.png" alt="" height="198" width="514" border="0" /></h2>
+	<ul>
+		<li>All accents are typed after the base character — x̌ is typed x then Right-Bracket ]
+            <ul>
+				<li>To type the combining comma use the Quote Key ' — m̓ is typed m then Quote</li>
+                <li>To type the acute accent use the Left-Bracket Key [ — í is typed i then Left-Bracket [</li>
+                <li>To type the hachek or caron use the the Right-Bracket Key — x̌ is typed x then Right-Bracket ]</li>
+            </ul>
+        </li>
+		<li>All changed punctuation can type their original value by holding down the Right-Alt or Option key — the Left-Bracket [ is typed Right-Alt+Left-Bracket (Windows) Option+Left-Bracket (Mac).</li>
+    </ul>
+						
+
+<h2>Unicode Fonts</h2>
+<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
+<h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
 
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
 <div class='vspace'></div>
 <table border='1' style='border-collapse:collapse' cellpadding='5' >
 	<tr >
-		<td  align='center'>q q̓</td>
-		<td  align='center'>w w̓</td>
-		<td  align='center'>e é</td>
-		<td  align='center'>ʼ</td>
-		<td  align='center'>t t̓</td>
-		<td  align='center'>y y̓</td>
-		<td  align='center'>u</td>
-		<td  align='center'>i í</td>
-		<td  align='center'>oo óo</td>
-		<td  align='center'>p p̓</td>
-	</tr>
-	<tr >
-		<td  align='center'>a á</td>
-		<td  align='center'>s</td>
-		<td  align='center'>:</td>
-		<td >&nbsp;</td>
-		<td  align='center'>´</td>
-		<td  align='center'>h</td>
-		<td  align='center'>ʼ ' "</td>
-		<td  align='center'>k k̓</td>
-		<td  align='center'>l l̓</td>
-	</tr>
-	<tr >
-		<td >&nbsp;</td>
-		<td  align='center'>x̌</td>
-		<td  align='center'>x</td>
-		<td  align='center'>c c̓</td>
-		<td  align='center'>?</td>
-		<td  align='center'>!</td>
-		<td  align='center'>n</td>
-		<td  align='center'>m</td>
-		<td  colspan='2'>&nbsp;</td>
-	</tr>
+        <td  align='center'>q q̓</td>
+        <td  align='center'>w w̓</td>
+        <td  align='center'>e é</td>
+    	<th  align='center'>ʼ</th>
+    	<td  align='center'>t t̓</td>
+        <td  align='center'>y y̓</td>
+        <td  align='center'>u</td>
+        <td  align='center'>i í</td>
+        <td  align='center'>oo óo</td>
+        <td  align='center'>p p̓</td>
+    </tr>
+    <tr >
+        <td  align='center'>a á</td>
+        <td  align='center'>s</td>
+        <td  align='center'>:</td>
+        <td >&nbsp;</td>
+        <th  align='center'>´</th>
+        <td  align='center'>h</td>
+        <td  align='center'>ʼ ' "</td>
+        <td  align='center'>k k̓</td>
+        <td  align='center'>l l̓</td>
+    </tr>
+    <tr >
+        <td  align='center'>⇧</td>
+        <td  align='center'>x̌</td>
+        <td  align='center'>x</td>
+    	<td  align='center'>c c̓</td>
+        <td  align='center'>?</td>
+        <td  align='center'>!</td>
+        <td  align='center'>n</td>
+        <td  align='center'>m</td>
+        <td  align='center'colspan='2'>⌫</td>
+    </tr>
+    <tr>
+        <td  align='center'>123</td>
+        <td  align='center'>🌐</td>
+        <td  align='center'>,</td>
+        <td  align='center'colspan="4">space</td>
+        <td  align='center'>.</td>
+        <td  align='center'colspan='2'>⏎</td>
+    </tr>
 </table>
-<div class='vspace'></div><ul><li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul>
-</div>
+<div class='vspace'></div>
+<ul>
+	<li>Keys with a red background are combining accents. On your mobile device, they will have green text.</li>
+	<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+	<li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space bar.</li>
+</ul>
 
 <br/>
 
 <h2>Keyboard Layout</h2>
-<div id='osk-tablet' data-states='default shift numeric'>
+<div id='osk' data-states='default'>
 </div>
