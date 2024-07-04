7d289957c9e1fb99f892f2ba9dcc96bfbc8cf901
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-07-04 12:04:38 -0700

    settled on a mobile layout, tidied documentation, bumped version

--- release/fv/fv_ktunaxa/source/help/fv_ktunaxa.php
+++ release/fv/fv_ktunaxa/source/help/fv_ktunaxa.php
@@ -13,49 +13,92 @@ END;
 ?>
 
 <p style='margin: 16px 0 0 0'>
-This keyboard is designed for the <b>Ktunaxa</b> language of the BC Interior region of Canada.
+    This keyboard is designed for the <b>Ktunaxa</b> language of the BC Interior region of Canada.
 </p>
-<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below),
+    please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 
 <img src="ktunaxa.png" alt="" height="198" width="514" border="0" /></h2>
-					<ul>
-						<li>The special characters of Ktunaxa have been assigned to the unshifted number keys. To type the numerals, hold down the shift key then press the appropriate number key. The Numeric keypad on the right side of many keyboards keep its original numbers.</li>
-						<li>To type the Glottal Accent, use the Apostrophe key '  —  p̓ is typed p then apostrophe</li>
-						<li>You can type the English value of the punctuation keys by holding down the Right-Alt or Option key. The Semicolon ; is typed Right-Alt+Semicolon (Windows) Option+Semicolon (Mac). The punctuation on the number keys (such as @ # $...) is accessed with the Right-Alt or Option key.</li>
-						<li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.
-							<ul>
-								<li>single: ‘ Backquote — ’ Zero</li>
-								<li>double: “ Shift+Left-Bracket — ” Shift+Right-Bracket</li>
-								<li>single ‹ Right-Alt+Shift+Hyphen — › Right-Alt+Shift+Equals</li>
-								<li>double « Right-Alt+Hyphen — » Right-Alt+Equals</li>
-							</ul>
-						</li>
-					</ul>
-					<h2>Unicode Fonts</h2>
-					<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
-    <h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
-    
+<ul>
+    <li>The special characters of Ktunaxa have been assigned to the unshifted number keys. To type the numerals, hold
+        down the shift key then press the appropriate number key. The Numeric keypad on the right side of many keyboards
+        keep its original numbers.</li>
+    <li>To type the Glottal Accent, use the Apostrophe key ' — p̓ is typed p then apostrophe</li>
+    <li>You can type the English value of the punctuation keys by holding down the Right-Alt or Option key — the
+        Semicolon ; is typed Right-Alt+Semicolon (Windows) Option+Semicolon (Mac). The punctuation on the number keys
+        (such as @ # $...) is accessed with the Right-Alt or Option key.</li>
+    <li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.
+        <ul>
+            <li>single: ‘ Backquote — ’ Zero</li>
+            <li>double: “ Shift+Left-Bracket — ” Shift+Right-Bracket</li>
+            <li>single ‹ Right-Alt+Shift+Hyphen — › Right-Alt+Shift+Equals</li>
+            <li>double « Right-Alt+Hyphen — » Right-Alt+Equals</li>
+        </ul>
+    </li>
+</ul>
+<h2>Unicode Fonts</h2>
+<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please
+    use these fonts when typing in your language as they have been specially built to represent your language as
+    accurately as possible. </p>
+<h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and
+        Aboriginal Serif</a></h3>
+
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
-<div class='vspace'></div>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>q q̓</td><td  align='center'>w w̓</td><td  align='center'>·</td><td  align='center'>-</td><td  align='center'>t t̓</td><td  align='center'>y y̓</td><td  align='center'>u</td><td  align='center'>i</td><th  align='center'>’</th><td  align='center'>p p̓</td></tr>
-<tr ><td  align='center'>a</td><td  align='center'>s</td><td  align='center'>ʔ</td><td  align='center'>ʾ</td><td >&nbsp;</td><td  align='center'>h</td><td >&nbsp;</td><td  align='center'>k k̓</td><td  align='center'>ⱡ l</td><td >&nbsp;</td></tr>
-<tr ><td >&nbsp;</td><td  align='center'>? !</td><td  align='center'>x</td><td  align='center'>ȼ ȼ̓</td><td  align='center'>,</td><td  align='center'>.</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
-</table>
-<div class='vspace'></div><ul><li>keys with a red background are combining accents.
-</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul><div class='vspace'></div><h2>Alternate Name</h2>
-<ul><li>Kootenay
-</li></ul>
+    <div class='vspace'></div>
+    <table border='1' style='border-collapse:collapse' cellpadding='5'>
+        <tr>
+            <td align='center'>q q̓</td>
+            <td align='center'>w w̓</td>
+            <td align='center'>·</td>
+            <td align='center'>t t̓</td>
+            <td align='center'>y y̓</td>
+            <td align='center'>u</td>
+            <td align='center'>i</td>
+            <td align='center'>p p̓</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td align='center'>a</td>
+            <td align='center'>s</td>
+            <td align='center'>ʔ</td>
+            <td align='center'>h</td>
+            <td align='center'>k k̓</td>
+            <td align='center'>ⱡ l</td>
+            <th align='center'>’</th>
+        </tr>
+        <tr>
+            <td align='center'>⇧</td>
+            <td align='center'>ʾ</td>
+            <td align='center'>-</td>
+            <td align='center'>x</td>
+            <td align='center'>ȼ ȼ̓</td>
+            <td align='center'>n n̓</td>
+            <td align='center'>m m̓</td>
+            <td align='center'>⌫</td>
+        </tr>
+        <tr>
+            <td align='center'>123</td>
+            <td align='center'>🌐</td>
+            <td align='center' colspan='4'>space</td>
+            <td align='center'>.</td>
+            <td align='center'>⏎</td>
+        </tr>
+    </table>
+    <div class='vspace'></div>
+    <ul>
+        <li>keys with a red background are combining accents. On your mobile device, these keys will have green text.
+        </li>
+        <li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+        <li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space
+            bar.</li>
+    </ul>
+    <div class='vspace'></div>
 </div>
 
-<br/>
+<br />
 
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk'>
-</div>
-
-<h2>Tablet Keyboard Layout</h2>
-<div id='osk-tablet'>
 </div>
