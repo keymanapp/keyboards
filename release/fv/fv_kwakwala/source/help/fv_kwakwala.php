6d791ec62099ef73a58c4a30df5363c4f6034898
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-07-09 16:54:52 -0700

    Minor updates
    Tidied documentation, tweaked mobile layout, removed web help

--- release/fv/fv_kwakwala/source/help/fv_kwakwala.php
+++ release/fv/fv_kwakwala/source/help/fv_kwakwala.php
@@ -13,49 +13,120 @@ END;
 ?>
 
 <p style='margin: 16px 0 0 0'>
-This keyboard is designed for the <b>Kwak̕wala</b> language of the BC Coast region of Canada.
+    This keyboard is designed for the <b>Kwak̕wala</b> language of the BC Coast region of Canada.
 </p>
-<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below),
+    please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 
 <img src="UKwakwala.png" alt="" height="198" width="514" border="0" /></h2>
-					<h2>Keyboard Layout </h2>
-					<ul>
-						<li>The special Characters of Kwak̕wala have been assigned to various punctuation keys</li>
-						<li>All accents are typed after the base character: <strong>a̱</strong> is typed <strong>a</strong> then <strong>semicolon ;</strong></li>
-						<li>To type the glottalized accent when it appears over a single letter use the <strong>slash key /</strong><br />
-								<strong>k̓</strong> is typed <strong>k</strong> then <strong>/</strong></li>
-						<li>To type the glottalized accent when it appears between two letters use the <strong>right bracket key ]</strong><br /> <strong>k̕w</strong> is typed <strong>k</strong> then <strong>]</strong> then <strong>w</strong></li>
-						<li>To type the underline accent use the <strong>semicolon key ;</strong><br /> <strong>ḵ</strong> is typed <strong>k</strong> then <strong>;</strong></li>
-						<li>To type the slash-ł use the <strong>left bracket key [</strong></li>
-						<li>All changed punctuation can type their original value by holding down the Right-Alt or Option key. To get the original value of the left-bracket [ type Right-Alt+Left-Bracket (Windows) Option+Left-Bracket (Mac).</li>
-            <ul>
-                  <li><b>NOTE</b>: To type / while using Google Drive, use <strong>shift</strong>, <strong>Right-Alt</strong>, then <strong>Slash /</strong></li>
-              </ul>
-						<ul><li>
-      Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys. </li>
-        <li>single: ‘ Backquote — ’ Shift+Right Bracket</li><li>double: “ Shift+Hyphen — ” Shift+Equals</li><li>
-      single ‹ Right-Alt+Shift+9 — › Right-Alt+Shift+0 </li><li>
-    double « Right-Alt+9 — » Right-Alt+0</li>
-      </ul>
+<h2>Keyboard Layout </h2>
+<ul>
+    <li>The special Characters of Kwak̕wala have been assigned to various punctuation keys</li>
+    <li>All accents are typed after the base character: <strong>a̱</strong> is typed <strong>a</strong> then
+        <strong>semicolon ;</strong>
+        <ul>
+            <li>To type the glottalized accent when it appears over a single letter use the <strong>slash key
+                    /</strong><br />
+                <strong>k̓</strong> is typed <strong>k</strong> then <strong>/</strong>
+            </li>
+            <li>To type the glottalized accent when it appears between two letters use the <strong>right bracket key
+                    ]</strong><br /> <strong>k̕w</strong> is typed <strong>k</strong> then <strong>]</strong> then
+                <strong>w</strong>
+            </li>
+            <li>To type the underline accent use the <strong>semicolon key ;</strong><br /> <strong>ḵ</strong> is typed
+                <strong>k</strong> then <strong>;</strong>
+            </li>
+        </ul>
+    </li>
+    <li>To type the slash - ł use the <strong>left bracket key [</strong></li>
+    <li>All changed punctuation can type their original value by holding down the Right-Alt or Option key. To get the
+        original value of the left-bracket [ type <strong>Right-Alt</strong>+<strong>[</strong> (Windows)
+        <strong>Option</strong>+<strong>[</strong> (Mac).
+    </li>
+    <ul>
+        <li><b>NOTE</b>: To type / while using Google Drive, use
+            <strong>Right-Alt</strong>+<strong>Shift</strong>+<strong>/</strong> (Windows) /
+            <strong>Option</strong>+<strong>Shift</strong>+<strong>/</strong> (Mac)
+        </li>
     </ul>
-    <h2>Unicode Fonts</h2>
-    <p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
-    <h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
-    
+    </li>
+    <li> Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.
+        <ul>
+            <li>single: ‘ Backquote — ’ Shift+Right Bracket</li>
+            <li>double: “ Shift+Hyphen — ” Shift+Equals</li>
+            <li>single ‹ Right-Alt+Shift+9 — › Right-Alt+Shift+0 </li>
+            <li> double « Right-Alt+9 — » Right-Alt+0</li>
+        </ul>
+    </li>
+</ul>
+<h2>Unicode Fonts</h2>
+<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please
+    use these fonts when typing in your language as they have been specially built to represent your language as
+    accurately as possible. </p>
+<h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and
+        Aboriginal Serif</a></h3>
+
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
-<div class='vspace'></div>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>a̱</td><td  align='center'>w</td><td  align='center'>e</td><td  align='center'>ł</td><td  align='center'>t t̓ t̕ł t̕s</td><td  align='center'>y</td><td  align='center'>u</td><td  align='center'>i</td><td  align='center'>o</td><td  align='center'>p p̓</td></tr>
-<tr ><td  align='center'>a</td><td  align='center'>s</td><td  align='center'>d</td><td  align='center'>g̱</td><td  align='center'>g</td><td  align='center'>h</td><td  align='center'>ḵ ḵ̓ ḵ̕w</td><td  align='center'>k k̓ k̕w</td><td  align='center'>l</td><td  align='center'>' "</td></tr>
-<tr ><td >&nbsp;</td><td  align='center'>z</td><td  align='center'>x</td><td  align='center'>x̱</td><th  align='center'>’</th><td  align='center'>b</td><td  align='center'>n</td><td  align='center'>m</td><td  colspan='2'>&nbsp;</td></tr>
-</table>
-<div class='vspace'></div><ul><li>keys with a red background are combining accents.
-</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul>
+    <div class='vspace'></div>
+    <table border='1' style='border-collapse:collapse' cellpadding='5'>
+        <tr>
+            <td align='center'>a̱</td>
+            <td align='center'>w</td>
+            <td align='center'>e</td>
+            <td align='center'>ł</td>
+            <td align='center'>t t̓ t̕ł t̕s</td>
+            <td align='center'>y</td>
+            <td align='center'>u</td>
+            <td align='center'>i</td>
+            <td align='center'>o</td>
+            <td align='center'>p p̓</td>
+        </tr>
+        <tr>
+            <td align='center'>a</td>
+            <td align='center'>s</td>
+            <td align='center'>d</td>
+            <td align='center'>g̱</td>
+            <td align='center'>g</td>
+            <td align='center'>h</td>
+            <td align='center'>ḵ ḵ̓ ḵ̕w</td>
+            <td align='center'>k k̓ k̕w</td>
+            <td align='center'>l</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td align='center'>⇧</td>
+            <td align='center'>z</td>
+            <td align='center'>x</td>
+            <td align='center'>x̱</td>
+            <th align='center'>’</th>
+            <td align='center'>b</td>
+            <td align='center'>n</td>
+            <td align='center'>m</td>
+            <td align='center' colspan='2'>⌫</td>
+        </tr>
+        <tr>
+            <td align='center'>123</td>
+            <td align='center'>🌐</td>
+            <td align='center'>' "</td>
+            <td align='center' colspan='4'>space</td>
+            <td align='center'>.</td>
+            <td align='center' colspan='2'>⏎</td>
+        </tr>
+    </table>
+    <div class='vspace'></div>
+    <ul>
+        <li>Keys with a red background are combining accents. On your mobile device, these keys will have green text.
+        </li>
+        <li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
+        </li>
+        <li>Common punctuation marks can be found by pressing and holding the period key to the right of the space bar.
+        </li>
+    </ul>
 </div>
 
-<br/>
+<br />
 
 <h2>Keyboard Layout</h2>
 <div id='osk'>
