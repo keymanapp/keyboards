014693453b843a61086e4a716d233832c79dd821
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-07-19 10:49:48 -0500

    Update xpert.php
    Removed phone layout div since there is no phone layout.

--- release/x/xpert/source/help/xpert.php
+++ release/x/xpert/source/help/xpert.php
@@ -20,9 +20,3 @@ This keyboard is designed for the English language. It will also be useful for o
 <div id='osk-tablet' data-states='default shift'>
 </div>
 
-<h2>Phone Keyboard Layout</h2>
-<p>Due to the size and number of keys, some characters are hidden in the long press. 
-	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
-<div id='osk-phone' data-states='default shift numeric symbol currency'>
-</div>
-
