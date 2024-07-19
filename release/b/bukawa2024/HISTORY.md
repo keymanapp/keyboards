ab952f88e97ac3f96c4c32866e146600522045e5
gawabill <tadmorebill@gmail.com>
2024-07-19 19:20:34 +0930

    [Bukawa2024] Initial submission of Bukawa2024 keyboard

---
+++ release/b/bukawa2024/HISTORY.md
@@ -0,0 +1,6 @@
+Bukawa2024 Change History
+====================
+
+2.0 (2024-07-18)
+----------------
+* Created by Bill Eckermann
---
+++ release/b/bukawa2024/source/help/Bukawa2024.php
@@ -0,0 +1,25 @@
+
+<?phpbani
+$pagename = 'Bukawa2024 Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+Bukawa2024 keyboard based on basic keyboard.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.</p>
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+
+
