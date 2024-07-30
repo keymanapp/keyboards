126e677b488e17e54516f965cc07384218b3c42f
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-07-29 19:01:54 -0700

    Minor update
    Tidied documentation, added alternate names, removed web help in favour of OSK

--- release/fv/fv_secwepemctsin/source/help/fv_secwepemctsin.php
+++ release/fv/fv_secwepemctsin/source/help/fv_secwepemctsin.php
@@ -1,7 +1,7 @@
-<?php 
-  $pagename = 'Secwepemctsín Keyboard Help';
-  $pagetitle = 'Secwepemctsín Keyboard Help';
-  $pagestyle = <<<END
+<?php
+$pagename = 'Secwepemctsín Keyboard Help';
+$pagetitle = 'Secwepemctsín Keyboard Help';
+$pagestyle = <<<END
   
 #wikitext table { margin: 12px; }
 #wikitext table td { border: solid 1px #888888 !important }
@@ -9,54 +9,116 @@
 .lang2 {font-family:"Aboriginal Sans"}  
 END;
 
-  require_once('header.php');
+require_once ('header.php');
 ?>
 
 <p style='margin: 16px 0 0 0'>
-This keyboard is designed for the <b>Secwepemctsín</b> language of the BC Interior region of Canada.
+	This keyboard is designed for the <b>Secwepemctsín</b> language of the BC Interior region of Canada.
 </p>
-<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below),
+	please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 
 <img src="Secwepemctsin.png" alt="" height="198" width="514" border="0" /></h2>
-					<ul><li>All accents are typed after the base character — á is typed a then Semicolon ; </li>
-						<li>To type the acute accent ˊ use the Semicolon key ; — é is typed e then Semicolon ; </li>
-						<li>To type the apostrophe accent ʼ use the Apostrophe key ' — k̓ is typed k then Apostophe '</li>
-						<li>To type the grave accent ` use the Backquote key ` — è is typed e then Backquote ` </li>
-						<li>You can type the English value of the punctuation keys by holding down the Right-Alt or Option key. </li>
-							<ul>
-                                <li>For example, the left bracket [ is typed Right-Alt + [ (Windows) or Option + [ (Mac).</li>
-                            </ul>
-						<li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.</li>
-						<ul><li>single: ‘ Left-Bracket — ’ Right-Bracket</li>
-						<li>
-							double: “ Shift+Left-Bracket — ” Shift+Right-Bracket</li>
-						<li>
-							single ‹ Right-Alt+Shift+9 — › Right-Alt+Shift+0</li>
-						<li>
-							double « Right-Alt+9 — » Right-Alt+0</li>
-						</ul> </ul> 
-						 
-						<h2>Unicode Fonts</h2>
-    <p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
-    <h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
-    
+<ul>
+	<li>All accents are typed after the base character — á is typed <kbd>a</kbd> then Semicolon <kbd>;</kbd>
+		<ul>
+			<li>To type the acute accent ˊ use the Semicolon key <kbd>;</kbd> — é is typed <kbd>e</kbd> then
+				<kbd>;</kbd>
+			</li>
+			<li>To type the apostrophe accent ʼ use the Apostrophe key <kbd>'</kbd> — k̓ is typed <kbd>k</kbd> then
+				<kbd>'</kbd>
+			</li>
+			<li>To type the grave accent ` use the Backquote key <kbd>`</kbd> — è is typed <kbd>e</kbd> then
+				<kbd>`</kbd>
+			</li>
+			<li>You can type the English value of the punctuation keys by holding down the Right-Alt or Option key.
+				<ul>
+					<li>For example, the left bracket [ is typed <kbd>Right-Alt</kbd>+<kbd>[</kbd> (Windows) or <kbd>⌥
+							Option</kbd>+<kbd>[</kbd> (Mac).</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.
+		<ul>
+			<li>single: ‘ <kbd>[</kbd> — ’ <kbd>]</kbd></li>
+			<li>double: “ <kbd>Shift</kbd>+<kbd>[</kbd> — ” <kbd>Shift</kbd>+<kbd>]</kbd></li>
+			<li>single ‹ <kbd>Right-Alt</kbd>+<kbd>Shift</kbd>+<kbd>9</kbd> — ›
+				<kbd>Right-Alt</kbd>+<kbd>Shift</kbd>+<kbd>0</kbd>
+			</li>
+			<li>double « <kbd>Right-Alt</kbd>+<kbd>9</kbd> — » <kbd>Right-Alt</kbd>+<kbd>0</kbd></li>
+		</ul>
+	</li>
+</ul>
+
+<h2>Unicode Fonts</h2>
+<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please
+	use these fonts when typing in your language as they have been specially built to represent your language as
+	accurately as possible. </p>
+<h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and
+		Aboriginal Serif</a></h3>
+
 <h2>Notes on touch layout</h2>
 <div id='wikitext'>
-<div class='vspace'></div>
-<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center'>q q̓</td><td  align='center'>w w̓</td><td  align='center'>e é</td><td  align='center'>r r̓</td><td  align='center'>t t̓</td><td  align='center'>y y̓</td><td  align='center'>u ú</td><td  align='center'>i í</td><td  align='center'>o ó</td><td  align='center'>p p̓</td></tr>
-<tr ><td  align='center'>a á</td><td  align='center'>s s̓</td><td  align='center'>7</td><td  align='center'>-</td><td  align='center'>g g̓</td><td  align='center'>h</td><th  align='center'>´</th><td  align='center'>k k̓</td><td  align='center'>l l̓</td><th  align='center'>’</th></tr>
-<tr ><td >&nbsp;</td><td  align='center'>? !</td><td  align='center'>x</td><td  align='center'>c</td><td  align='center'>,</td><td  align='center'>.</td><td  align='center'>n n̓</td><td  align='center'>m m̓</td><td  colspan='2'>&nbsp;</td></tr>
-</table>
-<div class='vspace'></div><ul><li>keys with a red background are combining accents.
-</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
-<ul><li>Shuswap
-</li></ul><div class='vspace'></div>
+	<div class='vspace'></div>
+	<table border='1' style='border-collapse:collapse' cellpadding='5'>
+		<tr>
+			<td align='center'>q q̓</td>
+			<td align='center'>w w̓</td>
+			<td align='center'>e é</td>
+			<td align='center'>r r̓</td>
+			<td align='center'>t t̓</td>
+			<td align='center'>y y̓</td>
+			<td align='center'>u ú</td>
+			<td align='center'>i í</td>
+			<td align='center'>o ó</td>
+			<td align='center'>p p̓</td>
+		</tr>
+		<tr>
+			<td align='center'>a á</td>
+			<td align='center'>s s̓</td>
+			<td align='center'>7</td>
+			<td align='center'>-</td>
+			<td align='center'>g g̓</td>
+			<td align='center'>h</td>
+			<th align='center'>´</th>
+			<td align='center'>k k̓</td>
+			<td align='center'>l l̓</td>
+			<th align='center'>’</th>
+		</tr>
+		<tr>
+			<td align='center'>⇧</td>
+			<td align='center'>? !</td>
+			<td align='center'>x</td>
+			<td align='center'>c</td>
+			<td align='center'>,</td>
+			<td align='center'>.</td>
+			<td align='center'>n n̓</td>
+			<td align='center'>m m̓</td>
+			<td align='center' colspan='2'>⌫</td>
+		</tr>
+		<tr>
+			<td align='center'>123</td>
+			<td align='center'>🌐</td>
+			<td align='center' colspan='5'>space</td>
+			<td align='center'>.</td>
+			<td align='center' colspan='2'>⏎</td>
+		</tr>
+	</table>
+	<div class='vspace'></div>
+	<ul>
+		<li>Keys with a red background are combining accents.
+		</li>
+		<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
+		</li>
+		<li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space
+			bar.</li>
+	</ul>
+	<div class='vspace'></div>
 </div>
 
-<br/>
+<br />
 
 <h2>Keyboard Layout</h2>
-<div id='osk'>
-</div>
+<div id='osk' data-states='default shift rightalt rightalt-shift'></div>
