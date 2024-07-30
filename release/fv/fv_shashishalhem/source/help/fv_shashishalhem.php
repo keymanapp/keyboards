8a125abeb7494e320370a85a7e0b45d925230f37
Helen <6339740+xyzhelen@users.noreply.github.com>
2024-07-30 11:24:47 -0700

    help doc formatting

--- release/fv/fv_shashishalhem/source/help/fv_shashishalhem.php
+++ release/fv/fv_shashishalhem/source/help/fv_shashishalhem.php
@@ -19,13 +19,23 @@ This keyboard is designed for the <b>Shashishalhem</b> language of the BC Coast 
 </p>
 
 <img src="Shashishalhem.png" alt="" height="198" width="514" border="0" /></h2>
-					<ul>
-						<li>All accents are typed after the base character: á is typed a then Backquote ` (next to the 1 key)</li>
-						<ul><li>To type the acute accent use the Backquote ` — é is typed e then Backquote `</li></ul>
-						<li>All changed punctuation can type their original value by holding down the Right-Alt or Option key. The Backquote ` is typed Right-Alt+Backquote (Windows) Option+Backquote (Mac).</li>
-						
-						<h2>Unicode Fonts</h2>
-						<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
+		<ul>
+			<li>To type the acute accent use the Backquote <kbd>`</kbd> (next to the 1 key)
+				<ul>
+					<li>é is typed <kbd>e</kbd> then Backquote <kbd>`</kbd></li>
+				</ul>
+			</li>
+			<li>The ḵ and x̱ characters have been assigned to the <kbd>[</kbd> and <kbd>]</kbd> keys.
+				<ul>
+					<li>ḵ is typed with the left bracket key <kbd>[</kbd></li>
+					<li>x̱ is typed with the right bracket key <kbd>]</kbd></li>
+				</ul>
+			</li>
+			<li>All changed punctuation can type their original value by holding down the <kbd>Right-Alt</kbd> key (Win) or <kbd>Option</kbd> key (Mac). 
+				<ul><li>The left bracket [ is typed <kbd>Right-Alt</kbd>+<kbd>[</kbd> (Win) or <kbd>Option</kbd>+<kbd>[</kbd> (Mac).</li></ul>
+		</ul>		
+		<h2>Unicode Fonts</h2>
+		<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
     <h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
     
 <h2>Notes on touch layout</h2>
@@ -37,10 +47,18 @@ This keyboard is designed for the <b>Shashishalhem</b> language of the BC Coast 
 	<tr align='center'><td >⇧</td><td>x̱</td><td>x</td><td>c</td><td>,</td><td>.</td><td>n</td><td>m</td><td  colspan='2'>⌫</td></tr>
 	<tr align="center"><td>123</td><td>🌐</td><td colspan="5">space</td><td>.</td><td colspan="2">⏎</td></tr>
 </table>
-<div class='vspace'></div><ul><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
-</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
+<div class='vspace'></div>
 <ul>
-<li>Sechelt</li>
+	<li>Keys with a red background are combining accents. On your mobile device, they will have green text.</li>
+	<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+	<li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space bar.</li>
+</ul>
+<div class='vspace'></div>
+
+<div class='vspace'></div>
+<h3>Alternate Name</h3>
+<ul>
+	<li>Sechelt</li>
 </ul>
 </div>
 
