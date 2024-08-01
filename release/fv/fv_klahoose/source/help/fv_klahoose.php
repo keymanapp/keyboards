cae121b34bf499db4331f99d7f69d6b2308bed67
Helen <6339740+xyzhelen@users.noreply.github.com>
2024-08-01 13:46:33 -0700

    help file - css formatting

--- release/fv/fv_klahoose/source/help/fv_klahoose.php
+++ release/fv/fv_klahoose/source/help/fv_klahoose.php
@@ -24,50 +24,61 @@ This keyboard is designed for the <b>Éy7á7juuthem</b> language of the BC Coast
 <ul>
 	<li>The "Default" keymap above shows how to type the following characters in Éy7á7juuthem.</li>
 		<ul>
-			<li>ƛ	—	[ <em>left bracket on English layout</em></li>
-			<li>θ	—	] <em>right bracket</em></li>
-			<li>ə	—	; <em>semicolon</em></em></li>
-			<li>ʔ	—	/ <em>slash</em></li>
+			<li>ƛ	—	<kbd>[</kbd> left bracket</li>
+			<li>θ	—	<kbd>]</kbd> right bracket</li>
+			<li>ə	—	<kbd>;</kbd> semicolon</li>
+			<li>ʔ	—	<kbd>/</kbd> slash</li>
 		</ul>
-	<li>To type a <strong>combining accent</strong>, first type the base alphabet character (such as <strong>a</strong>), and then type the accent key as listed below.</li>
+	<li>To type a <strong>combining accent</strong>, first type the base vowel and then type the accent key.</li>
 	<ul>
-		<li>Acute accent (◌́)	—	Backtick ` (next to 1)</li>
-			<ul><li>Example: e + ` = é </li></ul>
-		<li>Combining glottal apostrophe (◌̓)	—	Apostrophe '</li>
-			<ul><li>Example: m + ' = m̓ </li></ul>
-		<li>Caron (◌̌)	—	Backslash \ </li>
-			<ul><li>Example: c + \ = č</li></ul>
+		<li>Acute accent (◌́)	—	Backtick <kbd>`</kbd> (next to 1)
+			<ul><li><kbd>e</kbd>+<kbd>`</kbd> = <kbd>é</kbd> </li></ul>
+		</li>
+		<li>Combining glottal apostrophe (◌̓)	—	Apostrophe <kbd>'</kbd>
+			<ul><li><kbd>m<kbd>+<kbd>'</kbd> = m̓ </li></ul>
+		</li>
+		<li>Caron (◌̌)	—	Backslash <kbd>\</kbd>
+			<ul><li><kbd>c</kbd>+<kbd>\</kbd> = č</li></ul>
+		</li>
 	</ul>
-	<li>You can type the original English value of the keys by holding down the Right-Alt or Option key.</li>
-		<ul><li>Example: The left bracket [ is typed Right-Alt+[ (Windows) or Option+[ (Mac).</li></ul>
+	<li>You can type the original English value of the keys by holding down the <kbd>Right-Alt</kbd> key (Win) or <kbd>Option</kbd> key (Mac).
+		<ul>
+			<li>The barred lambda ƛ is typed <kbd>[</kbd>.</li>
+			<li>The right bracket [ is typed <kbd>Right-Alt</kbd>+<kbd>[</kbd> or <kbd>Option</kbd>+<kbd>[</kbd>.
+		</ul>
+	</li>
 </ul>
 
 <div id='osk' data-states='shift'></div>
 
 <ul>
-	<li>The "Shifted" keymap above shows characters in Éy7á7juuthem that can be typed with SHIFT key combinations.</li>
+	<li>The "Shifted" keymap above shows characters in Éy7á7juuthem that can be typed with <kbd>SHIFT</kbd> key combinations.</li>
 		<ul>
-			<li>ɛ	— SHIFT + e</li>
-			<li>ʊ	—	SHIFT + u</li>
-			<li>I	—	SHIFT + i</li>
-			<li>ɔ	— SHIFT + e</li>
-			<li>æ	—	SHIFT + a</li>
-			<li>ɬ	— SHIFT + l</li>
-			<li>χ	— SHIFT + x</li>
-			<li>w̥	— SHIFT + v</li>
+			<li>ɛ	— <kbd>SHIFT</kbd>+<kbd>e</kbd></li>
+			<li>ʊ	—	<kbd>SHIFT</kbd>+<kbd>u</kbd></li>
+			<li>I	—	<kbd>SHIFT</kbd>+<kbd>i</kbd></li>
+			<li>ɔ	— <kbd>SHIFT</kbd>+<kbd>e</kbd></li>
+			<li>æ	—	<kbd>SHIFT</kbd>+<kbd>a</kbd></li>
+			<li>ɬ	— <kbd>SHIFT</kbd>+<kbd>l</kbd></li>
+			<li>χ	— <kbd>SHIFT</kbd>+<kbd>x</kbd></li>
+			<li>w̥	— <kbd>SHIFT</kbd>+<kbd>v</kbd></li>
 		</ul>
-	<li>You can type the original English value of the keys by holding down the Right-Alt or Option key.</li>
-		<ul><li>Example: Capital A is typed RightAlt + a (Windows) or Option + a (Mac).</li></ul>
+	<li>You can type the original English value of the keys by holding down the <kbd>Right-Alt</kbd> key (Win) or <kbd>Option</kbd> key (Mac).
+		<ul>
+			<li>æ is typed <kbd>SHIFT</kbd>+<kbd>a</kbd></li>
+			<li>A is typed <kbd>RightAlt</kbd>+<kbd>a</kbd> or <kbd>Option</kbd>+<kbd>a</kbd>.</li>
+		</ul>
+	</li>
 </ul>
 	
 <h3>Typing raised characters</h3>
-	<li>To type a <strong>raised character</strong>, first type the base alphabet character (such as <strong>y</strong>, and then type the Caret ^ (Shift + 6).</li>
+	<li>To type a <strong>raised character</strong>, first type the base character and then type the Caret <kbd>^</kbd> (<kbd>Shift</kbd>+<kbd>6</kbd>).
 		<ul>
-		<li>Raised ᶿ: θ + ^ <em>or</em> t + ^</li> 
-		<li>Raised ʷ: w + ^</li>
-		<li>Raised ʸ: y + ^</li>
+			<li>Raised ᶿ: <kbd>θ</kbd>+<kbd>^</kbd> or <kbd>t</kbd>+<kbd>^</kbd></li> 
+			<li>Raised ʷ: <kbd>w</kbd>+<kbd>^</kbd></li>
+			<li>Raised ʸ: <kbd>y</kbd>+<kbd>^</kbd></li>
 		</ul>
-		
+	</li>
 </ul> 
 
 <p>The following Unicode fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
