e067cd543149719a57e37ea0b7a99f6a975fd529
Helen <6339740+xyzhelen@users.noreply.github.com>
2024-08-01 11:18:42 -0700

    updated help docs

--- release/fv/fv_nexwslayemucen/source/help/fv_nexwslayemucen.php
+++ release/fv/fv_nexwslayemucen/source/help/fv_nexwslayemucen.php
@@ -30,12 +30,18 @@ This keyboard is designed for the <b>Nəxʷsƛ̓ay̓əmúcən (Klallam)</b> lan
 			<li>g is typed with the <kbd>Right-Alt</kbd>+<kbd>g</kbd> (Win) or <kbd>Option</kbd>+<kbd>g</kbd> (Mac)</li>
 		</ul>
 	</li>
-	<li>To type the glottal accent, type the base character first, followed by the <kbd>d</kbd> key.
+	<li>The glottalized barred lambda ƛ̕  is typed <kbd>u</kbr> <kbd>i</kbd>
+		<ul>
+			<li>If you wish to type the English letters "ui", type <kbd>u</kbd> then <kbd>Right-Alt</kbd>+<kbd>i</kbd> (Win) or <kbd>Option</kbd>+<kbd>i</kbd> (Mac).</li>
+			<li>To type "fruit", type <kbd>Right-Alt</kbd>+<kbd>f</kbd> <kbd>Right-Alt</kbd>+<kbd>r</kbd> <kbd>u</kbd> <kbd>Right-Alt</kbd>+<kbd>i</kbd> <kbd>t</kbd> </li>
+		</ul>
+	</li>
+	<li>To type the glottal accent, type the base character first, followed by the <kbd>d</kbd> key (with no space in between).
 		<ul>
 			<li>q̕ is typed <kbd>q</kbd> then <kbd>d</kbd></li>
 		</ul>
 	</li>	
-	<li>To type acccented vowels, type the Semicolon key <kbd>;</kbd> first, followed by the base vowel.
+	<li>To type acccented vowels, type the Semicolon key <kbd>;</kbd> first, followed by the base vowel (with no space in between).
 		<ul>
 			<li><kbd>;</kbd> <kbd>a</kbd> types á</li>
 			<li><kbd>;</kbd> <kbd>e</kbd> types é</li>
@@ -51,7 +57,7 @@ This keyboard is designed for the <b>Nəxʷsƛ̓ay̓əmúcən (Klallam)</b> lan
 			<li><kbd>;</kbd> <kbd>z</kbd> or <kbd>;</kbd>+<kbd>x</kbd> types x̣ʷ</li>
 		</ul>
 	</li>
-	<li>The glottalized barred lambda ƛ̕  is typed <kbd>u</kbr> <kbd>i</kbd></li>
+
 	<li>Some additional symbols are listed here.
 		<ul>
 			<li><kbd>.</kbd> <kbd>.</kbd> types … (ellipsis)</li>
