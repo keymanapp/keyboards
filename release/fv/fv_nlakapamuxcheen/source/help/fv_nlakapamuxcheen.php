dfad3e0c7d56093d535c3000b8618117055ff53f
HopsAndHops <127360690+HopsAndHops@users.noreply.github.com>
2024-08-03 19:01:57 -0700

    fixing list tags but for real this time

--- release/fv/fv_nlakapamuxcheen/source/help/fv_nlakapamuxcheen.php
+++ release/fv/fv_nlakapamuxcheen/source/help/fv_nlakapamuxcheen.php
@@ -148,7 +148,7 @@ END;
                 text.
             </li>
             <li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys. </li>
-            </li>Common punctuation marks can be found by pressing and holding the period key on the right side of the
+            <li>Common punctuation marks can be found by pressing and holding the period key on the right side of the
             space bar.</li>
         </ul>
     </div>
