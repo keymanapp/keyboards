d079fb2c5c7d8029032c74ff329d370f9224405b
cathaylab <142644166+Cathaylaboratory@users.noreply.github.com>
2024-08-17 04:20:25 +0100

    teochew keyboard updating
    ready for review

--- release/t/teochew/source/help/teochew.php
+++ release/t/teochew/source/help/teochew.php
@@ -1,31 +1,39 @@
-<?php
-  $pagename = 'teochew Keyboard Help';
-	$pagetitle = $pagename ;
+<?php 
+  $pagename = 'taichow Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
   require_once('header.php');
+
+
 ?>
 
 <p>
     teochew for Teochow language, Tiê-chiu Pe̍h-ūe-jī.
 <p>
-specific letters: uu = ṳ, nn = ⁿ
+specific letters: w = ṳ, nn = ⁿ
 <p>
-tones: as = á, af = à, aw = â, ax = ã, aj =ā, ahj = a̍h
+tones: as = á, af = à, ar = â, ax = ã, aj =ā, ahj = a̍h
 </P>
 <!-- <p>See <a href="file:taigi_poj.pdf">this document</a> for all the key combinations.</p> -->
 
-<h2> Phonology Map </h2>
+<h1> Phonology Map </h1>
 <p> consonants <p/>
 <img src='consonants.png' alt='Keyboard Layout'>
 <p> vowels and combinations <p/>
 <img src='vowels.png' alt='Keyboard Layout'> <p/>
 <p> tones <p/>
 <img src='tones.png' alt='Keyboard Layout'>
-<h2>Keyboard Layout</h2>
+<h1>Keyboard Layout</h1>
 
 <p><b>Unshifted</b></p>
 <img src='uchauU_.png' alt='Keyboard Layout'>
 <p><b>Shifted</b></p>
 <img src='uchauU_S.png' alt='Keyboard Layout'>
+
+
+
+<p>© 2024 cathaylab</p>
+
 
 
 
