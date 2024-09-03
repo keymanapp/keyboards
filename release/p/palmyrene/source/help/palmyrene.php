0ea79522b020ecaf8f7240899199bfb07f70c4e5
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-09-03 16:57:25 -0500

    [palmyrene] Add new keyboard

---
+++ release/p/palmyrene/source/help/palmyrene.php
@@ -0,0 +1,21 @@
+<?php 
+  $pagename = 'Palmyrene Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+  This keyboard is for the Palmyrene script as used by Official Aramaic.
+</p>
+
+<h2>Desktop Keyboard layout</h2>
+<h3>Default (unshifted)</h3>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-phone' data-states='default numeric'>
+</div>
