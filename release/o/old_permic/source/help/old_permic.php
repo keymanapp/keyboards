584ce81570f79315e13df27026166488ff97303d
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-09-05 10:16:14 -0500

    [old_permic] New Keyboard

---
+++ release/o/old_permic/source/help/old_permic.php
@@ -0,0 +1,20 @@
+<?php 
+  $pagename = 'Old Permic Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+  This keyboard is for the Old Permic script. It is loosely based on a phonetic layout (using a US Qwerty layout).
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot (or green hint) on the top right to reveal and use them.</p>
+
+<div id='osk-phone' data-states='default numeric'>
+</div>
