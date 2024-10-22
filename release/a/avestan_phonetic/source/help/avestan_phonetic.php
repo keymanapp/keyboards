b7f2d5955bf1fc3c916072556d1a9485ea8f2cde
Lorna Evans <LornaSIL@users.noreply.github.com>
2024-10-22 15:04:21 -0500

    [avestan] New keyboard

---
+++ release/a/avestan_phonetic/source/help/avestan_phonetic.php
@@ -0,0 +1,13 @@
+<?php 
+  $pagename = 'Avestan Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+  This is a keyboard for the Avestan language and script. It is based on a phonetic layout. Feedback is welcome.
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
