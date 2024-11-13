f535e5bd2f75b6eaab5f48dc9a4e28efcdcd962b
Oscar Ewen <s3897420@student.rmit.edu.au>
2024-11-13 17:18:49 +1100

    [anglo furthorc english] Initial Submission of anglo furthorc english keyboard

---
+++ release/a/anglo_furthorc_english/source/help/anglo_furthorc_english.php
@@ -0,0 +1,18 @@
+<?php
+  $pagename = 'Anglo Furthorc English Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<h1>Start Using Anglo-Furthorc-English</h1>
+
+<p>
+  Anglo Furthorc keyboard designed to approximately match the QWERTY keyboard layout, based on the runes from https://rune.school
+</p>
+
+<h1>Keyboard Layout</h1>
+
+
+<img src="./englishfuthorcU_.png">
+
+<img src="./englishfuthorcU_S.png">
