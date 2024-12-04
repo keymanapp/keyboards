8af189f447513e223810df7b9f92033008726fb5
Gautam Sengupta <gsghyd@gmail.com>
2024-12-04 07:26:57 +0530

    Add files via upload

---
+++ release/gautami/gautami_thamizh_tamil/source/help/gautami_thamizh_tamil.php
@@ -0,0 +1,22 @@
+<body>
+
+<p>Copyright (c) 2024 Gautam Sengupta [gsghyd@gmail.com]</p>
+<h2>Vowels</h2>
+
+<p><a href="vowels.png"><span style='text-decoration:none'><img border=3 img width="90%" src="vowels.png" alt="Vowels"></span></a></p>
+
+<p style="width:90%"> The keys with superscripts are toggle keys. The superscript numbers indicate the position of the akṣara in the toggle cycle, e.g. n<sup>1</sup> i.e. n > ந, n<sup>2</sup> i.e. nn&nbsp>&nbspன, and n<sup>3</sup> i.e. nnn&nbsp>&nbspஞ. Press each of these keys several times in a row to see how it works.
+<br>
+The key x serves three functions: (1) it breaks a toggle cycle and moves the cursor on to the next akṣara, e.g. ii&nbsp>&nbspஈ, but ixi&nbsp>&nbspஇஇ; (2) it prevents a Pulli from combining two contiguous akṣaras into a single composite akṣara, e.g.  k.S&nbsp>&nbspக்ஷ but k.xS&nbsp>&nbspக்‌ஷ; and (3) it inserts the composite akṣara க்ஷ at the cursor location, e.g. xx&nbsp>&nbspக்ஷ.
+<br>
+<b>Pressing down and holding the Right-Alt key switches the keyboard back to its original QWERTY configuration</b>, e.g. [o]&nbsp>&nbspஒ, but hitting [o] while holding down the Right-Alt key yields o.
+</p>
+<h2>Consonants</h2>
+<p><a href="consonants.png"><span style='text-decoration:none'><img border=3 img width="90%" src="consonants.png" alt="Consonants"></span></a></p>
+
+<h2>Keyboard Layout (default/unshifted)</h2>
+<p><a href="LayoutU_.png"><span style='text-decoration:none'><img border=3 img width="90%" src="LayoutU_.png" alt="Unshifted Keyboard Layout"></span></a></p>
+
+<h2>Keyboard Layout (shifted)</h2>
+<p><a href="LayoutU_S.png"><span style='text-decoration:none'><img border=3 img width="90%" src="LayoutU_S.png" alt="Shifted Keyboard Layout"></span></a></p>
+</body>
