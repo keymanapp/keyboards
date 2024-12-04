18bde51fec7dd4bd67e0ec19060f12883251de53
Gautam Sengupta <gsghyd@gmail.com>
2024-12-04 23:28:12 +0530

    Update gautami_inditran.php

--- release/gautami/gautami_inditran/source/help/gautami_inditran.php
+++ release/gautami/gautami_inditran/source/help/gautami_inditran.php
@@ -12,17 +12,17 @@
 
 <p><a href="translit.png"><span style='text-decoration:none'><img border=3 img width=90% src="translit.png" alt="Transliteration Keymap"></span></a></p>
 
-<p> Use the PERIOD key [.] to get a dot above/below or a ring below a character, e.g. <br> r [.] > ṛ , &nbsp ṛ [.] > r̥ , &nbsp n [.] > ṅ , &nbsp ṅ [.] > ṇ , &nbsp m [.] > ṁ , &nbsp ṁ [.] > ṃ , &nbsp t [.] > ṭ , &nbsp g [.] > ġ etc.
-<br>Use the HYPHEN key [-] to get a macron above or a line below a character or pair of characters, e.g. &nbsp a [-] > ā , &nbsp l [-] > ḻ , &nbsp t [-] > ṯ , &nbsp y [-] > ȳ , &nbsp kh [-] > k͟h.
-<br>Use the TILDE key [~] to get a tilde or Chandrabindu ̐ above a character, e.g. &nbsp a [~] > ã <br> n [~] > ñ , &nbsp m [~] > m̐.
-<br>Use the CARET key [^] to get a circumflex, caron, breve or inverted breve on a character, <br> e.g. &nbsp e [^] > ê , &nbsp o [^] > ô , &nbsp r [^] > r̆ , &nbsp h [^] > ḫ.
-<br><b>All the transliteration symbols, except k͟h and K͟H, may also be obtained by pressing [=] <br> after typing a base character, e.g. &nbsp h [=] > ḥ , &nbsp ḥ [=] > ẖ , &nbsp ẖ [=] > h̤ , &nbsp h̤ [=] > ḫ.</b>
+<p style+"width: 90%"> Use the PERIOD key [.] to get a dot above/below or a ring below a character, e.g. r&nbsp[.]&nbsp>&nbspṛ, ṛ&nbsp[.]&nbsp>&nbspr̥&nbsp, n&nbsp[.]&nbsp>&nbspṅ&nbsp, ṅ&nbsp[.]&nbsp>&nbspṇ&nbsp, m&nbsp[.]&nbsp>&nbspṁ&nbsp, ṁ&nbsp[.]&nbsp>&nbspṃ&nbsp, t&nbsp[.]&nbsp>&nbspṭ&nbsp, g&nbsp[.]&nbsp>&nbspġ etc.
+<br>Use the HYPHEN key [-] to get a macron above or a line below a character or pair of characters, e.g. a&nbsp[-]&nbsp>&nbspā&nbsp, l&nbsp[-]&nbsp>&nbspḻ&nbsp, t&nbsp[-]&nbsp>&nbspṯ&nbsp, y&nbsp[-]&nbsp>&nbspȳ&nbsp, kh&nbsp[-]&nbsp>&nbspk͟h.
+<br>Use the TILDE key [~] to get a tilde or Chandrabindu ̐  above a character, e.g. a&nbsp[~]&nbsp>&nbspã&nbsp, n&nbsp[~]&nbsp>&nbspñ&nbsp, m&nbsp[~]&nbsp>&nbspm̐.
+<br>Use the CARET key [^] to get a circumflex, caron, breve or inverted breve on a character, e.g. e&nbsp[^]&nbsp>&nbspê&nbsp, o&nbsp[^]&nbsp>&nbspô&nbsp, r&nbsp[^]&nbsp>&nbspr̆&nbsp, h&nbsp[^]&nbsp>&nbspḫ.
+<br><b>All the transliteration symbols, except k͟h and K͟H, may also be obtained by pressing [=] after typing a base character, e.g. h&nbsp[=]&nbsp>&nbspḥ&nbsp, ḥ&nbsp[=]&nbsp>&nbspẖ&nbsp, ẖ&nbsp[=]&nbsp>&nbsph̤&nbsp, h̤&nbsp[=]&nbsp>&nbspḫ.</b>
 </p>
 <h2>Transcription Keymap</h2>
 
-<p><b>To access IPA transcription symbols press [\] after typing any base character, e.g. <br> a [\] > ə, &nbsp ə [\] > ɑ, &nbsp ɑ [\] > ä.</b></p>
+<p style="width: 90%"><b>To access IPA transcription symbols press [\] after typing any base character, e.g. a&nbsp[\]&nbsp>&nbspə, ə&nbsp[\]&nbsp>&nbspɑ, ɑ&nbsp[\]&nbsp>&nbspä.</b></p>
 
 <p><a href="transcrip.png"><span style='text-decoration:none'><img border=3 img width=90% src="transcrip.png" alt="Transcription Keymap"></span></a></p>
-<p><b>Pressing down and holding the Right-Alt key switches the keyboard back to its <br> original QWERTY configuration</b>, e.g. &nbsp [o~] > õ, but typing o and then holding down the <br> Right-Alt key while typing ~ yields o~.</p>
+<p style="width: 90%"><b>Pressing down and holding the Right-Alt key switches the keyboard back to its original QWERTY configuration</b>, e.g. [o~]&nbsp>&nbspõ, but typing o and then holding down the Right-Alt key while typing ~ yields o~.</p>
 <br> 
 </body>
