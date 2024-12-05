56871854d91d9088d5fa62e03dff1aac1b11ce89
Gautam Sengupta <gsghyd@gmail.com>
2024-12-05 06:47:27 +0530

    Update gautami_thamizh_tamil.php
    Fixing superscripts

--- release/gautami/gautami_thamizh_tamil/source/help/gautami_thamizh_tamil.php
+++ release/gautami/gautami_thamizh_tamil/source/help/gautami_thamizh_tamil.php
@@ -1,6 +1,11 @@
 <?php
   $pagename = 'Gautami Thamizh/Tamil Help';
   $pagetitle = $pagename;
+  $style =
+    sup {
+      vertical-align: super;
+      font-size: smaller;
+    }
   require_once('header.php');
 ?>
 
@@ -9,11 +14,13 @@
 
 <p><a href="vowels.png"><span style='text-decoration:none'><img border=3 img width="90%" src="vowels.png" alt="Vowels"></span></a></p>
 
-<p style="width:90%"> The keys with superscripts are toggle keys. The superscript numbers indicate the position of the akṣara in the toggle cycle, e.g. n<sup>1</sup> i.e. n > ந, n<sup>2</sup> i.e. nn&nbsp>&nbspன, and n<sup>3</sup> i.e. nnn&nbsp>&nbspஞ. Press each of these keys several times in a row to see how it works.
-<br>
-The key x serves three functions: (1) it breaks a toggle cycle and moves the cursor on to the next akṣara, e.g. ii&nbsp>&nbspஈ, but ixi&nbsp>&nbspஇஇ; (2) it prevents a Pulli from combining two contiguous akṣaras into a single composite akṣara, e.g.  k.S&nbsp>&nbspக்ஷ but k.xS&nbsp>&nbspக்‌ஷ; and (3) it inserts the composite akṣara க்ஷ at the cursor location, e.g. xx&nbsp>&nbspக்ஷ.
-<br>
-<b>Pressing down and holding the Right-Alt key switches the keyboard back to its original QWERTY configuration</b>, e.g. [o]&nbsp>&nbspஒ, but hitting [o] while holding down the Right-Alt key yields o.
+<p style="width:90%"> The keys with superscripts are toggle keys. The superscript numbers indicate the position of the akṣara in the toggle cycle, 
+  e.g. n<sup>1</sup> i.e. n > ந, n<sup>2</sup> i.e. nn&nbsp>&nbspன, and n<sup>3</sup> i.e. nnn&nbsp>&nbspஞ. Press each of these keys several times in a row to see how it works.
+</p>
+<p> The key x serves three functions: (1) it breaks a toggle cycle and moves the cursor on to the next akṣara, e.g. ii&nbsp>&nbspஈ, but ixi&nbsp>&nbspஇஇ; (2) it prevents a Pulli 
+  from combining two contiguous akṣaras into a single composite akṣara, e.g.  k.S&nbsp>&nbspக்ஷ but k.xS&nbsp>&nbspக்‌ஷ; and (3) it inserts the composite akṣara க்ஷ at the cursor location, e.g. xx&nbsp>&nbspக்ஷ.
+</p>
+<p><b>Pressing down and holding the Right-Alt key switches the keyboard back to its original QWERTY configuration</b>, e.g. [o]&nbsp>&nbspஒ, but hitting [o] while holding down the Right-Alt key yields o.
 </p>
 <h2>Consonants</h2>
 <p><a href="consonants.png"><span style='text-decoration:none'><img border=3 img width="90%" src="consonants.png" alt="Consonants"></span></a></p>
