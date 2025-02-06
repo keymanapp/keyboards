eabf048d20b7b4f48e5975fc4639cc95aa915da4
KenK <KenK-3-21@users.noreply.github.com>
2025-02-06 11:12:51 -0600

    Add files via upload

---
+++ release/b/budukh/budukh/source/help/budukh.php
@@ -0,0 +1,182 @@
+<?php 
+  $pagename = 'Budukh Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+
+Budukh keyboard developed by Ken Keyes, and prepared according to the alphabet devised by Adıgözəl Haciyev. Please give any suggestions or comments to https://www.budatlar.com/. Enjoy!</p>
+<p>Ken Keyes tərəfindən hazırlanmış Buduq klaviaturası və Adıgözəl Haciyev tərəfindən hazırlanan əlifba əsasında hazırlanmışdır. Zəhmət olmasa təklif və ya şərhlərinizi https://www.budatlar.com/ ünvanına verin. Zövq alın!
+
+</p>
+
+<h1>Keyboard Layout</h1>
+<p>
+    Lowercase
+</p>
+
+<img src="BudukhU_.png">
+
+<p>
+    Uppercase
+</p>
+
+<img src="BudukhU_S.png">
+
+<p>
+    Right Alt State
+</p>
+
+<img src="BudukhU_RA.png">
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>How to use
+the <span class=SpellE>Budugh</span> keyboard. The <span class=SpellE>Budugh</span>
+keyboard is a <span class=SpellE>rota</span>-based keyboard. That means, if you
+press a key for a special character more than once, you will get a different
+letter or combination of letters. After the final turn, the key returns to its
+original letter. <span style='mso-spacerun:yes'> </span>Additionally, the
+accent key produces the apostrophe symbol.<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:AZ-LATIN'>` = '<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>Below is a
+list of the characters produced by the <span class=SpellE>rota</span><o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>a 1x = a ; 2x = ä 3x = a<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>e 1x = e ; 2x = </span><span lang=AZ-LATIN
+style='font-size:10.0pt;line-height:107%;mso-ansi-language:AZ-LATIN'>&#601;</span><span
+lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:PT-BR'>
+3x = e<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>o 1x = o ; 2x = </span><span lang=AZ-LATIN
+style='font-size:10.0pt;line-height:107%;mso-ansi-language:AZ-LATIN'>ö</span><span
+lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:PT-BR'>
+3x = o<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:9.0pt;line-height:107%;
+font-family:Consolas;mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:
+"Times New Roman";color:black;mso-ansi-language:PT-BR;mso-bidi-language:AR-SA'>u
+</span><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:
+PT-BR'>1x = u ; 2x = </span><span lang=AZ-LATIN style='font-size:10.0pt;
+line-height:107%;mso-ansi-language:AZ-LATIN'>ü</span><span lang=AZ-LATIN
+style='font-size:10.0pt;line-height:107%;mso-ansi-language:PT-BR'> 3x = u<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:AZ-LATIN'>q 1x = q ; 2x = q' ; 3x = qh ; 4x = q <o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>t 1x = <span
+class=GramE>t<span style='mso-spacerun:yes'>  </span>2</span>x = t'<span
+style='mso-spacerun:yes'>  </span>3x = t<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>p 1x = p 2x
+= p' 3x = p<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>s 1x = <span
+class=GramE>s ;</span> 2x = <span class=SpellE>ts</span> 3x = <span
+class=SpellE>ts'</span> 4x = ss 5x = s<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>g 1x = g; 2x
+= &#287;; 3x = <span class=GramE>&#705; ;</span> 4x = ° 5x = g<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:AZ-LATIN'>h 1x = h; 2x = &#295;; 3x = &#704; ; 4x = h<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:AZ-LATIN'>k 1x = k 2x = k' 3x = k<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>x 1x = x 2x =
+<span class=SpellE>xh</span> 3x = x<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>c 1x = c 2x
+= ç 3x = ç' 4x = c<o:p></o:p></span></p>
+
+<p class=MsoNormal><span class=SpellE><span style='font-size:10.0pt;line-height:
+107%'>Buduq</span></span><span style='font-size:10.0pt;line-height:107%'> <span
+class=SpellE>klaviaturas&#305;ndan</span> <span class=SpellE>nec&#601;</span> <span
+class=SpellE>istifad&#601;</span> <span class=SpellE>olunur</span>. Cari <span
+class=SpellE>Buduq</span> <span class=SpellE>klaviaturas&#305;</span> <span
+class=SpellE>növb&#601;</span> <span class=SpellE>&#601;sasl&#305;</span> <span
+class=SpellE>bir</span> <span class=SpellE>klaviaturad&#305;r</span>. <span
+class=SpellE>Y&#601;ni</span> <span class=SpellE>xüsusi</span> <span
+class=SpellE>bir</span> <span class=SpellE>simvol</span> <span class=SpellE>üçün</span>
+<span class=SpellE>bir</span> <span class=SpellE>düym&#601;ni</span> <span
+class=SpellE>bir</span> <span class=SpellE>d&#601;f&#601;d&#601;n</span> <span
+class=SpellE>çox</span> <span class=SpellE>basarsan&#305;z</span>, <span
+class=SpellE>f&#601;rqli</span> <span class=SpellE>bir</span> <span
+class=SpellE>h&#601;rf</span> <span class=SpellE>v&#601;</span> <span
+class=SpellE>ya</span> <span class=SpellE>h&#601;rf</span> <span class=SpellE>birl&#601;&#351;m&#601;sini</span>
+<span class=SpellE>alacaqs&#305;n&#305;z</span>. Son <span class=SpellE>növb&#601;d&#601;n</span>
+<span class=SpellE>sonra</span> <span class=SpellE>düym&#601;</span> <span
+class=SpellE>orijinal</span> <span class=SpellE>h&#601;rf&#601;</span> <span
+class=SpellE>qay&#305;d&#305;r</span>. <span class=SpellE>Bundan</span> </span><span
+lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:AZ-LATIN'>&#601;lav&#601;,
+</span><span class=SpellE><span style='font-size:10.0pt;line-height:107%'>vurgu</span></span><span
+style='font-size:10.0pt;line-height:107%'> <span class=SpellE>düym&#601;si</span>
+<span class=SpellE>basanda</span> <span class=SpellE>ayr&#305;ca</span> <span
+class=SpellE>apostrof</span> <span class=SpellE>simvolu</span> <span
+class=SpellE>c&#305;xar&#305;r</span>. <o:p></o:p></span></p>
+
+<p class=MsoNormal><span class=SpellE><span style='font-size:10.0pt;line-height:
+107%'>A&#351;a&#287;&#305;da</span></span><span style='font-size:10.0pt;
+line-height:107%'> <span class=SpellE>növb&#601;</span> <span class=SpellE>ç&#305;xaran</span>
+<span class=SpellE>simvollar&#305;n</span> <span class=SpellE>siyah&#305;s&#305;</span>
+<span class=SpellE>verilmi&#351;dir</span>:<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:AZ-LATIN'>` = '<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>a 1x = a ; 2x = ä 3x = a<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>e 1x = e ; 2x = </span><span lang=AZ-LATIN
+style='font-size:10.0pt;line-height:107%;mso-ansi-language:AZ-LATIN'>&#601;</span><span
+lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:PT-BR'>
+3x = e<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>o 1x = o ; 2x = </span><span lang=AZ-LATIN
+style='font-size:10.0pt;line-height:107%;mso-ansi-language:AZ-LATIN'>ö</span><span
+lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:PT-BR'>
+3x = o<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:9.0pt;line-height:107%;
+font-family:Consolas;mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:
+"Times New Roman";color:black;mso-ansi-language:PT-BR;mso-bidi-language:AR-SA'>u
+</span><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;mso-ansi-language:
+PT-BR'>1x = u ; 2x = </span><span lang=AZ-LATIN style='font-size:10.0pt;
+line-height:107%;mso-ansi-language:AZ-LATIN'>ü</span><span lang=AZ-LATIN
+style='font-size:10.0pt;line-height:107%;mso-ansi-language:PT-BR'> 3x = u<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>q 1x = q ; 2x = q' ; 3x = qh ; 4x = q <o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>t 1x = <span
+class=GramE>t<span style='mso-spacerun:yes'>  </span>2</span>x = t'<span
+style='mso-spacerun:yes'>  </span>3x = t<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>p 1x = p 2x
+= p' 3x = p<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>s 1x = <span
+class=GramE>s ;</span> 2x = <span class=SpellE>ts</span> 3x = <span
+class=SpellE>ts'</span> 4x = ss 5x = s<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>g 1x = g; 2x
+= &#287;; 3x = <span class=GramE>&#705; ;</span> 4x = ° 5x = g<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>h 1x = h; 2x = &#295;; 3x = &#704; ; 4x = h<o:p></o:p></span></p>
+
+<p class=MsoNormal><span lang=AZ-LATIN style='font-size:10.0pt;line-height:107%;
+mso-ansi-language:PT-BR'>k 1x = k 2x = k' 3x = k<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>x 1x = x 2x =<span class=SpellE>xh</span> 3x = x<o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='font-size:10.0pt;line-height:107%'>c 1x = c 2x
+= ç 3x = ç' 4x = c<o:p></o:p></span></p>
