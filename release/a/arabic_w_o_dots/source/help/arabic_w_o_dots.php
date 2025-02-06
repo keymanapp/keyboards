f9768205e904ed9b0c4139d71da3ec13f1c85bd1
عٮـٮـد الرحمں <3ubaidUrRe7man@protonmail.com>
2025-02-06 21:03:07 +0500

    Add files via upload

--- release/a/arabic_w_o_dots/source/help/arabic_w_o_dots.php
+++ release/a/arabic_w_o_dots/source/help/arabic_w_o_dots.php
@@ -1,42 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Start Using Arabic W/O Dots</title>
-  <style type="text/css">
-    p { font: 10pt Tahoma; }
-    h1 { font: bold 16pt Tahoma; color: #4444cc; margin-bottom: 2px }
-    h2 { font: bold 12pt Tahoma; color: #4444cc; }
-  </style>
-</head>
-<body>
+<?php 
+  $pagename = 'Arabic W/O Dots Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
 
-<h1>Start Using Arabic W/O Dots</h1>
 
 <p>
-  Arabic Alphabet without the dots like the Olden Days
+   Arabic Alphabet without the dots like the Olden Days 
 </p>
 
-<h1>Keyboard Layout</h1>
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift alt alt-shift '>
+    <p>
+        <img src="typing-guide-desktop.png" alt="" srcset="">
+    </p>
+    <p>
+        <img src="typing-guide-desktop-shifted.png" alt="" srcset="">
+    </p>
+    <p>
+        <img src="typing-guide-desktop-alternate.png" alt="" srcset="">
+    </p>
+    <p>
+        <img src="typing-guide-desktop-alternate-shifted.png" alt="" srcset="">
+    </p>
+</div>
 
-<!-- Insert Keyboard Layout Images or HTML here -->
- <div>
-  <p style="flex-direction: row;">
+<h2>Mobile/Phone Keyboard Layout</h2>
+
+<div id='osk-phone' data-states='default symbols extra'>
+<p style="flex-direction: row;">
     <ul>Single press is main character displayed and Double tap is the character displayed on the top-right corner of the key.</ul>
     <ul>In case of "ا", swipe up is for hamza above it and swipe down is for hamza below; same goes for "و" and "ي" (with 'Waaw' double tap is for 'Hamza' above, swipe up and down is for small 'Waaw' up and down respectively; swipe up for 'hamza' above). In case of dotless Baa'"ب", double tap for Baa'"ب", swipe up for Taa'"ت" and swipe down for Thaa'"ث".</ul>
     <ul>In case of "ح", swipe up for Khaa'"خ" and swipe down for Jeem"ج". In case of dot".", swipe up is Fatha, swipe down is Kasra, swipe right is Damma, and swipe left is Sukuun.</ul>
     <ul>In case of comma"،", swipe up is 'Fathataan', swipe down is 'Kasrataan', swipe right is 'Dammataan', and swipe left is 'Shadda'. In case of Question mark"؟", swipe up is Small 'Alif' above and swipe down is small 'Alif' below.</ul>
     <ul>Long pressing all of them would just give some other less common characters.</ul>
-    <img src="help/typing-guide.jpg" alt="Arabic Mobile Keyboard Layout">
+    <img src="typing-guide.jpg" alt="Arabic Mobile Keyboard Layout">
   </p>
   <p style="flex-direction: row;">
     <ul>In case of numbers, single press in Arabic Numerals and double tap is Hindi Numerals, and long press is Fractions.</ul>
     <ul>As for others, single press is main symbol while double tap is character displayed on the top-right corner of the key; while long press is all of rest of characters.</ul>
-    <img src="help/typing-guide-numbers.jpg" alt="Arabic Mobile Keyboard Numbers" srcset="">
+    <img src="typing-guide-numbers.jpg" alt="Arabic Mobile Keyboard Numbers" srcset="">
   </p>
   <p>
-    <table>
+  <table>
       <tr>
         <th>Long Press LTR</th>
         <th>FullForm</th>
@@ -91,9 +97,5 @@
         <td>Pop Directional Formatting</td>
       </tr>
     </table>
-    <img src="help/typing-guide-symbols.jpg" alt="Arabic Mobile Keyboard Symbols" srcset="">
-  </p>
- </div>
-
-</body>
-</html>
+    <img src="typing-guide-symbols.jpg" alt="Arabic Mobile Keyboard Symbols" srcset="">
+</div>
