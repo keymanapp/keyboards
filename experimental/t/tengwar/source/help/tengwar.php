7e6e82c95607fe5080d50f571335f40f0bd23d02
عٮـٮـد الرحمں <3ubaidUrRe7man@protonmail.com>
2025-02-07 18:48:53 +0500

    Add files via upload

---
+++ experimental/t/tengwar/source/help/tengwar.php
@@ -0,0 +1,17 @@
+<?php
+    $pagename = 'Tengwar Keyboard Help';
+    $pagetitle = $pagename;
+    require_once('header.php');
+?>
+
+<p>
+    Tengwar
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id="osk" data-states='default shift rightalt rightalt-shift'>
+    <img src="typing-guide-desktop.png" alt="" srcset=""><br>
+    <img src="typing-guide-desktop-shifted.png" alt="" srcset=""><br>
+    <img src="typing-guide-desktop-alternate.png" alt="" srcset=""><br>
+    <img src="typing-guide-desktop-alternate-shifted.png" alt="" srcset="">
+</div>
