d1e46295284ca89ba8795f5c777966625636cb83
Lorna Evans <LornaSIL@users.noreply.github.com>
2025-02-11 11:32:24 -0600

    [tengwar] Link to new keyboard from old help file

--- legacy/t/tengwar/source/help/tengwar.php
+++ legacy/t/tengwar/source/help/tengwar.php
@@ -9,6 +9,12 @@
     This package contains two keyboards for entering Tengwar data coded according to the ConScript proposal of Michael Everson. One keyboard supports PUA characters in the BMP. The other keyboard supports PUA characters in Plane 15. No fonts are included in the package.
 </p>
 
+<p>
+    The <a href="https://keyman.com/keyboards/tengwar_quenya">https://keyman.com/keyboards/tengwar_quenya</a>
+ keyboard may be a better option.
+</p>
+
+  <h2>Desktop Keyboard layout</h2>
 	<h3>Default (unshifted)</h3>
 	<p><a href="LayoutU_.png"><img class="keyboard" src="LayoutU_.png" alt="Default (unshifted) state" /></a></p>
 	<h3>Shift</h3>
