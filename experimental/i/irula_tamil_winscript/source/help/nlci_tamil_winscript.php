634aa0342ba438c9a0e0c77fe45b9ea60ea0033b
DavidLRowe <DavidLRowe@users.noreply.github.com>
2025-02-14 16:06:06 -0900

    Update nlci_tamil_winscript.php

--- experimental/i/irula_tamil_winscript/source/help/nlci_tamil_winscript.php
+++ experimental/i/irula_tamil_winscript/source/help/nlci_tamil_winscript.php
@@ -80,7 +80,6 @@
 
 
     </style>
-</head>
 
 
     <p>This keyboard is based on the Tamil WinScript (NLCI) Keyboard Layout. This is a phonetic based keyboarding system designed with the objective of keyboarding text with minimal keystrokes. Keys are also layed out according the frequency of occurance, yet tried to follow a phonetic based keyboarding system. This keyboard includes experimental support for the Irula language.</p>
