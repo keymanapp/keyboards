af89ad09c2b6bc34a7889ceb27ce49f9ed968c5c
Lorna Evans <LornaSIL@users.noreply.github.com>
2025-02-20 08:18:29 -0600

    Update easy_arabic.php
    Remove `osk` syntax since author prefers graphics.

--- release/e/easy_arabic/source/help/easy_arabic.php
+++ release/e/easy_arabic/source/help/easy_arabic.php
@@ -7,12 +7,11 @@
 <p>Changes some keys to make it easier to type.</p>
 
 <h2>Desktop Keyboard Layout</h2>
-<div id='osk' data-states='default shift rightalt rightalt-shift'>
 
 <img src="layout.png" alt="" srcset=""><br>
  <img src="layout-shifted.png" alt="" srcset=""><br>
  <img src="layout-alternate.png" alt="" srcset=""><br>
  <img src="layout-alternate-shifted.png" alt="" srcset="">
 
-</div>
+
 
