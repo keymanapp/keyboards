40e80788ec45025861ba57cff81c9483db869f2a
nm <fhdfha@hhb.com>
2025-03-11 15:43:01 +0000

    Mingrelian Cyrillic

---
+++ release/m/mingrelian_cyrillic/README.md
@@ -0,0 +1,30 @@
+Mingrelian Cyrillic keyboard
+==============
+
+© SIL Global
+
+Version 1.0
+
+Description
+-----------
+
+Mingrelian Cyrillic generated from template
+
+Links
+-----
+https://keyman.com/keyboards/mingrelian_cyrillic
+
+Supported Platforms
+-------------------
+ * Windows
+ * macOS
+ * Linux
+ * Web
+ * iPhone
+ * iPad
+ * Android phone
+ * Android tablet
+ * Mobile devices
+ * Desktop devices
+ * Tablet devices
+
---
+++ release/m/mingrelian_cyrillic/source/help/mingrelian_cyrillic.php
@@ -0,0 +1,22 @@
+<?php 
+  $pagename = 'Mingrelian Cyrillic Keyboard Help';
+  $pagetitle = 'Mingrelian Cyrillic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+<p>
+    Mingrelian Cyrillic.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+
+<p>© SIL Global</p>
