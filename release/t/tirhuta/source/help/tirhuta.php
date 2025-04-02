ef998e7b7939826e352a8a7413f741ecae5ebc6b
Darcy Wong <darcy_wong@sil.org>
2025-04-02 07:53:09 +0700

    [tirhuta]: Convert help to php

--- release/t/tirhuta/source/help/tirhuta.php
+++ release/t/tirhuta/source/help/tirhuta.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Start Using Tirhuta</title>
-  <style type="text/css">
-    p { font: 10pt Tahoma; }
-    h1 { font: bold 16pt Tahoma; color: #4444cc; margin-bottom: 2px }
-    h2 { font: bold 12pt Tahoma; color: #4444cc; }
-  </style>
-</head>
-<body>
+<?php
+  $pagename = 'Tirhuta Keyboard Help';
+  $pagetitle = $pagename;
+  $style = <<<END
+p { font: 10pt Tahoma; }
+h1 { font: bold 16pt Tahoma; color: #4444cc; margin-bottom: 2px }
+h2 { font: bold 12pt Tahoma; color: #4444cc; }
 
-<h1>Start Using Tirhuta</h1>
+END;
+
+  require_once('header.php');
+?>	
 
 <p>
     Tirhuta 1.0 (INSCRIPT) Keyboard.
@@ -21,6 +18,3 @@ This is a Tirhuta aka Mithilakshar (INSCRIPT) keyboard layout for typing the Mai
 <h1>Keyboard Layout</h1>
 
 <!-- Insert Keyboard Layout Images or HTML here --><img src= "L1.png">
-
-</body>
-</html>
