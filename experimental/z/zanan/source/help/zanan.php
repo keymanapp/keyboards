c517dc1d475b2d24081d448946b55bb8787161e2
Oreen Yousuf <oreyo864@user.uu.se>
2025-04-03 12:03:44 +0200

    Hausa keyboard using Zanan script

---
+++ experimental/z/zanan/source/help/zanan.php
@@ -0,0 +1,24 @@
+<?php
+$pagename = Zanan Keyboard Help';
+$pagetitle = $pagename;
+// Header
+require_once('header.php');
+?>
+
+<p>
+Hausa language keyboard written in the Zanan script.
+</p>
+
+<h1>Keyboard Layout</h1>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press.
+Press and hold on the key with a little dot on the top right to reveal them.
+</p>
+
+<div id='osk-phone' data states='default shift numeric'>
+</div>
