0ad832a523af2f5cf4b99f2c01fe6e47fe26cc85
Oreen Yousuf <oreyo864@user.uu.se>
2025-05-22 10:13:50 +0200

    Moved files from nti/ to soninke_nti/ and removed old nti folder. Removed duplicate .kmn code. REnamed .kps package name to Soninke N'ti to be consistent. Added -Zzzz script tag. Fixed .php file desktop osk syntax.

--- experimental/s/soninke_n_ti/source/help/soninke_n_ti.php
+++ experimental/s/soninke_n_ti/source/help/soninke_n_ti.php
@@ -13,7 +13,7 @@ require_once('header.php');
 <h1>Keyboard Layout</h1>
 
 <h2>Desktop Keyboard Layout</h2?
-<div id='osk' data-states='default shift alt shift-alt'>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
