21795606791869007fa1223f2188cd8cad454a49
Lorna Evans <LornaSIL@users.noreply.github.com>
2025-06-11 13:13:00 -0500

    [mingrelian_cyrillic] Modify touch to use longpress instead of more layers

--- release/m/mingrelian_cyrillic/source/help/mingrelian_cyrillic.php
+++ release/m/mingrelian_cyrillic/source/help/mingrelian_cyrillic.php
@@ -12,6 +12,7 @@
 </div>
 
 <h2>Mobile/Tablet Keyboard Layout</h2>
-<div id='osk-tablet' data-states='default shift rightalt rightalt-shift'>
+<p>Due to the size and number of keys, some characters are hidden in the long press. Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+<div id='osk-tablet' data-states='default shift'>
 </div>
 
