1d9ddd3605fe29869e5a035b3034233302345570
Lorna Evans <LornaSIL@users.noreply.github.com>
2025-10-01 08:57:02 -0500

    Update hoisan.php
    Clean up html (using closing paragraphs rather than opening ones).

--- experimental/h/hoisan/source/help/hoisan.php
+++ experimental/h/hoisan/source/help/hoisan.php
@@ -1,22 +1,19 @@
 <?php 
   $pagename = 'hoisan Keyboard Help';
   $pagetitle = $pagename;
-  // Header we will tidy up later  
   require_once('header.php');
-
 
 ?>
 <p>
   hoisan for Hòi Sán Vạ Coc Ngũi Tụ (Hoisanese). 
-<p>
+</p>
   <p>the Script of National Language for Hoisanese台山話國語字</p>
- <p> 台山話國語字係一種羅馬化嘅粵語文字 <p>
-    <p> Hòi Sán Vạ Coc Ngũi Tụ hại yât chủng Lò-Mã phá ké Oạt Ngũi mồn tụ. <p>
-<p>
+ <p> 台山話國語字係一種羅馬化嘅粵語文字 </p>
+    <p> Hòi Sán Vạ Coc Ngũi Tụ hại yât chủng Lò-Mã phá ké Oạt Ngũi mồn tụ. </p>
 
 <h1>音韻表 phonologic map</h1>
-<p> 建議以下嘅台山話國語字音韻表打字<p>
-<p> The suggested form of spelling of the Script of National Language for Hoisanese<p></p>
+<p> 建議以下嘅台山話國語字音韻表打字</p>
+<p> The suggested form of spelling of the Script of National Language for Hoisanese</p>
 
 <h1>韻首consonants</h1>
 <img src='consonants.png' alt='Keyboard Layout'>
@@ -33,7 +30,7 @@
 <h1>韻腹vowels</h1>
 <img src='vowels.png' alt='Keyboard Layout'>
 <p>如果韻腹i單獨出現，音調係陰聲í ỉ，頭先嘅韻首係y，咁韻首y可以俾省略</p>
-<p>If vowel "i" appear alone, the tone is a high tone, such as "í" or "ỉ", and the consonant is "y", then the consonant "y" can be omitted.
+<p>If vowel "i" appear alone, the tone is a high tone, such as "í" or "ỉ", and the consonant is "y", then the consonant "y" can be omitted.</p>
 <p>喺台山話國語字系統入面，使用正寫"iêng"同韻腹"ênh"嘅作用一樣，正寫"iêc"同韻腹"êc"嘅作用一樣</p>
  <p>In the Hoisanese National Language system, the spelling "iêng" has the same effect with "ênh" sound, the spelling "iêc" has the same effect with "êc" sound</p>
  <p>使用正寫"im"同韻腹"âm"嘅作用一樣，正寫"in"同韻腹"ân"嘅作用一樣，正寫"iu"同韻腹"ưu"嘅作用一樣</p>
@@ -44,10 +41,7 @@
  <p>the spelling "ôi" has the same effect with "ui" sound</p>
 <h1>音調tones</h1>
 <img src='tones.png' alt='Keyboard Layout'>
-
-
-</p>
-<p>Specific Alphabet: 特別字母<p>
+<p>Specific Alphabet: 特別字母</p>
 <table>
 <tbody>
 <tr>
@@ -88,9 +82,9 @@
 </tbody>
 </table>
 
-<p>tone rules: 音調規則<p>
-<p> 對應粵拼嘅第一聲同第三聲打s，第二聲打r， 第四聲打f，第五聲打x，第六聲打j <p>
-<p> tone 2 = r, tone 1 and tone 3 = s, tone 4 = f, tone 5 = x, tone 6 = j <p>
+<p>tone rules: 音調規則</p>
+<p> 對應粵拼嘅第一聲同第三聲打s，第二聲打r， 第四聲打f，第五聲打x，第六聲打j </p>p>
+<p> tone 2 = r, tone 1 and tone 3 = s, tone 4 = f, tone 5 = x, tone 6 = j </p>p>
 <h2>Tonal diacritics</h2>
 
 <table>
@@ -166,18 +160,18 @@
 </tbody>
 </table>
 
-<p>examples: 舉例： <p>
+<p>examples: 舉例： </p>
 <p> ar > ả, as > á, af > à, ax > ã, aj > ạ <p>
-<p> 譬如話：‘兩’ 嘅羅馬字係‘lểnh’， 咁輸入內容就係‘leenhr’<p>
+<p> 譬如話：‘兩’ 嘅羅馬字係‘lểnh’， 咁輸入內容就係‘leenhr’</p>
 
 
-<p> 雙擊音調鍵同特殊轉化鍵，取消轉化，變返原本嘅拼寫 <p>
-<p> doubletaping leads to generate original letters:  <p>
+<p> 雙擊音調鍵同特殊轉化鍵，取消轉化，變返原本嘅拼寫 </p>
+<p> doubletaping leads to generate original letters:  </p>
   <p>
     If more than one tone marking key is pressed, the last one will be used. For example, typing <code>asf</code> will return "à". To write a tone marking key as a normal character, one has to press it twice: <code>her</code> becomes <code>hẻ</code>, while <code>herr</code> becomes <code>her</code>.
   </p>
-<p> 使用Z鍵取消音調轉換 <p>
-<p> tap Z key to remove tonations  <p>
+<p> 使用Z鍵取消音調轉換 </p>
+<p> tap Z key to remove tonations  
 
 
 <a href="https://github.com/Cathaylaboratory/quokyuzi/blob/main/quocnguytihoisanva.dict.yaml">台山話國語字字典dictionary for Hoisanese Coc Ngũy Tụ</a>
@@ -185,10 +179,10 @@
 <!-- <p>See <a href="file:taigi_poj.pdf">this document</a> for all the key combinations.</p> -->
 
 
-<p>範文：南國山河 Nàm Coc Sán Hò<p>
-<p>南國山河南帝居 Nàm Coc Sán Hò Nàm Ái Cúi<p>
-<p>截然定分在天書 Tiệt Ngình Ịnh Phốn Tọi Hính Sí<p>
-<p>如何逆虜來侵犯 Ngùi Hò Ngịch Lũ Lòi Xấm Phạn<p>
-<p>汝等行看取敗虛 Nỹ Ảnh Hành Hón Xủi Bại Húi<p>
+<p>範文：南國山河 Nàm Coc Sán Hò</p>
+<p>南國山河南帝居 Nàm Coc Sán Hò Nàm Ái Cúi</p>
+<p>截然定分在天書 Tiệt Ngình Ịnh Phốn Tọi Hính Sí</p>
+<p>如何逆虜來侵犯 Ngùi Hò Ngịch Lũ Lòi Xấm Phạn</p>
+<p>汝等行看取敗虛 Nỹ Ảnh Hành Hón Xủi Bại Húi</p>
 
 <p>© cathaylab</p>
