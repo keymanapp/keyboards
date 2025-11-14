dae178ebf303ca9f941cc0406f1c39ed4b11c943
Darcy Wong <darcy_wong@sil.org>
2025-11-14 11:05:36 +0700

    [sil_taang] Add touch layout

--- release/sil/sil_taang/source/help/sil_taang.php
+++ release/sil/sil_taang/source/help/sil_taang.php
@@ -14,3 +14,12 @@ This keyboard is designed for Rumai Palaung language spoken in Myanmar and many 
 
 <div id='osk' data-states='default shift'>
 </div>
+
+<h2>Mobile Keyboard Layout</h2>
+
+<p>Press the top left key (dotted circle) to combine consonants.</p>
+<p>Flick down on the top row of keys on the default layer to get numbers.</p>
+<p>Several punctuation characters are on the longpress of the "." key.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
