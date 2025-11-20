657fba746b115ec62cda6dcca070561138010710
thohahente <66953205+thohahente@users.noreply.github.com>
2025-11-19 20:24:57 -0500

    Review issues fixed tfnalgonquin
    Add final tfnalgonquin keyboard folder structure per SIL reviewal tfnalgonquin keyboard folder structure per SIL review

---
+++ release/t/tfnalgonquin/source/help/tfnjalgonquin.php
@@ -0,0 +1,10 @@
+<?php 
+  $pagename = 'TFN Algonquin Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+<p>Keyboard for the Standardized Algonquin language as used at Temiskaming First Nation.</p>
+
+<h2>/Users/thohahente/Projects/keyboards/release/t/tfnalgonquin/Source/images/keyboard small.png</h2>
+<div id='osk' data-states='default shift'>
+</div>
