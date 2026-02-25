37c0d6a2e16086ad2f6306032e23bc877d71bfb9
DavidLRowe <DavidLRowe@users.noreply.github.com>
2026-02-25 09:24:28 -0900

    fix typo in tay_latn.php

--- release/t/tay_latn/source/help/tay_latn.php
+++ release/t/tay_latn/source/help/tay_latn.php
@@ -8,7 +8,7 @@
 </p>
 
 <ul>
-  <li>a or A followed by w or W produces ă or Ă and o, O, u, or followed by w or W produces the vowel with a horn.</li>
+  <li>a or A followed by w or W produces ă or Ă and o, O, u, or U followed by w or W produces the vowel with a horn.</li>
   <li>Typing an "a", "e" or "o" twice produces the vowel with a circumflex. Typing an "o" a third time will convert to "oo".</li>
   <li>Any vowel (aAeEiIoOuUyYâÂêÊôÔăĂăĂơƠưƯ) followed by "f", "s", "r", "j", "x" will produce the vowel plus grave, acute, hook above, dot below, or tilde above respectively.</li>
   <li>Any vowel (aAeEiIoOuUyYâÂêÊôÔăĂăĂơƠưƯ) followed by "q" or "k" will produce the vowel plus macron below or double grave above respectively.</li>
@@ -19,3 +19,4 @@
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift'>
 </div>
+
