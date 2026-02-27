b096702f4b558b5bfccdb8bb730319f734bc9b9d
SEKOU GORO <gorosekoi@gmail.com>
2026-02-27 23:46:32 +0300

    Fix RALT, case sensitivity and help/bambara.php as requested

---
+++ release/b/bambara/source/help/bambara.php
@@ -0,0 +1,14 @@
+<?php 
+  $pagename = 'Bambara Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+<p>
+  Bamanankan (Bamanankan) sɛbɛn ka ɲɛ Windows, Mac, iOS ani Android kan. Nin klaviye in bɛ Bamanankan alfabɛ dafalen di aw ma k'a fara sigidin kɛrɛnkɛrɛnlenw kan(Ɛɛ, Ŋŋ, Ɔɔ).
+
+Type Bambara (Bamanankan) accurately on Windows, Mac, iOS and Android. This keyboard puts the full alphabet-Including special characters(Ɛɛ, Ŋŋ, Ɔɔ).
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt'>
+</div>
