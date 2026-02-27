cd3c61ef4292a173b2ccb9ad9f2feb257aff00e1
Lorna Evans <LornaSIL@users.noreply.github.com>
2026-02-27 11:41:25 -0600

    [farsi_phonetic] New keyboard based on MSKLC

---
+++ release/f/farsi_phonetic/source/help/farsi_phonetic.php
@@ -0,0 +1,16 @@
+<?php 
+  $pagename = 'Farsi Phonetic Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+  Persian phonetic keyboard which also supports related languages.
+</p>
+
+<p>The standard "\" is a deadkey. This can be typed preceding a space or any of the following characters: space, zero, waw, heh, fatha, alef, guillemet open, guillemet closed. The resulting characters will be backslash, latin zero, alef with hamza, fathatan, heh with yeh above, waw with hamza above, double guillemet open, double guillemet closed.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
