106494254752ca7a562f3fceb0650203ac6397cc
Juli Baumler <juli@baumler.com>
2026-05-23 01:04:14 -0700

    Add initial version of northern_chinook_jargon

---
+++ release/n/northern_chinook_jargon/source/help/northern_chinook_jargon.php
@@ -0,0 +1,59 @@
+<?php 
+  $pagename = 'northern_chinook_jargon keyboard help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+    This is a keyboard for typing Northern Chinook Jargon for both desktop (Windows/Mac) and mobile (Android/iPhone/iPad) client.
+</p>
+To type the following Northern Chinook Jargon characters, use the right ALT key on desktop or long press on mobile:
+
+<table><table border="width">
+  <tr>
+    <th>key combo</th>
+    <th>Northern Chinook Jargon character</th>
+  </tr>
+  <tr>
+
+<td>SHIFT + right ALT + h</td> 
+<td>H̱</td>
+  </tr>
+  <tr>
+<td>right ALT + h</td><td>ẖ</td>
+  </tr>
+  <tr>
+<td>SHIFT + right ALT + k</td><td>Ḵ</td>
+  </tr>
+  <tr>
+<td>right ALT + k</td><td>ḵ</td>
+  </tr>
+  <tr>
+<td>SHIFT + right ALT + w</td><td>W̱H̱</td>
+  </tr>
+  <tr>
+<td>right ALT + w</td><td>w̱ẖ</td>
+  </tr>
+  <tr>
+<td>SHIFT + right ALT + x</td><td>X̱</td>
+  </tr>
+  <tr>
+<td>right ALT + x</td><td>x̱</td>
+  </tr>
+  <tr>
+<td>right ALT + \<single quote\></td><td>ʼ (Unicode U+02BC "MODIFIER LETTER APOSTROPHE")</td>
+
+  </tr>
+</table>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+
+<div id='osk-tablet' data-states='default numeric'>
+</div>
+
+<p>©Juli Baumler</p>
+
