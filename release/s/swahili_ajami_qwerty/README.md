2f6247a51a222cea09bbbb553387aebb3df66a40
LornaSIL <lorna_evans@sil.org>
2026-06-26 13:09:05 -0500

    [swahili_ajami_qwerty] New keyboard

---
+++ release/s/swahili_ajami_qwerty/README.md
@@ -0,0 +1,29 @@
+Swahili Ajami (QWERTY) keyboard
+==============
+
+Description
+-----------
+This is an Ajami keyboard for Swahili. It is based on a phonetic layout for a US Qwerty keyboard.
+
+Links
+-----
+Keyboard Homepage: https://keyman.com/keyboards/swahili_ajami_qwerty
+
+Copyright
+---------
+See [LICENSE.md](LICENSE.md)
+
+Supported Platforms
+-------------------
+ * Windows
+ * macOS
+ * Linux
+ * Web
+ * iPhone
+ * iPad
+ * Android phone
+ * Android tablet
+ * Mobile devices
+ * Desktop devices
+ * Tablet devices
+
---
+++ release/s/swahili_ajami_qwerty/source/help/swahili_ajami_qwerty.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Swahili Ajami (QWERTY) Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<p>
+  This is an Ajami keyboard for Swahili. It is based on a phonetic layout for a US Qwerty keyboard.
+</p>
+
+ <h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+<p>
+  This keyboard is based on an Ubuntu keyboard layout designed by Kevin Donnelly. We are grateful for his permission to do this.
+</p>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-phone' data-states='default numeric'>
+</div>
+
+
