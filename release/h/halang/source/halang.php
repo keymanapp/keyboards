1ce971e4176adfb8aa4833182b4b517d2b0c3cb6
Gak-Ha Vietnam <yhwang4@gordonconwell.edu>
2026-07-02 16:19:44 +0900

    Refactor: Move help files to source root and update keyboard rule, readme, welcome files

--- release/h/halang/source/halang.php
+++ release/h/halang/source/halang.php
@@ -28,3 +28,14 @@
 <br>
 <p>ơ, ư :  Type "o" or "u" followed by a "w" to produce the "ơ" or "ư".</p>
 <p>ươ: Type "uo" followed by a "w" to produce "ươ".</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
