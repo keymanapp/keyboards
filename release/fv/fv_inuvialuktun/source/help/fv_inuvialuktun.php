d685ab8c67252872fe9ff685bb76b31c2b634d98
Titima <colonruizanabel@gmail.com>
2026-07-28 10:06:29 -0400

    Remove square

--- release/fv/fv_inuvialuktun/source/help/fv_inuvialuktun.php
+++ release/fv/fv_inuvialuktun/source/help/fv_inuvialuktun.php
@@ -15,7 +15,7 @@ END;
 <p style='margin: 16px 0 0 0'>
 This keyboard is designed for the <b>Inuvialuktun</b> language of the Arctic region of Canada.
 </p>
-<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+<p>If boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
 </p>
 
 
