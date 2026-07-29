a4b8ccff1fe73bf967adf7f57e7532bf55e5bada
LornaSIL <lorna_evans@sil.org>
2026-07-29 08:17:52 -0500

    [sil_lisu] Update keyboards to remove 02D7 and use Lisu Bosa font

--- release/sil/sil_lisu_standard/source/help/sil_lisu_standard.php
+++ release/sil/sil_lisu_standard/source/help/sil_lisu_standard.php
@@ -6,7 +6,9 @@
 
 
   <p>This keyboard uses the standard Lisu keyboard layout.</p>
-
+  <p>The word-forming modifier letter U+02CD is on the SHIFT+O key.</p>
+  <p>U+2010 HYPHEN is on the SHIFT+L key. It is preferred to U+002D HYPHEN-MINUS for the dash used to separate syllables in names, as its semantics are less ambiguous than U+002D.</p>
+  
 <h2>Desktop Keyboard Layout</h2>
 <div id='osk' data-states='default shift'>
 </div>
