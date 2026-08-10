0c66f3b2ec4334d2363ae684b16c75b8e3916eec
LornaSIL <lorna_evans@sil.org>
2026-08-10 16:04:11 -0500

    [lainong] Fix up keyboard files on behalf of author

--- release/l/lainong/source/help/lainong.php
+++ release/l/lainong/source/help/lainong.php
@@ -1,5 +1,17 @@
-Lainong keyboard layout for Keyman.
+<?php 
+  $pagename = 'Lainong Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
 
+<p>This keyboard is for the Lainong language. Type the combining mark after the base character.</p>
 
-Keyboard Layout
-Standard keyboard layout for writing Lainong.
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+
