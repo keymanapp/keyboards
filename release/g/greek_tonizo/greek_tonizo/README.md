34421d00f458bd15edf08a9f997c234f33190cc2
tonizo2025 <adeliyanni@yahoo.gr>
2026-08-13 01:02:03 +0300

    [greek_tonizo] new version 1.0.9

---
+++ release/g/greek_tonizo/greek_tonizo/README.md
@@ -0,0 +1,17 @@
+# greek\_tonizo keyboard
+
+## Description
+
+Greek polytonic keyboard for any device
+
+## Links
+
+Keyboard Homepages: https://greek.tonizo.gr  https://keyman.com/keyboards/greek\_tonizo
+
+## Copyright
+
+See [LICENSE.md](LICENSE.md)
+
+## Supported Platforms
+
+* any
---
+++ release/g/greek_tonizo/greek_tonizo/source/help/greek_tonizo.php
@@ -0,0 +1,10 @@
+<?php 
+  $pagename = 'greek_tonizo Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+<h1>Καλῶς ὁρίσατε στὸ Greek Tonizo!</h1>
+<p>Πατῆστε OK ἢ Done γιὰ νὰ ὁλοκληρώσετε τὴν ἐγκατάσταση.</p>
+<p>Γιὰ τὴν λεπτομερῆ παρουσίαση τῶν πληκτρολογίων Greek Tonizo, παρακαλοῦμε πηγαίνετε στὴν ἱστοσελίδα <a href="https://greek.tonizo.gr">greek.tonizo.gr</a></p>
+<h1>Εὐχαριστοῦμε!</h1>
